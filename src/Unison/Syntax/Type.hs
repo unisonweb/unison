c568357355030544d82cdb8f1c21a8f84948f611
Paul Chiusano <paul.chiusano@gmail.com>
2013-09-22 00:13:40 -0400

    Context substitution

--- src/Unison/Syntax/Type.hs
+++ src/Unison/Syntax/Type.hs
@@ -22,8 +22,8 @@ type Polytype = Type Poly
 data Type (t :: T) c k v where
   Unit :: Type t c k v
   Arrow :: Type t c k v -> Type t c k v -> Type t c k v
-  Var :: Var v -> Type t c k v
-  Exists :: Var v -> Type t c k v
+  Universal :: Var v -> Type t c k v
+  Existential :: Var v -> Type t c k v
   Ann :: Type t c k v -> k -> Type t c k v
   Constrain :: Type t c k v -> c -> Type t c k v
   Forall :: Type Poly c k v -> Type Poly c k v
@@ -36,18 +36,18 @@ deriving instance (Show c, Show k, Show v) => Show (Type t c k v)
 
 abstract1 :: Eq v => v -> Type t c k v -> Maybe (Type t c k v2)
 abstract1 v = collect go where
-  go (Right v2) | v2 == v = Just (Var V.bound1)
+  go (Right v2) | v2 == v = Just (Universal V.bound1)
   go _ = Nothing
 
 abstract :: Eq v => v -> Type t c k v -> ([v], Type t c k v)
 abstract v = collect go where
-  go (Right v2) | v2 == v = ([], (Var V.bound1))
-  go (Right v2) = ([v2], Var (Free v2))
-  go (Left v2) = ([v2], Exists (Free v2))
+  go (Right v2) | v2 == v = ([], (Universal V.bound1))
+  go (Right v2) = ([v2], Universal (Free v2))
+  go (Left v2) = ([v2], Existential (Free v2))
 
 -- | Type variable which is bound by the nearest enclosing `Forall`
 bound1 :: Type t c k v
-bound1 = Var V.bound1
+bound1 = Universal V.bound1
 
 closed :: Type t c k v -> Maybe (Type t c k v2)
 closed = collect (const Nothing)
@@ -60,26 +60,26 @@ collect f = go where
   go e = case e of
     Unit  -> pure Unit
     Arrow i o -> Arrow <$> go i <*> go o
-    Var (Free v) -> f (Right v)
-    Var (Bound ind) -> pure (Var (Bound ind))
-    Exists (Free v) -> f (Left v)
-    Exists (Bound ind) -> pure (Exists (Bound ind))
+    Universal (Free v) -> f (Right v)
+    Universal (Bound ind) -> pure (Universal (Bound ind))
+    Existential (Free v) -> f (Left v)
+    Existential (Bound ind) -> pure (Existential (Bound ind))
     Ann e' t -> Ann <$> go e' <*> pure t
     Constrain e' t -> Constrain <$> go e' <*> pure t
     Forall body -> Forall <$> go body
 
 forall1 :: (forall v . Type t c k v -> Type Poly c k v) -> Type Poly c k v2
-forall1 f = Forall . fromJust . abstract1 () . f $ Var (Free ())
+forall1 f = Forall . fromJust . abstract1 () . f $ Universal (Free ())
 
 subst1 :: Type t c k v -> Type t c k v -> Type t c k v
 subst1 = go D.bound1 where
   go ind body e = case body of
     Unit -> Unit
     Arrow i o -> Arrow (go ind i e) (go ind o e)
-    Var (Bound i) | i == ind -> e
-    Var _ -> body
-    Exists (Bound i) | i == ind -> e
-    Exists _ -> body
+    Universal (Bound i) | i == ind -> e
+    Universal _ -> body
+    Existential (Bound i) | i == ind -> e
+    Existential _ -> body
     Ann body' t -> Ann (go ind body' e) t
     Constrain body' t -> Constrain (go ind body' e) t
     Forall body' -> Forall (go (D.succ ind) body' e)
--- src/Unison/Type/Context.hs
+++ src/Unison/Type/Context.hs
@@ -4,6 +4,7 @@
 
 module Unison.Type.Context where
 
+import Control.Applicative
 import Data.List
 import Unison.Syntax.Type as T hiding (vars)
 import Unison.Syntax.Var as V
@@ -20,19 +21,22 @@ extend e (Context ctx) = Context (e : ctx)
 -- incrementing the DeBruijn index of all previous variables.
 extendUniversal :: Context t sa a (Var v) -> Context t sa a (Var v)
 extendUniversal (Context ctx) =
-  Context (Universal V.bound1 : map (fmap V.succ) ctx)
+  Context (E.Universal V.bound1 : map (fmap V.succ) ctx)
 
 universals :: Context t sa a v -> [v]
-universals (Context ctx) = [v | Universal v <- ctx]
+universals (Context ctx) = [v | E.Universal v <- ctx]
 
 markers :: Context t sa a v -> [v]
 markers (Context ctx) = [v | Marker v <- ctx]
 
 existentials :: Context t sa a v -> [v]
 existentials (Context ctx) = ctx >>= go where
-  go (Existential v) = [v]
-  go (Solved v _) = [v]
+  go (E.Existential v) = [v]
+  go (E.Solved v _) = [v]
   go _ = []
+
+solved :: Context t sa a v -> [(v, sa)]
+solved (Context ctx) = [(v, sa) | Solved v sa <- ctx]
 
 bindings :: Context t sa a v -> [(v, a)]
 bindings (Context ctx) = [(v,a) | E.Ann v a <- ctx]
@@ -44,8 +48,8 @@ vars = fmap fst . bindings
 wellformedType :: Eq v => Context t sa a (V.Var v) -> Type t' c k v -> Bool
 wellformedType c t = case t of
   Unit -> True
-  Var v -> v `elem` universals c
-  Exists v -> v `elem` existentials c
+  T.Universal v -> v `elem` universals c
+  T.Existential v -> v `elem` existentials c
   Arrow i o -> wellformedType c i && wellformedType c o
   T.Ann t' _ -> wellformedType c t'
   Constrain t' _ -> wellformedType c t'
@@ -57,16 +61,48 @@ wellformedType c t = case t of
 -- mentioned in either `Ann` or `Solved` elements must be
 -- wellformed with respect to the prefix of the context
 -- leading up to these elements.
-wellformed :: Eq v => Context t (Polytype c k v) (Monotype c k v) (V.Var v) -> Bool
+wellformed :: Eq v => TContext t c k v -> Bool
 wellformed (Context ctx) = all go (zip' ctx) where
-  go (Universal v, ctx') = v `notElem` universals ctx'
-  go (Existential v, ctx') = v `notElem` existentials ctx'
+  go (E.Universal v, ctx') = v `notElem` universals ctx'
+  go (E.Existential v, ctx') = v `notElem` existentials ctx'
   go (Solved v sa, ctx') = v `notElem` existentials ctx' && wellformedType ctx' sa
   go (E.Ann v t, ctx') = v `notElem` vars ctx' && wellformedType ctx' t
   go (Marker v, ctx') = v `notElem` vars ctx' && v `notElem` existentials ctx'
   -- zip each element with the remaining elements
   zip' ctx' = zip ctx' (map Context $ tail (tails ctx'))
 
+substitute :: Eq v => TContext t c k v -> Polytype c k v -> Polytype c k v
+substitute ctx t = case t of
+  T.Universal _ -> t
+  Unit -> t
+  T.Existential v -> maybe t (substitute ctx) (lookup v (solved ctx))
+  T.Arrow i o -> T.Arrow (substitute ctx i) (substitute ctx o)
+  T.Ann v k -> T.Ann (substitute ctx v) k
+  T.Constrain v c -> T.Constrain (substitute ctx v) c
+  T.Forall v -> T.Forall (substitute ctx v) -- not sure this is correct
 
+type TContext t c k v = Context t (Polytype c k v) (Monotype c k v) (V.Var v)
+
+type Note = String
+
+note :: String -> Note
+note = id
+
+-- | `subtype ctx t1 t2` returns successfully if `t1` is a subtype of `t2`.
+-- This may have the effect of altering the context.
+subtype :: (Eq v, Show c, Show k, Show v)
+        => TContext t c k v
+        -> Polytype c k v
+        -> Polytype c k v
+        -> Either Note (TContext t c k v)
+subtype ctx = go where
+  go Unit Unit = pure ctx
+  go (T.Universal v1) (T.Universal v2)
+    | v1 == v2 && v1 `elem` universals ctx
+    = pure ctx
+  go (T.Existential v1) (T.Existential v2)
+    | v1 == v2 && v1 `elem` existentials ctx
+    = pure ctx
+  go t1 t2 = Left $ note "not a subtype " ++ show t1 ++ " " ++ show t2
 
 
