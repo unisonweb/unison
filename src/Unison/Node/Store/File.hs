8f7940b2b6903ff491975d8f350132e139f2022d
Paul Chiusano <paul.chiusano@gmail.com>
2014-07-03 21:34:00 -0400

    finished with File Store

--- src/Unison/Node/Store/File.hs
+++ src/Unison/Node/Store/File.hs
@@ -5,6 +5,8 @@ import qualified Data.Set as S
 import Data.Set (Set)
 import Data.Aeson as J
 import qualified Data.Text as T
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as LB
 import System.FilePath
 import System.Directory
 import Unison.Syntax.Hash as H
@@ -19,17 +21,27 @@ store root =
   let
     hashesIn :: FilePath -> Noted IO (Set Hash)
     hashesIn dir = N.lift $
-      S.fromList . (map (H.fromBase64 . T.pack)) <$>
+      S.fromList . (map (H.fromBase64 . T.pack . reverse . drop 5 . reverse)) <$>
         getDirectoryContents (joinPath [root, dir])
+
+    n :: Either String a -> Either Note a
+    n (Left e) = Left (N.note e)
+    n (Right a) = Right a
+
     read :: FromJSON a => FilePath -> Hash -> Noted IO a
-    read dir h = undefined
+    read dir h =
+      let file = joinPath [root, dir, T.unpack (H.base64 h) ++ ".json"]
+      in N.noted $ (n . J.eitherDecodeStrict) <$> B.readFile file
 
     write :: ToJSON a => FilePath -> Hash -> a -> Noted IO ()
-    write dir h v = undefined
+    write dir h v =
+      let file = joinPath [root, dir, T.unpack (H.base64 h) ++ ".json"]
+      in N.lift $ B.writeFile file (LB.toStrict (J.encode v))
+      -- unfortunate that writeFile takes a strict bytestring
 
     hashes limit =
       let limitf = maybe id S.intersection limit
-      in liftA2 S.union (hashesIn "terms") (hashesIn "types")
+      in liftA2 S.union (limitf <$> hashesIn "terms") (limitf <$> hashesIn "types")
 
     readTerm = read "terms"
     writeTerm = write "terms"
