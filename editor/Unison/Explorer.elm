8b182ffccbc60869d3c64bf318ff57f94192df02
Paul Chiusano <paul.chiusano@gmail.com>
2014-12-31 17:27:44 -0500

    Futzing with styles of explorer

--- editor/Unison/Explorer.elm
+++ editor/Unison/Explorer.elm
@@ -106,15 +106,15 @@ autocomplete s =
                                     (Layout.embed Nothing s.current)
       renderCompletion i (e,v) = Layout.embed (Just i) e
       invalids = List.map (Layout.embed Nothing) s.invalidCompletions
-      fldLayout = Layout.embed Nothing fld
+      top = Layout.embed Nothing fld
       spacer = Layout.embed Nothing (E.spacer 1 7)
-      [top,bot] = Layout.leftAlignedColumn
-        [ fldLayout
-        , Styles.explorerCells Nothing (status :: List.indexedMap renderCompletion s.completions
-          `List.append` invalids) ]
+      bot = Styles.explorerCells Nothing <|
+        status :: List.indexedMap renderCompletion s.completions
+        `List.append` invalids
+      top' = Layout.transform (E.width (Layout.widthOf bot)) top
       box = Layout.above Nothing
         (Layout.embed Nothing (E.beside (E.spacer 14 1) insertion))
-        (Layout.above Nothing (Layout.above (Layout.tag top) top spacer) bot)
+        (Layout.above Nothing (Layout.above (Layout.tag top) top' spacer) bot)
       boxTopLeft = { x = s.focus.topLeft.x, y = s.focus.topLeft.y + s.focus.height }
       h = boxTopLeft.y + Layout.heightOf box + 50
   in Layout.container Nothing s.width h boxTopLeft box
@@ -171,8 +171,8 @@ main =
       s vs c w =
         Just
           { isKeyboardOpen = True
-          , prompt = "Enter a name"
-          , goal = Styles.codeText "Goal: blah"
+          , prompt = ""
+          , goal = Styles.codeText ": "
           , current = Styles.codeText "status"
           , input = c
           , searchbox = search
