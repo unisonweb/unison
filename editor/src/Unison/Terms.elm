80a597cdbf1816813b3e75b87891f830aead4090
Paul Chiusano <paul.chiusano@gmail.com>
2015-02-09 10:34:21 -0500

    changed example term

--- editor/src/Unison/Terms.elm
+++ editor/src/Unison/Terms.elm
@@ -18,12 +18,15 @@ vec es = E.Vector (Array.fromList es)
 nums : E.Term
 nums = vec (List.map int [0..20])
 
+names : E.Term
+names = vec (List.map str ["Alice", "Bob", "Burt", "Dave", "Carol"])
+
 rgbTerm : Int -> Int -> Int -> E.Term
 rgbTerm r g b =
   builtin "Color.rgba" `ap` int r `ap` int g `ap` int b `ap` int 1
 
 -- expr = E.App (E.App (E.Ref "foo") nums) (E.App (E.Ref "baz") (E.Builtin "View.cell" `ap` E.Builtin "View.swatch" `ap` rgbTerm 230 126 34))
-expr0 = derived "foo" `ap` nums `ap` (derived "baz" `ap` rgbTerm 230 126 34)
+expr0 = derived "foo" `ap` names `ap` (derived "baz" `ap` int 230 `ap` int 126)
 expr = derived "foo" `ap` nums `ap` (derived "baz" `ap` (builtin "View.cell" `ap` builtin "View.swatch" `ap` rgbTerm 230 126 34))
 -- this bombs
 -- expr = E.Ref "uno" `ap` E.Ref "dos" `ap` E.Ref "tres" `ap` E.Ref "quatro" `ap` E.Ref "cinco" `ap` E.Ref "seis" `ap` E.Ref "siete" `ap` E.Ref "ocho"
