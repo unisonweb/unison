4bebb003bba15c329aa5985803a4c9ee831be92b
Paul Chiusano <paul.chiusano@gmail.com>
2015-02-26 17:31:55 -0500

    fixed UI glitch in displaying of explorer completions

--- editor/src/Unison/Explorer.elm
+++ editor/src/Unison/Explorer.elm
@@ -76,7 +76,7 @@ view origin searchbox model = case model of
         statusColor = Styles.statusColor ok
         fld = Field.field (Styles.autocomplete ok) searchbox s.prompt s.input
         completions =
-          let fit e = E.width (E.widthOf s.above - 12 `max` E.widthOf e) e
+          let fit e = E.width ((E.widthOf s.above - 12) `max` E.widthOf e) e
           in List.indexedMap (\i e -> Layout.embed (Result.Ok i) (fit e)) s.completions
         inside = Result.Err Inside
         bottom = Styles.explorerOutline statusColor <|
