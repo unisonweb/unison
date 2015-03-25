1d7aabaa9f2cb534c0e3abcd861cccc5feb3708e
Paul Chiusano <paul.chiusano@gmail.com>
2015-03-25 16:12:06 -0400

    More progress on term explorer, about to gut to try using moore machines

--- editor/src/Unison/TermExplorer.elm
+++ editor/src/Unison/TermExplorer.elm
@@ -1,6 +1,7 @@
 module Unison.TermExplorer where
 
 import Debug
+import Elmz.Moore (Moore)
 import Elmz.Layout as Layout
 import Elmz.Layout (Layout)
 import Elmz.Movement as Movement
@@ -11,6 +12,7 @@ import List
 import Maybe
 import Result
 import Signal
+import String
 import Unison.Metadata as Metadata
 import Unison.Metadata (Query)
 import Unison.Node as Node
@@ -24,8 +26,31 @@ import Unison.Type as Type
 import Unison.Type (Type)
 import Unison.View as View
 
+{-
+type alias LocalFocus =
+  { rootTerm : Term
+  , pathToClosedSubterm : Path
+  , closedSubterm : Term
+  , pathFromClosedSubterm : Path }
+
+type Event
+  = Open LocalFocus
+  | Move Movement.D1
+  | Mouse (Int,Int)
+  | Click (Int,Int)
+  | SearchResults Node.SearchResults
+  | LocalInfo Node.LocalInfo
+  | FieldContent Field.Content
+
+type alias Model' =
+  Moore Event { selection : Maybe Term
+              , view : Element
+              , request : Maybe Request }
+-}
+
 type alias S0 =
   { path : Path
+  , term : Term
   , isKeyboardOpen : Bool
   , prompt : String
   , content : Field.Content
@@ -35,6 +60,7 @@ type alias S0 =
 s00 : S0
 s00 =
   { path = []
+  , term = Term.Blank
   , isKeyboardOpen = True
   , prompt = ""
   , content = Field.noContent
@@ -43,7 +69,9 @@ s00 =
 type alias S1 =
   { info : Node.LocalInfo
   -- includes invalid completions (which have `Nothing` as edit function)
-  , keyedCompletions : List (String, Element, Maybe (Term -> Term))
+  , localCompletions : List (String, Element, Maybe Term)
+  -- Moore (Either SearchResults String) (List (String,Element,Maybe Term), Maybe Request)
+  , searchCompletions : List (String, Element, Maybe Term)
   , s0 : S0 }
 
 type Model
@@ -53,7 +81,7 @@ type Model
   | Closed
 
 type Request
-  = Open (Term,Path)
+  = Open (Term, Path)
   | Search (Term, Path, Int, Metadata.Query, Maybe Type)
   | Noop
 
@@ -61,59 +89,27 @@ type Response
   = LocalInfo Node.LocalInfo
   | SearchResults Node.SearchResults
 
-inputString model = case model of
-  Closed -> ""
-  Initializing s -> s.content.string
-  Ready s -> s.s0.content.string
-  Searching s -> s.s0.content.string
+-- Open the explorer at the given location
+openAt : Term -> Path -> Model -> (Model, Request)
+openAt term path _ =
+  (Initializing { s00 | path <- path, term <- term }, Open (term,path))
 
-path : Model -> Path
-path model = case model of
-  Closed -> []
-  Initializing s -> s.path
-  Ready s -> s.s0.path
-  Searching s -> s.s0.path
+-- Provide `LocalInfo` to the explorer
+localInfo : View.Env -> Node.LocalInfo -> Model -> (Model, Request)
+localInfo viewEnv info model = case model of
+  Initializing s0 ->
+    let search = Search (s0.term, s0.path, 7, Metadata.Query s0.content.string, Just info.admissible)
+        la cur n = (String.padLeft (n+1) '.' "", showAppBlanks viewEnv s0.path n, Just (appBlanks n cur))
+        currentApps = case Term.at s0.path s0.term of
+          Nothing -> []
+          Just cur -> List.map (la cur) info.localApplications
+        completions = currentApps ++ List.map (searchEntry True viewEnv s0.path) info.wellTypedLocals
+    in (Searching { info = info, localCompletions = completions, searchCompletions = [], s0 = s0 }, search)
+  _ -> (model, Noop)
 
-s1 : Model -> Maybe S1
-s1 model = case model of
-  Ready s1 -> Just s1
-  Searching s1 -> Just s1
-  _ -> Nothing
-
-admissible : Model -> Maybe Type
-admissible model = Maybe.map (\s1 -> s1.info.admissible) (s1 model)
-
-parseSearchbox : Model -> List (String, Element, Maybe (Term -> Term))
-parseSearchbox model = let s = inputString model
-  in case SearchboxParser.parseTerm s of
-    Result.Err _   -> [(s, Styles.codeText s, Nothing )]
-    Result.Ok term -> case View.literalKey term of
-      Just k ->
-        let
-          edit = admissible model `Maybe.andThen`
-            \typ -> if Term.checkLiteral term typ then Just (always term)
-                    else Nothing
-        in [(inputString model, Styles.codeText k, edit)]
-      Nothing -> Debug.crash "unpossible"
-
-keyedCompletions : Model -> List (String, Element, Maybe (Term -> Term))
-keyedCompletions model = parseSearchbox model ++ case s1 model of
-  Nothing -> []
-  Just s1 -> s1.keyedCompletions
-
-validCompletions' : List (x, y, Maybe z) -> List ((x,y),z)
-validCompletions' entries =
-  List.filterMap (\(x,y,z) -> Maybe.map (\z -> ((x, y),z)) z) entries
-
-validCompletions : Model -> List ((String, Element), Term -> Term)
-validCompletions model = validCompletions' (keyedCompletions model)
-
-invalidCompletions : Model -> List (String, Element)
-invalidCompletions model =
-  let f (x,y,f) = case f of
-    Nothing -> Just (x,y)
-    Just _ -> Nothing
-  in List.filterMap f (keyedCompletions model)
+-- searchResults : Node.SearchResults ->
+-- mouse : (Int,Int)
+-- content : Field.Content ->
 
 layout : View.Env -> (Field.Content -> Signal.Message) -> Model -> Layout (Maybe Int)
 layout viewEnv searchbox model =
@@ -121,9 +117,6 @@ layout viewEnv searchbox model =
     valids = validCompletions model
     invalids = invalidCompletions model
     ok = not (List.isEmpty valids)
-    pad e = let s = Element.spacer 10 1 in Element.flow Element.right [s, e, s]
-    render expr = pad << Layout.element <|
-      View.layout' viewEnv { term = expr, path = path model, boundAt = Path.boundAt }
     fld s w = Layout.embed Nothing <| Element.flow Element.down
         [ Element.spacer 1 15
         , Element.spacer 9 1 `Element.beside` Styles.carotUp 6 (Styles.statusColor ok)
@@ -145,7 +138,7 @@ layout viewEnv searchbox model =
           , pad <| Styles.currentSymbol `Element.beside`
                    Styles.codeText (" : " ++ Type.key { metadata = viewEnv.metadata } s.info.current)
           ]
-          ++ List.map render s.info.locals
+          ++ List.map (renderTerm viewEnv (path model)) s.info.locals
           ++ [ Element.spacer 1 10 ]
         fit e = Element.width ((Element.widthOf above - 12) `max` (Element.widthOf e)) e
         renderedValids = List.indexedMap (\i ((_,e),_) -> Layout.embed (Just i) (fit e)) valids
@@ -175,16 +168,80 @@ layout viewEnv searchbox model =
         in Layout.above Nothing (fld s1.s0 (Just (Layout.widthOf b))) b
       Closed -> Layout.empty Nothing
 
--- Open the explorer at the given location
-openAt : Term -> Path -> Model -> (Model, Request)
-openAt term path _ =
-  (Initializing s00, Open (term,path))
+inputString model = case model of
+  Closed -> ""
+  Initializing s -> s.content.string
+  Ready s -> s.s0.content.string
+  Searching s -> s.s0.content.string
 
--- Provide a `LocalInfo` at the given location
-localInfo : Term -> Path -> Node.LocalInfo -> Model -> (Model, Request)
-localInfo term path info model = case model of
-  Initializing s0 ->
-    let search = Search (term, path, 7, Metadata.Query s0.content.string, Just info.admissible)
-    -- todo - actually populate keyedCompletions
-    in (Searching { info = info, keyedCompletions = [], s0 = s0 }, search)
-  _ -> (model, Noop)
+path : Model -> Path
+path model = case model of
+  Closed -> []
+  Initializing s -> s.path
+  Ready s -> s.s0.path
+  Searching s -> s.s0.path
+
+s1 : Model -> Maybe S1
+s1 model = case model of
+  Ready s1 -> Just s1
+  Searching s1 -> Just s1
+  _ -> Nothing
+
+admissible : Model -> Maybe Type
+admissible model = Maybe.map (\s1 -> s1.info.admissible) (s1 model)
+
+parseSearchbox : Model -> List (String, Element, Maybe Term)
+parseSearchbox model = let s = inputString model
+  in case SearchboxParser.parseTerm s of
+    Result.Err _   -> [(s, Styles.codeText s, Nothing )]
+    Result.Ok term -> case View.literalKey term of
+      Just k ->
+        let
+          edit = admissible model `Maybe.andThen`
+            \typ -> if Term.checkLiteral term typ then Just term
+                    else Nothing
+        in [(inputString model, Styles.codeText k, edit)]
+      Nothing -> Debug.crash "unpossible"
+
+keyedCompletions : Model -> List (String, Element, Maybe Term)
+keyedCompletions model = parseSearchbox model ++ case s1 model of
+  Nothing -> []
+  Just s1 -> s1.localCompletions ++ s1.searchCompletions
+
+validCompletions' : List (x, y, Maybe z) -> List ((x,y),z)
+validCompletions' entries =
+  List.filterMap (\(x,y,z) -> Maybe.map (\z -> ((x, y),z)) z) entries
+
+validCompletions : Model -> List ((String, Element), Term)
+validCompletions model = validCompletions' (keyedCompletions model)
+
+invalidCompletions : Model -> List (String, Element)
+invalidCompletions model =
+  let f (x,y,f) = case f of
+    Nothing -> Just (x,y)
+    Just _ -> Nothing
+  in List.filterMap f (keyedCompletions model)
+
+box = Term.Embed (Layout.embed { path = [], selectable = False } Styles.currentSymbol)
+appBlanks n e = List.foldl (\_ cur -> Term.App cur Term.Blank) e [1 .. n]
+
+showAppBlanks viewEnv path n = renderTerm viewEnv path (appBlanks n box)
+
+searchKey : View.Env -> Path -> Term -> String
+searchKey viewEnv path e =
+  View.key viewEnv { path = path, term = e, boundAt = Path.boundAt }
+
+renderExplorerEntry : View.Env -> Path -> Term -> Element
+renderExplorerEntry viewEnv path e = Layout.element <|
+  View.layout' viewEnv { path = path, term = e, boundAt = Path.boundAt }
+
+searchEntry : Bool -> View.Env -> Path -> Term -> (String,Element,Maybe Term)
+searchEntry valid viewEnv path e =
+  (searchKey viewEnv path e, renderExplorerEntry viewEnv path e, if valid then Just e else Nothing)
+
+localCompletions : View.Env -> Node.LocalInfo -> List (String,Element,Maybe (Term -> Term))
+localCompletions viewEnv info = Debug.crash "todo"
+
+pad e = let s = Element.spacer 10 1 in Element.flow Element.right [s, e, s]
+renderTerm viewEnv path expr = pad << Layout.element <|
+  View.layout' viewEnv { term = expr, path = path, boundAt = Path.boundAt }
