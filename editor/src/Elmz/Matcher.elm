f74fee13b3764d619608789e3049dbae96b1a6e2
Paul Chiusano <paul.chiusano@gmail.com>
2015-03-27 14:20:45 -0400

    Fixed up matcher

--- editor/src/Elmz/Matcher.elm
+++ editor/src/Elmz/Matcher.elm
@@ -5,10 +5,12 @@ import Elmz.Moore (Moore(..))
 import List
 import String
 
+type alias Q a = { string : String, values : List a }
+
 type Event a
-  = Query String
-  | Results { query : String
-            , values : List a
+  = Query (Q a)
+  | Results { query : Q a
+            , results : List a
             , additionalResults : Int
             , positionsExamined : List Int }
 
@@ -18,10 +20,12 @@ type alias Model a =
 model : (String -> a -> Bool) -> Model a
 model matches =
   let
-    missingResults q = { query = q, values = [], additionalResults = 0, positionsExamined = [] }
     empty e = case e of
-      Query q -> Just (Moore { matches = [], query = Just q } (nonempty (missingResults q)))
+      Query q -> Just <|
+        let o = { matches = List.filter (matches q.string) q.values, query = Just q.string }
+        in Moore o (nonempty (missingResults q))
       _ -> Nothing
+
     nonempty r e = Just <| case e of
       Query q ->
         let
@@ -31,13 +35,15 @@ model matches =
           full = r.additionalResults <= 0
           lastExamined = List.maximum (-1 :: r.positionsExamined)
           ok = -- we've added characters to a search with all results
-             (full && String.startsWith r.query q) ||
+             (full && String.startsWith r.query.string q.string) ||
              -- we've deleted characters, but not past where we have complete results
-             (full && String.startsWith q r.query && lastExamined < String.length q)
+             (full && String.startsWith q.string r.query.string && lastExamined < String.length q.string)
         in
-          Moore { o | query <- if ok then Nothing else Just q }
+          Moore { o | query <- if ok then Nothing else Just q.string }
                 (nonempty (if ok then r' else missingResults q))
       Results r' -> Moore (out r') (nonempty r')
-    out r = { matches = List.filter (matches r.query) r.values, query = Nothing }
+
+    missingResults q = { query = q, results = [], additionalResults = 0, positionsExamined = [] }
+    out r = { matches = List.filter (matches r.query.string) (r.query.values ++ r.results), query = Nothing }
   in
     Moore { matches = [], query = Nothing } empty
