a9220e997ad0ac7637fcdb7942797d20d2599270
Paul Chiusano <paul.chiusano@gmail.com>
2015-04-08 17:03:30 -0400

    tracking available width in editable term output

--- editor/src/Unison/EditableTerm.elm
+++ editor/src/Unison/EditableTerm.elm
@@ -36,10 +36,11 @@ type alias Out =
   , layout : Layout View.L
   , scope : Scope.Model
   , dirtyPaths : Trie Path.E ()
+  , availableWidth : Maybe Int
   , selection : Maybe Region }
 
 out : Term -> Layout View.L -> Scope.Model -> Out
-out term layout scope = Out term layout scope Trie.empty Nothing
+out term layout scope = Out term layout scope Trie.empty Nothing Nothing
 
 type alias Model =
   Moore Event
@@ -47,13 +48,14 @@ type alias Model =
   , layout : Layout View.L
   , scope : Scope.Model
   , dirtyPaths : Trie Path.E ()
-  , selection : Maybe Region }
+  , selection : Maybe Region
+  , availableWidth : Maybe Int }
 
 model : Term -> Model
 model term =
   let
-    refresh : Maybe Int -> (Reference -> Metadata) -> Bool -> Trie Path.E Term -> Out -> Out
-    refresh w md raw evals s = case w of
+    refresh : (Reference -> Metadata) -> Bool -> Trie Path.E Term -> Out -> Out
+    refresh md raw evals s = case s.availableWidth of
       Nothing -> { s | layout <- Layout.empty { path = [], selectable = False } }
       Just w ->
         let l = View.layout s.term
@@ -63,32 +65,32 @@ model term =
                   , raw = if raw then Maybe.map .focus s.scope else Nothing }
         in { s | layout <- l }
 
-    next : Maybe Int -> (Reference -> Metadata) -> Bool -> Trie Path.E Term -> Out -> Event
+    next : (Reference -> Metadata) -> Bool -> Trie Path.E Term -> Out -> Event
         -> Maybe (Moore Event Out)
-    next w md raw evals s e = case e of
+    next md raw evals s e = case e of
       Metadata md ->
-        let s' = refresh w md raw evals s
-        in Just <| Moore s' (next w md raw evals s')
+        let s' = refresh md raw evals s
+        in Just <| Moore s' (next md raw evals s')
       AvailableWidth w ->
-        let s' = refresh (Just w) md raw evals s
-        in Just <| Moore s' (next (Just w) md raw evals s')
+        let s' = refresh md raw evals { s | availableWidth <- Just w }
+        in Just <| Moore s' (next md raw evals s')
       ToggleRaw ->
-        let s' = refresh w md (not raw) evals s
-        in Just <| Moore s' (next w md (not raw) evals s')
+        let s' = refresh md (not raw) evals s
+        in Just <| Moore s' (next md (not raw) evals s')
       Evaluations es ->
         let
           f e = if Term.at e.path s.term == Just e.old then Just (e.path, e.new) else Nothing
           evals' = Trie.fromList (List.filterMap f es)
-          s' = refresh w md raw evals' s
+          s' = refresh md raw evals' s
         in
           if Trie.isEmpty evals' then Nothing
-          else Just <| Moore s' (next w md raw evals' s')
+          else Just <| Moore s' (next md raw evals' s')
       Mouse xy -> case Scope.mouse xy s.layout Nothing of
-        scope -> let s' = refresh w md raw evals { s | scope <- scope }
-                 in Just <| Moore s' (next w md raw evals s')
+        scope -> let s' = refresh md raw evals { s | scope <- scope }
+                 in Just <| Moore s' (next md raw evals s')
       Movement d2 -> case Scope.movement s.term d2 s.scope of
-        scope -> let s' = refresh w md raw evals { s | scope <- scope }
-                 in Just <| Moore s' (next w md raw evals s')
+        scope -> let s' = refresh md raw evals { s | scope <- scope }
+                 in Just <| Moore s' (next md raw evals s')
       Modify f -> s.scope `Maybe.andThen`
         \scope -> Term.modify scope.focus f s.term `Maybe.andThen`
         \term ->
@@ -97,11 +99,11 @@ model term =
           -- for this need to build dependency graph
           let
              evals' = Trie.deleteSubtree scope.focus evals
-             s' = refresh w md raw evals' { s | term <- term
+             s' = refresh md raw evals' { s | term <- term
                                               , dirtyPaths <- View.reactivePaths term
                                               , scope <- Just (Scope.scope scope.focus) }
           in
-            Just <| Moore s' (next w md raw evals' { s' | dirtyPaths <- Trie.empty })
+            Just <| Moore s' (next md raw evals' { s' | dirtyPaths <- Trie.empty })
     highlight o = Maybe.withDefault { o | selection <- Nothing } <|
       o.scope `Maybe.andThen`
         \scope -> Scope.view o.layout scope `Maybe.andThen`
@@ -110,5 +112,5 @@ model term =
           in Just { o | selection <- Just region, layout <- Layout.transform f o.layout }
   in
     let s0 = out term (Layout.empty { path = [], selectable = False }) (Just (Scope.scope []))
-    in Moore s0 (next Nothing Metadata.defaultMetadata False Trie.empty s0)
+    in Moore s0 (next Metadata.defaultMetadata False Trie.empty s0)
        |> Moore.map highlight
