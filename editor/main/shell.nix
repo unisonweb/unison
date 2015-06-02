e761000523315768d5f3b358f7d57617f4f8d471
John Ericson <Ericson2314@Yahoo.com>
2015-06-02 06:35:24 +0000

    Remove try-reflex stuff

--- editor/main/shell.nix
+++
@@ -1,8 +0,0 @@
-with (import <nixpkgs> {}).pkgs;
-let modifiedHaskellPackages = haskellngPackages.override {
-  overrides = self: super: {
-    unison-shared = self.callPackage ../shared {};
-    unison-editor = self.callPackage ./. {};
-  };
-};
-in modifiedHaskellPackages.unison-editor.env
--- editor/shell.nix
+++ editor/shell.nix
@@ -1,15 +1,8 @@
-{ }:
-let this = import ./.;
-    reflexEnv = 
-      platform: (builtins.getAttr platform this).ghcWithPackages 
-                (p: import ./packages.nix { haskellPackages = p; inherit platform; });
-in this.nixpkgs.runCommand "shell" {
-  buildCommand = ''
-    echo "$propagatedBuildInputs $buildInputs $nativeBuildInputs $propagatedNativeBuildInputs" > $out
-  '';
-  buildInputs = [
-    this.nixpkgs.nodejs
-    this.nixpkgs.curl
-    this.ghc.cabal-install
-  ] ++ builtins.map reflexEnv this.platforms;
-} ""
+with (import <nixpkgs> {}).pkgs;
+let modifiedHaskellPackages = haskellngPackages.override {
+  overrides = self: super: {
+    unison-shared = self.callPackage ../shared {};
+    unison-editor = self.callPackage ./. {};
+  };
+};
+in modifiedHaskellPackages.unison-editor.env
