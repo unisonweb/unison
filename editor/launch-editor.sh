6fcdc581a5105bd89255f9a1b144851515f47bed
Paul Chiusano <paul.chiusano@gmail.com>
2015-07-07 17:36:22 -0400

    Cleanup build, get rid of old shared sandboxes junk

---
+++ editor/launch-editor.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# I always forget where this file gets generated
+open dist/build/editor/editor.jsexe/index.html
---
+++ shared/shell.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+nix-shell --option extra-binary-caches https://ryantrinkle.com:5443/ -j 8 -A ghc7101
