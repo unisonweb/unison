cd384cb9d0d4e62bbeb4958564308d96868d0d00
Paul Chiusano <paul.chiusano@gmail.com>
2015-07-14 16:59:20 -0400

    Added rendering functions to `Doc` module

--- shared/default.nix
+++ shared/default.nix
@@ -1,12 +1,12 @@
 { mkDerivation, aeson, base, base64-bytestring, bytestring
-, containers, mtl, prelude-extras, stdenv, text, transformers, vector
+, containers, comonad, free, mtl, prelude-extras, stdenv, text, transformers, vector
 }:
 mkDerivation {
   pname = "unison-shared";
   version = "0.1";
   src = ./.;
   buildDepends = [
-    aeson base base64-bytestring bytestring containers mtl text
+    aeson base base64-bytestring bytestring containers free mtl text
     transformers prelude-extras vector
   ];
   homepage = "http://unisonweb.org";
--- shared/src/Unison/Doc.hs
+++ shared/src/Unison/Doc.hs
@@ -8,173 +8,260 @@
 -- to a given location in the layout.
 
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 module Unison.Doc where
 
+import Control.Comonad.Cofree (Cofree(..)) -- (:<)
+import Control.Comonad (extract)
 import Control.Monad.State.Strict
 import Data.Functor
 import Unison.Path (Path)
 import qualified Unison.Path as Path
 
+data Padded e r =
+  Padded { top :: e, bottom :: e, left :: e, right :: e, element :: r } deriving Functor
+
 data D e r
   = Empty
   | Embed e
+  | Pad (Padded e r)
   | Breakable e
   | Linebreak
-  | Append r r
   | Group r
   | Nest e r
+  | Append r r deriving Functor
 
--- | A `Doc p e` describes a layout that may be rendered at
+-- | A `Doc e p` describes a layout that may be rendered at
 -- multiple widths. The `e` parameter is the type of primitive documents,
 -- possibly `String` or `Text`. The `p` parameter is the path type,
 -- generally a list. Note that the full path corresponding to a
 -- subtree in the document is the concatenation of all paths starting
 -- from the root.
-data Doc p e = Doc p (D e (Doc p e))
+type Doc e p = Cofree (D e) p
 
 data L e r
-  = LEmbed e
-  | LEmpty
+  = LEmpty
+  | LEmbed e
+  | LPad (Padded e r)
   | LLinebreak
   | LNest e r
-  | LAppend r r
+  | LAppend r r deriving Functor
 
 -- A `Doc` without the nondeterminism. All layout decisions have been fixed.
-data Layout p e = Layout p (L e (Layout p e))
-
-lmapPath :: (p -> p2) -> Layout p e -> Layout p2 e
-lmapPath f (Layout p l) = Layout (f p) $ case l of
-  LEmpty -> LEmpty
-  LLinebreak -> LLinebreak
-  LEmbed e -> LEmbed e
-  LNest e r -> LNest e (lmapPath f r)
-  LAppend a b -> LAppend (lmapPath f a) (lmapPath f b)
+type Layout e p = Cofree (L e) p
 
 -- | Produce a `Layout` which tries to fit in the given width,
 -- assuming that embedded `e` elements have the computed width.
 -- Runs in linear time without backtracking.
-layout :: (e -> Int) -> Int -> Doc p e -> Layout p e
+layout :: (e -> Int) -> Int -> Doc e p -> Layout e p
 layout width maxWidth doc =
-  lmapPath fst $ evalState (go (preferredWidth width doc)) (maxWidth, maxWidth)
+  fmap fst $ evalState (go (preferredWidth width doc)) (maxWidth, maxWidth)
   where
   go doc = do
     (maxWidth, remainingWidth) <- get
     case doc of
-      Doc (_,w) _ | w <= remainingWidth ->
+      (_,w) :< _ | w <= remainingWidth ->
         put (maxWidth, remainingWidth - w) $> flow doc
-      Doc _ (Group doc) -> break doc
+      _ :< Group doc -> break doc
       _ -> break doc
 
   -- | Break a document into a list of documents, separated by lines,
   -- respecting the linebreak constraints of the input `Doc`.
-  break (Doc p doc) = get >>= \(maxWidth, remainingWidth) -> case doc of
-    Empty -> pure $ Layout p LEmpty
-    Embed e -> put (maxWidth, remainingWidth - width e) $> Layout p (LEmbed e)
-    Breakable _ -> put (maxWidth, maxWidth) $> Layout p LLinebreak
-    Linebreak -> put (maxWidth, maxWidth) $> Layout p LLinebreak
-    Append a b -> Layout p <$> (LAppend <$> break a <*> break b)
+  break (p :< doc) = get >>= \(maxWidth, remainingWidth) -> case doc of
+    Empty -> pure $ p :< LEmpty
+    Embed e -> put (maxWidth, remainingWidth - width e) $> (p :< LEmbed e)
+    Breakable _ -> put (maxWidth, maxWidth) $> (p :< LLinebreak)
+    Linebreak -> put (maxWidth, maxWidth) $> (p :< LLinebreak)
+    Append a b -> (:<) p <$> (LAppend <$> break a <*> break b)
+    Pad padded ->
+      let borderWidth = width (left padded) + width (right padded)
+      in do
+        put (maxWidth - borderWidth, remainingWidth - borderWidth)
+        inner <- break (element padded)
+        modify (\(_, remainingWidth) -> (maxWidth, remainingWidth - borderWidth))
+        return $ p :< LPad (padded { element = inner })
     Nest e doc -> do
       case maxWidth == remainingWidth of
         -- we're immediately preceded by newline, insert `e` and indent
         True -> do
           put $ let newMax = maxWidth - width e in (newMax, newMax)
           doc <- break doc
-          return $ Layout p (LNest e doc)
+          return $ p :< LNest e doc
         -- we're in the middle of a line, ignore `e`
         False -> break doc
     Group doc -> go doc -- we try to avoid breaking subgroups
 
 -- | Layout the `Doc` assuming infinite available width
-flow :: Doc p e -> Layout p e
-flow (Doc p doc) = case doc of
-  Empty -> Layout p LEmpty
-  Embed e -> Layout p (LEmbed e)
-  Linebreak -> Layout p LLinebreak
-  Breakable e -> Layout p (LEmbed e) -- don't linebreak, it fits
-  Append a b -> Layout p (flow a `LAppend` flow b)
+flow :: Doc e p -> Layout e p
+flow (p :< doc) = case doc of
+  Empty -> p :< LEmpty
+  Embed e -> p :< LEmbed e
+  Pad padded -> p :< LPad (padded { element = flow (element padded) })
+  Linebreak -> p :< LLinebreak
+  Breakable e -> p :< LEmbed e -- don't linebreak, it fits
+  Append a b -> p :< (flow a `LAppend` flow b)
   Group r -> flow r
   Nest _ r -> flow r
 
 -- | Annotate the document with the preferred width of each subtree,
 -- assuming that embedded elements have the given width function.
-preferredWidth :: (e -> Int) -> Doc p e -> Doc (p,Int) e
-preferredWidth width (Doc p d) = case d of
-  Empty -> Doc (p, 0) Empty
-  Embed e -> Doc (p, width e) (Embed e)
+preferredWidth :: (e -> Int) -> Doc e p -> Doc e (p,Int)
+preferredWidth width (p :< d) = case d of
+  Empty -> (p, 0) :< Empty
+  Embed e -> (p, width e) :< Embed e
+  Pad padded ->
+    let borderWidth = width (left padded) + width (right padded)
+        inner = preferredWidth width (element padded)
+        innerWidth = snd (root inner)
+    in (p, borderWidth + innerWidth) :< Pad (padded { element = inner })
   -- Since we just use this to decide whether to break or not,
   -- as long as `flow` and `break` both interpret `Linebreak` properly,
   -- a zero width for linebreaks is okay
-  Linebreak -> Doc (p, 0) Linebreak
-  Breakable e -> Doc (p, width e) (Breakable e) -- assuming we fit on the line
+  Linebreak -> (p, 0) :< Linebreak
+  Breakable e -> (p, width e) :< Breakable e -- assuming we fit on the line
   Append left right ->
-    let left'@(Doc (_,n) _) = preferredWidth width left
-        right'@(Doc (_,m) _) = preferredWidth width right
-    in Doc (p, n+m) (Append left' right')
+    let left' = preferredWidth width left
+        right' = preferredWidth width right
+    in (p, snd (extract left') + snd (extract right')) :< Append left' right'
   Group d ->
-    let pd@(Doc (_,n) _) = preferredWidth width d
-    in Doc (p, n) (Group pd)
+    let pd@((_,n) :< _) = preferredWidth width d
+    in (p, n) :< Group pd
   Nest e d -> -- assume it fits, so ignore the `e`
-    let pd@(Doc (_,n) _) = preferredWidth width d
-    in Doc (p,n) (Nest e pd)
+    let pd@((_,n) :< _) = preferredWidth width d
+    in (p,n) :< Nest e pd
 
 -- | The root path of this document
-root :: Doc p e -> p
-root (Doc p _) = p
+root :: Cofree f p -> p
+root (p :< _) = p
 
 -- | The empty document
-empty :: Path p => Doc p e
-empty = Doc Path.root Empty
+empty :: Path p => Doc e p
+empty = Path.root :< Empty
 
 -- | Append two documents
-append :: Path p => Doc p e -> Doc p e -> Doc p e
-append (Doc p1 d1) (Doc p2 d2) =
+append :: Path p => Doc e p -> Doc e p -> Doc e p
+append (p1 :< d1) (p2 :< d2) =
   case Path.factor p1 p2 of
-    (lca, (p1, p2)) -> Doc lca (Doc p1 d1 `Append` Doc p2 d2)
+    (lca, (p1, p2)) -> lca :< ((p1 :< d1) `Append` (p2 :< d2))
 
 -- | Replace the path component of this `Doc`
-reroot :: p -> Doc p e -> Doc p e
-reroot p (Doc _ d) = Doc p d
+reroot :: p -> Cofree f p -> Cofree f p
+reroot p (_ :< d) = p :< d
 
 -- | Make a `Doc` from a token and give it an empty path
-embed :: Path p => e -> Doc p e
-embed e = Doc Path.root (Embed e)
+embed :: Path p => e -> Doc e p
+embed e = Path.root :< Embed e
 
 -- | Make a `Doc` from a token and give it the specified path
-embed' :: p -> e -> Doc p e
-embed' p e = Doc p (Embed e)
+embed' :: p -> e -> Doc e p
+embed' p e = p :< Embed e
 
 -- | Wrap this `Doc` in a group, which constrains all layout
 -- choices in the group to be the same. For instance,
 -- `group $ embed "a" <> line " " <> embed "b" <> line " " <> embed "c"`
 -- will layout as either "a b c" OR as both spaces replaced by newlines.
-group :: Path p => Doc p e -> Doc p e
-group (Doc p d) = Doc p (Group (Doc Path.root d))
+group :: Path p => Doc e p -> Doc e p
+group (p :< d) = p :< Group (Path.root :< d)
 
 -- | If immediately preceded by a newline, indent the `Doc` by the given element
 -- otherwise ignore the `e` argument.
-nest :: Path p => e -> Doc p e -> Doc p e
-nest e (Doc p d) = Doc p (Nest e (Doc Path.root d))
+nest :: Path p => e -> Doc e p -> Doc e p
+nest e (p :< d) = p :< Nest e (Path.root :< d)
 
 -- | Specify that layout may insert a line break at this point in the document.
 -- If a line break is not inserts, the given `e` is inserted instead.
-breakable :: Path p => e -> Doc p e
+breakable :: Path p => e -> Doc e p
 breakable e = breakable' Path.root e
 
 -- | Like `breakable`, but supply a path to attach to the returned `Doc`.
-breakable' :: Path p => p -> e -> Doc p e
-breakable' p e = Doc p (Breakable e)
+breakable' :: Path p => p -> e -> Doc e p
+breakable' p e = p :< Breakable e
 
 -- | Insert a linebreak. Unlike `breakable`, this guarantees we insert
 -- a linebreak at the location in the layout where this `Doc` appears,
 -- whereas `breakable` just specifies that a linebreak _may_ be inserted.
-linebreak :: Path p => Doc p e
+linebreak :: Path p => Doc e p
 linebreak = linebreak' Path.root
 
 -- | Like `linebreak`, but supply a path to attach to the returned `Doc`.
-linebreak' :: Path p => p -> Doc p e
-linebreak' p = Doc p Linebreak
+linebreak' :: Path p => p -> Doc e p
+linebreak' p = p :< Linebreak
 
-instance Path p => Monoid (Doc p e) where
-  mempty = empty
-  mappend = append
+data Renderer e =
+  Renderer { rhorizontal :: [e] -> e, rvertical :: [e] -> e }
+
+data Renderer' e =
+  Renderer' { rconcat :: [e] -> e, rnewline :: e }
+
+render :: Renderer e -> (e0 -> e) -> Layout e0 p -> e
+render r f l =
+  finish (execState (go l) ([],[],[]))
+  where
+  finish (_, vstack, []) = rvertical r (reverse vstack)
+  finish (_, vstack, hbuf) = finish ([], rhorizontal r (reverse hbuf) : vstack, [])
+  col3 p top mid bot = p :< (LAppend (p :< (LAppend (p :< LEmbed top) mid)) (p :< LEmbed bot))
+  row3 p left mid right = p :< (LAppend (p :< (LAppend (p :< LEmbed left) mid)) (p :< LEmbed right))
+  -- state is (indentation snoc list, vertical buffer snoc list, current line snoc list)
+  go (p :< l) = case l of
+    LEmpty -> return ()
+    LLinebreak -> modify cr where
+      cr (indent, vstack, hbuf) = (indent, rhorizontal r (reverse hbuf) : vstack, [])
+    LEmbed e -> modify g where
+      -- we indent if we're the first token on this line
+      g (indent, vstack, []) = (indent, vstack, f e : indent)
+      g (indent, vstack, hbuf) = (indent, vstack, f e : hbuf)
+    LPad padded -> modify g where
+      inner = render r f (col3 p (top padded)
+                                 (row3 p (left padded) (element padded) (right padded))
+                                 (bottom padded))
+      -- we indent if we're the first token on this line
+      g (indent, vstack, []) = (indent, vstack, inner : indent)
+      g (indent, vstack, hbuf) = (indent, vstack, inner : hbuf)
+    LNest e r -> do
+      modify (\(i,v,h) -> (f e : i, v, h))
+      go r
+      modify (\(i,v,h) -> (tail i, v, h))
+    LAppend a b -> go a *> go b
+
+render' :: Renderer' e -> (e0 -> e) -> Layout e0 p -> e
+render' r f l = finish (execState (go l) ([],[]))
+  where
+  finish (_, buf) = rconcat r (reverse buf)
+  col3 p top mid bot = p :< (LAppend (p :< (LAppend (p :< LEmbed top) mid)) (p :< LEmbed bot))
+  row3 p left mid right = p :< (LAppend (p :< (LAppend (p :< LEmbed left) mid)) (p :< LEmbed right))
+  -- state is (indentation snoc list, token buffer snoc list)
+  go (p :< l) = case l of
+    LEmpty -> return ()
+    LLinebreak -> modify cr where
+      cr (indent, buf) = (indent, rnewline r : buf)
+    LEmbed e -> modify g where
+      -- we indent if we're the first token on this line
+      g (indent, []) = (indent, f e : indent)
+      g (indent, buf) = (indent, f e : buf)
+    LPad padded -> modify g where
+      inner = render' r f (col3 p (top padded)
+                                  (row3 p (left padded) (element padded) (right padded))
+                                  (bottom padded))
+      -- we indent if we're the first token on this line
+      g (indent, []) = (indent, inner : indent)
+      g (indent, buf) = (indent, inner : buf)
+    LNest e r -> do
+      modify (\(i,b) -> (f e : i, b))
+      go r
+      modify (\(i,b) -> (tail i, b))
+    LAppend a b -> go a *> go b
+
+renderString :: Layout String p -> String
+renderString = render' (Renderer' concat "\n") id
+
+sep :: Path p => e -> [Doc e p] -> Doc e p
+sep _ [] = empty
+sep delim ds = group (foldr1 combine ds)
+  where combine a b = a `append` breakable delim `append` b
+
+sep' :: Path p => e -> [e] -> Doc e p
+sep' delim ds = sep delim (map embed ds)
+
+ex = putStrLn . renderString . layout length 10 $
+  (sep' " " ["a", "thre", "bo"] :: Doc String [Int])
