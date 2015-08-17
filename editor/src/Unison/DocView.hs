da1d7366dcbb55f25abfd36d1494fb0298df3f0d
Paul Chiusano <paul.chiusano@gmail.com>
2015-08-17 13:37:17 -0400

    comment out noncompiling code for now

--- editor/src/Unison/DocView.hs
+++ editor/src/Unison/DocView.hs
@@ -12,16 +12,10 @@ import qualified Data.Text as Text
 import qualified Unison.Doc as Doc
 import qualified Unison.UI as UI
 
--- render :: Renderer e -> (e0 -> e) -> Layout e0 p -> e
-
-data Picker p =
-  Picker { at :: (X,Y) -> Maybe p
-         , region :: p -> Maybe (X,Y,Width,Height) }
-
-docWidget :: MonadWidget t m => Width -> Doc Text p -> m (Picker p)
-docWidget available d = _g $ Doc.etraverse layout d
-  where
-  layout txt = do
-    (e,_) <- el' "div" $ text (Text.unpack txt)
-    (w,h) <- liftIO $ UI.preferredDimensions e
-    pure (e, (w,h))
+--docWidget :: MonadWidget t m => Width -> Doc Text p -> m (Picker p)
+--docWidget available d = error "todo" $ Doc.etraverse layout d
+--  where
+--  layout txt = do
+--    (e,_) <- el' "div" $ text (Text.unpack txt)
+--    (w,h) <- liftIO $ UI.preferredDimensions e
+--    pure (e, (w,h))
