6a95f53640b95b7f1c81c174dd168ec550435148
Paul Chiusano <paul.chiusano@gmail.com>
2015-09-30 14:52:54 -0400

    Updated node to work with tweaked BasicNode, just needs a From/ToJSON DFO instance

--- node/src/Node.hs
+++ node/src/Node.hs
@@ -9,8 +9,8 @@ import Unison.Term.Extra ()
 import Unison.Hash.Extra ()
 import Unison.Node.Store (Store)
 import Unison.Var (Var)
-import qualified Unison.Hash
 import qualified Unison.ABT as ABT
+import qualified Unison.Hash
 import qualified Unison.Hash as Hash
 import qualified Unison.Node.BasicNode as BasicNode
 import qualified Unison.Node.FileStore as FileStore
@@ -18,13 +18,14 @@ import qualified Unison.NodeServer as NodeServer
 import qualified Unison.Reference as Reference
 import qualified Unison.Symbol as Symbol
 import qualified Unison.Term as Term
+import qualified Unison.View as View
 
-hash :: (Serial v, Var v) => Term.Term v -> Reference
+hash :: Var v => Term.Term v -> Reference
 hash (Term.Ref' r) = r
 hash t = Reference.Derived (ABT.hash t)
 
 main :: IO ()
 main = do
-  store <- FileStore.make "store" :: IO (Store IO (Symbol.Symbol ()))
+  store <- FileStore.make "store" :: IO (Store IO (Symbol.Symbol View.DFO))
   node <- BasicNode.make hash store
   NodeServer.server 8080 node
