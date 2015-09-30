dc50bee8c0fa6d0cd5c4578137c1f229224beaa0
Paul Chiusano <paul.chiusano@gmail.com>
2015-09-30 14:42:10 -0400

    Added MemNode, which uses murmur hash

--- shared/default.nix
+++ shared/default.nix
@@ -1,13 +1,13 @@
 { mkDerivation, aeson, base, base64-bytestring, bifunctors, bytestring
-, containers, comonad, free, mtl, prelude-extras, stdenv, text, transformers, vector
+, containers, comonad, free, murmur-hash, mtl, prelude-extras, stdenv, text, transformers, vector
 }:
 mkDerivation {
   pname = "unison-shared";
   version = "0.1";
   src = ./.;
   buildDepends = [
-    aeson base base64-bytestring bifunctors bytestring containers free mtl 
-    text transformers prelude-extras vector
+    aeson base base64-bytestring bifunctors bytestring containers free 
+    murmur-hash mtl text transformers prelude-extras vector
   ];
   homepage = "http://unisonweb.org";
   description = "The Unison programming language and platform";
---
+++ shared/src/Unison/Node/MemNode.hs
@@ -0,0 +1,49 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Unison.Node.MemNode where
+
+import Data.List
+import Unison.Hash (Hash)
+import Unison.Node (Node)
+import Unison.Node.Store (Store)
+import Unison.Reference (Reference(Derived))
+import Unison.Term (Term)
+import Unison.Type (Type)
+import Unison.Var (Var)
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.ByteString.Builder as Builder
+import qualified Data.Digest.Murmur64 as Murmur
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Encoding
+import qualified Unison.ABT as ABT
+import qualified Unison.Hash as Hash
+import qualified Unison.Hashable as Hashable
+import qualified Unison.Node.BasicNode as BasicNode
+import qualified Unison.Node.MemStore as MemStore
+import qualified Unison.Symbol as Symbol
+import qualified Unison.Term as Term
+import qualified Unison.View as View
+
+hash :: (Var v) => Term.Term v -> Reference
+hash (Term.Ref' r) = r
+hash t = Derived (ABT.hash t)
+
+instance Hashable.Hash Hash where
+  hash tokens = finish $ foldl' step (Murmur.hash64 ()) tokens where
+    step acc t = case t of
+      Hashable.Tag i -> Murmur.hash64AddInt (fromIntegral i) acc
+      Hashable.Bytes bs -> Murmur.hash64Add bs acc
+      Hashable.VarInt i -> Murmur.hash64AddInt i acc
+      Hashable.Text txt -> Murmur.hash64Add (Encoding.encodeUtf8 txt) acc
+      Hashable.Hashed h -> Murmur.hash64Add (Hash.toBytes h) acc
+      Hashable.Double d -> Murmur.hash64Add (Encoding.encodeUtf8 (Text.pack (show d))) acc
+    finish h64 = (Hash.fromBytes . LB.toStrict . Builder.toLazyByteString . Builder.word64LE . Murmur.asWord64) h64
+  fromBytes = Hash.fromBytes
+  toBytes = Hash.toBytes
+
+type V = Symbol.Symbol View.DFO
+
+make :: IO (Node IO V Reference (Type V) (Term V))
+make = do
+  store <- MemStore.make :: IO (Store IO V)
+  BasicNode.make hash store
