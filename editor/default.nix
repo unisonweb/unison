5789edaca192c16532e9cb89bc0db4b7564ca577
Paul Chiusano <paul.chiusano@gmail.com>
2015-11-22 17:20:06 -0500

    switch to using symbolic link for shared / editor dependency, for faster turnaround times

--- editor/default.nix
+++ editor/default.nix
@@ -1,14 +1,55 @@
-{ mkDerivation, base, containers, data-default, free, ghcjs-dom, 
-  ghcjs-base, mtl, reflex, reflex-dom, semigroups, stdenv, text, 
-  transformers, unison-shared }:
+{ mkDerivation, stdenv,
+  base,
+  aeson,
+  base64-bytestring,
+  bifunctors,
+  bytestring,
+  comonad,
+  containers,
+  data-default,
+  free,
+  ghcjs-base,
+  ghcjs-dom,
+  mtl,
+  murmur-hash,
+  prelude-extras,
+  reflex,
+  reflex-dom,
+  semigroups,
+  text,
+  these,
+  transformers,
+  vector
+}:
 mkDerivation {
   pname = "unison-editor";
   version = "0.1";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  buildDepends = [ base free ghcjs-base ghcjs-dom mtl reflex reflex-dom 
-                   semigroups text transformers unison-shared ];
+  buildDepends = [ 
+    base
+    aeson
+    base64-bytestring
+    bifunctors
+    bytestring
+    comonad
+    containers
+    data-default
+    free
+    ghcjs-base
+    ghcjs-dom
+    mtl
+    murmur-hash
+    prelude-extras
+    reflex
+    reflex-dom
+    semigroups
+    text
+    these
+    transformers
+    vector
+  ];
   homepage = "http://unisonweb.org";
   description = "The Unison programming language and platform";
   license = stdenv.lib.licenses.mit;
