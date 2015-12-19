d1476c59e7db92dce763686282d3857feb3be8b1
Paul Chiusano <paul.chiusano@gmail.com>
2015-12-19 17:26:25 -0500

    another tweak to `at` function

--- node/src/Unison/Runtime/Pcbt.hs
+++ node/src/Unison/Runtime/Pcbt.hs
@@ -4,10 +4,8 @@
 
 module Unison.Runtime.Pcbt where
 
-import Data.Maybe
 import Unison.Runtime.Free (Free)
 import Unison.Runtime.Stream (Stream)
-import qualified Data.Map as Map
 import qualified Unison.Runtime.Vector as V
 import qualified Unison.Runtime.Stream as Stream
 import qualified Unison.Runtime.Free as Free
@@ -26,18 +24,19 @@ data Pcbt m p a = Pcbt
 
 data View m p a
   = View { stream :: Stream m (Choices p, a) ()
-         , at     :: V.Vector (p,Bit) -> Free m (Pcbt' p a) }
+         , at     :: (Choices p -> p -> p -> Maybe Bit) -> Free m (Pcbt' p a) }
 
 data Pcbt' p a
   = Bin' (Maybe a) p (Pcbt' p a) (Pcbt' p a)
   | Tip' (Maybe a)
+  | More' (Maybe a)
 
 bin' :: Maybe a -> p -> Pcbt' p a -> Pcbt' p a -> Pcbt' p a
 bin' hit _ (Tip' Nothing) (Tip' Nothing) = Tip' hit
 bin' hit p l r = Bin' hit p l r
 
 view :: Ord p => Pcbt m p a -> View m p a
-view t = View (stream V.empty) at where
+view t = View (stream V.empty) (flip at V.empty) where
   stream cursor = do
     let bp = bitpath cursor
     isLeaf <- Stream.eval (structure t bp)
@@ -54,24 +53,19 @@ view t = View (stream V.empty) at where
   -- Move down the 0-branch
   descend p cursor = stream (cursor `V.snoc` (p,False)) -- go down 0 branch
   tryDescend p isLeaf c = if isLeaf then next c else descend p c
-  at cursor = at' (Map.fromList (V.toList cursor)) V.empty
-  at' query cursor = do
-    l <- Free.eval (labels t (V.toList cursor))
+  at query cursor = do
+    l <- Free.eval (labels t (bitpath cursor))
     case l of
       Nothing -> pure (Tip' Nothing)
-      Just (Labels path maxPath hit)
-        | Map.null query -> pure (Tip' hit)
-        | maxPath < fst (Map.findMin query) -> pure (Tip' hit)
-        | otherwise -> case fromMaybe Both $ Map.lookup path query of
-          -- query picks a definite branch
-          Zero ->
-            bin' hit path <$> at' (Map.delete path query) (cursor `V.snoc` False)
-                          <*> pure (Tip' Nothing)
-          One ->
-            bin' hit path (Tip' Nothing) <$> at' (Map.delete path query) (cursor `V.snoc` True)
-          -- query doesn't say, check both 0-branch and 1-branch
-          Both -> bin' hit path <$> at' query (cursor `V.snoc` False)
-                                   <*> at' query (cursor `V.snoc` True)
+      Just (Labels path maxPath hit) -> case query cursor path maxPath of
+        Nothing -> pure (More' hit)
+        Just Zero -> -- query picks a definite branch
+          bin' hit path <$> at query (cursor `V.snoc` (path,False)) <*> pure (Tip' Nothing)
+        Just One ->
+          bin' hit path (Tip' Nothing) <$> at query (cursor `V.snoc` (path,True))
+        Just Both -> -- query doesn't say, check both 0-branch and 1-branch
+          bin' hit path <$> at query (cursor `V.snoc` (path,False))
+                        <*> at query (cursor `V.snoc` (path,True))
 
 bitpath :: Choices p -> Bitpath
 bitpath c = map snd (V.toList c)
