7e10b641e6fbb93bb4c96e4ca2361408cf1a4ecd
Jack <steve.a.shogren@gmail.com>
2016-03-03 15:50:18 -0500

    Get the cache to correctly clear and finalize
    Unfortunately, the threadGCsResourcesFromCacheTest isn't working as
    expected, some rouge thread seems to be finded and mutating the cache
    without correctly finalizing the release. Also, switched from the nasty
    file-system based state to use MVar!

--- node/src/Unison/Runtime/ResourcePool.hs
+++ node/src/Unison/Runtime/ResourcePool.hs
@@ -2,40 +2,65 @@ module Unison.Runtime.ResourcePool where
 
 import qualified Data.Map as M
 import qualified Control.Concurrent.MVar as MVar
-import Data.Time (UTCTime,getCurrentTime)
-import Control.Concurrent
+import Data.Time (UTCTime, getCurrentTime, addUTCTime,secondsToDiffTime)
+import qualified Control.Concurrent as CC
+import qualified Control.Monad
 
 -- acquire returns the resource, and the cleanup action ("finalizer") for that resource
 data Pool p r = Pool { acquire :: p -> Int -> IO (r, IO ()) }
 
-type Cache p r = MVar.MVar (M.Map p (r, UTCTime))
+type Cache p r = MVar.MVar (M.Map p (r, UTCTime, IO ()))
+
+addResourceToMap :: (Ord p) =>  (r -> IO()) -> Cache p r -> p -> r -> Int -> IO ()
+addResourceToMap releaser cache p r wait = do
+  now <- getCurrentTime
+  let expiry = addUTCTime (fromIntegral wait) now
+  cachemap <- MVar.takeMVar cache
+  newCacheMap <-
+        if wait > 0 then
+          -- add the resource to the cache with the releaser
+          return $ M.insert p (r,expiry, (releaser r)) cachemap
+        else -- immedately release and dont add to cache
+          releaser r >> return cachemap
+  MVar.putMVar cache newCacheMap
 
 _acquire :: (Ord p) => (p -> IO r) -> (r -> IO()) -> Cache p r -> p -> Int -> IO (r, IO ())
 _acquire acquirer releaser cache p wait = do
-  now <- getCurrentTime
   cachemap <- MVar.takeMVar cache
   r <- case M.lookup p cachemap of
-          Just (r, _) -> return r
+          Just (r, _, _) -> return r
           Nothing -> acquirer p
-  let newCacheMap =
-        if wait > 0 then M.insert p (r,now) cachemap
-        else cachemap
-  MVar.putMVar cache newCacheMap >> return (r, (releaser r))
+  MVar.putMVar cache cachemap
+    >> return (r, (addResourceToMap releaser cache p r wait))
 
 cleanCache :: (Ord p) => Cache p r -> IO ()
 cleanCache cache = do
-  now <- getCurrentTime
+  now <- CC.threadDelay 1 >> getCurrentTime
   cachemap <- MVar.takeMVar cache
-  let keys = M.keys cachemap
-      newmap = foldr (\k m ->
-                    case M.lookup k cachemap of
-                        Just (_, expiry) ->
-                          if expiry < now then M.delete k m else m
-                        Nothing -> m) cachemap keys
-  MVar.putMVar cache newmap
+  let emptyKeys = [] :: [(p, IO())]
+  let keysNReleasers = M.foldrWithKey (\k _ knrs ->
+                                      case M.lookup k cachemap of
+                                          Just (_, expiry, releaser) ->
+                                            if expiry < now then (k, releaser) : knrs
+                                            else knrs
+                                          Nothing -> knrs) emptyKeys cachemap
+      newMap = foldr (\(k,_) m -> M.delete k m) cachemap keysNReleasers
+  id <- CC.myThreadId
+  putStrLn ("\nthread: " ++ (show id) ++ " found: " ++ (show . length $ keysNReleasers))
+  (sequence $ map snd keysNReleasers)
+    >> MVar.putMVar cache newMap
 
+cleanCacheLoop cache =
+  cleanCache cache >> cleanCacheLoop cache
 
 pool :: Ord p => Int -> (p -> IO r) -> (r -> IO ()) -> IO (Pool p r)
 pool maxPoolSize acquirer releaser = do
   cache <- MVar.newMVar M.empty
+  return Pool { acquire = _acquire acquirer releaser cache }
+
+poolWithGC :: Ord p => Int -> (p -> IO r) -> (r -> IO ()) -> IO (Pool p r)
+poolWithGC maxPoolSize acquirer releaser = do
+  cache <- MVar.newMVar M.empty
+  id <- CC.forkIO (cleanCacheLoop cache)
+  putStrLn ("spawing: " ++ (show id))
   return Pool { acquire = _acquire acquirer releaser cache }
--- node/tests/Unison/Test/ResourcePool.hs
+++ node/tests/Unison/Test/ResourcePool.hs
@@ -3,7 +3,6 @@ module Unison.Test.ResourcePool where
 import qualified Unison.Runtime.ResourcePool as RP
 import Test.Tasty
 import Test.Tasty.HUnit
-import qualified System.IO.Strict as ST
 import Control.Concurrent
 import Data.Time (UTCTime,getCurrentTime, addUTCTime)
 import qualified Control.Concurrent.MVar as MVar
@@ -11,64 +10,74 @@ import qualified Data.Map as M
 
 type Resource = String
 type Params = String
+type TestState = MVar.MVar (M.Map String String)
 
-loadState :: FilePath -> IO String
-loadState p = do
-  c <- ST.readFile p
-  length c `seq` return c
+lookupState :: M.Map String String -> String -> String
+lookupState m k =
+  case (M.lookup k m) of
+    Just s -> s
+    Nothing -> ""
 
-saveState :: String -> FilePath -> IO ()
-saveState x p = do
-  s <- loadState p
-  length s `seq` writeFile p (s++x)
+loadState :: TestState -> String -> IO String
+loadState var k = do
+  m <- MVar.readMVar var
+  return (lookupState m k)
 
-clearState :: FilePath -> IO ()
-clearState p = writeFile p ""
+saveState :: TestState -> String -> String -> IO ()
+saveState var k newState = do
+  m <- MVar.takeMVar var
+  let s = lookupState m k
+  MVar.putMVar var (M.insert k (s++newState) m)
 
-fakeAcquire :: Params -> IO Resource
-fakeAcquire p = do
-  saveState (p++"r") "testacquires"
+fakeAcquire :: TestState -> Params -> IO Resource
+fakeAcquire s p = do
+  saveState s "testacquires" (p++"r")
   >> return (p++"r")
 
-cleanTestFiles =
-  clearState "testreleases"
-  >> clearState "testacquires"
+cleanTestFiles s =
+  saveState s "testacquires" ""
+  >> saveState s "testacquires" ""
 
-fakeRelease :: Resource -> IO ()
-fakeRelease r = do
-  saveState r "testreleases"
-  >> return ()
+fakeRelease :: TestState -> Resource -> IO ()
+fakeRelease s r = do
+  saveState s "testreleases" r
+
+getPool = do
+  state <- MVar.newMVar M.empty
+  pool <- cleanTestFiles state
+          >> (RP.pool 3 (fakeAcquire state) (fakeRelease state))
+  return (pool, state)
 
 correctlyAcquiresTest :: Assertion
 correctlyAcquiresTest = do
-  x <- cleanTestFiles
-  pool <- (RP.pool 3 fakeAcquire fakeRelease)
-  (resource, _) <- RP.acquire pool "p1" 0
-  (r2, _) <- RP.acquire pool "p1" 0
-  didAcquire <- loadState "testacquires"
-  didRelease <- loadState "testreleases"
+  (pool, ts) <- getPool
+  (resource, release1) <- RP.acquire pool "p1" 0
+  didAcquire <- release1
+                >> RP.acquire pool "p1" 0
+                >> loadState ts "testacquires"
+  didRelease <- loadState ts "testreleases"
 
   assertEqual "the correct resource is returned" "p1r" resource
     >> assertEqual "r is acquired twice" "p1rp1r" didAcquire
-    >> assertEqual "didn't call release" "" didRelease
+    >> assertEqual "called release once" "p1r" didRelease
 
 correctlyReleasesTest :: Assertion
 correctlyReleasesTest = do
-  x <- cleanTestFiles
-  pool <- (RP.pool 3 fakeAcquire fakeRelease)
+  (pool, ts) <- getPool
   (_, releaser) <- RP.acquire pool "p1" 0
-  didRelease <- releaser >> loadState "testreleases"
+  didRelease <- releaser >> loadState ts "testreleases"
   assertEqual "r was released after use" "p1r" didRelease
 
 acquireShouldCacheConnectionTest :: Assertion
 acquireShouldCacheConnectionTest = do
-  x <- cleanTestFiles
-  pool <- (RP.pool 3 fakeAcquire fakeRelease)
-  (r, _) <- RP.acquire pool "p1" 100000 -- 100 seconds
-  (r2, _) <- RP.acquire pool "p1" 100000 -- 100 seconds
-  didAcquire <- loadState "testacquires"
+  (pool, ts) <- getPool
+  (r, releaser1) <- RP.acquire pool "p1" 100000 -- 100 seconds
+  (r2, releaser2) <- releaser1 >> RP.acquire pool "p1" 100000 -- 100 seconds
+  didAcquire <- releaser2 >> loadState ts "testacquires"
+  didRelease <- loadState ts "testreleases"
+  -- a 100 second wait would prevent immediate release both times
   assertEqual "r was only acquired once" "p1r" didAcquire
-
+    >> assertEqual "didn't call release" "" didRelease
 
 tenSecondsAgo :: UTCTime -> UTCTime
 tenSecondsAgo now = addUTCTime (-10) now
@@ -76,30 +85,44 @@ tenSecondsAgo now = addUTCTime (-10) now
 inTenSeconds :: UTCTime -> UTCTime
 inTenSeconds now = addUTCTime (10) now
 
-emptyCache now = M.fromList [("p1", ("p1r", now))
-                        , ("p2", ("p2r", tenSecondsAgo now))
-                        , ("p3", ("p3r", inTenSeconds now))
-                        ]
-cleanCacheTest :: Assertion
-cleanCacheTest = do
+aThreeFullCache now f1 f2 f3=
+  M.fromList [("p1", ("p1", now, f1))
+             , ("p2", ("p2", tenSecondsAgo now, f2))
+             , ("p3", ("p3", inTenSeconds now, f3))
+             ]
+
+cleanCacheShouldReleaseFinalizer :: Assertion
+cleanCacheShouldReleaseFinalizer = do
+  (pool, ts) <- getPool
   now <- getCurrentTime
-  cache <- MVar.newMVar (emptyCache now)
+  cache <- MVar.newMVar (aThreeFullCache now
+                         (fakeRelease ts "p1")
+                         (fakeRelease ts "p2")
+                         (fakeRelease ts "p3"))
   RP.cleanCache cache
   c <- MVar.takeMVar cache
+  didRelease <- loadState ts "testreleases"
+
   assertEqual "p1 and p2 are cleaned from cache" ["p3"] (M.keys c)
+    >> assertEqual "p1 and p2 are released" "p1p2" didRelease
+
+
+getPoolWithGC = do
+  state <- MVar.newMVar M.empty
+  pool <- cleanTestFiles state
+          >> (RP.poolWithGC 3 (fakeAcquire state) (fakeRelease state))
+  return (pool, state)
 
 threadGCsResourcesFromCacheTest :: Assertion
 threadGCsResourcesFromCacheTest = do
-  x <- cleanTestFiles
-  pool <- (RP.pool 3 fakeAcquire fakeRelease)
-  (resource, _) <- RP.acquire pool "p1" 1
-  threadDelay 3
-  (r2, _) <- RP.acquire pool "p1" 1
-  didAcquire <- loadState "testacquires"
-  didRelease <- loadState "testreleases"
-
-  assertEqual "r is acquired twice" "p1rp1r" didAcquire
-    >> assertEqual "didn't call release" "" didRelease
+  (pool, ts) <- getPoolWithGC
+  (resource, release1) <- RP.acquire pool "p1" 4
+  didRelease1 <- release1
+                    >> threadDelay 1
+                    >> loadState ts "testreleases"
+  didRelease2 <- threadDelay 5 >> loadState ts "testreleases"
+  assertEqual "didn't immediately release" "" didRelease1
+    >> assertEqual "auto released after 3 seconds" "p1r" didRelease2
 
 tests :: TestTree
 tests = testGroup "Doc"
@@ -107,8 +130,8 @@ tests = testGroup "Doc"
     testCase "AcquiresTest" $ correctlyAcquiresTest
     , testCase "ReleasesTest" $ correctlyReleasesTest
     , testCase "acquireShouldCacheConnectionTest" $  acquireShouldCacheConnectionTest
-    -- , testCase "threadGCsResourcesFromCacheTest " $  threadGCsResourcesFromCacheTest
-    , testCase "cleanCacheTest" $  cleanCacheTest
+    , testCase "threadGCsResourcesFromCacheTest " $  threadGCsResourcesFromCacheTest
+    , testCase "cleanCacheShouldReleaseFinalizer" $  cleanCacheShouldReleaseFinalizer
    ]
 
 main = defaultMain tests
