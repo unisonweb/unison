ff875348ee9eec344e2b8ea800fadba58a8c4f7c
Jack <steve.a.shogren@gmail.com>
2016-03-17 07:16:14 -0400

    WIP for removing MVar

--- node/src/Unison/Runtime/ResourcePool.hs
+++ node/src/Unison/Runtime/ResourcePool.hs
@@ -1,64 +1,69 @@
 module Unison.Runtime.ResourcePool where
 
-import qualified Data.Map as M
-import qualified Control.Concurrent.MVar as MVar
 import Data.Time (UTCTime, getCurrentTime, addUTCTime)
 import qualified Control.Concurrent as CC
+import qualified Control.Concurrent.Map as M
+import qualified Control.Concurrent.STM.TQueue as TQ
+import qualified Control.Concurrent.MVar as MVar
+import qualified Data.Hashable as H
 
 -- acquire returns the resource, and the cleanup action ("finalizer") for that resource
 data Pool p r = Pool { acquire :: p -> Int -> IO (r, IO ()) }
 
 type ResourceKey p = (p,CC.ThreadId)
-type Cache p r = MVar.MVar (M.Map (ResourceKey p) (r, UTCTime, IO ()))
+type Cache p r = M.Map (ResourceKey p) (r, UTCTime, IO ())
 
-addResourceToMap :: Ord p =>  (r -> IO ()) -> Cache p r -> p -> r -> Int -> Int -> IO CC.ThreadId -> IO ()
-addResourceToMap releaser mVarCache p r wait maxPoolSize getThread = do
+addResourceToMap :: (Ord p, H.Hashable p) =>  (r -> IO ()) -> Cache p r -> p -> r -> Int -> Int -> IO CC.ThreadId -> MVar.MVar Int -> IO ()
+addResourceToMap releaser map p r wait maxPoolSize getThread ps = do
   now <- getCurrentTime
+  poolSize <- MVar.takeMVar poolSize
   threadId <- getThread
   let expiry = addUTCTime (fromIntegral wait) now
-  cacheMap <- MVar.takeMVar mVarCache
-  newCacheMap <- if wait > 0 && ((length cacheMap) < maxPoolSize) then
-                   -- add the resource to the mVarCache with the releaser
-                   return $ M.insert (p,threadId) (r,expiry, (releaser r)) cacheMap
-                 else -- immedately release and dont add to mVarCache
-                   releaser r >> return cacheMap
-  MVar.putMVar mVarCache newCacheMap
+  if wait > 0 && (poolSize < maxPoolSize) then
+    -- add the resource to the map with the releaser
+    M.insert (p,threadId) (r,expiry, (releaser r)) map
+      >> MVar.putMVar ps (poolSize + 1)
+  else -- immedately release
+    releaser r
 
-_acquire :: Ord p => (p -> IO r) -> (r -> IO ()) -> Cache p r -> Int -> IO CC.ThreadId -> p -> Int -> IO (r, IO ())
-_acquire acquirer releaser mVarCache maxPoolSize getThread p wait = do
+_acquire :: (Ord p, H.Hashable p) => (p -> IO r) -> (r -> IO ()) -> Cache p r -> Int -> IO CC.ThreadId -> MVar.MVar Int -> p -> Int -> IO (r, IO ())
+_acquire acquirer releaser map maxPoolSize getThread ps p wait = do
   threadId <- getThread
-  cacheMap <- MVar.takeMVar mVarCache
-  (newMap, r) <- case M.lookup (p,threadId) cacheMap of
-                  Just (r, _, _) -> return (M.delete (p,threadId) cacheMap, r)
-                  Nothing -> ((,) cacheMap) <$> acquirer p
-  MVar.putMVar mVarCache newMap
-    >> return (r, (addResourceToMap releaser mVarCache p r wait maxPoolSize getThread))
+  m <- M.lookup (p,threadId) map
+  poolSize <- MVar.takeMVar poolSize
+  r <- case m of
+        Just (r, _, _) -> M.delete (p,threadId) map
+          >> MVar.putMVar ps (poolSize - 1)
+          >> return r
+        Nothing -> acquirer p
+  return (r, (addResourceToMap releaser map p r wait maxPoolSize getThread))
 
-cleanCache :: Ord p => Cache p r -> IO ()
-cleanCache mVarCache = do
-  now <- getCurrentTime
-  cacheMap <- MVar.takeMVar mVarCache
-  let keysNReleasers = M.foldrWithKey (\k _ knrs ->
-                                      case M.lookup k cacheMap of
-                                          Just (_, expiry, releaser) ->
-                                            if expiry < now then (k, releaser) : knrs
-                                            else knrs
-                                          Nothing -> knrs) [] cacheMap
-      newMap = foldr (\(k,_) m -> M.delete k m) cacheMap keysNReleasers
-  (sequence $ map snd keysNReleasers)
-    >> MVar.putMVar mVarCache newMap
+cleanCache :: (Ord p, H.Hashable p) => Cache p r -> IO ()
+cleanCache map = do
+  return ()
+  -- now <- getCurrentTime
+  -- let keysNReleasers = M.foldrWithKey (\k _ knrs ->
+  --                                     case M.lookup k map of
+  --                                         Just (_, expiry, releaser) ->
+  --                                           if expiry < now then (k, releaser) : knrs
+  --                                           else knrs
+  --                                         Nothing -> knrs) [] map
+  --     newMap = foldr (\(k,_) m -> M.delete k m) map keysNReleasers
+  -- (sequence $ map snd keysNReleasers)
 
-cleanCacheLoop :: Ord p => Cache p r -> IO b
+cleanCacheLoop :: (Ord p, H.Hashable p) => Cache p r -> IO b
 cleanCacheLoop mVarCache =
   cleanCache mVarCache >> CC.threadDelay 1000000 >> cleanCacheLoop mVarCache
 
-pool :: Ord p => Int -> (p -> IO r) -> (r -> IO ()) -> IO (Pool p r)
+pool :: (Ord p, H.Hashable p) => Int -> (p -> IO r) -> (r -> IO ()) -> IO (Pool p r)
 pool maxPoolSize acquirer releaser = do
-  mVarCache <- MVar.newMVar M.empty
-  _ <- CC.forkIO (cleanCacheLoop mVarCache)
-  return Pool { acquire = _acquire acquirer releaser mVarCache maxPoolSize CC.myThreadId }
+  map <- M.empty
+  ps  <- MVar.newMVar 0
+  _ <- CC.forkIO (cleanCacheLoop map)
+  return Pool { acquire = _acquire acquirer releaser map maxPoolSize CC.myThreadId ps }
 
-poolWithoutGC :: Ord p => Int -> (p -> IO r) -> (r -> IO ()) -> IO (Pool p r)
+poolWithoutGC :: (Ord p, H.Hashable p) => Int -> (p -> IO r) -> (r -> IO ()) -> IO (Pool p r)
 poolWithoutGC maxPoolSize acquirer releaser = do
-  mVarCache <- MVar.newMVar M.empty
-  return Pool { acquire = _acquire acquirer releaser mVarCache maxPoolSize CC.myThreadId }
+  map <- M.empty
+  ps  <- MVar.newMVar 0
+  return Pool { acquire = _acquire acquirer releaser map maxPoolSize CC.myThreadId ps }
