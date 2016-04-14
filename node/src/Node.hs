68eb060302085ee1cbaf8316590560cd9f12fdf2
Sam Griffin <sam.griffin@gmail.com>
2016-04-14 18:41:26 +0000

    added in leveldb store backend

--- node/src/Node.hs
+++ node/src/Node.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE PatternSynonyms, CPP #-}
 
 module Main where
 
@@ -10,7 +10,11 @@ import Unison.Node.Store (Store)
 import Unison.Var (Var)
 import qualified Unison.ABT as ABT
 import qualified Unison.Node.BasicNode as BasicNode
+#ifdef leveldb
+import qualified Unison.Node.LeveldbStore as DBStore
+#else
 import qualified Unison.Node.FileStore as FileStore
+#endif
 import qualified Unison.NodeServer as NodeServer
 import qualified Unison.Reference as Reference
 import qualified Unison.Symbol as Symbol
@@ -21,8 +25,15 @@ hash :: Var v => Term.Term v -> Reference
 hash (Term.Ref' r) = r
 hash t = Reference.Derived (ABT.hash t)
 
+store :: IO (Store IO (Symbol.Symbol View.DFO))
+#ifdef leveldb
+store = DBStore.make "store"
+#else
+store = FileStore.make "store"
+#endif
+
 main :: IO ()
 main = do
-  store <- FileStore.make "store" :: IO (Store IO (Symbol.Symbol View.DFO))
-  node <- BasicNode.make hash store
+  store' <- store
+  node <- BasicNode.make hash store'
   NodeServer.server 8080 node
