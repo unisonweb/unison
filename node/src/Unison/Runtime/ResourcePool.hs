57745be8b44c4c51e321e7d3d40a99c5467baef1
Jack <steve.a.shogren@gmail.com>
2016-04-19 07:03:21 -0400

    Add type aliases for Ints to clarify

--- node/src/Unison/Runtime/ResourcePool.hs
+++ node/src/Unison/Runtime/ResourcePool.hs
@@ -15,28 +15,30 @@ type ResourceKey p = (p,CC.ThreadId)
 type Cache p r = M.Map (ResourceKey p) (r, UTCTime, IO ())
 type ReaperQueue p = TQ.TQueue (ResourceKey p)
 type MaxPoolSize = Int
+type Seconds = Int
+type PoolSize = Int
 
 incRef :: Num t => t -> (t, ())
 incRef a = (a+1,())
 decRef :: Num t => t -> (t, ())
 decRef a = (a-1,())
 
-addResourceToMap :: (Ord p, H.Hashable p) =>  (r -> IO ()) -> Cache p r -> p -> r -> Int -> MaxPoolSize -> IO CC.ThreadId -> IORef.IORef Int -> ReaperQueue p -> IO ()
-addResourceToMap release pool p r wait maxPoolSize getThread poolSizeR queue = do
+addResourceToMap :: (Ord p, H.Hashable p) =>  (r -> IO ()) -> Cache p r -> p -> r -> Seconds -> MaxPoolSize -> IO CC.ThreadId -> IORef.IORef PoolSize -> ReaperQueue p -> IO ()
+addResourceToMap release pool p r waitInSeconds maxPoolSize getThread poolSizeR queue = do
   now <- getCurrentTime
   poolSize <- IORef.readIORef poolSizeR
   threadId <- getThread
-  let expiry = addUTCTime (fromIntegral wait) now
+  let expiry = addUTCTime (fromIntegral waitInSeconds) now
   let key = (p,threadId)
-  if wait > 0 && (poolSize < maxPoolSize) then
+  if waitInSeconds > 0 && (poolSize < maxPoolSize) then
     -- add the resource to the pool with the release
     M.insert key (r, expiry, (release r)) pool
       >> IORef.atomicModifyIORef poolSizeR incRef
       >> STM.atomically (TQ.writeTQueue queue key)
     else release r
 
-_acquire :: (Ord p, H.Hashable p) => (p -> IO r) -> (r -> IO ()) -> Cache p r -> Int -> MaxPoolSize -> IORef.IORef Int -> ReaperQueue p -> IO CC.ThreadId -> p -> IO (r, IO ())
-_acquire acquire release pool wait maxPoolSize poolSizeR queue getThread p = do
+_acquire :: (Ord p, H.Hashable p) => (p -> IO r) -> (r -> IO ()) -> Cache p r -> Seconds -> MaxPoolSize -> IORef.IORef PoolSize -> ReaperQueue p -> IO CC.ThreadId -> p -> IO (r, IO ())
+_acquire acquire release pool waitInSeconds maxPoolSize poolSizeR queue getThread p = do
   threadId <- getThread
   m <- M.lookup (p,threadId) pool
   r <- case m of
@@ -44,7 +46,7 @@ _acquire acquire release pool wait maxPoolSize poolSizeR queue getThread p = do
           >> IORef.atomicModifyIORef poolSizeR decRef
           >> return r
         Nothing -> acquire p
-  return (r, (addResourceToMap release pool p r wait maxPoolSize getThread poolSizeR queue))
+  return (r, (addResourceToMap release pool p r waitInSeconds maxPoolSize getThread poolSizeR queue))
 
 cleanPool :: (Ord p, H.Hashable p) => Cache p r -> ReaperQueue p -> IO ()
 cleanPool pool queue = do
@@ -64,17 +66,17 @@ cleanPool pool queue = do
              >> cleanPool pool queue
     Nothing -> return ()
 
-pool :: (Ord p, H.Hashable p) => Int -> MaxPoolSize -> (p -> IO r) -> (r -> IO ()) -> IO (Pool p r)
-pool wait maxPoolSize acquire release = do
+pool :: (Ord p, H.Hashable p) => Seconds -> MaxPoolSize -> (p -> IO r) -> (r -> IO ()) -> IO (Pool p r)
+pool waitInSeconds maxPoolSize acquire release = do
   pool <- M.empty
   q <- STM.atomically TQ.newTQueue
   ps  <- IORef.newIORef 0
   _ <- CC.forkIO (cleanPool pool q)
-  return Pool { acquire = _acquire acquire release pool wait maxPoolSize ps q CC.myThreadId }
+  return Pool { acquire = _acquire acquire release pool waitInSeconds maxPoolSize ps q CC.myThreadId }
 
-poolWithoutGC :: (Ord p, H.Hashable p) => Int -> MaxPoolSize -> (p -> IO r) -> (r -> IO ()) -> IO (Pool p r)
-poolWithoutGC wait maxPoolSize acquire release = do
+poolWithoutGC :: (Ord p, H.Hashable p) => Seconds -> MaxPoolSize -> (p -> IO r) -> (r -> IO ()) -> IO (Pool p r)
+poolWithoutGC waitInSeconds maxPoolSize acquire release = do
   pool <- M.empty
   q <- STM.atomically TQ.newTQueue
   ps  <- IORef.newIORef 0
-  return Pool { acquire = _acquire acquire release pool wait maxPoolSize ps q CC.myThreadId }
+  return Pool { acquire = _acquire acquire release pool waitInSeconds maxPoolSize ps q CC.myThreadId }
