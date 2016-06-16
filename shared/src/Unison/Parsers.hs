71d04fafa421e2342126f8966d8ffa6d1a707958
Arya Irani <arya.irani@gmail.com>
2016-06-16 18:00:28 -0400

    add more from Builtins to Parsers

--- shared/src/Unison/Parsers.hs
+++ shared/src/Unison/Parsers.hs
@@ -48,17 +48,65 @@ unsafeParseType' :: [(V, Type V)] -> String -> Type V
 unsafeParseType' tr = unsafeGetSucceed . parseType' tr
 
 termBuiltins :: [(V, Term V)]
-termBuiltins = (Var.named *** Term.ref) <$>
+termBuiltins = (Var.named *** Term.ref) <$> (
     [ alias "+" "Number.plus"
     , alias "-" "Number.minus"
-    , builtin "Number.plus"
-    , builtin "Number.minus"
-    ]
+    , alias "*" "Number.times"
+    , alias "/" "Number.divide"
+    , builtin "()"
+    -- optional
+    , alias "some" "Optional.Some"
+    , alias "none" "Optional.None"
+    -- vector
+    , alias "single" "Vector.single"
+    , alias "prepend" "Vector.prepend"
+    , alias "map" "Vector.map"
+    , alias "fold-left" "Vector.fold-left"
+    , alias "empty" "Vector.empty"
+    , alias "concatenate" "Vector.concatenate"
+    , alias "append" "Vector.append"
+    -- Text
+    , alias "concatenate" "Text.concatenate"
+    , alias "left" "Text.left"
+    , alias "right" "Text.right"
+    , alias "center" "Text.center"
+    , alias "justify" "Text.justify"
+    -- Remote
+    , alias "fork" "Remote.fork"
+    , alias "receive" "Remote.receive"
+    , alias "receiveAsync" "Remote.receiveAsync"
+    , alias "pure" "Remote.pure"
+    , alias "bind" "Remote.bind"
+    , alias "channel" "Remote.channel"
+    , alias "send" "Remote.send"
+    , alias "here" "Remote.here"
+    , alias "at" "Remote.at"
+    -- Color
+    , alias "rgba" "Color.rgba"
+    -- Symbol
+    , alias "Symbol" "Symbol.Symbol"
+    ] >>= unpackAliases)
     where
       alias new known = (new, R.Builtin known)
       builtin t = (t, R.Builtin t)
+      unpackAliases p@(t1, R.Builtin t2) =
+        if t1 == t2 then [p] else [p, builtin t2]
+      unpackAliases p = [p]
 
 typeBuiltins :: [(V, Type V)]
 typeBuiltins = (Var.named *** Type.lit) <$>
   [ ("Number", Type.Number)
+  , builtin "Unit"
+  , ("Optional", Type.Optional)
+  -- ???
+  , builtin "Symbol"
+  , builtin "Alignment"
+  , builtin "Color"
+  , builtin "Fixity"
+  -- distributed
+  , builtin "Channel"
+  , builtin "Future"
+  , builtin "Remote"
+  , builtin "Remote!"
   ]
+  where builtin t = (t, Type.Ref $ R.Builtin t)
