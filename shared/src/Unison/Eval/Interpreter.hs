aaefe7a1e73202108582bdcaaf04aed43c69c14d
Paul Chiusano <paul.chiusano@gmail.com>
2016-08-08 23:41:07 -0400

    added let handling to interpreter

--- shared/src/Unison/Eval/Interpreter.hs
+++ shared/src/Unison/Eval/Interpreter.hs
@@ -37,6 +37,7 @@ eval env = Eval whnf step
           pure . Just $ foldl E.app e (drop (arity op) args)
       Just _ | otherwise -> pure Nothing
     reduce (E.App' f x) args = reduce f (x:args)
+    reduce (E.Let1' binding body) xs = reduce (ABT.bind body binding) xs
     reduce _ _ = pure Nothing
 
     step resolveRef e = case e of
@@ -48,13 +49,17 @@ eval env = Eval whnf step
         f <- E.link resolveRef (E.ref h)
         e <- reduce f []
         maybe (pure f) pure e
+      E.Let1' binding body -> step resolveRef (ABT.bind body binding)
+      E.LetRecNamed' bs body -> step resolveRef (ABT.substs substs body) where
+        substs = [ (v, ABT.subst v (E.letRec bs (E.var v)) b) | (v,b) <- bs ]
       _ -> pure e
 
     whnf resolveRef e = case e of
-      E.Let1' binding body -> whnf resolveRef (ABT.bind body binding)
-      -- E.LetRecNamed' bs body -> whnf resolveRef (ABT.bind body binding)
       E.App' f x -> do
         f' <- E.link resolveRef f
         e' <- reduce f' [x]
         maybe (pure e) (whnf resolveRef) e'
+      E.Let1' binding body -> whnf resolveRef (ABT.bind body binding)
+      E.LetRecNamed' bs body -> whnf resolveRef (ABT.substs substs body) where
+        substs = [ (v, ABT.subst v (E.letRec bs (E.var v)) b) | (v,b) <- bs ]
       _ -> pure e
