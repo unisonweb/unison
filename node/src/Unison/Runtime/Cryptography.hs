d44b144dd0e77723da9a53bddc60622d72df9c4e
Tim McIver <tim@timmciver.com>
2016-08-23 00:05:13 -0400

    Added an implementation for `randomBytes` function. Note that this impl uses the system's DRG and not ChaCha (yet).

--- node/src/Unison/Runtime/Cryptography.hs
+++ node/src/Unison/Runtime/Cryptography.hs
@@ -2,16 +2,21 @@ module Unison.Runtime.Cryptography where
 
 import Unison.Cryptography
 import Data.ByteString (ByteString)
+import qualified Crypto.Random as R
 
 -- Creates a Unison.Cryptography object specialized to use the noise protocol
 -- (http://noiseprotocol.org/noise.html).
 noise :: ByteString -> Cryptography ByteString () () () () () ()
 noise key = Cryptography key gen hash sign verify randomBytes encryptAsymmetric decryptAsymmetric encrypt decrypt pipeInitiator pipeResponder where
+  -- generates an elliptic curve keypair, for use in ECDSA
   gen = undefined
   hash = undefined
   sign _ = undefined
   verify _ _ _ = undefined
-  randomBytes n = undefined
+  randomBytes n = do
+    drg <- R.getSystemDRG
+    let bts = fst $ R.randomBytesGenerate n drg
+    return bts
   encryptAsymmetric _ cleartext = undefined
   decryptAsymmetric ciphertext = undefined
   encrypt _ bs = undefined
