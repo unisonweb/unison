39f52aa34c8875d56e6ca9fe583b4f24b8d62bb2
Paul Chiusano <paul.chiusano@gmail.com>
2016-08-24 22:01:07 -0400

    fix node tests to propagate logger

--- node/tests/Unison/Test/NodeUtil.hs
+++ node/tests/Unison/Test/NodeUtil.hs
@@ -30,6 +30,7 @@ import qualified Unison.Reference as Reference
 import qualified Unison.Runtime.ExtraBuiltins as EB
 import qualified Unison.Term as Term
 import qualified Unison.View as View
+import qualified Unison.Util.Logger as L
 
 type DFO = View.DFO
 type V = Symbol DFO
@@ -43,17 +44,18 @@ hash t = Reference.Derived (ABT.hash t)
 makeRandomAddress :: C.Cryptography k syk sk skp s h c -> IO Address
 makeRandomAddress crypt = Address <$> C.randomBytes crypt 64
 
-loadDeclarations :: FilePath -> Node IO V Reference (Type V) (Term V) -> IO ()
-loadDeclarations path node = do
+loadDeclarations :: L.Logger -> FilePath -> Node IO V Reference (Type V) (Term V) -> IO ()
+loadDeclarations logger path node = do
   -- note - when run from repl current directory is root, but when run via stack test, current
   -- directory is the shared subdir - so we check both locations
   txt <- Text.IO.readFile path <|> Text.IO.readFile (".." `FP.combine` path)
   let str = Text.unpack txt
   _ <- Note.run $ Node.declare' Term.ref str node
-  putStrLn $ "loaded file: " ++ path
+  L.info logger $ "loaded file: " ++ path
 
 makeTestNode :: IO (TestNode, String -> Term V)
 makeTestNode = do
+  logger <- L.atomic (L.atInfo L.toStandardOut)
   let crypto = C.noop "dummypublickey"
   putStrLn "creating block store..."
   blockStore <- MBS.make' (makeRandomAddress crypto) makeAddress
@@ -61,13 +63,13 @@ makeTestNode = do
   store' <- UBS.make blockStore
   -- store' <- FS.make "blockstore.file"
   putStrLn "created Node store..., building extra builtins"
-  extraBuiltins <- EB.makeAPI blockStore crypto
+  extraBuiltins <- EB.make logger blockStore crypto
   putStrLn "extra builtins created"
-  let makeBuiltins whnf = concat [Builtin.makeBuiltins whnf, extraBuiltins whnf]
+  let makeBuiltins whnf = concat [Builtin.makeBuiltins logger whnf, extraBuiltins whnf]
   node <- BasicNode.make hash store' makeBuiltins
-  putStrLn "Node created"
-  loadDeclarations "unison-src/base.u" node
-  loadDeclarations "unison-src/extra.u" node
+  L.info logger "Node created"
+  loadDeclarations logger "unison-src/base.u" node
+  loadDeclarations logger "unison-src/extra.u" node
   builtins <- Note.run $ Node.allTermsByVarName Term.ref node
   let parse = Parsers.bindBuiltins builtins [] . Parsers.unsafeParseTerm
   pure (node, parse)
