8c99adb495e166b66cb6447c0465ac50da0aa6f0
Tim McIver <tim@timmciver.com>
2016-09-02 20:57:57 -0400

    Added implementation for `encrypt` and `decrypt` with test.

--- node/src/Unison/Runtime/Cryptography.hs
+++ node/src/Unison/Runtime/Cryptography.hs
@@ -2,24 +2,46 @@ module Unison.Runtime.Cryptography where
 
 import Unison.Cryptography
 import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
+import Data.Either
+import qualified Data.ByteArray as BA
 import qualified Crypto.Random as R
+import qualified Crypto.Cipher.ChaCha as ChaCha
+
+nonce' :: IO ByteString
+nonce' = randomBytes' 8
+
+numRounds :: Int
+numRounds = 20
+
+nonce'' :: ByteString
+nonce'' = BS8.pack "\xe1\x04\x7b\xa9\x47\x6b\xf8\xff"
 
 -- Creates a Unison.Cryptography object specialized to use the noise protocol
 -- (http://noiseprotocol.org/noise.html).
-noise :: ByteString -> Cryptography ByteString () () () () () ()
+noise :: (BA.ByteArray cleartext, BA.ByteArray symmetricKey) => ByteString -> Cryptography ByteString symmetricKey () () () () cleartext
 noise key = Cryptography key gen hash sign verify randomBytes encryptAsymmetric decryptAsymmetric encrypt decrypt pipeInitiator pipeResponder where
   -- generates an elliptic curve keypair, for use in ECDSA
   gen = undefined
   hash = undefined
   sign _ = undefined
   verify _ _ _ = undefined
-  randomBytes n = do
-    drg <- R.getSystemDRG
-    let bts = fst $ R.randomBytesGenerate n drg
-    return bts
+  randomBytes = randomBytes'
   encryptAsymmetric _ cleartext = undefined
   decryptAsymmetric ciphertext = undefined
-  encrypt _ bs = undefined
-  decrypt _ bs = undefined
+  encrypt sk bss = do let cleartext = BA.concat bss
+                          chacha = ChaCha.initialize numRounds sk nonce''
+                          (cyphertext, _) = ChaCha.combine chacha cleartext
+                      return cyphertext
+  decrypt sk bs = Right $ let chacha = ChaCha.initialize numRounds sk nonce''
+                              ba = BA.pack . BS.unpack $ bs
+                              (cleartext, _) = ChaCha.combine chacha ba
+                          in cleartext
   pipeInitiator _ = undefined
   pipeResponder = undefined
+
+randomBytes' :: Int -> IO ByteString
+randomBytes' n = do drg <- R.getSystemDRG
+                    let bts = fst $ R.randomBytesGenerate n drg
+                    return bts
---
+++ node/tests/Unison/Test/Cryptography.hs
@@ -0,0 +1,24 @@
+
+
+module Unison.Test.Cryptography where
+
+import qualified Unison.Cryptography as C
+import qualified Data.ByteString.Char8 as B
+import Unison.Runtime.Cryptography
+import Test.Tasty
+import Test.Tasty.HUnit
+
+cryptoTest :: IO Assertion
+cryptoTest = do
+  let crypto = noise (B.pack "dummypublickey")
+      cleartext = (B.pack "cleartext")
+  symkey <- C.randomBytes crypto 16
+  cyphertext <- C.encrypt crypto symkey [cleartext]
+  let decypheredtext = C.decrypt crypto symkey cyphertext
+  case decypheredtext of
+    Left _ -> fail "Roundtrip encryption failure."
+    Right d -> return $ assertEqual "Original cleartext is not equal to decrypted message." d cleartext
+
+ioTests :: IO TestTree
+ioTests = fmap (testCase "Cryptography roundtrip test.") cryptoTest
+
