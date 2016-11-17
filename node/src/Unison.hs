d531021fa140d71a551b01f6723f3f2ed81d7fc9
Tim McIver <tim@timmciver.com>
2016-11-16 22:27:17 -0500

    Added some error messages to `unison` command.

--- node/src/Unison.hs
+++ node/src/Unison.hs
@@ -182,7 +182,7 @@ process codebase ("rename" : src : target) = do
       Note.run $ Codebase.updateMetadata codebase r md'
       putStrLn $ if null target then "OK appended " ++ suffix ++ " onto name(s)"
                                 else "OK"
-    _ -> pure ()
+    _ -> putStrLn "Could not find a definition with name \"" ++ src ++ "\" for renaming."
 process codebase ("edit" : rest) = do
   codebase <- codebase
   results <- search codebase (intercalate " " rest)
@@ -196,7 +196,7 @@ process codebase ("edit" : rest) = do
       let mdpath = name ++ ".markdown"
       writeFile mdpath ""
       tryEdits [name ++ ".u"]
-    _ -> pure ()
+    _ -> putStrLn "Could not find definition for editing."
 process codebase ("add" : []) = do
   files <- Directory.getDirectoryContents "."
   let ufiles = filter (".u" `Text.isSuffixOf`) (map Text.pack files)
