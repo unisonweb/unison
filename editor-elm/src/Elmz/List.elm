59acec921244395898f761efca0b85221e2c0fa7
Paul Chiusano <paul.chiusano@gmail.com>
2016-12-27 09:10:17 -0500

    deleted editor-elm, which is no longer being maintained

--- editor-elm/src/Elmz/List.elm
+++
@@ -1,8 +0,0 @@
-module Elmz.List where
-
-import List
-
-prefixes : List a -> List (List a)
-prefixes xs =
-  -- rather horrible quadratic implementation
-  List.map (\n -> List.take n xs) [0 .. List.length xs]
