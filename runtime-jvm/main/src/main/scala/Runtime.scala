946bbbc0289e3df259393b5e944068c6feb0fae4
Paul Chiusano <paul.chiusano@gmail.com>
2017-06-09 16:05:44 -0400

    finished filling in let rec compilation

--- runtime-jvm/main/src/main/scala/Runtime.scala
+++ runtime-jvm/main/src/main/scala/Runtime.scala
@@ -142,57 +142,107 @@ object Runtime {
     case Lam(names, body) =>
       compileLambda(builtins, e, Some(names.toSet), recursiveVars -- names)(names, body)
     case LetRec(bindings, body) =>
-      // ex:
-      //   let rec
-      //     blah = 42
-      //     let rec
-      //       ping x = pong (x + 1)
-      //       pong x = ping (x - 1)
-      //       ping blah
-      // add to recursive vars
-      // ping = (let rec ping = ...; pong = ...; ping)
-      val recursiveVars2 = recursiveVars ++ bindings.view.map(_._1).map { name =>
-        val bindings2 = bindings map { case (name, b) => (name, b map (_._1)) }
-        // easiest to compute annotateBound 'locally', then fixup by adding parent scopes bound vars
-        val appendEnv = (p: (Set[Name], Vector[Name])) => (p._1, p._2 ++ env(e)) // parent scopes vars appear later in the stack
-        (name, ABT.annotateBound(LetRec(bindings2:_*)(Var(name))) map appendEnv)
-      }
-      val boundByCurrentLambda2 = boundByCurrentLambda map (_ ++ bindings.map(_._1))
-      val compiledBindings = bindings.view.map(_._2).map(e => compile(builtins, e, boundByCurrentLambda2, recursiveVars2, IsNotTail)).toArray
-      val compiledBody = compile(builtins, body, boundByCurrentLambda2, recursiveVars2, isTail)
-      val names = bindings.map(_._1).toArray
-      // todo: consider doing something fancy to avoid needing to iterate over compiledBindings at runtime
-      // compile all the bindings and the body
-      // to evaluate, evaluate all the bindings, getting back a `Rt` for each
-      // then call bind on each
-      trait B { self : Rt =>
-        override def bind(env: Map[Name,Rt]) = {
-          // remove any bindings shadowed in local let rec
-          val env2 = env -- names
-          if (env2.nonEmpty) {
-            compiledBindings.foreach(_.bind(env2))
-            compiledBody.bind(env2)
-          }
-        }
-      }
-      // observation - most of the time, bindings will be lambdas, so doesn't really matter whether
-      // evaluation of bindings is super fast
-      // might want to 'de-rec' useless let recs since regular let code is going to be faster probably
-      arity(freeVars(e), env(e)) match {
-        case 0 => new Arity0(e,()) with B {
-          def apply(r: R) = {
-            val evaluatedBindings = compiledBindings.map(b => { eval(b, r); r.toRuntime })
-            val env = names.zip(evaluatedBindings).toMap
-            evaluatedBindings.foreach(b => b.bind(env))
-            compiledBody.bind(env) // note - compiledBindings expect evaluated bindings to be bound via `bind`
-            compiledBody(r)
-          }
-        }
-        // todo: finish filling in these let rec cases
-      }
+      compileLetRec(builtins, e, boundByCurrentLambda, recursiveVars, isTail, bindings, body)
     case Let1(name, binding, body) => // `let name = binding; body`
       compileLet1(name, binding, body, builtins, e, boundByCurrentLambda, recursiveVars, isTail)
-    // todo: finish Apply, LetRec
+    // todo: finish Apply
+  }
+
+  def compileLetRec(builtins: String => Rt, e: TermC, boundByCurrentLambda: Option[Set[Name]],
+                    recursiveVars: Map[Name,TermC], isTail: Boolean, bindings: List[(Name,TermC)], body: TermC): Rt = {
+    // ex:
+    //   let rec
+    //     blah = 42
+    //     let rec
+    //       ping x = pong (x + 1)
+    //       pong x = ping (x - 1)
+    //       ping blah
+    // ping = (let rec ping = ...; pong = ...; ping)
+    // todo: am hazy on how we are using recursive vars to decompile
+    val recursiveVars2 = recursiveVars ++ bindings.view.map(_._1).map { name =>
+      val bindings2 = bindings map { case (name, b) => (name, b map (_._1)) }
+      // easiest to compute annotateBound 'locally', then fixup by adding parent scopes bound vars
+      val appendEnv = (p: (Set[Name], Vector[Name])) => (p._1, p._2 ++ env(e)) // parent scopes vars appear later in the stack
+      (name, ABT.annotateBound(LetRec(bindings2:_*)(Var(name))) map appendEnv)
+    }
+    val boundByCurrentLambda2 = boundByCurrentLambda map (_ ++ bindings.map(_._1))
+    val compiledBindings = bindings.view.map(_._2).map(e => compile(builtins, e, boundByCurrentLambda2, recursiveVars2, IsNotTail)).toArray
+    val compiledBody = compile(builtins, body, boundByCurrentLambda2, recursiveVars2, isTail)
+    val names = bindings.map(_._1).toArray
+    // todo: consider doing something fancy to avoid needing to iterate over compiledBindings at runtime
+    // compile all the bindings and the body
+    // to evaluate, evaluate all the bindings, getting back a `Rt` for each
+    // then call bind on each
+    trait B { self : Rt =>
+      override def bind(env: Map[Name,Rt]) = {
+        // remove any bindings shadowed in local let rec
+        val env2 = env -- names
+        if (env2.nonEmpty) {
+          compiledBindings.foreach(_.bind(env2))
+          compiledBody.bind(env2)
+        }
+      }
+    }
+    // observation - most of the time, bindings will be lambdas, so doesn't really matter whether
+    // evaluation of bindings is super fast
+    // might want to 'de-rec' useless let recs since regular let code is going to be faster probably
+    arity(freeVars(e), env(e)) match {
+      case 0 => new Arity0(e,()) with B {
+        def apply(r: R) = {
+          val evaluatedBindings = compiledBindings.map(b => { eval(b, r); r.toRuntime })
+          val env = names.zip(evaluatedBindings).toMap
+          evaluatedBindings.foreach(b => b.bind(env))
+          compiledBody.bind(env) // note - compiledBindings expect evaluated bindings to be bound via `bind`
+          compiledBody(r)
+        }
+      }
+      case 1 => new Arity1(e,()) with B {
+        def apply(x1: D, x1b: Rt, r: R) = {
+          val evaluatedBindings = compiledBindings.map(b => { eval(b, x1, x1b, r); r.toRuntime })
+          val env = names.zip(evaluatedBindings).toMap
+          evaluatedBindings.foreach(b => b.bind(env))
+          compiledBody.bind(env) // note - compiledBindings expect evaluated bindings to be bound via `bind`
+          compiledBody(x1, x1b, r)
+        }
+      }
+      case 2 => new Arity2(e,()) with B {
+        def apply(x1: D, x1b: Rt, x2: D, x2b: Rt, r: R) = {
+          val evaluatedBindings = compiledBindings.map(b => { eval(b, x1, x1b, x2, x2b, r); r.toRuntime })
+          val env = names.zip(evaluatedBindings).toMap
+          evaluatedBindings.foreach(b => b.bind(env))
+          compiledBody.bind(env) // note - compiledBindings expect evaluated bindings to be bound via `bind`
+          compiledBody(x1, x1b, x2, x2b, r)
+        }
+      }
+      case 3 => new Arity3(e,()) with B {
+        def apply(x1: D, x1b: Rt, x2: D, x2b: Rt, x3: D, x3b: Rt, r: R) = {
+          val evaluatedBindings = compiledBindings.map(b => { eval(b, x1, x1b, x2, x2b, x3, x3b, r); r.toRuntime })
+          val env = names.zip(evaluatedBindings).toMap
+          evaluatedBindings.foreach(b => b.bind(env))
+          compiledBody.bind(env) // note - compiledBindings expect evaluated bindings to be bound via `bind`
+          compiledBody(x1, x1b, x2, x2b, x3, x3b, r)
+        }
+      }
+      case 4 => new Arity4(e,()) with B {
+        def apply(x1: D, x1b: Rt, x2: D, x2b: Rt, x3: D, x3b: Rt, x4: D, x4b: Rt, r: R) = {
+          val evaluatedBindings = compiledBindings.map(b => { eval(b, x1, x1b, x2, x2b, x3, x3b, x4, x4b, r); r.toRuntime })
+          val env = names.zip(evaluatedBindings).toMap
+          evaluatedBindings.foreach(b => b.bind(env))
+          compiledBody.bind(env) // note - compiledBindings expect evaluated bindings to be bound via `bind`
+          compiledBody(x1, x1b, x2, x2b, x3, x3b, x4, x4b, r)
+        }
+      }
+      case n => new ArityN(n,e,()) with B {
+        def apply(args: Array[Slot], r: R) = {
+          val evaluatedBindings = compiledBindings.map(b => { evalN(b, args, r); r.toRuntime })
+          val env = names.zip(evaluatedBindings).toMap
+          evaluatedBindings.foreach(b => b.bind(env))
+          compiledBody.bind(env) // note - compiledBindings expect evaluated bindings to be bound via `bind`
+          compiledBody(args, r)
+        }
+      }
+      // todo: finish filling in these let rec cases
+    }
   }
 
   def compileLet1(name: Name, binding: TermC, body: TermC,
