3a5660d8c21c3eea2bc33a01febdf5353e7df128
Paul Chiusano <paul.chiusano@gmail.com>
2017-06-14 14:07:36 -0400

    experimenting with different runtime representation where unboxed values are returned on the right

--- runtime-jvm/benchmark/src/main/scala/Fib.scala
+++ runtime-jvm/benchmark/src/main/scala/Fib.scala
@@ -84,13 +84,103 @@ object Fib extends App {
   // wonder if there's some way to decrease that overhead?
   // n - 1
 
+  case class R2(var get: Rt2)
+
+  trait Rt2 {
+    def apply(rec: Rt2, r: R2): Double
+    def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2): Double
+    def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2): Double
+  }
+  object Rt2 {
+    val x1 = new Rt2 {
+      def apply(rec: Rt2, r: R2): Double = ???
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2): Double = { r.get = x1b; x1 }
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2): Double = { r.get = x1b; x1 }
+    }
+    val x2 = new Rt2 {
+      def apply(rec: Rt2, r: R2): Double = ???
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2): Double = ???
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2): Double = { r.get = x2b; x2 }
+    }
+    val rec = new Rt2 {
+      def apply(rec: Rt2, r: R2): Double = { r.get = rec; 0.0 }
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2): Double = { r.get = rec; 0.0 }
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2): Double = { r.get = rec; 0.0 }
+    }
+    def if0(cond: Rt2, if0: Rt2, ifNot0: Rt2): Rt2 = new Rt2 {
+      def apply(rec: Rt2, r: R2): Double =
+        if (cond(rec, r) == 0.0) if0(rec, r) else ifNot0(rec, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2): Double =
+        if (cond(rec, x1, x1b, r) == 0.0) if0(rec, r) else ifNot0(rec, x1, x1b, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2): Double =
+        if (cond(rec, x1, x1b, x2, x2b, r) == 0.0) if0(rec, r) else ifNot0(rec, x1, x1b, x2, x2b, r)
+    }
+    def plus(x: Rt2, y: Rt2): Rt2 = new Rt2 {
+      def apply(rec: Rt2, r: R2): Double = x(rec, r) + y(rec, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2): Double = x(rec, x1, x1b, r) + y(rec, x1, x1b, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2): Double =
+        x(rec, x1, x1b, x2, x2b, r) + y(rec, x1, x1b, x2, x2b, r)
+    }
+    def minus(x: Rt2, y: Rt2): Rt2 = new Rt2 {
+      def apply(rec: Rt2, r: R2): Double = x(rec, r) - y(rec, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2): Double = x(rec, x1, x1b, r) - y(rec, x1, x1b, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2): Double =
+        x(rec, x1, x1b, x2, x2b, r) - y(rec, x1, x1b, x2, x2b, r)
+    }
+    def num(n: Double): Rt2 = new Rt2 {
+      def apply(rec: Rt2, r: R2): Double = n
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2): Double = n
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2): Double = n
+    }
+    def apRec(a: Rt2): Rt2 = new Rt2 {
+      def apply(rec: Rt2, r: R2) = rec(rec, a(rec, r), r.get, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2) = rec(rec, a(rec, x1, x1b, r), r.get, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2) = rec(rec, a(rec, x1, x1b, x2, x2b, r), r.get, r)
+    }
+    def ap(fn: Rt2, a: Rt2) = new Rt2 {
+      def apply(rec: Rt2, r: R2) = fn(fn, a(rec, r), r.get, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2) = fn(fn, a(rec, x1, x1b, r), r.get, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2) = fn(fn, a(rec, x1, x1b, x2, x2b, r), r.get, r)
+    }
+    def if1(cond: Rt2, if1: Rt2, ifNot1: Rt2): Rt2 = new Rt2 {
+      def apply(rec: Rt2, r: R2): Double =
+        if (cond(rec, r) == 1.0) if1(rec, r) else ifNot1(rec, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2): Double =
+        if (cond(rec, x1, x1b, r) == 1.0) if1(rec, r) else ifNot1(rec, x1, x1b, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2): Double =
+        if (cond(rec, x1, x1b, x2, x2b, r) == 1.0) if1(rec, r) else ifNot1(rec, x1, x1b, x2, x2b, r)
+    }
+    def decrement(x: Rt2, by: Double): Rt2 = new Rt2 {
+      def apply(rec: Rt2, r: R2): Double = x(rec, r) - by
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2): Double = x(rec, x1, x1b, r) - by
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2): Double = x(rec, x1, x1b, x2, x2b, r) - by
+    }
+
+    val fib = new Rt2 {
+      val body =
+        if0(x1, num(0.0),
+                if1(x1,
+                    num(1.0),
+                    plus(apRec(decrement(x1, 1.0)), apRec(decrement(x1, 2.0)))))
+
+      def apply(rec: Rt2, r: R2) = ???
+      def apply(rec: Rt2, x1: D, x1b: Rt2, r: R2): Double = body(rec, x1, x1b, r)
+      def apply(rec: Rt2, x1: D, x1b: Rt2, x2: D, x2b: Rt2, r: R2): Double = ???
+    }
+    val fibN = ap(fib, num(N))
+  }
+
   println(normalize(builtins)(fib))
   println(normalize(builtins)(Compiled(manuallyCompiledFib)(Num(N))))
   println(fibScala(N))
+  println(Rt2.fibN(null, R2(null)))
 
   val compiledFib = compile(builtins)(fib)
 
   QuickProfile.suite(
+    QuickProfile.timeit("manually-compiled (3)", 0.03) {
+      (Rt2.fibN(null, R2(null)) + math.random).toLong
+    },
     QuickProfile.timeit("unison", 0.03) {
       val r = Result()
       compiledFib(null, r)
