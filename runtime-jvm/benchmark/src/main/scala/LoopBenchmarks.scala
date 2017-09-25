902b2b9528efc3e7028ab33121a332e47db255b6
Paul Chiusano <paul.chiusano@gmail.com>
2017-09-25 11:57:05 -0400

    added benchmarks for Unison encoding of tail calls

--- runtime-jvm/benchmark/src/main/scala/LoopBenchmarks.scala
+++ runtime-jvm/benchmark/src/main/scala/LoopBenchmarks.scala
@@ -51,111 +51,191 @@ object LoopBenchmarks extends App {
     else a
 
   suite(
-    profile("iterateWhile") { iterateWhile(math.random)(_ + 1.0, _ < N).toLong },
-    profile("iterateWhile2") { iterateWhile2(N, math.random)((n,a) => n - 1.0, (n,a) => n + a, _ > 0.0).toLong },
     {
-      case object TC extends Throwable { override def fillInStackTrace = this }
-      case class Result(var boxed: AnyRef = null,
-                        var tailCall: AnyRef = null,
-                        var n: Double = 0.0,
-                        var nb: AnyRef = null,
-                        var sum: Double = 0.0,
-                        var sumb: AnyRef = null)
+      type V = AnyRef
+      type D = Double
+      case object SelfTC extends Throwable { override def fillInStackTrace = this }
+      case class Result(var boxed: V, var fn: V, var x0: D, var x0b: V, var x1: D, var x1b: V)
+      type R = Result
+      abstract class Lambda {
+        def apply(rec: Lambda, x0: D, x0b: V, x1: D, x1b: V, r: R): D
+        // def apply(rec: Lambda, x0: D, x0b: V, x1: D, x1b: V, x2: D, x2b: V, r: R): D = ???
+      }
+      val x0var = new Lambda {
+        def apply(rec: Lambda, x0: D, x0b: V, x1: D, x1b: V, r: R): D = x0
+      }
+      def foo(r: R) = ???
+      val step = new Lambda {
+        def apply(rec: Lambda, n: D, x0b: V, acc: D, x1b: V, r: R): D =
+          if ({ try x0var(rec, n, x0b, acc, x1b, r) catch { case SelfTC => foo(r) }} == 0.0) acc
+          // if (n == 0) acc
+          else { r.x0 = n - 1; r.x1 = acc + n; throw SelfTC }
+      }
+      // sum1toN n acc = if n == 0 then acc else sum1toN (n - 1) (acc + n)
 
-      profile("ex-based loop (1a)") {
-        var n = N + math.random
-        var sum = 0.0
-        def step0(n: Double, sum: Double, r: Result): Long = {
-          r.n = n - 1.0
-          r.sum = sum + n
-          if (n > 0.0) throw TC
-          else return r.sum.toLong
+      val sum1toN = new Lambda {
+        def apply(rec: Lambda, n0: D, x0b: V, acc0: D, x1b: V, r: R): D = {
+          var n = n0
+          var acc = acc0
+          var unused1 = x0b
+          var unused2 = x1b
+          while (true) {
+            try return step(step, n, x0b, acc, x1b, r)
+            catch { case SelfTC =>
+              n = r.x0
+              acc = r.x1
+              unused1 = r.x0b
+              unused2 = r.x1b
+            }
+          }
+          0.0
         }
-        val r = Result(n = N)
-        def go(): Long = { while (true) {
-        // def go(): Long = { val r = Result(n = N); while (true) { // 50x slower, todo: why??
-          try { return step0(r.n, r.sum, r) }
-          // catch { case TC => n = r.n; sum = r.sum } // this is slower than just letting r have loop state
-          catch { case TC => () }
-        }; 0L }
-        go()
+      }
+      profile("unison-based exception loop (1)") {
+        val r = Result(null, null, 0.0, null, 0.0, null)
+        sum1toN(sum1toN, (N + math.random).floor, null, 0.0, null, r).toLong
       }
     },
-    {
-      case object TC extends Throwable { override def fillInStackTrace = this }
-      case class Result(var boxed: AnyRef = null,
-                        var tailCall: AnyRef = null,
-                        var n: Double = 0.0,
-                        var nb: AnyRef = null,
-                        var sum: Double = 0.0,
-                        var sumb: AnyRef = null)
+    //{
+    //  // only difference between this and above is the while loop is encoded via
+    //  // an @annotation.tailrec function
+    //  type V = AnyRef
+    //  type D = Double
+    //  case object SelfTC extends Throwable { override def fillInStackTrace = this }
+    //  case class Result(var boxed: V, var fn: V, var x0: D, var x0b: V, var x1: D, var x1b: V)
+    //  type R = Result
+    //  abstract class Lambda {
+    //    def apply(rec: Lambda, x0: D, x0b: V, x1: D, x1b: V, r: R): D
+    //    // def apply(rec: Lambda, x0: D, x0b: V, x1: D, x1b: V, x2: D, x2b: V, r: R): D
+    //  }
+    //  val step = new Lambda {
+    //    def apply(rec: Lambda, n: D, x0b: V, acc: D, x1b: V, r: R): D =
+    //      if (n == 0) acc
+    //      else { r.x0 = n - 1; r.x1 = acc + n; throw SelfTC }
+    //  }
+    //  // sum1toN n acc = if n == 0 then acc else sum1toN (n - 1) (acc + n)
 
-      profile("ex-based loop (1b)") {
-        var n = N + math.random
-        var sum = 0.0
-        def step0(n: Double, sum: Double, r: Result): Long = {
-          r.n = n - 1.0
-          r.sum = sum + n
-          if (n > 0.0) throw TC
-          else return r.sum.toLong
-        }
-        val r = Result(n = N)
-        def go(): Long = { val r = Result(n = N); while (true) { // 50x slower, todo: why??
-          try { return step0(r.n, r.sum, r) }
-          catch { case TC => () }
-        }; 0L }
-        go()
-      }
-    },
-    {
-      case object TC extends Throwable { override def fillInStackTrace = this }
+    //  // mysteriously does not compile, scalac reports there is a call in non-tail position
+    //  // (but won't tell me what it is)
+    //  @annotation.tailrec
+    //  def loop(x0: D, x0b: V, x1: D, x1b: V, r: R): D =
+    //    try step(step, x0, x0b, x1, x1b, r)
+    //    catch { case e: SelfTC.type => loop(r.x0, r.x0b, r.x1, r.x1b, r) }
 
-      profile("ex-based loop (1)") {
-        var n = N + math.random
-        var sum = 0.0
-        while (n > 0.0) {
-          try { sum += n; n -= 1; throw TC }
-          catch { case TC => () }
-        }
-        sum.toLong
-      }
-    },
-    {
-      def step(n: Double, sum: Double) = throw new TC(n - 1, sum + n)
-      class TC(val n: Double, val sum: Double) extends Throwable {
-        override def fillInStackTrace = this
-      }
-      profile("ex-based loop (2)") {
-        var n = N + math.random
-        var sum = 0.0
-        while (n > 0.0) {
-          sum += n
-          try step(n, sum)
-          catch { case tc: TC => n = tc.n; sum = tc.sum }
-        }
-        sum.toLong
-      }
-    },
+    //  val sum1toN = new Lambda {
+    //    def apply(rec: Lambda, n0: D, x0b: V, acc0: D, x1b: V, r: R): D =
+    //      loop(n0, x0b, acc0, x1b, r)
+    //  }
+    //  profile("unison-based exception loop (2)") {
+    //    val r = Result(null, null, 0.0, null, 0.0, null)
+    //    sum1toN(sum1toN, (N + math.random).floor, null, 0.0, null, r).toLong
+    //  }
+    //},
     profile("double-based loop") {
-      var n = N + math.random
+      var n = (N + math.random).floor
       var sum = 0.0
       def go(): Long = { while (true) {
         sum += n
         n -= 1.0
-        if (n < 0.0) return sum.toLong
+        if (n == 0.0) return sum.toLong
       }; 0L }
       go()
-    },
-    profile("long-based loop") {
-      var n = N.toLong + (math.random * 2).toLong
-      var sum = 0L
-      def go(): Long = { while (true) {
-        sum += n
-        n -= 1L
-        if (n < 0) return sum
-      }; 0L }
-      go()
-    },
+    }
+    //profile("iterateWhile") { iterateWhile(math.random)(_ + 1.0, _ < N).toLong },
+    //profile("iterateWhile2") { iterateWhile2(N, math.random)((n,a) => n - 1.0, (n,a) => n + a, _ > 0.0).toLong },
+    //{
+    //  case object TC extends Throwable { override def fillInStackTrace = this }
+    //  case class Result(var boxed: AnyRef = null,
+    //                    var tailCall: AnyRef = null,
+    //                    var n: Double = 0.0,
+    //                    var nb: AnyRef = null,
+    //                    var sum: Double = 0.0,
+    //                    var sumb: AnyRef = null)
+
+    //  profile("ex-based loop (1a)") {
+    //    var n = N + math.random
+    //    var sum = 0.0
+    //    def step0(n: Double, sum: Double, r: Result): Long = {
+    //      r.n = n - 1.0
+    //      r.sum = sum + n
+    //      if (n > 0.0) throw TC
+    //      else return r.sum.toLong
+    //    }
+    //    val r = Result(n = N)
+    //    def go(): Long = { while (true) {
+    //    // def go(): Long = { val r = Result(n = N); while (true) { // 50x slower, todo: why??
+    //      try { return step0(r.n, r.sum, r) }
+    //      // catch { case TC => n = r.n; sum = r.sum } // this is slower than just letting r have loop state
+    //      catch { case TC => () }
+    //    }; 0L }
+    //    go()
+    //  }
+    //},
+    //{
+    //  case object TC extends Throwable { override def fillInStackTrace = this }
+    //  case class Result(var boxed: AnyRef = null,
+    //                    var tailCall: AnyRef = null,
+    //                    var n: Double = 0.0,
+    //                    var nb: AnyRef = null,
+    //                    var sum: Double = 0.0,
+    //                    var sumb: AnyRef = null)
+
+    //  profile("ex-based loop (1b)") {
+    //    var n = N + math.random
+    //    var sum = 0.0
+    //    def step0(n: Double, sum: Double, r: Result): Long = {
+    //      r.n = n - 1.0
+    //      r.sum = sum + n
+    //      if (n > 0.0) throw TC
+    //      else return r.sum.toLong
+    //    }
+    //    val r = Result(n = N)
+    //    def go(): Long = { val r = Result(n = N); while (true) { // 50x slower, todo: why??
+    //      try { return step0(r.n, r.sum, r) }
+    //      catch { case TC => () }
+    //    }; 0L }
+    //    go()
+    //  }
+    //},
+    //{
+    //  case object TC extends Throwable { override def fillInStackTrace = this }
+
+    //  profile("ex-based loop (1)") {
+    //    var n = N + math.random
+    //    var sum = 0.0
+    //    while (n > 0.0) {
+    //      try { sum += n; n -= 1; throw TC }
+    //      catch { case TC => () }
+    //    }
+    //    sum.toLong
+    //  }
+    //},
+    //{
+    //  def step(n: Double, sum: Double) = throw new TC(n - 1, sum + n)
+    //  class TC(val n: Double, val sum: Double) extends Throwable {
+    //    override def fillInStackTrace = this
+    //  }
+    //  profile("ex-based loop (2)") {
+    //    var n = N + math.random
+    //    var sum = 0.0
+    //    while (n > 0.0) {
+    //      sum += n
+    //      try step(n, sum)
+    //      catch { case tc: TC => n = tc.n; sum = tc.sum }
+    //    }
+    //    sum.toLong
+    //  }
+    //},
+    //profile("long-based loop") {
+    //  var n = N.toLong + (math.random * 2).toLong
+    //  var sum = 0L
+    //  def go(): Long = { while (true) {
+    //    sum += n
+    //    n -= 1L
+    //    if (n < 0) return sum
+    //  }; 0L }
+    //  go()
+    //},
   )
 }
 
