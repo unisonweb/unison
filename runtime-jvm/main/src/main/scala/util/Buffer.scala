ec2160f98190831c1c8d850ef220d5a0f377d8bb
Paul Chiusano <paul.chiusano@gmail.com>
2018-02-14 21:34:57 -0500

    delete old Buffer.fromString

--- runtime-jvm/main/src/main/scala/util/Buffer.scala
+++ runtime-jvm/main/src/main/scala/util/Buffer.scala
@@ -67,63 +67,6 @@ object Buffer {
   def empty[A](implicit newArray: NewArray[A]): Buffer[A] =
     viewArray(newArray(16))
 
-  /**
-   * A `Buffer[String]`. Indices into this `Buffer` refer to codepoints,
-   * not Java `char` values. The `i`th value of this `Buffer` is a
-   * `String` consisting of the `i`th codepoint (which might need 1 or 2
-   * `char` values to encode, since the Java `char` type is only 16 bits and
-   * 21 bits are needed to express any codepoint in Unicode).
-   *
-   * NB: There is a huge amount of broken code out there that indexes directly
-   * into an `Array[Char]` or `String` and assumes the result is something meaningful,
-   * when in fact that code only works if all characters being used fit in the 16 bit
-   * `char` type that Java provides.
-   *
-   * TODO: untested
-   */
-  def fromString(s0: String): Buffer[String] = new Buffer[String] {
-    val buf = new java.lang.StringBuilder(s0)
-    var multicharPositions = Array.empty[Int]
-    var numMultichar = 0
-
-    def apply(i: Int) = {
-      val ind0 = java.util.Arrays.binarySearch(multicharPositions, i)
-      val offset = if (ind0 < 0) (ind0 + 1).abs else ind0
-      new java.lang.String(java.lang.Character.toChars(buf.codePointAt(i + offset)))
-    }
-    def copy(from: Int, len: Int) = fromString(buf.subSequence(from, from + len).toString)
-    def copyTo(i: Int, dest: Buffer[String], len: Int) =
-      dest :+ (i, buf.subSequence(0, len).toString)
-
-    private def addMulticharPosition(i: Int) = {
-      if (numMultichar >= multicharPositions.length) {
-        multicharPositions = multicharPositions ++ Array.fill(multicharPositions.length)(Int.MaxValue)
-      }
-      multicharPositions(numMultichar) = i
-    }
-
-    def :+(i: Int, s: String) = {
-      if (i == buf.length) buf.append(s)
-      else buf.insert(i, s)
-      var j = i
-      while (j < buf.length) {
-        val j2 = if (buf.charAt(j).isHighSurrogate) j + 2 else j + 1
-        if (j2 - j == 2) addMulticharPosition(j)
-        j = j2
-      }
-      this
-    }
-
-    def :++(i0: Int, es: Array[String], len: Int) = {
-      var i = i0
-      var j = 0
-      while (j < es.length) { this :+ (i, es(j)); i += 1; j += 1 }
-      this
-    }
-    def empty = fromString("")
-    def toArray(size: Int) = Array(buf.subSequence(0, size).toString)
-  }
-
   abstract class NewArray[A] { def apply(size: Int): Array[A] }
 
   object NewArray extends LowPriorityNewArrays {
