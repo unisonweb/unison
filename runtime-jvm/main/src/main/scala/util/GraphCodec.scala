288c261c8b20b7255b1fc80e661bed4e1118beaa
Paul Chiusano <paul.chiusano@gmail.com>
2018-03-26 22:17:07 -0400

    figured out GraphCodec.encode, sketched out decode

--- runtime-jvm/main/src/main/scala/util/GraphCodec.scala
+++ runtime-jvm/main/src/main/scala/util/GraphCodec.scala
@@ -1,73 +1,67 @@
-//package org.unisonweb.util
-//
-//import scala.collection.mutable
-//import java.util.IdentityHashMap
-//
-//object GraphCodec {
-//
-//  type Position = Long
-//
-//  trait Graphstream {
-//    def byteCount: Long
-//  }
-//
-//  object Graphstream {
-//
-//    trait Nested {
-//      def bytePrefix: Array[Byte]
-//
-//      /**
-//       * `f` receives the accumulated `r`, the current child `Graphstream`,
-//       * and the position of that `Graphstream`. The first child has a position
-//       * of `start + bytePrefix.length`.
-//       */
-//      def childGraphs[R](r: R)(f: (R,Graphstream) => R): R
-//
-//      lazy val byteCount: Long =
-//        childGraphs(prefix.length)(prefix.length.toLong)((r,pos,g) => r + g.byteCount)
-//    }
-//
-//    /** A reference to an earlier position in the stream. */
-//    // just need a way of refering to the Ref, could be anything, but ideally not
-//    // based on pointer equality - actually, that is perfectly fine - it is later
-//    // in serialization that we convert to numbered positions
-//    trait Ref extends Graphstream {
-//      def referencePosition: Position
-//      def byteCount = 8L
-//    }
-//  }
-//
-//  // todo - figure out how childGraphs works
-//
-//  trait Cyclic[A] { def cyclicFold[R](pure: A => R, ref: (Name, => A) => R): R }
-//
-//  def encode[A](expand: A => (Array[Byte], Array[A])): A => Graphstream =
-//    root => {
-//      import Graphstream._
-//      val m = new IdentityHashMap[A,Position]
-//      def ref(pos: Position): Array[Byte] = ??? // todo
-//
-//      def go(pos: Position, a: A): Graphstream = {
-//        val pos0 = m.get(a)
-//        if (pos0 eq null) {
-//          m.put(a, pos)
-//          val (prefix0, children) = expand(a)
-//          var pos1 = pos
-//          Nested(prefix0, children.map(a => { val s = go(pos, a); pos1 += s.size; s }))
-//        }
-//        else Ref(pos0)
-//      }
-//      go(0, root)
-//    }
-//
-//  def decode[A](make: (Array[Byte], Sequence[A]) => A): Graphstream => A = {
-//    val m = new mutable.HashMap[Position,A]
-//    def go(g: Graphstream): A = g match {
-//      case Graphstream.Ref(pos) =>
-//        m.getOrElse(pos, sys.error("reference to nonexistent position"))
-//      case Graphstream.Nested(prefix, children) =>
-//        make(prefix, children map (go))
-//    }
-//    go(_)
-//  }
-//}
+package org.unisonweb.util
+
+import java.util.IdentityHashMap
+
+object GraphCodec {
+
+  trait Graphstream {
+    def bytePrefix: Array[Byte]
+    def foldLeft[B](b: B)(f: (B,Graphstream) => B): B
+  }
+
+  object Graphstream {
+    trait Nested extends Graphstream
+    trait Ref extends Graphstream {
+      def dereference: Graphstream
+    }
+  }
+
+  trait Sink {
+    def putInt(n: Int): Unit
+    def put(bs: Array[Byte]): Unit
+    def putByte(b: Byte): Unit
+    def putLong(n: Long): Unit
+    def putDouble(n: Double): Unit
+    def position: Long
+  }
+
+  val NestedStartMarker = 0 : Byte
+  val NestedEndMarker = 1 : Byte
+  val RefMarker = 2 : Byte
+
+  /** Encode a `Graphstream` and return the number of bytes written. */
+  def encode(g: Graphstream)(buf: Sink): Unit = {
+    val seen = new IdentityHashMap[Graphstream.Ref,Long]()
+    def go(g: Graphstream): Unit = g match {
+      case g : Graphstream.Nested =>
+        buf.putByte(NestedStartMarker) // indicates a Nested follows
+        buf.putInt(g.bytePrefix.length)
+        buf.put(g.bytePrefix)
+        g.foldLeft(())((_,g) => go(g))
+        buf.putByte(NestedEndMarker)
+      case r : Graphstream.Ref =>
+        val pos = seen.get(r)
+        if (seen.get(r) eq null) {
+          seen.put(r, buf.position)
+          go(r.dereference)
+        }
+        else {
+          val before = buf.position
+          buf.putByte(RefMarker)
+          buf.putLong(pos)
+        }
+    }
+    go(g)
+  }
+
+  // pretty simple - keep a LongMap[Graphstream] mapping positions
+  // to decoded Graphstream
+  // read first byte, if it's a 0, read a long, lookup in the map
+  // otherwise if it's a 1, read the prefix, then read the children, recursively
+  def decode(bytes: Sequence[Byte]): Graphstream = ???
+
+  // def decode
+  //  def decode[A](make: (Array[Byte], Sequence[A]) => A): Graphstream => A = {
+
+}
+
