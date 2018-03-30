c9ae59bc2f351787541f5ddddaa9df0144b142b3
Paul Chiusano <paul.chiusano@gmail.com>
2018-03-30 16:18:21 -0400

    Got rid of object orientation

--- runtime-jvm/main/src/main/scala/util/GraphCodec.scala
+++ runtime-jvm/main/src/main/scala/util/GraphCodec.scala
@@ -2,77 +2,99 @@ package org.unisonweb.util
 
 import java.util.IdentityHashMap
 import scala.collection.immutable.LongMap
+import Codecs.{Source,Sink}
 
 object GraphCodec {
 
-  trait Graphstream {
-    def bytePrefix: Array[Byte]
-    def foldLeft[B](b: B)(f: (B,Graphstream) => B): B
-  }
+  trait Graphstream[G,R<:G] {
+    def bytePrefix(graph: G): Array[Byte]
+    def foldLeft[B](graph: G)(b: B)(f: (B,G) => B): B
+    def foreach(graph: G)(f: G => Unit): Unit
 
-  object Graphstream {
-    trait Nested extends Graphstream
-    trait Ref extends Graphstream {
-      def dereference: Graphstream
-    }
-  }
+    def isReference(graph: G): Boolean
+    def dereference(graph: R): G
+    def reference(position: Long, prefix: Array[Byte]): R
+    def setReference(ref: R, referent: G): Unit
+    def nested(prefix: Array[Byte], children: Sequence[G]): G
 
-  trait Sink {
-    def put(bs: Array[Byte]): Unit
-    def putByte(b: Byte): Unit
-    def putInt(n: Int): Unit
-    def putLong(n: Long): Unit
-    def putDouble(n: Double): Unit
-    def position: Long
-  }
-
-  /** Encode a `Graphstream`. `includeRefMetadata` controls whether the `bytePrefix`
-   *  of each `Ref` is written to the output as well. */
-  def encode(buf: Sink, includeRefMetadata: Boolean): Graphstream => Unit = {
-    val seen = new IdentityHashMap[Graphstream,Long]()
-    def go(g: Graphstream): Unit = g match {
-      case g : Graphstream.Nested =>
-        val pos = seen.get(g)
-        if (pos eq null) {
-          seen.put(g, buf.position)
-          buf.putByte(NestedStartMarker) // indicates a Nested follows
-          buf.putInt(g.bytePrefix.length)
-          buf.put(g.bytePrefix)
-          g.foldLeft(())((_,g) => go(g))
-          buf.putByte(NestedEndMarker)
-        }
-        else {
-          buf.putByte(SeenMarker)
-          buf.putLong(pos)
-        }
-      case r : Graphstream.Ref =>
-        val pos = seen.get(r)
-        if (pos eq null) {
-          seen.put(r, buf.position)
-          buf.putByte(RefMarker)
-          if (includeRefMetadata) {
-            buf.putByte(RefMetadata)
-            buf.putInt(r.bytePrefix.length)
-            buf.put(r.bytePrefix)
+    /**
+     * Encode a `G` to the given `Sink`.
+     * `includeRefMetadata` controls whether the `bytePrefix`
+     * of each `g: G` which passes `isReference(g)` is written
+     * to the output as well.
+     */
+    def encode(buf: Sink, includeRefMetadata: Boolean): G => Unit = {
+      val seen = new IdentityHashMap[G,Long]()
+      def go(g: G): Unit = {
+        if (isReference(g)) {
+          val r = g.asInstanceOf[R]
+          val pos = seen.get(g)
+          if (pos eq null) {
+            seen.put(g, buf.position)
+            buf.putByte(RefMarker)
+            if (includeRefMetadata) {
+              buf.putByte(RefMetadata)
+              buf.putFramed(bytePrefix(r))
+            }
+            else buf.putByte(RefNoMetadata)
+            go(dereference(r))
           }
-          else buf.putByte(RefNoMetadata)
-          go(r.dereference)
+          else {
+            buf.putByte(RefSeenMarker.toByte)
+            buf.putLong(pos)
+          }
         }
         else {
-          buf.putByte(RefSeenMarker.toByte)
-          buf.putLong(pos)
+          val pos = seen.get(g)
+          if (pos eq null) {
+            seen.put(g, buf.position)
+            buf.putByte(NestedStartMarker) // indicates a Nested follows
+            buf.putFramed(bytePrefix(g))
+            foreach(g)(go)
+            buf.putByte(NestedEndMarker)
+          }
+          else {
+            buf.putByte(SeenMarker)
+            buf.putLong(pos)
+          }
         }
+      }
+      go(_)
     }
-    go(_)
-  }
 
-  trait Source {
-    def get(n: Int): Array[Byte]
-    def getByte: Byte
-    def getInt: Int
-    def getLong: Long
-    def getDouble: Double
-    def position: Long
+    def decode(src: Source): G = {
+      case object NestedEnd extends Throwable { override def fillInStackTrace = this }
+      var decoded = LongMap.empty[G]
+
+      def read1: G = { val pos = src.position; (src.getByte.toInt: @annotation.switch) match {
+        case NestedStartMarker =>
+          val g = nested(src.get(src.getInt), readN(Sequence.empty))
+          decoded = decoded.updated(pos, g)
+          g
+        case NestedEndMarker => throw NestedEnd
+        case SeenMarker => decoded(src.getLong)
+        case RefMarker =>
+          val r =
+            if (src.getByte.toInt == RefMetadata)
+              reference(src.position, src.get(src.getInt))
+            else
+              reference(src.position, Array.empty)
+          decoded = decoded.updated(pos, r)
+          val g = read1
+          setReference(r, g)
+          g
+        case RefSeenMarker => decoded(src.getLong)
+      }}
+
+      def readN(buf0: Sequence[G]): Sequence[G] = {
+        var buf = buf0; while (true) {
+          try { buf = buf :+ read1 }
+          catch { case NestedEnd => return buf }
+        }
+        buf
+      }
+      read1
+    }
   }
 
   final val NestedStartMarker = 0
@@ -82,53 +104,5 @@ object GraphCodec {
   final val RefSeenMarker = 4
   final val RefMetadata = 0
   final val RefNoMetadata = 1
-
-  def decode[A,R<:A](nested: (Array[Byte], Sequence[A]) => A,
-                     ref: (Long,Array[Byte]) => R,
-                     setRef: (R,A) => Unit)(src: Source): A = {
-    case object NestedEnd extends Throwable { override def fillInStackTrace = this }
-    var decoded = LongMap.empty[A]
-
-    def read1: A = { val pos = src.position; (src.getByte.toInt: @annotation.switch) match {
-      case NestedStartMarker =>
-        val a = nested(src.get(src.getInt), readNestedChildren(Sequence.empty))
-        decoded = decoded.updated(pos, a)
-        a
-      case NestedEndMarker => throw NestedEnd
-      case SeenMarker => decoded(src.getLong)
-      case RefMarker =>
-        val r =
-          if (src.getByte.toInt == RefMetadata)
-            ref(src.position, src.get(src.getInt))
-          else
-            ref(src.position, Array.empty)
-        decoded = decoded.updated(pos, r)
-        val a = read1
-        setRef(r, a)
-        a
-      case RefSeenMarker => decoded(src.getLong)
-    }}
-
-    def readNestedChildren(buf0: Sequence[A]): Sequence[A] = {
-      var buf = buf0
-      while (true) {
-        try { buf = buf :+ read1 }
-        catch { case NestedEnd => return buf }
-      }
-      buf
-    }
-    read1
-  }
-
-  def toBytes(n: Long): Array[Byte] = {
-    val result = new Array[Byte](8)
-    var m = n
-    var i = 8 - 1; while (i >= 0) {
-      result(i) = (m & 0xFF).toByte
-      m >>= 8
-      i -= 1
-    }
-    return result;
-  }
 }
 
