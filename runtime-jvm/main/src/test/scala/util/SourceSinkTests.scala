cf630f4fb3812f3dc29e6f32ece82dfafaf43a8e
Paul Chiusano <paul.chiusano@gmail.com>
2018-05-11 11:19:08 -0400

    test stubbed out and compiles

--- runtime-jvm/main/src/test/scala/util/SourceSinkTests.scala
+++ runtime-jvm/main/src/test/scala/util/SourceSinkTests.scala
@@ -5,13 +5,14 @@ import org.unisonweb.EasyTest._
 object SourceSinkTests {
   val tests =
     test("source/sink") { implicit T =>
-      val input = replicate(20)
-      val bytes = Sink.toChunks(10) { sink =>
-        replicate(20)
-      }
+      fail("this test needs writing")
+      //val input = replicate(20)
+      //val bytes = Sink.toChunks(10) { sink =>
+      //  replicate(20)
+      //}
       ok
     }
 
-  def source: Source =
+  // def source: Source =
 }
 
