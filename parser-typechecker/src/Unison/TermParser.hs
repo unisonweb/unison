e2c3a4a278acc6c81ff418acee393e5bd65703fc
Paul Chiusano <paul.chiusano@gmail.com>
2018-05-17 18:23:58 -0400

    TermParsers.block compiling, also deleted some unused old parsers

--- parser-typechecker/src/Unison/TermParser.hs
+++ parser-typechecker/src/Unison/TermParser.hs
@@ -21,6 +21,7 @@ import qualified Unison.Term as Term
 import           Unison.Type (Type)
 import qualified Unison.Type as Type
 import qualified Unison.TypeParser as TypeParser
+import qualified Unison.Typechecker.Components as Components
 import           Unison.Var (Var)
 import qualified Unison.Var as Var
 
@@ -61,7 +62,7 @@ term2 = lam term2 <|> term3
 
 term3 :: Var v => Parser (S v) (Term v)
 term3 = do
-  t <- let_ <|> ifthen <|> infixApp
+  t <- ifthen <|> infixApp
   ot <- optional (token (char ':') *> TypeParser.type_)
   pure $ case ot of
     Nothing -> t
@@ -74,10 +75,7 @@ infixApp = chainl1 term4 (f <$> infixVar)
     f op lhs rhs = Term.apps (Term.var op) [lhs,rhs]
 
 term4 :: Var v => Parser (S v) (Term v)
-term4 = effectBlock <|> term5
-
-term5 :: Var v => Parser (S v) (Term v)
-term5 = f <$> some termLeaf
+term4 = f <$> some termLeaf
   where
     f (func:args) = Term.apps func args
     f [] = error "'some' shouldn't produce an empty list"
@@ -103,45 +101,6 @@ tupleOrParenthesized rec =
     go terms = foldr pair unit terms -- it's a tuple literal
     pair t1 t2 = Term.builtin "pair" `Term.app` t1 `Term.app` t2
     unit = Term.builtin "()"
-
--- |
--- do Remote x := pure 23; y := at node2 23; pure 19;;
--- do Remote action1; action2;;
--- do Remote action1; x = 1 + 1; action2;;
--- do Remote
---   x := pure 23
---   y = 11
---   pure (f x)
-effectBlock :: forall v . Var v => Parser (S v) (Term v)
-effectBlock = (token (string "do") *> wordyId keywords) >>= go where
-  go name = do
-    bindings <- L.laidout $ asum [Right <$> binding, Left <$> action]
-    Just result <- pure $ foldr bind Nothing bindings
-    pure result
-    where
-    qualifiedPure, qualifiedBind :: Term v
-    qualifiedPure = ABT.var' (Text.pack name `mappend` Text.pack ".pure")
-    qualifiedBind = ABT.var' (Text.pack name `mappend` Text.pack ".bind")
-    bind :: (Either (Term v) (v, Term v)) -> Maybe (Term v) -> Maybe (Term v)
-    bind = go where
-      go (Right (lhs,rhs)) (Just acc) = Just $ qualifiedBind `Term.apps` [Term.lam lhs acc, rhs]
-      go (Right (_,_)) Nothing = Nothing
-      go (Left action) (Just acc) = Just $ qualifiedBind `Term.apps` [Term.lam (ABT.v' "_") acc, action]
-      go (Left action) _ = Just action
-    interpretPure :: Term v -> Term v
-    interpretPure = ABT.subst (ABT.v' "pure") qualifiedPure
-    binding :: Parser (S v) (v, Term v)
-    binding = do
-      (lhs, eff) <- attempt $ do
-        lhs <- ABT.v' . Text.pack <$> token (wordyId keywords)
-        eff <- token $ (True <$ string ":=") <|> (False <$ string "=")
-        pure (lhs, eff)
-      rhs <- L.block term
-      let rhs' = if eff then interpretPure rhs
-                 else qualifiedPure `Term.app` rhs
-      pure (lhs, rhs')
-    action :: Parser (S v) (Term v)
-    action = interpretPure <$> L.block term
 
 text' :: Parser s Text.Text
 text' =
@@ -186,19 +145,6 @@ vector p = Term.app (Term.builtin "Vector.force") . Term.vector <$> (lbracket *>
     comma = token (char ',')
     rbracket = token (char ']')
 
-let_ :: Var v => Parser (S v) (Term v)
-let_ = join $ fixup <$> (let_ *> optional rec_) <*> L.laidout (many alias *> bindingOrTerm)
-  where
-    let_ = token (string "let")
-    rec_ = token (string "rec") $> ()
-    bindingOrTerm = (Left <$> binding) <|> (Right <$> term2)
-    fixup r bs = case reverse bs of
-      (Left _b : _) -> fail "let block must end with an expression"
-      (Right e : bs) -> pure (f r (reverse [ b | Left b <- bs ], e))
-      [] -> fail "empty let block"
-    f Nothing (bindings,body) = Term.let1 bindings body
-    f (Just _) (bindings,body) = Term.letRec bindings body
-
 binding :: Var v => Parser (S v) (v, Term v)
 binding = label "binding" $ do
   typ <- optional typedecl <* optional semicolon
@@ -209,13 +155,13 @@ binding = label "binding" $ do
     Nothing -> do
       -- we haven't seen a type annotation, so lookahead to '=' before commit
       (name, args) <- attempt (lhs <* token (char '='))
-      body <- L.block term
+      body <- block
       pure $ mkBinding name args body
     Just (nameT, typ) -> do
       (name, args) <- lhs
       when (name /= nameT) $
         fail ("The type signature for ‘" ++ show (Var.name nameT) ++ "’ lacks an accompanying binding")
-      body <- token (char '=') *> L.block term
+      body <- token (char '=') *> block
       pure $ fmap (\e -> Term.ann e typ) (mkBinding name args body)
   where
   mkBinding f [] body = (f, body)
@@ -223,10 +169,6 @@ binding = label "binding" $ do
 
 typedecl :: Var v => Parser (S v) (v, Type v)
 typedecl = (,) <$> attempt (prefixVar <* token (char ':')) <*> L.block TypeParser.type_
-
-bindingEqBody :: Parser (S v) (Term v) -> Parser (S v) (Term v)
-bindingEqBody p = eq *> L.block p
-  where eq = token (char '=')
 
 infixVar :: Var v => Parser s v
 infixVar = (Var.named . Text.pack) <$> (backticked <|> symbolyId keywords)
@@ -260,7 +202,22 @@ keywords =
   ]
 
 block :: Var v => Parser (S v) (Term v)
-block = L.laidout (binding <|> term)
+block = go =<< L.block (sepBy (L.spaced L.semi) statement)
+  where
+  statement = (Right <$> binding) <|> (Left <$> blockTerm)
+  toBinding (Right (v, e)) = (v,e)
+  toBinding (Left e) = (Var.named "_", e)
+  go bs = case reverse bs of
+    (Right _e : _) -> fail "let block must end with an expression"
+    (Left e : bs) -> pure . Components.minimize' $ Term.letRec (toBinding <$> reverse bs) e
+    [] -> fail "empty block"
+
+-- We disallow type annotations and lambdas,
+-- just function application and operators
+blockTerm :: Var v => Parser (S v) (Term v)
+blockTerm =
+  lam term <|> ifthen <|> infixApp
+  -- todo: pattern matching in here once we have a parser for it
 
 lam :: Var v => Parser (S v) (Term v) -> Parser (S v) (Term v)
 lam p = attempt (Term.lam'' <$> vars <* arrow) <*> body
