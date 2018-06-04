bc2cc500695fb1ba5f1237a08d42ab1b964d4959
Arya Irani <arya.irani@gmail.com>
2018-06-04 14:34:35 -0400

    started a file

---
+++ parser-typechecker/src/Unison/Codecs.hs
@@ -0,0 +1,12 @@
+module Unison.Codecs where
+
+-- serializeTerm :: MonadPut m => Term v -> m ()
+--
+--
+-- serializeDataDeclaration :: MonadPut m => DataDeclaration v -> m ()
+--
+--
+-- serializeEffectDeclaration :: MonadPut m => EffectDeclaration v -> m ()
+--
+--
+-- serializeFile :: MonadPut m => UnisonFile
