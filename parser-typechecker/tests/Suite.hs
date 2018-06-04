dcf6acb372b4e27b28f30473a0e804ebe94b2dff
Arya Irani <arya.irani@gmail.com>
2018-06-04 12:27:22 -0400

    add canonicalization tests for data type hashing
    also have the test suite take a scope prefix on the command-line

--- parser-typechecker/tests/Suite.hs
+++ parser-typechecker/tests/Suite.hs
@@ -2,18 +2,21 @@ module Main where
 
 import EasyTest
 import System.IO
+import System.Environment (getArgs)
 import qualified Unison.Test.Common as Common
 import qualified Unison.Test.TermParser as TermParser
 import qualified Unison.Test.FileParser as FileParser
+import qualified Unison.Test.DataDeclaration as DataDeclaration
 
 test :: Test ()
-test = scope "unison" $ tests [
-  TermParser.test
-  ,
-  FileParser.test
+test = tests [
+  TermParser.test,
+  FileParser.test,
+  DataDeclaration.test
  ]
 
 main :: IO ()
 main = do
+  args <- getArgs
   mapM_ (`hSetEncoding` utf8) [stdout, stdin, stderr]
-  run test
+  runOnly (case args of [] -> ""; [prefix] -> prefix) test
