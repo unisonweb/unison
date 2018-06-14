8c5c4847abb09a2801f413def2a39ac57ceb7512
Arya Irani <arya.irani@gmail.com>
2018-06-14 19:46:41 -0400

    use _updated_ body of synthesized UnisonFile when serializing

--- parser-typechecker/bootstrap/Bootstrap.hs
+++ parser-typechecker/bootstrap/Bootstrap.hs
@@ -36,11 +36,11 @@ main = do
   case args of
     [sourceFile, outputFile] -> do
       unisonFile <- Parsers.unsafeReadAndParseFile Parser.penv0 sourceFile
-      let r = FileParsers.synthesizeFile unisonFile
-          f typ = do
+      let r = FileParsers.synthesizeUnisonFile unisonFile
+          f (unisonFile', typ) = do
             putStrLn ("typechecked as " ++ show typ)
-            let bs = runPutS $ flip evalStateT 0 $ Codecs.serializeFile unisonFile
+            let bs = runPutS $ flip evalStateT 0 $ Codecs.serializeFile unisonFile'
             BS.writeFile outputFile bs
-      either (putStrLn . show) (f . snd) r
+      either (putStrLn . show) f r
 
     _ -> putStrLn "usage: bootstrap <in-file.u> <out-file.ub>"
