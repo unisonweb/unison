5af27e2e3383c49b08f94be31becf8c55a4d1733
Arya Irani <arya.irani@gmail.com>
2018-06-17 14:28:21 -0400

    factor pure stuff out of parser-typechecker Bootstrap

--- parser-typechecker/bootstrap/Bootstrap.hs
+++ parser-typechecker/bootstrap/Bootstrap.hs
@@ -3,32 +3,10 @@
 module Main where
 
 import System.Environment (getArgs)
-import Unison.Codecs as Codecs
-import Data.Bytes.Put (runPutS)
-import Data.Foldable (toList)
-import Data.Maybe (fromMaybe)
-import Control.Monad.State (evalStateT)
-import qualified Unison.Builtin as B
 import qualified Unison.FileParsers as FileParsers
 import qualified Unison.Parser as Parser
 import qualified Unison.Parsers as Parsers
-import qualified Unison.Term as Term
-import qualified Unison.Typechecker as Typechecker
-import qualified Unison.UnisonFile as UF
 import qualified Data.ByteString as BS
-import           Data.Map (Map)
-import qualified Data.Map as Map
-
-import Unison.DataDeclaration (DataDeclaration)
-import qualified Unison.Note as Note
-import Unison.Note (Noted)
-import Unison.Reference (Reference)
-import Unison.Symbol (Symbol)
-import Unison.Term (Term)
-import Unison.Type (Type)
-import Unison.UnisonFile (UnisonFile)
-import Unison.Var (Var)
-import Debug.Trace (trace)
 
 main :: IO ()
 main = do
@@ -36,11 +14,10 @@ main = do
   case args of
     [sourceFile, outputFile] -> do
       unisonFile <- Parsers.unsafeReadAndParseFile Parser.penv0 sourceFile
-      let r = FileParsers.synthesizeUnisonFile unisonFile
-          f (unisonFile', typ) = do
+      let r = FileParsers.serializeUnisonFile unisonFile
+          f (_unisonFile', typ, bs) = do
             putStrLn ("typechecked as " ++ show typ)
-            let bs = runPutS $ flip evalStateT 0 $ Codecs.serializeFile unisonFile'
             BS.writeFile outputFile bs
-      either (putStrLn . show) f r
+      either print f r
 
     _ -> putStrLn "usage: bootstrap <in-file.u> <out-file.ub>"
