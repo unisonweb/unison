06528814c93c33ad57b476138904b54653047041
Paul Chiusano <paul.chiusano@gmail.com>
2018-06-21 18:42:34 -0400

    deleted complicated logic for instantiateR, since effect checking is now handled in subtype and all effects are eliminated before reaching instantiateL/R

--- parser-typechecker/src/Unison/Typechecker/Context.hs
+++ parser-typechecker/src/Unison/Typechecker/Context.hs
@@ -486,6 +486,7 @@ instantiateL v t = getContext >>= \ctx -> case Type.monotype t >>= solve ctx v o
       v <- extendUniversal =<< ABT.freshen body freshenTypeVar
       instantiateL v (ABT.bind body (Type.universal v))
       modifyContext (retract (Universal v))
+    Type.Effect' _es _vt -> error "the impossible happened: subtype should eliminate all effects before reaching here"
     _ -> do
       let msg = "could not instantiate left: '" ++ show v ++ " <: " ++ show t
       logContext msg
@@ -507,20 +508,6 @@ instantiateR t v = getContext >>= \ctx -> case Type.monotype t >>= solve ctx v o
       setContext (replace (Existential v) (context [Existential o', Existential i', s]) ctx)
       ctx <- instantiateL i' i >> getContext
       instantiateR (apply ctx o) o'
-    Type.Effect' es vt -> do
-      es' <- replicateM (length es) (freshNamed "effect")
-      vt' <- freshNamed "vt"
-      let s = Solved v (Type.Monotype (Type.effect (Type.existential <$> es')
-                                                   (Type.existential vt')))
-      setContext $ replace (Existential v)
-                           (context $ (Existential <$> es') ++
-                                      [Existential vt', s])
-                           ctx
-      Foldable.for_ (es `zip` es') $ \(e, e') -> do
-        ctx <- getContext
-        instantiateR (apply ctx e) e'
-      ctx <- getContext
-      instantiateR (apply ctx vt) vt'
     Type.App' x y -> do -- analogue of InstRArr
       -- example foo a <: v' will
       -- 1. create foo', a', add these to the context
@@ -538,6 +525,7 @@ instantiateR t v = getContext >>= \ctx -> case Type.monotype t >>= solve ctx v o
       setContext $ ctx `append` context [Marker x', Existential x']
       instantiateR (ABT.bind body (Type.existential x')) v
       modifyContext (retract (Marker x'))
+    Type.Effect' _es _vt -> error "the impossible happened: subtype should eliminate all effects before reaching here"
     _ -> do
       logContext ("failed: instantiateR " <> show t <> " " <> show v)
       fail $ "could not instantiate right " ++ show t
