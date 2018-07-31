5c19631fbc61f9e79eafc507b10e987d473d8a7e
Arya Irani <arya.irani@gmail.com>
2018-07-31 00:21:18 -0400

    summarize a block of text into short segments according to its highlighting (non-working)

--- parser-typechecker/src/Unison/Util/ColorText.hs
+++ parser-typechecker/src/Unison/Util/ColorText.hs
@@ -4,12 +4,77 @@ module Unison.Util.ColorText where
 -- import Control.Monad (join)
 -- import Data.Foldable (toList)
 import Data.Sequence (Seq)
+import Data.Foldable (foldl')
+import Data.Set (Set)
+import qualified Data.Set as Set
 -- import qualified Data.Sequence as Seq
 import Data.String (IsString(..))
+import Unison.Lexer (Pos(..))
+import Debug.Trace
 
-data Color = Normal | Color1 | Color2
+data Style = Normal | Highlighted Color
+
+data Color = Color1 | Color2 deriving (Eq, Ord, Show)
 
 newtype ColorText = ColorText { chunks :: Seq (Color,String) }
+
+data Range = Range { start :: Pos, end :: Pos } deriving (Eq, Ord, Show)
+
+contains :: Range -> Range -> Bool
+contains (Range _ (Pos line2 col2)) (Range _ (Pos line4 col4)) =
+           line4 < line2 || (line4 == line2 && col4 <= col2)
+
+overlaps :: Range -> Range -> Bool
+overlaps (Range _ (Pos line2 col2)) (Range (Pos line3 col3) _) =
+           line2 > line3 || (line2 == line3 && col2 >= col3)
+
+
+instance Semigroup Range where
+  (Range start end) <> (Range start2 end2) =
+    Range (min start start2) (max end end2)
+
+deoffsetRange :: Int -> Range -> Range
+deoffsetRange lineOffset (Range (Pos startLine startCol) (Pos endLine endCol)) =
+  Range (Pos (startLine - lineOffset) startCol)
+        (Pos (endLine - lineOffset) endCol)
+
+data AnnotatedText =
+  AnnotatedText { lineOffset :: Int
+                , text :: String
+                , annotations :: Set (Range, Color)
+                } deriving (Show)
+
+instance IsString AnnotatedText where
+  fromString s = AnnotatedText 1 s mempty
+
+snipWithContext :: Int -> AnnotatedText -> [AnnotatedText]
+snipWithContext margin source =
+  case last (traceShowId $ scanl whileWithinMargin
+              (Nothing, mempty, mempty)
+              (Set.toList $ annotations source)) of
+    (Nothing, _, _) -> []
+    (Just (Range (Pos startLine' _) (Pos endLine' _)), group', rest') ->
+      let text', text2' :: [String]
+          (text', text2') =
+            (splitAt (endLine' - startLine' + 1)
+                     (drop (startLine' - lineOffset source)
+                           (lines (text source))))
+      in AnnotatedText start' (unlines text') group'
+        : snipWithContext margin (AnnotatedText end' (unlines text2') rest')
+  where
+    withinMargin :: Range -> Range -> Bool
+    withinMargin (Range _start1 (Pos l1 _)) (Range (Pos l2 _) _end2) =
+      (l1 + margin >= l2)
+    whileWithinMargin :: (Maybe Range, Set (Range, Color), Set (Range, Color))
+                      -> (Range, Color)
+                      -> (Maybe Range, Set (Range, Color), Set (Range, Color))
+    whileWithinMargin (Nothing, _taken, rest) a@(r1, _) | null rest =
+      (Just r1, Set.singleton a, mempty)
+    whileWithinMargin (Just r0, taken, rest) a@(r1, _) | null rest =
+      if withinMargin r0 r1
+      then (Just $ r0 <> r1, Set.insert a taken, mempty)
+      else (Just r0, taken, Set.singleton a)
+    whileWithinMargin (r0, taken, rest) a = (r0, taken, Set.insert a rest)
 
 {-
 
@@ -17,19 +82,52 @@ newtype ColorText = ColorText { chunks :: Seq (Color,String) }
    2    | foo = 42
 ❌ 3:80 |
 > Hello, world!
-^^^^^
-Goodbye, world!
-^^^^^^^
+  ^^^^^
+> Goodbye, world!
+  ^^^^^^^
 
 Highlight: Line 1, Cols 1-5
 Highlight: Line 2, Cols 1-7
 -}
 
-color1 :: ColorText -> ColorText
-color1 s = ColorText $ fmap (\(_,s) -> (Color1,s)) (chunks s)
+-- [(Nothing,fromList [],fromList [])
+-- ,(Just (Range {start = Pos 3 1, end = Pos 3 5}),fromList [(Range {start = Pos 3 1, end = Pos 3 5},Color1)],fromList [])
+-- ,(Just (Range {start = Pos 3 1, end = Pos 3 5}),fromList [(Range {start = Pos 3 1, end = Pos 3 5},Color1)],fromList [(Range {start = Pos 5 1, end = Pos 5 5},Color1)])
+-- ,(Just (Range {start = Pos 3 1, end = Pos 3 5}),fromList [(Range {start = Pos 3 1, end = Pos 3 5},Color1)],fromList [(Range {start = Pos 5 1, end = Pos 5 5},Color1),(Range {start = Pos 7 1, end = Pos 13 44},Color1)])
+-- ,(Just (Range {start = Pos 3 1, end = Pos 3 5}),fromList [(Range {start = Pos 3 1, end = Pos 3 5},Color1)],fromList [(Range {start = Pos 5 1, end = Pos 5 5},Color1),(Range {start = Pos 7 1, end = Pos 13 44},Color1),(Range {start = Pos 14 1, end = Pos 14 4},Color2)])]
+--
+-- [AnnotatedText {lineOffset = 3, text = "SCENE I. On a ship at sea: a tempestuous noise\n", annotations = fromList [(Range {start = Pos 3 1, end = Pos 3 5},Color1)]}
+-- ,AnnotatedText {lineOffset = 5, text = "\n", annotations = fromList [(Range {start = Pos 5 1, end = Pos 5 5},Color1)]}
+-- ,AnnotatedText {lineOffset = 7, text = "Boatswain\nHere, master: what cheer?\nMaster\nGood, speak to the mariners: fall to't, yarely,\nor we run ourselves aground: bestir, bestir.\nExit\n\nEnter Mariners\n", annotations = fromList [(Range {start = Pos 7 1, end = Pos 13 44},Color1),(Range {start = Pos 14 1, end = Pos 14 4},Color2)]}
+--
+-- [(Nothing,fromList [],fromList [])
+-- ,(Just (Range {start = Pos 5 1, end = Pos 5 5}),fromList [(Range {start = Pos 5 1, end = Pos 5 5},Color1)],fromList [])
+-- ,(Just (Range {start = Pos 5 1, end = Pos 5 5}),fromList [(Range {start = Pos 5 1, end = Pos 5 5},Color1)],fromList [(Range {start = Pos 7 1, end = Pos 13 44},Color1)])
+-- ,(Just (Range {start = Pos 5 1, end = Pos 5 5}),fromList [(Range {start = Pos 5 1, end = Pos 5 5},Color1)],fromList [(Range {start = Pos 7 1, end = Pos 13 44},Color1),(Range {start = Pos 14 1, end = Pos 14 4},Color2)])]
+--
+-- [(Nothing,fromList [],fromList [])
+-- ,(Just (Range {start = Pos 7 1, end = Pos 13 44}),fromList [(Range {start = Pos 7 1, end = Pos 13 44},Color1)],fromList [])
+-- ,(Just (Range {start = Pos 7 1, end = Pos 14 4}),fromList [(Range {start = Pos 7 1, end = Pos 13 44},Color1),(Range {start = Pos 14 1, end = Pos 14 4},Color2)],fromList [])]
+--
+-- [(Nothing,fromList [],fromList [])]
+-- ]
+--
+--
+-- [(Nothing,fromList [],fromList [])
+-- ,(Just (Range {start = Pos 3 1, end = Pos 3 5}),fromList [(Range {start = Pos 3 1, end = Pos 3 5},Color1)],fromList [])
+-- ,(Just (Range {start = Pos 3 1, end = Pos 5 5}),fromList [(Range {start = Pos 3 1, end = Pos 3 5},Color1),(Range {start = Pos 5 1, end = Pos 5 5},Color1)],fromList [])
+-- ,(Just (Range {start = Pos 3 1, end = Pos 13 44}),fromList [(Range {start = Pos 3 1, end = Pos 3 5},Color1),(Range {start = Pos 5 1, end = Pos 5 5},Color1),(Range {start = Pos 7 1, end = Pos 13 44},Color1)],fromList [])
+-- ,(Just (Range {start = Pos 3 1, end = Pos 14 4}),fromList [(Range {start = Pos 3 1, end = Pos 3 5},Color1),(Range {start = Pos 5 1, end = Pos 5 5},Color1),(Range {start = Pos 7 1, end = Pos 13 44},Color1),(Range {start = Pos 14 1, end = Pos 14 4},Color2)],fromList [])]
+-- [AnnotatedText {lineOffset = 3, text = "SCENE I. On a ship at sea: a tempestuous noise\nof thunder and lightning heard.\nEnter a Master and a Boatswain\n\nMaster\nBoatswain!\nBoatswain\nHere, master: what cheer?\nMaster\nGood, speak to the mariners: fall to't, yarely,\nor we run ourselves aground: bestir, bestir.\nExit\n", annotations = fromList [(Range {start = Pos 3 1, end = Pos 3 5},Color1),(Range {start = Pos 5 1, end = Pos 5 5},Color1),(Range {start = Pos 7 1, end = Pos 13 44},Color1),(Range {start = Pos 14 1, end = Pos 14 4},Color2)]}[(Nothing,fromList [],fromList [])]
+-- ]
 
-color2 :: ColorText -> ColorText
-color2 s = ColorText $ fmap (\(_,s) -> (Color2,s)) (chunks s)
+
+
+-- color1 :: ColorText -> ColorText
+-- color1 s = ColorText $ fmap (\(_,s) -> (Color1,s)) (chunks s)
+--
+-- color2 :: ColorText -> ColorText
+-- color2 s = ColorText $ fmap (\(_,s) -> (Color2,s)) (chunks s)
 
 -- highlight :: [(String,Int)] -> Map (Pos,Pos) Color -> ColorText
 -- highlight s regions = ColorText _ where
@@ -64,16 +162,16 @@ color2 s = ColorText $ fmap (\(_,s) -> (Color2,s)) (chunks s)
 -- excerpt :: [Line] -> ColorText -> ColorText
 --
 
-instance IsString ColorText where
-  fromString s = ColorText (pure (Normal, s))
+-- instance IsString ColorText where
+--   fromString s = ColorText (pure (Normal, s))
 
 
-instance Monoid ColorText where
-  mempty = ColorText mempty
-  mappend x y = ColorText $ chunks x `mappend` chunks y
+-- instance Monoid ColorText where
+--   mempty = ColorText mempty
+--   mappend x y = ColorText $ chunks x `mappend` chunks y
 
-instance Semigroup ColorText where
-  (<>) = mappend
+-- instance Semigroup ColorText where
+--   (<>) = mappend
 
 -- instance Show ColorText where
 --   show = renderAnntatedText Nothing . renderInColor
