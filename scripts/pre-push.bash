409802edfbc51cd5b7dffb45e6c40da7c1664f38
Arya Irani <arya.irani@gmail.com>
2018-08-03 14:22:56 -0400

    Tweak pre-push failure message

--- scripts/pre-push.bash
+++ scripts/pre-push.bash
@@ -2,18 +2,17 @@
 # Run the following command in the root of your project to install this pre-push hook:
 # cp git-hooks/pre-push .git/hooks/pre-push; chmod 700 .git/hooks/pre-push
 
-CMD="./scripts/test.sh"
-
 # Check if we actually have commits to push
 commits=`git log @{u}..`
 if [ -z "$commits" ]; then
     exit 0
 fi
 
+CMD="./scripts/test.sh"
 eval $CMD
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
-    echo "failed $CMD"
+    echo "The git push operation was canceled because \`$CMD\` did not complete successfully."
     exit 1
 fi
 exit 0
