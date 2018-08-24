dad98b2a3abeb49d24a3384d364eca0b6fa6a41b
Paul Chiusano <paul.chiusano@gmail.com>
2018-08-24 17:13:55 -0400

    moar progress

--- parser-typechecker/src/Unison/Runtime/Rt0.hs
+++ parser-typechecker/src/Unison/Runtime/Rt0.hs
@@ -9,7 +9,6 @@ import Data.Functor
 import Data.Foldable
 import Data.Int (Int64)
 import Data.Text (Text)
-import Data.Vector (Vector)
 import Data.Word (Word64)
 import Data.List
 import Unison.Symbol (Symbol)
@@ -17,7 +16,6 @@ import Unison.Term (AnnotatedTerm)
 import qualified Unison.ABT as ABT
 import qualified Unison.Reference as R
 import qualified Unison.Term as Term
-import qualified Data.Set as Set
 
 type Arity = Int
 type ConstructorId = Int
@@ -29,7 +27,7 @@ type Term v = AnnotatedTerm v ()
 data V e
   = I Int64 | F Double | U Word64 | B Bool | T Text
   | Lam Arity (Term Symbol) (IR e)
-  | Data R.Reference ConstructorId (Vector (V e))
+  | Data R.Reference ConstructorId [V e]
   | Ext e deriving (Eq,Show)
 
 data IR e
@@ -41,10 +39,15 @@ data IR e
   | LetRec [IR e] (IR e)
   | V (V e)
   | Apply Pos [Pos]
+  | Construct R.Reference Int [Pos]
+  | Request R.Reference Int [Pos]
+  | Handle Pos (IR e)
   | If Pos (IR e) (IR e) deriving (Eq,Show)
 
 type Rt = Machine -> V R.Reference
 type Machine = [V R.Reference] -- a stack of values
+
+-- change `run` Rt to return `Either Req` or `V`
 
 push :: V R.Reference -> Machine -> Machine
 push = (:)
@@ -80,43 +83,59 @@ att i m = case at i m of
   T t -> t
   _ -> error "type error"
 
-run :: IR R.Reference -> [V R.Reference] -> V R.Reference
+-- Contains the effect ref and ctor id, the args, and the continuation
+-- which expects the result at the top of the stack
+data Req = Req R.Reference Int [V R.Reference] (IR R.Reference)
+  deriving Show
+
+appendCont :: Req -> IR R.Reference -> Req
+appendCont (Req r cid args k) k2 = Req r cid args (Let k k2)
+
+type Result = Either Req (V R.Reference)
+
+run :: IR R.Reference -> Machine -> Result
 run ir m = case ir of
-  Var i -> at i m
-  V v -> v
   If c t f -> if atb c m then run t m else run f m
-  Let b body -> run body (run b m : m)
+  Let b body -> case run b m of
+    Left req -> Left $ req `appendCont` body
+    Right v -> run body (v : m)
   LetRec bs body ->
     let m' = pushes bs' m
-        bs' = map (\ir -> run ir m') bs
+        g (Left e) = error ("bindings in a let rec must not have effects " ++ show e)
+        g (Right a) = a
+        bs' = map (\ir -> g $ run ir m') bs
     in run body m'
-
   Apply fnPos args -> call (at fnPos m) args m
+  Request r cid args -> Left (Req r cid ((`at` m) <$> args) (Var 0))
+  ir -> pure $ case ir of
+    Var i -> at i m
+    V v -> v
+    Construct r cid args -> Data r cid ((`at` m) <$> args)
+    AddI i j -> I (ati i m + ati j m)
+    SubI i j -> I (ati i m - ati j m)
+    MultI i j -> I (ati i m * ati j m)
+    DivI i j -> I (ati i m `div` ati j m)
+    AddF i j -> F (atf i m + atf j m)
+    SubF i j -> F (atf i m - atf j m)
+    MultF i j -> F (atf i m * atf j m)
+    DivF i j -> F (atf i m / atf j m)
+    AddU i j -> U (atu i m + atu j m)
+    SubU i j -> U (atu i m - atu j m)
+    MultU i j -> U (atu i m * atu j m)
+    DivU i j -> U (atu i m `div` atu j m)
+    _ -> error "should be caught by above cases"
 
-  AddI i j -> I (ati i m + ati j m)
-  SubI i j -> I (ati i m - ati j m)
-  MultI i j -> I (ati i m * ati j m)
-  DivI i j -> I (ati i m `div` ati j m)
-
-  AddF i j -> F (atf i m + atf j m)
-  SubF i j -> F (atf i m - atf j m)
-  MultF i j -> F (atf i m * atf j m)
-  DivF i j -> F (atf i m / atf j m)
-
-  AddU i j -> U (atu i m + atu j m)
-  SubU i j -> U (atu i m - atu j m)
-  MultU i j -> U (atu i m * atu j m)
-  DivU i j -> U (atu i m `div` atu j m)
-
-call :: V R.Reference -> [Pos] -> Machine -> V R.Reference
+call :: V R.Reference -> [Pos] -> Machine -> Result
 call (Lam arity term body) args m = let nargs = length args in
   case nargs of
     _ | nargs == arity -> run body (map (`at` m) args `pushes` m)
     _ | nargs > arity ->
-      let fn' = run body (map (`at` m) args `pushes` m)
-      in call fn' (drop arity args) m
-    _ -> {- nargs < arity -} case term of
-      Term.LamsNamed' vs body -> Lam (arity - nargs) lam (compile lam)
+      case run body (map (`at` m) args `pushes` m) of
+        Left req -> Left $ req `appendCont` error "todo"
+        Right fn' -> call fn' (drop arity args) m
+    -- nargs < arity
+    _ -> case term of
+      Term.LamsNamed' vs body -> pure $ Lam (arity - nargs) lam (compile lam)
         where
         lam = Term.lam'() (drop nargs vs) $
           ABT.substs (vs `zip` (map decompile . reverse . take nargs $ m)) body
@@ -140,13 +159,11 @@ compile = compile0 []
 compile0 :: [Symbol] -> Term Symbol -> IR R.Reference
 compile0 bound t = go ((++ bound) <$> ABT.annotateBound' (Term.anf t)) where
   go t = case t of
-    Term.LamsNamed' vs body ->
-      if Set.null $ ABT.freeVars t
-      then V (Lam (length vs) (void t) (go body))
-      else let
-        body0 = void body
+    Term.LamsNamed' vs body
+      | ABT.isClosed t -> V (Lam (length vs) (void t) (go body))
+      | otherwise -> let
         fvs = toList $ ABT.freeVars t
-        lifted = Term.lam'() (fvs ++ vs) body0
+        lifted = Term.lam'() (fvs ++ vs) (void body)
         in compile0 (ABT.annotation t) (Term.apps' lifted (Term.var() <$> fvs))
     Term.Int64' n -> V (I n)
     Term.UInt64' n -> V (U n)
@@ -154,15 +171,20 @@ compile0 bound t = go ((++ bound) <$> ABT.annotateBound' (Term.anf t)) where
     Term.Boolean' b -> V (B b)
     Term.Text' t -> V (T t)
     Term.Ref' r -> V (Ext r)
-    Term.Var' v -> case elemIndex v (ABT.annotation t) of
-      Nothing -> error $ "free variable during compilation: " ++ show v
-      Just i -> Var i
+    Term.Var' v -> maybe (unknown v) Var $ elemIndex v (ABT.annotation t)
     Term.Let1Named' _ b body -> Let (go b) (go body)
     Term.LetRecNamed' bs body -> LetRec (go . snd <$> bs) (go body)
-    Term.Apps' f args -> Apply (ind t f) (map (ind t) args) where
+    Term.Constructor' r cid -> V (Data r cid mempty)
+    Term.Apps' f args -> case f of
+      Term.Request' r cid -> Request r cid (ind t <$> args)
+      Term.Constructor' r cid -> Construct r cid (ind t <$> args)
+      _ -> Apply (ind t f) (map (ind t) args) where
+    Term.Handle' _h _body ->
+      error "todo - make Machine model a bit richer, push and pop stack o' handlers"
+    _ -> error $ "TODO - don't know how to compile " ++ show t
+    where
+      unknown v = error $ "free variable during compilation: " ++ show v
       ind t (Term.Var' v) = case elemIndex v (ABT.annotation t) of
         Nothing -> error $ "free variable during compilation: " ++ show v
         Just i -> i
       ind _ _ = error "ANF should eliminate any non-var arguments to apply"
-    Term.Handle' _h _body -> error "TODO - compile handle"
-    _ -> error $ "TODO - don't know how to compile " ++ show t
