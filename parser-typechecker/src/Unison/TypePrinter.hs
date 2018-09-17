14be343515bd16f147aac9284ca2084bdf76f36d
Chris Gibbs <atacratic@users.noreply.github.com>
2018-09-17 23:08:37 +0100

    More work on Type pretty-printer
    * Various tidy-ups.
    * Fix bug with nested arrows.
    * Enable ' sugar for delay even for chained delays.
    * Add a 'Group' node everywhere parentheses might be output.
    
    Also added a note to say I need to make it undo the effect of generalizeEffects.  But I plan to leave that til after the Term printer is done.

--- parser-typechecker/src/Unison/TypePrinter.hs
+++ parser-typechecker/src/Unison/TypePrinter.hs
@@ -19,43 +19,42 @@ import qualified Unison.Util.PrettyPrint as PP
 import           Unison.Util.PrettyPrint (PrettyPrint(..))
 
 pretty :: Var v => (Reference -> Text) -> Int -> AnnotatedType v a -> PrettyPrint String
--- p is the operator precedence of the enclosing context (a number from 0
--- to 11).  Function application has precedence 10.
+-- p is the operator precedence of the enclosing context (a number from 0 to 11, or
+-- -1 to avoid outer parentheses unconditionally).  Function application has precedence 10.
 pretty n p = \case
   Var' v       -> l $ Text.unpack (Var.name v)
   Ref' r       -> l $ Text.unpack (n r)
   Cycle' _ _   -> l $ "error" -- TypeParser does not currently emit Cycle
   Abs' _       -> l $ "error" -- TypeParser does not currently emit Abs
   Ann' _ _     -> l $ "error" -- TypeParser does not currently emit Ann
-  App' (Ref' (Builtin "Sequence")) x -> l"[" <> pretty n 0 x <> l"]"
-  Tuple' xs    -> l"(" <> commaList xs <> l")"
+  App' (Ref' (Builtin "Sequence")) x -> PP.Group $ l"[" <> pretty n 0 x <> l"]"
+  Tuple' xs    -> paren True $ commaList xs
   App' f x     -> paren (p >= 10) $ pretty n 9 f <> b" " <> pretty n 10 x
   Effect1' e t -> paren (p >= 10) $ pretty n 9 e <> l" " <> pretty n 10 t
-  Effects' es  -> l"{" <> commaList es <> l"}"
-  ForallNamed' v body -> case p of
-    0 -> pretty n p body
-    _ -> paren True $ l"∀ " <> l (Text.unpack (Var.name v)) <> l". " <> pretty n 0 body
+  Effects' es  -> effects (Just es)
+  ForallNamed' v body ->
+    if (p <= 0)
+    then pretty n p body
+    else paren True $ l"∀ " <> l (Text.unpack (Var.name v)) <> l". " <> pretty n 0 body
+  --TODO undo generalizeEffects before printing
   EffectfulArrows' (Ref' (Builtin "()")) rest -> arrows True True rest
-  EffectfulArrows' fst rest -> PP.Group $ paren (p > 0) $ pretty n 0 fst <> arrows False False rest
+  EffectfulArrows' fst rest -> paren (p >= 0) $ pretty n 0 fst <> arrows False False rest
   _ -> l"error"
   where commaList xs = fold $ intersperse (l"," <> b" ") (map (pretty n 0) xs)
         effects Nothing = Empty
-        effects (Just es) = l"{" <> commaList es <> l"}"
+        effects (Just es) = PP.Group $ l"{" <> commaList es <> l"}"
         arrow delay first mes = (if first then Empty else b" " <> l"->" ) <>
                                 (if delay
-                                  then (if first then l"'" else (if isJust mes then l" '" else l" '"))
-                                  -- really we want to drop the space in the second literal above,
-                                  -- but the lexer won't let us.  e.g. we want "a ->'{e} b"
-                                  -- rather than "a -> '{e} b"
+                                  then (if first then l"'" else l" '")
                                   else Empty) <>
                                 effects mes <>
                                 if (isJust mes) || (not delay) && (not first) then l" " else Empty
 
-        arrows delay first [(mes, Ref' (Builtin "()"))] = arrow delay first mes <> l"()"
-        arrows False first ((mes, Ref' (Builtin "()")) : rest) =
-          if (isJust mes)
-          then arrow False first mes <> arrows True True rest
-          else arrows True first rest
+        arrows delay first [(mes, Ref' (Builtin "()"))] =
+                                                 arrow delay first mes <> l"()"
+        arrows delay first ((mes, Ref' (Builtin "()")) : rest) =
+                                                 arrow delay first mes <>
+                                                 (paren delay $ arrows True True rest)
         arrows delay first ((mes, arg) : rest) = arrow delay first mes <>
                                                  (paren (delay && (not $ null rest)) $
                                                    pretty n 0 arg <> arrows False False rest)
@@ -63,16 +62,16 @@ pretty n p = \case
         arrows False True [] = Empty  -- not reachable
         arrows True _ [] = Empty      -- not reachable
 
-        paren True s = l"(" <> s <> l")"
-        paren False s = s
+        paren True s = PP.Group $ l"(" <> s <> l")"
+        paren False s = PP.Group s
         l = Literal
         b = Breakable
 
--- TODO group pretty much everywhere parens are used
+-- TODO test `Forall (uppercase)`
 -- TODO `parse . pretty = id` test on all types in test suite
 -- TODO some renderBroken testing
 -- TODO PR for type pretty-printer
 -- TODO terms etc, and more attention to line-breaking behaviour
 
 pretty' :: Var v => (Reference -> Text) -> AnnotatedType v a -> String
-pretty' n t = PP.renderUnbroken $ pretty n 0 t
+pretty' n t = PP.renderUnbroken $ pretty n (-1) t
--- parser-typechecker/tests/Unison/Test/TypePrinter.hs
+++ parser-typechecker/tests/Unison/Test/TypePrinter.hs
@@ -13,14 +13,15 @@ import qualified Unison.Util.PrettyPrint as PP
 -- Test the result of the pretty-printer.  Expect the pretty-printer to
 -- produce output that differs cosmetically from the original code we parsed.
 -- Check also that re-parsing the pretty-printed code gives us the same ABT.
+-- (Skip that latter check if rtt is false.)
 -- Note that this does not verify the position of the PrettyPrint Break elements.
-tc_diff :: String -> String -> Test ()
-tc_diff s expected =
+tc_diff_rtt :: Bool -> String -> String -> Test ()
+tc_diff_rtt rtt s expected =
    let input_term = Unison.Builtin.t s :: Unison.Type.AnnotatedType Symbol Ann
        get_names x = case x of
                        Builtin t -> t
                        Derived _ -> Text.empty
-       actual = PP.renderUnbroken $ pretty get_names 0 input_term
+       actual = PP.renderUnbroken $ pretty get_names (-1) input_term
        actual_reparsed = Unison.Builtin.t actual
    in scope s $ tests [(
        if actual == expected then ok
@@ -29,12 +30,15 @@ tc_diff s expected =
                note $ "show(input)  : "   ++ show input_term
                crash "actual != expected"
        ), (
-       if input_term == actual_reparsed then ok
+       if (not rtt) || (input_term == actual_reparsed) then ok
        else do note $ "round trip test..."
                note $ "single parse: " ++ show input_term
                note $ "double parse: " ++ show actual_reparsed
                crash "single parse != double parse"
        )]
+
+-- As above, but do the round-trip test unconditionally.
+tc_diff s expected = tc_diff_rtt True s expected
 
 -- As above, but expect not even cosmetic differences between the input string
 -- and the pretty-printed version.
@@ -67,38 +71,57 @@ test = scope "typeprinter" . tests $
   , tc "'Pair a a"
   , tc "a -> 'b"
   , tc "'(a -> b)"
---TODO, tc "(a -> b) -> c" -- I need to strip out the effect variable added into argument for effect polymorphism.
---TODO, tc "'a -> b" -- same as above; pretty = "'{\120518} a -> b"; show input = "(𝛆. (a. (b. (() -> (({[𝛆]} a))) -> b)))"
---TODO, tc "a -> 'b -> c" -- ditto
+  -- The next three tests will change after the pretty-printer can reverse
+  -- generalizeEffects.  Also, they currently fail the round-trip test because
+  -- generalizeEffects leaves the `Forall 𝛆` on the outside of the expression,
+  -- whereas the parse of the current explicit post-pretty-print version does not.
+  , tc_diff_rtt False "(a -> b) -> c" $ "(a ->{𝛆} b) -> c"
+  , tc_diff_rtt False "'a -> b" $ "'{𝛆} a -> b"
+  , tc_diff_rtt False "a -> 'b -> c" $ "a -> '{𝛆} b -> c"
+  , tc_diff_rtt False "a -> (b -> c) -> d" $ "a -> (b ->{𝛆} c) -> d"
+  , tc_diff_rtt False "(a -> b) -> c -> d" $ "(a ->{𝛆} b) -> c -> d"
+  , tc_diff_rtt False "((a -> b) -> c) -> d" $ "((a ->{𝛆} b) ->{𝛆} c) -> d"
   , tc "a -> '(b -> c)"
+  , tc "a -> b -> c -> d"
   , tc "a -> 'Pair b c"
   , tc "a -> b -> 'c"
   , tc "a ->{e} 'b"
-  , tc "a -> '{e} b"  -- Ideally this would output "a ->'{e} b" but the lexer can't yet handle that.
+  , tc "a -> '{e} b"
   , tc "a -> '{e} b -> c"
   , tc "a -> '{e} b ->{f} c"
   , tc "a -> '{e} (b -> c)"
   , tc "a -> '{e} (b ->{f} c)"
   , tc "a -> 'b"
+  , tc "a -> '('b)"
+  , tc "a -> '('(b -> c))"
+  , tc "a -> '('('(b -> c)))"
+  , tc "a -> '{e} ('('(b -> c)))"
+  , tc "a -> '('{e} ('(b -> c)))"
+  , tc "a -> '('('{e} (b -> c)))"
   , tc "a -> 'b ->{f} c"
   , tc "a -> '(b -> c)"
   , tc "a -> '(b ->{f} c)"
-  , tc "a -> '{e} (() -> b)"
-  , tc_diff "a -> '{e} ('b)" $ "a -> '{e} (() -> b)"  -- parser can't handle "a -> '{e} 'b"
-    -- TODO re above, I should remove the prevention of outputting chained delays
-  , tc_diff "a -> () ->{e} () -> b -> c" $ "a -> '{e} (() -> b -> c)"
-  , tc "a -> '{e} (() -> b -> c)"
+  , tc "a -> '{e} ('b)"
+  , pending $ tc "a -> '{e} 'b"      -- issue #249
+  , pending $ tc "a -> '{e} '{f} b"  -- issue #249
+  , tc "a -> '{e} ('b)"
+  , tc_diff "a -> () ->{e} () -> b -> c" $ "a -> '{e} ('(b -> c))"
+  , tc "a -> '{e} ('(b -> c))"
   , tc_diff "a ->{e} () ->{f} b" $ "a ->{e} '{f} b"
   , tc "a ->{e} '{f} b"
-  , tc_diff "a -> () ->{e} () ->{f} b" $ "a -> '{e} (() ->{f} b)"
+  , tc_diff "a -> () ->{e} () ->{f} b" $ "a -> '{e} ('{f} b)"
+  , tc "a -> '{e} ('{f} b)"
   , tc "a -> '{e} () ->{f} b"
+  , tc "a -> '{e} ('{f} (b -> c))"
   , tc "a ->{e} '(b -> c)"
   , tc "a -> '{e} (b -> c)"
-  , tc_diff "a -> () ->{e} () -> b" $ "a -> '{e} (() -> b)"
+  , tc_diff "a -> () ->{e} () -> b" $ "a -> '{e} ('b)"
   , tc "'{e} a"
   , tc "'{e} (a -> b)"
   , tc "'{e} (a ->{f} b)"
   , tc "'(a -> 'a)"
   , tc "'()"
-  , tc_diff "'('a)" $ "'(() -> a)"  -- lexer can't handle "''a"
+  , tc "'('a)"
+  , pending $ tc "''a"  -- issue #249
+  , pending $ tc "'''a" -- issue #249
   ]
