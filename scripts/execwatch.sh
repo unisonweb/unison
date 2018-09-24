245049c3d103ff2d35e420c55c3c682b3c73022d
Arya Irani <arya.irani@gmail.com>
2018-09-24 16:41:27 -0400

    add scripts/watchwatch.sh to reboot the watcher if typechecker or runtime sources change

---
+++ scripts/execwatch.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+haskell_needs_rebuild=x
+scala_needs_rebuild=x
+haskell_pid=
+
+function maybe_build_haskell {
+  if [ -n "$haskell_needs_rebuild" ]; then
+    echo "Building typechecker..."
+    if stack build; then
+      haskell_needs_rebuild=""
+      echo "Typechecker built!"
+    fi
+  fi
+}
+
+function maybe_build_scala {
+  if [ -n "$scala_needs_rebuild" ]; then
+    echo "Building runtime..."
+    (cd runtime-jvm; sbt main/compile) && scala_needs_rebuild="" && echo "Runtime built!"
+  fi
+}
+
+function kill_watcher {
+  # echo "debug: kill $haskell_pid"
+  kill $haskell_pid 2> /dev/null
+  haskell_pid=
+}
+
+function start_watcher {
+  stack exec watcher &
+  haskell_pid=$!
+  echo "Launched haskell watcher as pid $haskell_pid."
+}
+
+function go {
+  # echo "debug: haskell_needs_rebuild=$haskell_needs_rebuild"
+  if [ -n "$haskell_needs_rebuild" ] || [ -n "$scala_needs_rebuild" ]; then
+    kill_watcher
+  fi
+  maybe_build_haskell
+  maybe_build_scala
+  # echo "debug: haskell_needs_rebuild=$haskell_needs_rebuild, scala_needs_rebuild=$scala_needs_rebuild, haskell_pid=$haskell_pid"
+  if [ -z "$haskell_needs_rebuild" ] && [ -z "$scala_needs_rebuild" ] && [ -z "$haskell_pid" ]; then
+    start_watcher
+  fi
+}
+
+trap kill_watcher EXIT
+go
+
+while IFS= read -r changed; do
+  # echo "debug: $changed"
+  case $changed in
+    NoOp)
+      go
+      ;;
+    *.hs)
+      echo "detected change in $changed"
+      # hasktags -cx parser-typechecker
+      haskell_needs_rebuild=x
+      ;;
+    *.scala)
+      echo "detected change in $changed"
+      scala_needs_rebuild=x
+      ;;
+  esac
+done
