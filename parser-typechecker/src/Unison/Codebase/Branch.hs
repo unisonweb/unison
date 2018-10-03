29041c80915e7ced62b5e6eeca3905a392804e42
Runar Bjarnason <runarorama@gmail.com>
2018-10-03 17:55:04 -0400

    wip

--- parser-typechecker/src/Unison/Codebase/Branch.hs
+++ parser-typechecker/src/Unison/Codebase/Branch.hs
@@ -5,28 +5,28 @@
 
 module Unison.Codebase.Branch where
 
---import Control.Monad (join)
---import Data.List.NonEmpty (nonEmpty)
+-- import Unison.Codebase.NameEdit (NameEdit)
+
 import           Control.Monad              (foldM)
-import           Data.Map                   (Map)
+import           Data.Foldable
+import           Data.Maybe                 (fromMaybe)
+import           Data.Relation              (Relation)
+import qualified Data.Relation              as R
 import           Data.Set                   (Set)
 import qualified Data.Set                   as Set
---import Data.Semigroup (sconcat)
-import           Data.Foldable
-import qualified Data.Map                   as Map
+--import Control.Monad (join)
 import           Unison.Codebase.Causal     (Causal)
 import qualified Unison.Codebase.Causal     as Causal
-import           Unison.Codebase.Conflicted (Conflicted)
-import qualified Unison.Codebase.Conflicted as Conflicted
 import           Unison.Codebase.Name       (Name)
-import           Unison.Hashable            (Hashable)
-import qualified Unison.Hashable            as H
--- import Unison.Codebase.NameEdit (NameEdit)
 import           Unison.Codebase.TermEdit   (TermEdit, Typing)
 import qualified Unison.Codebase.TermEdit   as TermEdit
 import           Unison.Codebase.TypeEdit   (TypeEdit)
 import qualified Unison.Codebase.TypeEdit   as TypeEdit
+import           Unison.Hashable            (Hashable)
+import qualified Unison.Hashable            as H
 import           Unison.Reference           (Reference)
+--import Data.Semigroup (sconcat)
+--import Data.List.NonEmpty (nonEmpty)
 
 -- todo:
 -- probably should refactor Reference to include info about whether it
@@ -62,23 +62,21 @@ import           Unison.Reference           (Reference)
 newtype Branch = Branch (Causal Branch0)
 
 data Branch0 =
-  Branch0 { termNamespace :: Map Name (Conflicted Reference)
-          , typeNamespace :: Map Name (Conflicted Reference)
-          , edited        :: Map Reference (Conflicted TermEdit)
-          , editedDatas   :: Map Reference (Conflicted TypeEdit)
-          , editedEffects :: Map Reference (Conflicted TypeEdit)
-          , backupNames   :: Map Reference (Set Name)
+  Branch0 { termNamespace :: Relation Name Reference
+          , typeNamespace :: Relation Name Reference
+          , editedTerms   :: Relation Reference TermEdit
+          , editedTypes   :: Relation Reference TypeEdit
+          , backupNames   :: Relation Reference Name
           -- , codebase       :: Set Reference
           }
 
 instance Semigroup Branch0 where
-  Branch0 n1 nt1 t1 d1 e1 dp1 <> Branch0 n2 nt2 t2 d2 e2 dp2 = Branch0
-    (Map.unionWith (<>) n1 n2)
-    (Map.unionWith (<>) nt1 nt2)
-    (Map.unionWith (<>) t1 t2)
-    (Map.unionWith (<>) d1 d2)
-    (Map.unionWith (<>) e1 e2)
-    (Map.unionWith (<>) dp1 dp2)
+  Branch0 n1 nt1 t1 d1 dp1 <> Branch0 n2 nt2 t2 d2 dp2 = Branch0
+    (R.union n1 n2)
+    (R.union nt1 nt2)
+    (R.union t1 t2)
+    (R.union d1 d2)
+    (R.union dp1 dp2)
 
 merge :: Branch -> Branch -> Branch
 merge (Branch b) (Branch b2) = Branch (Causal.merge b b2)
@@ -111,22 +109,26 @@ data ReferenceOps m = ReferenceOps
 -- foo' -> Replace foo'' *optional
 -- bar' -> Replace bar'' *optional
 
+replaceType
+  :: Monad m => ReferenceOps m -> Reference -> Reference -> Branch -> m Branch
+replaceType = undefined
+
 add :: Monad m => ReferenceOps m -> Name -> Reference -> Branch -> m Branch
 add ops n r (Branch b) = Branch <$> Causal.stepM go b where
   go b = do
     -- add dependencies to `backupNames`
     backupNames' <- updateBackupNames1 ops r b
     -- add (n,r) to backupNames
-    let backupNames'' = Map.insertWith (<>) r (Set.singleton n) backupNames'
+    let backupNames'' = R.insert r n backupNames'
     -- add to appropriate namespace
     isTerm <- isTerm ops r
     isType <- isType ops r
     if isTerm then
-      pure b { termNamespace = Conflicted.singletonMap n r <> termNamespace b
+      pure b { termNamespace = R.insert n r $ termNamespace b
              , backupNames = backupNames''
              }
     else if isType then
-      pure b { typeNamespace = Conflicted.singletonMap n r <> typeNamespace b
+      pure b { typeNamespace = R.insert n r $ typeNamespace b
              , backupNames = backupNames''
              }
     else error $ "Branch.add received unknown reference " ++ show r
@@ -135,32 +137,53 @@ updateBackupNames :: Monad m
                   => ReferenceOps m
                   -> Set Reference
                   -> Branch0
-                  -> m (Map Reference (Set Name))
+                  -> m (Relation Reference Name)
 updateBackupNames ops refs b = do
   transitiveClosure <- transitiveClosure (dependencies ops) refs
   foldM insertNames (backupNames b) transitiveClosure
   where
-    insertNames m r = Map.insertWith (<>) r <$> name ops r <*> pure m
+    insertNames m r = foldl' (flip $ R.insert r) m <$> name ops r
 
 updateBackupNames1 :: Monad m
                    => ReferenceOps m
                    -> Reference
                    -> Branch0
-                   -> m (Map Reference (Set Name))
+                   -> m (Relation Reference Name)
 updateBackupNames1 ops r b = updateBackupNames ops (Set.singleton r) b
+
+lookupRan :: b -> Relation a b -> Set a
+lookupRan b r = fromMaybe Set.empty $ R.lookupRan b r
+
+lookupDom :: a -> Relation a b -> Set b
+lookupDom a r = fromMaybe Set.empty $ R.lookupDom a r
+
+replaceDom :: a -> a -> Relation a b -> Relation a b
+replaceDom a a' r =
+  foldl' (\r b -> R.insert a' b $ R.delete a b r) r (lookupDom a r)
+
+-- Todo: fork the relation library
+replaceRan :: b -> b -> Relation a b -> Relation a b
+replaceRan b b' r =
+  foldl' (\r a -> R.insert a b' $ R.delete a b r) r (lookupRan b r)
+
+deleteRan :: b -> Relation a b -> Relation a b
+deleteRan b r = foldl' (\r a -> R.delete a b r) r $ lookupRan b r
+
+deleteDom :: a -> Relation a b -> Relation a b
+deleteDom a r = foldl' (\r b -> R.delete a b r) r $ lookupDom a r
 
 replaceTerm :: Monad m
             => ReferenceOps m
             -> Reference -> Reference -> Typing
             -> Branch -> m Branch
 replaceTerm ops old new typ (Branch b) = Branch <$> Causal.stepM go b where
-  edit = Conflicted.one (TermEdit.Replace new typ)
-  replace cs = Conflicted.map (\r -> if r == old then new else r) cs
+  edit = TermEdit.Replace new typ
   go b = do
     backupNames <- updateBackupNames1 ops new b
-    pure b { edited = Map.insertWith (<>) old edit (edited b)
-    -- todo: can we use backupNames to find the keys to update, instead of fmap
-           , termNamespace = replace <$> termNamespace b
+    pure b { editedTerms = R.insert old edit (editedTerms b)
+    -- todo: can we use backupNames to find the keys to update, instead of
+    -- fmap
+           , termNamespace = replaceRan old new $ termNamespace b
            , backupNames = backupNames
            }
 
@@ -169,9 +192,8 @@ codebase ops (Branch (Causal.head -> Branch0 {..})) =
   let initial = Set.fromList $
         (toList termNamespace >>= toList) ++
         (toList typeNamespace >>= toList) ++
-        (toList edited >>= toList >>= TermEdit.references) ++
-        (toList editedDatas >>= toList >>= TypeEdit.references) ++
-        (toList editedEffects >>= toList >>= TypeEdit.references)
+        (toList editedTerms >>= toList >>= TermEdit.references) ++
+        (toList editedTypes >>= toList >>= TypeEdit.references)
   in transitiveClosure (dependencies ops) initial
 
 transitiveClosure :: forall m a. (Monad m, Ord a)
@@ -189,58 +211,35 @@ transitiveClosure getDependencies open =
           go (Set.insert h closed) (toList deps ++ t)
   in go Set.empty (toList open)
 
--- foo -> (bar, baz)
--- bar -> id
--- baz -> goo
-
--- transitiveClosure ... { foo, baz }
--- go {} [foo, baz]
---   deps foo = {bar, baz}
--- go {foo} [bar, baz, baz]
---   deps bar = {id}
--- go {foo, bar} [id, baz, baz]
---   deps id = {}
--- go {foo, bar, id} [baz, baz]
---   deps baz = {goo}
--- go {foo, bar, id, baz} [goo, baz]
---   deps goo = {}
--- go {foo, goo, bar, baz, id} [baz]
--- go {foo, goo, bar, baz, id} []
--- {foo, goo, bar, baz, id}
-
-
-
-
---apply :: Branch -> Map Name Reference -> Map Name (Conflicted Reference)
---apply (Branch (Causal.head -> Branch0 {..})) ns = let
---  nsOut = Map.unionWith (<>) termNamespace typeNamespace
---  error "todo"
-
-
-
 deprecateTerm :: Reference -> Branch -> Branch
 deprecateTerm old (Branch b) = Branch $ Causal.step go b where
-  edit = Conflicted.one TermEdit.Deprecate
-  delete c = Conflicted.delete old c
-  go b = b { edited = Map.insertWith (<>) old edit (edited b)
-           , termNamespace = Map.fromList
-             [ (k, v) | (k, v0) <- Map.toList (termNamespace b),
-                        Just v <- [delete v0] ] }
+  go b = b { editedTerms = R.insert old TermEdit.Deprecate (editedTerms b)
+           , termNamespace = deleteRan old (termNamespace b)
+           }
+
+deprecateType :: Reference -> Branch -> Branch
+deprecateType old (Branch b) = Branch $ Causal.step go b where
+  go b = b { editedTypes = R.insert old TypeEdit.Deprecate (editedTypes b)
+           , typeNamespace = deleteRan old (typeNamespace b)
+           }
+
+
 
 instance Hashable Branch0 where
   tokens (Branch0 {..}) =
     H.tokens termNamespace ++ H.tokens typeNamespace ++
-    H.tokens edited ++ H.tokens editedDatas ++ H.tokens editedEffects
+    H.tokens editedTerms ++ H.tokens editedTypes ++ H.tokens editedEffects
 
 type ResolveReference = Reference -> Maybe Name
 
-resolveTerm :: Name -> Branch -> Maybe (Conflicted Reference)
-resolveTerm n (Branch (Causal.head -> b)) =
-  Map.lookup n (termNamespace b)
+resolveTerm :: Name -> Branch -> Set Reference
+resolveTerm n (Branch (Causal.head -> b)) = lookupDom n (termNamespace b)
 
 resolveTermUniquely :: Name -> Branch -> Maybe Reference
-resolveTermUniquely n b = resolveTerm n b >>= Conflicted.asOne
-
+resolveTermUniquely n b =
+  case resolveTerm n b of
+    s | Set.size s == 1 -> lookupMin s
+    _ -> Nothing
 
 -- probably not super common
 --addName :: Reference -> Name -> Branch -> Branch
@@ -248,25 +247,15 @@ resolveTermUniquely n b = resolveTerm n b >>= Conflicted.asOne
 --  ro = Conflicted.one r
 --  go b = b { termNamespace = Map.insert n ro (termNamespace b) }
 
-addTerm :: Name -> Reference -> Branch -> Branch
-addTerm n r (Branch b) = Branch $ Causal.step go b where
-  ro = Conflicted.one r
-  go b = b { termNamespace = Map.insert n ro (termNamespace b) }
-
-addType :: Name -> Reference -> Branch -> Branch
-addType n r (Branch b) = Branch $ Causal.step go b where
-  ro = Conflicted.one r
-  go b = b { termNamespace = Map.insert n ro (typeNamespace b) }
-
 renameType :: Name -> Name -> Branch -> Branch
 renameType old new (Branch b) =
   let
     bh = Causal.head b
     m0 = typeNamespace bh
-  in Branch $ case Map.lookup old m0 of
+  in Branch $ case R.lookupDom old m0 of
     Nothing -> b
     Just rs ->
-      let m1 = Map.insertWith (<>) new rs . Map.delete old $ m0
+      let m1 = replaceDom old new m0
       in Causal.cons (bh { typeNamespace = m1 }) b
 
 renameTerm :: Name -> Name -> Branch -> Branch
@@ -274,10 +263,10 @@ renameTerm old new (Branch b) =
   let
     bh = Causal.head b
     m0 = termNamespace bh
-  in Branch $ case Map.lookup old m0 of
+  in Branch $ case R.lookupDom old m0 of
     Nothing -> b
     Just rs ->
-      let m1 = Map.insertWith (<>) new rs . Map.delete old $ m0
+      let m1 = replaceDom old new m0
       in Causal.cons (bh { termNamespace = m1 }) b
 
 --
