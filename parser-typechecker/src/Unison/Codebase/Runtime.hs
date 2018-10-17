e2b83dac01e47f29f8421c362554aab39a0cee7c
Arya Irani <arya.irani@gmail.com>
2018-10-17 13:19:20 -0400

    implementation of Runtime for java runtime

--- parser-typechecker/src/Unison/Codebase/Runtime.hs
+++ parser-typechecker/src/Unison/Codebase/Runtime.hs
@@ -1,17 +1,13 @@
 module Unison.Codebase.Runtime where
 
--- import qualified Unison.UnisonFile as UF
+import qualified Unison.Codebase   as Codebase
+import           Unison.Parser     (Ann)
+import qualified Unison.UnisonFile as UF
 
--- type UnisonFile v = UF.UnisonFile v Ann
+type Codebase v = Codebase.Codebase IO v Ann
+type UnisonFile v = UF.UnisonFile v Ann
 
--- data Runtime v = Runtime { launch :: IO (IO (), Term v -> Codebase v -> IO ()) }
-
-{-
-
-foo x =
-  x + 1
-  x
-
-> foo 3
-
--}
+data Runtime v = Runtime
+  { terminate :: IO ()
+  , evaluate  :: UnisonFile v -> Codebase v -> IO () -- but eventually IO (Term v)
+  }
--- parser-typechecker/src/Unison/Codebase/Watch.hs
+++ parser-typechecker/src/Unison/Codebase/Watch.hs
@@ -33,9 +33,57 @@ import           Unison.Util.Monoid
 import           Unison.Util.TQueue (TQueue)
 import qualified Unison.Util.TQueue as TQueue
 import qualified System.IO.Streams as Streams
+import System.IO.Streams (InputStream, OutputStream)
 import qualified System.Process as P
 import           System.Random      (randomIO)
 import Control.Exception (finally)
+import Data.ByteString (ByteString)
+import Unison.Var (Var)
+
+import Unison.Codebase.Runtime (Runtime(..))
+import qualified Unison.Codebase.Runtime as RT
+import qualified Unison.Codecs as Codecs
+import           Data.Bytes.Put (runPutS)
+import           Control.Monad.State (evalStateT)
+
+javaRuntime :: Var v => Int -> IO (Runtime v)
+javaRuntime suggestedPort = do
+  (listeningSocket, port) <- choosePortAndListen suggestedPort
+  (killme, input, output) <- connectToRuntime listeningSocket port
+  pure $ Runtime killme (feedme input output)
+  where
+    feedme :: Var v
+           => InputStream ByteString -> OutputStream ByteString
+           -> RT.UnisonFile v -> RT.Codebase v -> IO ()
+    feedme _input output unisonFile _codebase = do
+      -- todo: runtime should be able to request more terms/types/arities by hash
+      let bs = runPutS $ flip evalStateT 0 $ Codecs.serializeFile unisonFile
+      Streams.write (Just bs) output
+
+    -- open a listening socket for the runtime to connect to
+    choosePortAndListen :: Int -> IO (Socket, Int)
+    choosePortAndListen suggestedPort = do
+      sock <- socket AF_INET Stream 0
+      setSocketOption sock ReuseAddr 1
+      let bindLoop port =
+            (port <$ bind sock (SockAddrInet (fromIntegral port) iNADDR_ANY))
+            <|> bindLoop (port + 1) -- try the next port if that fails
+      chosenPort <- bindLoop suggestedPort
+      listen sock 2
+      pure (sock, chosenPort)
+
+    -- start the runtime and wait for it to connect to us
+    connectToRuntime ::
+      Socket -> Int -> IO (IO (), InputStream ByteString, OutputStream ByteString)
+    connectToRuntime listenSock port = do
+      let cmd = "scala"
+          args = ["-cp", "runtime-jvm/main/target/scala-2.12/classes",
+                  "org.unisonweb.BootstrapStream", show port]
+      (_,_,_,ph) <- P.createProcess (P.proc cmd args) { P.cwd = Just "." }
+      (socket, _address) <- accept listenSock -- accept a connection and handle it
+      (input, output) <- N.socketToStreams socket
+      pure (P.terminateProcess ph, input, output)
+
 
 watchDirectory' :: FilePath -> IO (IO (FilePath, UTCTime))
 watchDirectory' d = do
