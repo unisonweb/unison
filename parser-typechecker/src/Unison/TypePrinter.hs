39ed4a0acbad73298df43824da4d55b907bf872a
Chris Gibbs <atacratic@users.noreply.github.com>
2018-10-24 21:37:03 +0100

    Revive nesting in TypePrinter

--- parser-typechecker/src/Unison/TypePrinter.hs
+++ parser-typechecker/src/Unison/TypePrinter.hs
@@ -5,15 +5,14 @@
 
 module Unison.TypePrinter where
 
-import           Data.List
 import           Data.Text (Text)
 import qualified Data.Text as Text
-import           Data.Foldable (fold)
 import           Data.Maybe (isJust)
 import           Unison.Reference (Reference(..))
 import           Unison.Type
 import           Unison.Var (Var)
 import qualified Unison.Var as Var
+import           Unison.Util.Monoid (intercalateMap)
 import qualified Unison.Util.PrettyPrint as PP
 import           Unison.Util.PrettyPrint (PrettyPrint(..))
 
@@ -29,7 +28,8 @@ pretty n p tp = case tp of
   App' (Ref' (Builtin "Sequence")) x -> PP.Group $ l"[" <> pretty n 0 x <> l"]"
   Tuple' [x]   -> parenNest (p >= 10) $ l"Pair" <> b" " <> pretty n 10 x <> b" " <> l"()"
   Tuple' xs    -> parenNest True $ commaList xs
-  Apps' f xs   -> parenNest (p >= 10) $ pretty n 9 f <> appArgs xs
+  Apps' f xs   -> parenNoGroup (p >= 10) $ pretty n 9 f <> 
+                    (PP.Nest "  " $ PP.Group (mconcat $ map (\x -> b" " <> pretty n 10 x) xs))
   Effect1' e t -> parenNest (p >= 10) $ pretty n 9 e <> l" " <> pretty n 10 t
   Effects' es  -> effects (Just es)
   ForallNamed' v body ->
@@ -40,7 +40,7 @@ pretty n p tp = case tp of
   EffectfulArrows' (Ref' (Builtin "()")) rest -> arrows True True rest
   EffectfulArrows' fst rest -> parenNest (p >= 0) $ pretty n 0 fst <> arrows False False rest
   _ -> l"error"
-  where commaList xs = fold $ intersperse (l"," <> b" ") (map (pretty n 0) xs)
+  where commaList xs = intercalateMap (l"," <> b" ") (pretty n 0) xs  
         effects Nothing = Empty
         effects (Just es) = PP.Group $ l"{" <> commaList es <> l"}"
         arrow delay first mes = (if first then Empty else b" " <> l"->" ) <>
@@ -54,22 +54,24 @@ pretty n p tp = case tp of
                                                  arrow delay first mes <> l"()"
         arrows delay first ((mes, Ref' (Builtin "()")) : rest) =
                                                  arrow delay first mes <>
-                                                 (paren delay $ arrows True True rest)
+                                                 (parenNoGroup delay $ arrows True True rest)
         arrows delay first ((mes, arg) : rest) = arrow delay first mes <>
-                                                 (paren (delay && (not $ null rest)) $
+                                                 (parenNoGroup (delay && (not $ null rest)) $
                                                    pretty n 0 arg <> arrows False False rest)
         arrows False False [] = Empty
         arrows False True [] = Empty  -- not reachable
         arrows True _ [] = Empty      -- not reachable
 
-        appArgs (x : xs) = b" " <> pretty n 10 x <> appArgs xs
-        appArgs [] = Empty
-
         paren True s = PP.Group $ l"(" <> s <> l")"
         paren False s = PP.Group s
-        -- TODO fix bug in Nest rendering
-        parenNest useParen contents = {-PP.Nest " " $ -} paren useParen contents
+
+        parenNoGroup True s = l"(" <> s <> l")"
+        parenNoGroup False s = s
+
+        parenNest useParen contents = PP.Nest "  " $ paren useParen contents
+
         l = Literal
+
         b = Breakable
 
 pretty' :: Var v => (Reference -> Text) -> AnnotatedType v a -> String
