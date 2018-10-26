5eb58a6de0188da823113049b8429564377f838a
Runar Bjarnason <runarorama@gmail.com>
2018-10-26 11:29:49 -0400

    wip

--- runtime-jvm/main/src/main/scala/Serialization.scala
+++ runtime-jvm/main/src/main/scala/Serialization.scala
@@ -1,116 +1,191 @@
 package org.unisonweb
 
+import util.Sink
+import org.unisonweb.Term._
+import ABT.{AnnotatedTerm}
+
 object Serialization {
 
-  object V0 {
-    def putABT[F[_],R](sink: Sink,
-                       term: ABT[F,R],
-                       putVar: ABT.Name => Unit,
-                       putR: R => Unit,
-                       putF: Forall[λ[A => ((A => Unit), F[A]) => Unit]]
-                      ): Unit =
-      term.freeVars
+  type Term = org.unisonweb.ABT.Term[F]
 
+  object V0 {
+    /**
+     * Serializes a Unison term. Expects the term to be annotated with its
+     * free variables.
+     */
     def putTerm(sink: Sink, term: Term): Unit = {
       import sink._
-      term match {
-        case Unboxed(u, t) =>
-          t match {
-            case Int =>
-              putByte(0)
-              putLong(u)
-            case Nat =>
+
+      def go(t: AnnotatedTerm[Term.F,(Set[Name], Vector[Name])]): Unit = {
+        t match {
+          case ABT.Var(name) =>
+            putByte(0)
+            putVarRef(t.annotation._2, name)
+          case ABT.Tm(f) =>
+            putByte(1)
+            putF(t.map(_._1))
+          case ABT.Abs(name, body) =>
+            putByte(2)
+            putVar(name)
+            go(body)
+        }
+      }
+
+      def putVar(v: Name) = {
+        putVarLong(0)
+        putString(v.toString)
+      }
+
+      def putVarRef(env: Vector[Name], v: Name) =
+        env indexOf v match {
+          case -1 => freeVars indexOf v match {
+            case -1 => sys.error("impossible: var not free or bound")
+            case i =>
               putByte(1)
-              putLong(u)
-            case Float =>
-              putByte(2)
-              putDouble(unboxedToDouble(u))
-            case Boolean =>
-              putByte(3)
-              putBoolean(unboxedToBool(u))
+              putVarLong(i)
           }
-        case Text(txt) =>
-          putByte(4)
-          putString(txt)
-        case Id(r) =>
-          putByte(5)
-          putReference(sink, r)
-        case Constructor(id, ConstructorId(ctor)) =>
+          case i =>
+            putByte(0)
+            putVarLong(i)
+        }
+
+      def putF(f: Term): Unit =
+        f match {
+          case Unboxed(u, t) =>
+            t match {
+              case UnboxedType.Int =>
+                putByte(0)
+                putLong(unboxedToLong(u))
+              case UnboxedType.Nat =>
+                putByte(1)
+                putLong(u)
+              case UnboxedType.Float =>
+                putByte(2)
+                putDouble(unboxedToDouble(u))
+              case UnboxedType.Boolean =>
+                putByte(3)
+                putBoolean(unboxedToBool(u))
+            }
+          case Text(txt) =>
+            putByte(4)
+            putString(util.Text toString txt)
+          case Term.Id(r) =>
+            putByte(5)
+            putReference(r)
+          case Constructor(id, ConstructorId(ctor)) =>
+            putByte(6)
+            putReference(id)
+            putVarLong(ctor)
+          case Request(id, ConstructorId(ctor)) =>
+            putByte(7)
+            putReference(id)
+            putVarLong(ctor)
+          case Handle(h, a) =>
+            putByte(8)
+            putTerm(sink, h)
+            putTerm(sink, a)
+          case Apply(f, Nil) => ()
+          case Apply(f, a::Nil) =>
+            putByte(9)
+            putTerm(sink, f)
+            putTerm(sink, a)
+          case Apply(f, args) =>
+            // Expand out applications, as V0 expects them one at a time
+            putF(args.foldLeft(f)((t,a) => Apply(t, a)))
+          // Type annotations are byte 10, but we don't have any
+          case Sequence(seq) =>
+            putByte(11)
+            putFramedSeq(seq.toList)(putTerm)
+          case If(cond, t, f) =>
+            putByte(12)
+            putTerm(sink, cond)
+            putTerm(sink, t)
+            putTerm(sink, f)
+          case And(x,y) =>
+            putByte(13)
+            putTerm(sink, x)
+            putTerm(sink, y)
+          case Or(x,y) =>
+            putByte(14)
+            putTerm(sink, x)
+            putTerm(sink, y)
+          case Lam(_, body) =>
+            putByte(15)
+            putTerm(sink, body)
+          case LetRec(bindings, body) =>
+            // Put a Cycle
+            putByte(3)
+            putLet(16, bindings, body)
+          case Let(bindings, body) =>
+            putLet(17, bindings, body)
+          case Match(scrutinee, cases) =>
+            putByte(18)
+            putTerm(sink, scrutinee)
+            putFramedSeq1(cases)(putMatchCase _)
+        }
+
+      def putMatchCase[F[_]](c: MatchCase[Term]): Unit = {
+        val MatchCase(pattern, guard, body) = c
+        putPattern(pattern)
+        putOption1(guard)(putTerm(sink,_))
+        putTerm(sink, body)
+      }
+
+      def putPattern(pattern: Pattern): Unit = pattern match {
+        case Pattern.Uncaptured => putByte(0)
+        case Pattern.Wildcard => putByte(1)
+        case Pattern.LiteralU(u, t) => t match {
+          case UnboxedType.Boolean =>
+            putByte(2)
+            putBoolean(unboxedToBool(u))
+          case UnboxedType.Int =>
+            putByte(3)
+            putLong(unboxedToLong(u))
+          case UnboxedType.Nat =>
+            putByte(4)
+            putLong(u)
+          case UnboxedType.Float =>
+            putByte(5)
+            putDouble(unboxedToDouble(u))
+        }
+        case Pattern.Data(id, cid, patterns) =>
           putByte(6)
-          putReference(sink, id)
-          putVarLong(ctor)
-        case Request(id, ConstructorId(ctor)) =>
-          putByte(7)
-          putReference(sink, id)
-          putVarLong(ctor)
-        case Handle(h, a) =>
-          putByte(8)
-          putTerm(sink, h)
-          putTerm(sink, a)
-        case Apply(f, arg) =>
-          putByte(9)
-          putTerm(sink, f)
-          putTerm(sink, arg)
-        // Type annotations are byte 10, but we don't have any
-        case Sequence(seq) =>
-          putByte(11)
-          putFramedSeq(seq)(putTerm)
-        case If(cond, t, f) =>
-          putByte(12)
-          putTerm(sink, cond)
-          putTerm(sink, t)
-          putTerm(sink, f)
-        case And(x,y) =>
-          putByte(13)
-          putTerm(sink, x)
-          putTerm(sink, y)
-        case Or(x,y) =>
-          putByte(14)
-          putTerm(sink, x)
-          putTerm(sink, y)
-        case Lam(_, body) =>
-          putByte(15)
-          putTerm(sink, body)
-        case LetRec(bindings, body) =>
-          // Put a Cycle
-          putByte(3)
-          putLet(s, 16, bindings, body)
-        case Let(bindings, body) =>
-          putLet(s, 17, bindings, body)
-        case Match(scrutinee, cases) =>
-          putByte(18)
-          putTerm(sink, scrutinee)
-          putFramedSeq(cases)(putMatchCase(_, _))
+          putReference(id)
+          putVarLong(cid.toInt)
+          putFramedSeq1(patterns)(putPattern)
       }
-    }
 
-    def putLet(sink: Sink,
-               b: Byte,
-               bindings: List[(Name, AnnotatedTerm[F,A])],
-               body: AnnotatedTerm[F,A]): Unit = {
-      bindings.foreach {
-        // Abs all the bound variables
-        case (name, binding) =>
-          putByte(2)
-          putVar(name)
-      }
-      // Finally put the letrec term
-      putByte(1)
-      putByte(b)
-      putFramedSeq(bindings) {
-        case (s, (_, binding)) => putTerm(s, binding)
-      }
-      putTerm(sink, body)
-    }
-
-    def putReference(sink: Sink, id: Id): Unit = id match {
-      case Builtin(Name(name)) =>
-        putByte(0)
-        putString(name)
-      case HashRef(Hash(bytes)) =>
+      def putLet[F[_]](b: Byte,
+                       bindings: List[(Name, AnnotatedTerm[F,Set[Name]])],
+                       body: AnnotatedTerm[F,Set[Name]]): Unit = {
+        bindings.foreach {
+          // Abs all the bound variables
+          case (name, binding) =>
+            putByte(2)
+            putVar(name)
+        }
+        // Finally put the letrec term
         putByte(1)
-        put(bytes)
-    }
-  }
+        putByte(b)
+        putFramedSeq(bindings) {
+          case (s, (_, binding)) => putTerm(s, binding)
+        }
+        putTerm(sink, body)
+      }
 
+      def putReference(id: Id): Unit = id match {
+        case Builtin(Name(name)) =>
+          putByte(0)
+          putString(name)
+        case HashRef(Hash(bytes)) =>
+          putByte(1)
+          put(bytes)
+      }
+      val freeVars = term.annotation.toList
+      putFramedSeq1(freeVars)(putVar _)
+      go(ABT.annotateBound(term))
+    }
+
+
+  }
 }
