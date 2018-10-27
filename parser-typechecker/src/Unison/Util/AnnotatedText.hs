6572c4edf40054dca3e8c5a76a0211541d7f7af6
Arya Irani <arya.irani@gmail.com>
2018-10-27 16:24:25 -0400

    eliminate AnnotatedDocument...

--- parser-typechecker/src/Unison/Util/AnnotatedText.hs
+++ parser-typechecker/src/Unison/Util/AnnotatedText.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE RankNTypes                 #-}
 
 module Unison.Util.AnnotatedText where
 
@@ -14,34 +15,16 @@ import           Data.Sequence      (Seq ((:|>)))
 import qualified Data.Sequence      as Seq
 import           Data.String        (IsString (..))
 import           Data.Void          (Void)
-import           Safe               (lastMay)
+import           Safe               (headMay, lastMay)
 import           Unison.Lexer       (Line, Pos (..))
 import           Unison.Util.Monoid (intercalateMap)
-import           Unison.Util.Range  (Range (..))
+import           Unison.Util.Range  (Range (..), inRange)
 
-newtype AnnotatedDocument a = AnnotatedDocument (Seq (Section a))
-  deriving (Functor, Semigroup, Monoid)
-
--- Prose with subsequences that may have an annotation.
--- A textual reference to an annotation style.
--- Quoted text (indented, with source line numbers) with annotated portions.
-
--- The reason the current API deals with a bunch of different types instead of
--- a single one is because not all of the combinators make sense on all of the
--- types.
-
--- Question: Should every bit of text be forced to have an annotation?
--- Answer: No.  That doesn't make sense for the Excerpt text — especially
---              in the context of multiple rendering options.
-data Section a
-  = Text (AnnotatedText (Maybe a))
-  -- | NoBreak (AnnotatedText (Maybe a))
-  | Blockquote (AnnotatedExcerpt a)
-  deriving (Functor)
-
-newtype AnnotatedText a = AnnotatedText (Seq (String, a))
+type AnnotatedText a = AnnotatedText' (Maybe a)
+newtype AnnotatedText' a = AnnotatedText' (Seq (String, a))
   deriving (Functor, Foldable, Semigroup, Monoid)
 
+  -- Quoted text (indented, with source line numbers) with annotated portions.
 data AnnotatedExcerpt a = AnnotatedExcerpt
   { lineOffset  :: Line
   , text        :: String
@@ -51,49 +34,70 @@ data AnnotatedExcerpt a = AnnotatedExcerpt
 newtype Rendered a = Rendered { rawRender :: Seq String }
   deriving (Semigroup, Monoid)
 
-sectionToDoc :: Section a -> AnnotatedDocument a
-sectionToDoc = AnnotatedDocument . pure
+pairToText :: (String, a) -> AnnotatedText' a
+pairToText (str, a) = AnnotatedText' . Seq.singleton $ (str, a)
 
-pairToDoc :: (String, Maybe a) -> AnnotatedDocument a
-pairToDoc (str, a) = textToDoc . AnnotatedText . Seq.singleton $ (str, a)
-
-pairToDoc' :: (String, a) -> AnnotatedDocument a
-pairToDoc' (str, a) = pairToDoc (str, Just a)
-
-textToDoc :: AnnotatedText (Maybe a) -> AnnotatedDocument a
-textToDoc = AnnotatedDocument . pure . Text
-
-excerptToDoc :: AnnotatedExcerpt a -> AnnotatedDocument a
-excerptToDoc = AnnotatedDocument . pure . Blockquote
+pairToText' :: (String, a) -> AnnotatedText a
+pairToText' (str, a) = pairToText (str, Just a)
 
 trailingNewLine :: AnnotatedText a -> Bool
-trailingNewLine (AnnotatedText (init :|> (s,_))) =
+trailingNewLine (AnnotatedText' (init :|> (s,_))) =
   case lastMay s of
          Just '\n' -> True
          Just _    -> False
-         _         -> trailingNewLine (AnnotatedText init)
+         _         -> trailingNewLine (AnnotatedText' init)
 trailingNewLine _ = False
 
 markup :: AnnotatedExcerpt a -> Map Range a -> AnnotatedExcerpt a
 markup a r = a { annotations = r `Map.union` annotations a }
 
-renderTextUnstyled :: AnnotatedText a -> Rendered Void
-renderTextUnstyled (AnnotatedText chunks) = foldl' go mempty chunks
+renderTextUnstyled :: AnnotatedText' a -> Rendered Void
+renderTextUnstyled (AnnotatedText' chunks) = foldl' go mempty chunks
   where go r (text, _) = r <> fromString text
 
-textLength :: AnnotatedText a -> Int
+textLength :: AnnotatedText' a -> Int
 textLength = length . show . renderTextUnstyled
 
-textEmpty :: AnnotatedText a -> Bool
+textEmpty :: AnnotatedText' a -> Bool
 textEmpty = (==0) . textLength
 
-splitAndRender :: Int
-               -> (AnnotatedExcerpt a -> Rendered b)
-               -> AnnotatedExcerpt a -> Rendered b
-splitAndRender n f e = intercalateMap "    .\n" f $ snipWithContext n e
+condensedExcerptToText :: Int -> AnnotatedExcerpt a -> AnnotatedText a
+condensedExcerptToText margin e =
+  intercalateMap "    .\n" excerptToText $ snipWithContext margin e
 
--- | drops lines and replaces with "." if there are more than `n` unannotated
--- | lines in a row.
+excerptToText :: forall a. AnnotatedExcerpt a -> AnnotatedText a
+excerptToText e =
+  track (Pos line1 1) [] (Map.toList $ annotations e) (renderLineNumber line1) (text e)
+  where
+    line1 :: Int
+    line1 = lineOffset e
+    renderLineNumber :: Int -> AnnotatedText a
+    renderLineNumber n = fromString $ " " ++ spaces ++ sn ++ " | "
+      where sn = show n
+            spaces = replicate (lineNumberWidth - length sn) ' '
+            lineNumberWidth = 4
+
+    -- step through the source characters and annotations
+    track _ _ _ rendered "" = rendered
+    track _ _ _ rendered "\n" = rendered <> "\n"
+    track pos@(Pos line col) stack annotations rendered _input@(c:rest) =
+      let
+        (poppedAnnotations, remainingAnnotations) =  span (inRange pos . fst) annotations
+        -- drop any stack entries that will be closed after this char
+        -- and add new stack entries
+        stack' = foldl' pushColor stack0 poppedAnnotations
+          where pushColor s (Range _ end, style) = (style, end) : s
+                stack0 = dropWhile ((<=pos) . snd) stack
+        maybeColor = fst <$> headMay stack'
+        -- on new line, advance pos' vertically and set up line header
+        -- additions :: AnnotatedText (Maybe a)
+        pos' :: Pos
+        (additions, pos') =
+          if c == '\n'
+          then ("\n" <> renderLineNumber (line + 1), Pos (line + 1) 1)
+          else (pairToText ([c], maybeColor), Pos line (col + 1))
+      in track pos' stack' remainingAnnotations (rendered <> additions) rest
+
 snipWithContext :: Int -> AnnotatedExcerpt a -> [AnnotatedExcerpt a]
 snipWithContext margin source =
   case foldl' whileWithinMargin
@@ -133,14 +137,8 @@ snipWithContext margin source =
           -- once we've added to the second set, anything more goes there too
           else (Just r0, taken, Map.insert r1 a1 rest)
 
-instance IsString (AnnotatedDocument a) where
-  fromString = AnnotatedDocument . pure . fromString
-
-instance IsString (Section a) where
-  fromString = Text . fromString
-
-instance IsString (AnnotatedText (Maybe a)) where
-  fromString s = AnnotatedText . pure $ (s, Nothing)
+instance IsString (AnnotatedText a) where
+  fromString s = AnnotatedText' . pure $ (s, Nothing)
 
 instance IsString (AnnotatedExcerpt a) where
   fromString s = AnnotatedExcerpt 1 s mempty
