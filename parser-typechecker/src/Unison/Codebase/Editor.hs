d60d7ebcb14eb5c845811fed1ebffa21195363b8
Arya Irani <arya.irani@gmail.com>
2018-11-28 15:40:51 -0500

    finished Editor.loop

--- parser-typechecker/src/Unison/Codebase/Editor.hs
+++ parser-typechecker/src/Unison/Codebase/Editor.hs
@@ -1,7 +1,12 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+
 
 module Unison.Codebase.Editor where
 
+import Control.Monad.Extra (ifM)
 import Data.Text (Text)
 import Data.Sequence (Seq)
 import Data.Foldable (toList)
@@ -46,25 +51,32 @@ data Input
   | MergeBranchI BranchName
   | QuitI
 
-data Notification
+data Notification v
   = Success Input Bool
+  | NoUnisonFile
+  | UnknownBranch BranchName
+  | BranchAlreadyExists BranchName
+  | ListOfBranches [BranchName]
+  | AddOutput (AddOutput v)
 
 data Command v a where
   Input :: Command v (Either (TypecheckingResult v) Input)
 
   ReportParseErrors :: [Parser.Err v] -> Command v ()
   ReportTypeErrors :: PPE.PrettyPrintEnv -> [Context.ErrorNote v loc] -> Command v ()
-  Notify :: Notification -> Command v ()
+  Notify :: Notification v -> Command v ()
 
   Add :: BranchName -> UF.TypecheckedUnisonFile' v Ann -> Command v (AddOutput v)
 
   Typecheck :: SourceName -> Source -> Command v (TypecheckingResult v)
 
+  ListBranches :: Command v [BranchName]
+
   -- Loads a branch by name from the codebase, returning `Nothing` if not found.
   LoadBranch :: BranchName -> Command v (Maybe Branch)
 
   -- Returns `Nothing` if a branch by that name already exists.
-  NewBranch :: BranchName -> Command v (Maybe Branch)
+  NewBranch :: BranchName -> Command v Bool
 
   -- Create a new branch which is a copy of the given branch, and assign the
   -- forked branch the given name. Returns `False` if the forked branch name
@@ -73,7 +85,7 @@ data Command v a where
 
   -- Merges the branch with the existing branch with the given name. Returns
   -- `Nothing` if no branch with that name exists.
-  MergeBranch :: BranchName -> Branch -> Command v (Maybe Branch)
+  MergeBranch :: BranchName -> Branch -> Command v Bool
 
   -- Return the subset of the branch tip which is in a conflicted state
   GetConflicts :: BranchName -> Command v (Maybe Branch0)
@@ -129,12 +141,13 @@ commandLine _codebase command = do
 
       _ -> error "todo"
 
-type LoopState = BranchName
+data LoopState v
+  = LoopState BranchName (Maybe (UF.TypecheckedUnisonFile' v Ann))
 
-loop :: LoopState -> Free (Command v) ()
-loop branchName = Free.unfold' go branchName where
-  go :: LoopState -> Free (Command v) (Either () LoopState)
-  go branchName = do
+loop :: LoopState v -> Free (Command v) ()
+loop s = Free.unfold' go s where
+  go :: forall v. LoopState v -> Free (Command v) (Either () (LoopState v))
+  go s@(LoopState currentBranchName uf) = do
     e <- Free.eval Input
     case e of
       Left (Result.Result notes r) -> case r of
@@ -147,31 +160,66 @@ loop branchName = Free.unfold' go branchName where
             Free.eval $ ReportTypeErrors errorEnv
                           [ err | Result.TypeError err <- toList notes]
             repeat
-          Just unisonFile -> undefined unisonFile
+          Just unisonFile -> updateUnisonFile unisonFile
       Right input -> case input of
         AliasI nameTarget oldName newName -> do
-          (Free.eval $ Alias branchName nameTarget oldName newName) >>=
+          (Free.eval $ Alias currentBranchName nameTarget oldName newName) >>=
             (Free.eval . Notify . Success input)
           repeat
         RenameI nameTarget oldName newName -> do
-          (Free.eval $ Rename branchName nameTarget oldName newName) >>=
+          (Free.eval $ Rename currentBranchName nameTarget oldName newName) >>=
             (Free.eval . Notify . Success input)
           repeat
         UnnameI nameTarget name -> do
-          (Free.eval $ Unname branchName nameTarget name) >>=
+          (Free.eval $ Unname currentBranchName nameTarget name) >>=
             (Free.eval . Notify . Success input)
           repeat
-        AddI -> error "todo"
-        ListBranchesI -> error "todo"
-        SwitchBranchI _branchName -> pure $ Right branchName
-        ForkBranchI _branchName -> do
-          error "todo"
-          -- repeat
-        MergeBranchI _branchName -> error "todo"
+        AddI -> case uf of
+          Nothing -> do
+            Free.eval . Notify $ NoUnisonFile
+            repeat
+          Just uf -> do
+            (Free.eval $ Add currentBranchName uf) >>=
+              (Free.eval . Notify . AddOutput)
+            repeat
+        ListBranchesI -> do
+          (Free.eval $ ListBranches) >>= Free.eval . Notify . ListOfBranches
+          repeat
+        SwitchBranchI branchName -> switchBranch branchName
+        ForkBranchI targetBranchName ->
+          loadBranchOrComplain currentBranchName $ \branch -> do
+            ifM (Free.eval $ ForkBranch branch targetBranchName)
+                (do
+                  Free.eval . Notify $ Success input True
+                  switchBranch targetBranchName)
+                (do
+                  Free.eval . Notify $ BranchAlreadyExists targetBranchName
+                  repeat)
+        MergeBranchI inputBranch -> do
+          branch <- Free.eval $ LoadBranch inputBranch
+          case branch of
+            Nothing -> do
+              Free.eval . Notify $ UnknownBranch inputBranch
+            Just branch -> do
+              (Free.eval $ MergeBranch currentBranchName branch) >>=
+                (Free.eval . Notify . Success input)
+          repeat
         QuitI -> quit
     where
-      repeat = pure $ Right branchName
+      repeat = pure $ Right s
+      switchBranch branchName = pure . Right $ LoopState branchName uf
+      updateUnisonFile :: forall f v. Applicative f => UF.TypecheckedUnisonFile' v Ann -> f (Either () (LoopState v))
+      updateUnisonFile = pure . Right . LoopState currentBranchName . Just
       quit = pure $ Left ()
+      loadBranchOrComplain ::
+        BranchName -> (Branch -> Free (Command v) (Either () (LoopState v))) -> Free (Command v) (Either () (LoopState v))
+      loadBranchOrComplain branchName f = do
+        branch <- Free.eval $ LoadBranch branchName
+        case branch of
+          Nothing -> do
+            Free.eval . Notify $ UnknownBranch branchName
+            repeat
+          Just branch -> f branch
 
 -- commandLine :: Codebase -> IO (Command v) a -> IO a
 --
