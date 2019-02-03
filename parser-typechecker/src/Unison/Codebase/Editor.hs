0bf77c9c108d8d43e6d965a7914665b41a21905a
Runar Bjarnason <runarorama@gmail.com>
2019-02-02 22:55:49 -0500

    Actually quit

--- parser-typechecker/src/Unison/Codebase/Editor.hs
+++ parser-typechecker/src/Unison/Codebase/Editor.hs
@@ -320,8 +320,6 @@ data Command i v a where
 
   Propagate :: Branch -> Command i v Branch
 
-  Quit :: Command i v ()
-
 data Outcome
   -- New definition that was added to the branch
   = Added
@@ -644,7 +642,6 @@ commandLine awaitInput rt branchChange notifyUser codebase command = do
     Propagate b -> do
       b0 <- Codebase.propagate codebase (Branch.head b)
       pure $ Branch.append b0 b
-    Quit -> pure ()
 
 doTodo :: Monad m => Codebase m v a -> Branch0 -> m (TodoOutput v a)
 doTodo code b = do
