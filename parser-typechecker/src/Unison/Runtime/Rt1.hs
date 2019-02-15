f028eca7f04f5daa2173f7eedf72473903d3492b
Paul Chiusano <paul.chiusano@gmail.com>
2019-02-15 18:02:44 -0500

    filled in Rt1, except for pattern matching

--- parser-typechecker/src/Unison/Runtime/Rt1.hs
+++ parser-typechecker/src/Unison/Runtime/Rt1.hs
@@ -1,8 +1,9 @@
-{-# Language OverloadedStrings #-}
-{-# Language StrictData #-}
-{-# Language Strict #-}
 {-# Language BangPatterns #-}
 {-# Language LambdaCase #-}
+{-# Language OverloadedStrings #-}
+{-# Language Strict #-}
+{-# Language StrictData #-}
+{-# Language TupleSections #-}
 
 module Unison.Runtime.Rt1 where
 
@@ -15,7 +16,9 @@ import Data.Traversable (for)
 import Data.Word (Word64)
 import Unison.Runtime.IR
 import Unison.Symbol (Symbol)
+import qualified Data.Vector as Vector
 import qualified Data.Vector.Mutable as MV
+import qualified Unison.Term as Term
 
 type Stack = MV.IOVector Value
 
@@ -81,7 +84,7 @@ arity (Lam n _ _) = n
 arity _ = 0
 
 run :: CompilationEnv -> IR -> IO Result
-run _env ir = do
+run env ir = do
   supply <- newIORef 0
   m0 <- MV.new 256
   MV.set m0 (T "uninitialized")
@@ -91,6 +94,8 @@ run _env ir = do
 
     go :: Size -> Stack -> IR -> IO Result
     go size m ir = case ir of
+      Leaf (Val v) -> done v
+      Leaf s -> done =<< at size s m
       If c t f -> atb size c m >>= \case
         True -> go size m t
         False -> go size m f
@@ -101,20 +106,133 @@ run _env ir = do
         True -> done (B True)
         False -> go size m j
       Not i -> atb size i m >>= (done . B . not)
-      Leaf (Val v) -> done v
-      Leaf s -> done =<< at size s m
       Let b body -> go size m b >>= \case
         RRequest req -> pure $ RRequest (req `appendCont` body)
         RDone v -> push size v m >>= \m -> go (size + 1) m body
         e -> error $ show e
       LetRec bs body -> letrec size m bs body
-      _ -> error $ "TODO - fill in the rest of Rt1.go " <> show ir
+      MakeSequence vs ->
+        done . Sequence . Vector.fromList =<< traverse (\i -> at size i m) vs
+      Construct r cid args ->
+        done . Data r cid =<< traverse (\i -> at size i m) args
+      Request r cid args ->
+        req <$> traverse (\i -> at size i m) args
+        where
+        -- The continuation of the request is initially the identity function
+        -- and we append to it in `Let` as we unwind the stack
+        req vs = RRequest (Req r cid vs (Leaf $ Slot 0))
+      Handle handler body -> do
+        h <- at size handler m
+        runHandler size m h body
+      Apply fn args -> do
+        RDone fn <- go size m fn -- ANF should ensure this match is OK
+        call size m fn args
 
+      -- Builtins
+      AddI i j -> do x <- ati size i m; y <- ati size j m; done (I (x + y))
+      SubI i j -> do x <- ati size i m; y <- ati size j m; done (I (x - y))
+      MultI i j -> do x <- ati size i m; y <- ati size j m; done (I (x * y))
+      DivI i j -> do x <- ati size i m; y <- ati size j m; done (I (x `div` y))
+      GtI i j -> do x <- ati size i m; y <- ati size j m; done (B (x > y))
+      GtEqI i j -> do x <- ati size i m; y <- ati size j m; done (B (x >= y))
+      LtI i j -> do x <- ati size i m; y <- ati size j m; done (B (x < y))
+      LtEqI i j -> do x <- ati size i m; y <- ati size j m; done (B (x <= y))
+      EqI i j -> do x <- ati size i m; y <- ati size j m; done (B (x == y))
+
+      AddN i j -> do x <- atn size i m; y <- atn size j m; done (N (x + y))
+      -- cast to `Int` and subtract
+      SubN i j -> do x <- atn size i m; y <- atn size j m
+                     done (I (fromIntegral x - fromIntegral y))
+      -- subtraction truncated at 0 (don't wrap around)
+      DropN i j -> do x <- atn size i m; y <- atn size j m
+                      done (N (x - (y `min` x)))
+      MultN i j -> do x <- atn size i m; y <- atn size j m; done (N (x * y))
+      DivN i j -> do x <- atn size i m; y <- atn size j m; done (N (x `div` y))
+      GtN i j -> do x <- atn size i m; y <- atn size j m; done (B (x > y))
+      GtEqN i j -> do x <- atn size i m; y <- atn size j m; done (B (x >= y))
+      LtN i j -> do x <- atn size i m; y <- atn size j m; done (B (x < y))
+      LtEqN i j -> do x <- atn size i m; y <- atn size j m; done (B (x <= y))
+      EqN i j -> do x <- atn size i m; y <- atn size j m; done (B (x == y))
+
+      AddF i j -> do x <- atf size i m; y <- atf size j m; done (F (x + y))
+      SubF i j -> do x <- atf size i m; y <- atf size j m; done (F (x - y))
+      MultF i j -> do x <- atf size i m; y <- atf size j m; done (F (x * y))
+      DivF i j -> do x <- atf size i m; y <- atf size j m; done (F (x / y))
+      GtF i j -> do x <- atf size i m; y <- atf size j m; done (B (x > y))
+      GtEqF i j -> do x <- atf size i m; y <- atf size j m; done (B (x >= y))
+      LtF i j -> do x <- atf size i m; y <- atf size j m; done (B (x < y))
+      LtEqF i j -> do x <- atf size i m; y <- atf size j m; done (B (x <= y))
+      EqF i j -> do x <- atf size i m; y <- atf size j m; done (B (x == y))
+      -- _ -> error $ "TODO - fill in the rest of Rt1.go " <> show ir
+
+    runHandler :: Size -> Stack -> Value -> IR -> IO Result
+    runHandler size m handler body = go size m body >>= \case
+      RRequest req -> do
+        m <- push size (Requested req) m
+        result <- call (size + 1) m handler [Slot 0]
+        case result of
+          RMatchFail -> pure $ RRequest (wrapHandler handler req)
+          r -> pure r
+      RDone v -> do
+        m <- push size (Pure v) m
+        call (size + 1) m handler [Slot 0]
+      r -> pure r
+
+    ensureSize :: Size -> Stack -> IO Stack
+    ensureSize size m =
+      if (size >= MV.length m) then MV.grow m size
+      else pure m
+
+    call :: Size -> Stack -> Value -> [Z] -> IO Result
+    call size m fn@(Lam arity underapply body) args = let nargs = length args in
+      -- fully applied call, `(x y -> ..) 9 10`
+      if nargs == arity then do
+        m <- ensureSize (size + arity) m
+        let pushArg size' z = do
+              val <- at size z m -- variable lookup uses current size
+              MV.write m size' val
+              pure (size' + 1)
+        _ <- foldM pushArg 0 args
+        go (size + arity) m body
+      -- overapplied call, e.g. `id id 42`
+      else if nargs > arity then do
+        let (usedArgs, extraArgs) = splitAt arity args
+        result <- call size m fn usedArgs
+        case result of
+          RDone fn' -> call size m fn' extraArgs
+          RRequest req -> pure . RRequest $ req `appendCont` error "todo"
+          e -> error $ "type error, tried to apply: " <> show e
+      -- underapplied call, e.g. `(x y -> ..) 9`
+      else do
+        argvs <- for args $ \arg -> at size arg m
+        case underapply of
+          Specialize (Term.LamsNamed' vs body) -> do
+            let
+              Just argterms = traverse decompile argvs
+              toBound vs = reverse ((,Nothing) <$> vs)
+              bound = toBound (drop nargs vs) ++ reverse (vs `zip` map Just argvs)
+              compiled = compile0 env bound body
+              lam = Term.let1' False (vs `zip` argterms) $
+                    Term.lam'() (drop nargs vs) body
+            done $ Lam (arity - nargs) (Specialize lam) compiled
+          Specialize e -> error $ "can't underapply a non-lambda: " <> show e
+          FormClosure tm -> do
+            let Just argterms = traverse decompile argvs
+            done $ Lam (arity - nargs)
+                       (FormClosure $ Term.apps' tm argterms)
+                       (error "todo - gotta form an IR that calls the original body with args in the correct order")
+    call _ _ fn args =
+      error $ "type error - tried to apply a non-function: " <> show (fn, args)
+
+    -- To evaluate a `let rec`, we push an empty `Ref` onto the stack for each
+    -- binding, then evaluate each binding and set that `Ref` to its result.
+    -- As long as the variable references occur within a function body,
+    -- there's no problem.
     letrec :: Size -> Stack -> [(Symbol, IR)] -> IR -> IO Result
     letrec size m bs body = do
       let size' = size + length bs
       refs <- for bs $ \(v,b) -> do
-        r <- newIORef (N 99)
+        r <- newIORef (N 99) -- dummy value, could be null if `Value` had that
         i <- fresh
         pure (Ref i v r, b)
       m <- foldM (\m ((r,_), i) -> push (size + i) r m) m (refs `zip` [0..])
