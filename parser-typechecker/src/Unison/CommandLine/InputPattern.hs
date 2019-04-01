0b9eb587543b5a6d5740de6b3d9e50bb1e0fdcd1
Arya Irani <arya.irani@gmail.com>
2019-04-01 13:17:33 -0700

    make the arg autocomplete spec for InputPattern a little richer
    todo: automatically complain when passing an invalid number of args to a command?

--- parser-typechecker/src/Unison/CommandLine/InputPattern.hs
+++ parser-typechecker/src/Unison/CommandLine/InputPattern.hs
@@ -17,7 +17,12 @@ import           Unison.Codebase.Editor         (Input (..))
 import qualified Unison.Util.ColorText          as CT
 import qualified Unison.Util.Pretty             as P
 
-type IsOptional = Bool
+data IsOptional
+  = Optional -- 0 or 1, at the end
+  | Required -- 1, at the start
+  | ZeroPlus -- 0 or more, at the end
+  | OnePlus -- 1 or more, at the end
+  deriving Show
 
 data InputPattern = InputPattern
   { patternName :: String
@@ -35,6 +40,39 @@ data ArgumentType = ArgumentType
                 -> Branch
                 -> m [Line.Completion]
   }
+instance Show ArgumentType where
+  show at = "ArgumentType " <> typeName at
+
+-- `argType` gets called when the user tries to autocomplete an `i`th argument.
+-- todo: would be nice if we could alert the user if they try to autocomplete
+-- past the end.
+argType :: InputPattern -> Int -> Maybe ArgumentType
+argType ip i = go (i, args ip) where
+  go (_, []) = Nothing
+  go (0, (_, t) : _) = Just t
+  go (n, (Required, _) : args) = go (n - 1, args)
+  go (_, (ZeroPlus, t) : []) = Just t
+  go (_, (OnePlus, t) : []) = Just t
+  go _ = Nothing
+
+minArgs :: InputPattern -> Int
+minArgs ip@(fmap fst . args -> args) = go args where
+  go [] = 0
+  go (Required : args) = 1 + go args
+  go (_ : []) = 0
+  go _ = error $ "Invalid args for InputPattern ("
+                  <> show (patternName ip) <> "): " <> show args
+
+maxArgs :: InputPattern -> Maybe Int
+maxArgs ip@(fmap fst . args -> args) = go args where
+  go [] = Just 0
+  go (Required : args) = (1 +) <$> go args
+  go (Optional : []) = Just 0
+  go (_ : []) = Nothing
+  go _ = error $ "Invalid args for InputPattern ("
+                  <> show (patternName ip) <> "): " <> show args
+
+
 
 noSuggestions ::
   Monad m => String -> Codebase m v a -> Branch -> m [Line.Completion]
