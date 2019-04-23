dfd5eb06db0057a68c9cbfb349a6ea8278339366
Ben Fradet <benjamin.fradet@gmail.com>
2019-04-23 21:31:10 +0200

    Add test for the UnknownDataConstructor parser error

--- parser-typechecker/tests/Unison/Test/FileParser.hs
+++ parser-typechecker/tests/Unison/Test/FileParser.hs
@@ -60,6 +60,7 @@ module Unison.Test.FileParser where
     , signatureNeedsAccompanyingBodyTest
     , emptyBlockTest
     , expectedBlockOpenTest
+    , unknownDataConstructorTest
     ]
 
   expectFileParseFailure :: String -> (P.Error Symbol -> Test ()) -> Test ()
@@ -109,6 +110,15 @@ module Unison.Test.FileParser where
         expectation e = case e of
           P.ExpectedBlockOpen _ _ -> ok
           _ -> crash "Error wasn't ExpectedBlockOpen"
+
+  unknownDataConstructorTest :: Test ()
+  unknownDataConstructorTest = scope "unknownDataConstructorTest" $
+    expectFileParseFailure "m a = case a of A -> 1" expectation
+      where
+        expectation :: Var e => P.Error e -> Test ()
+        expectation e = case e of
+          P.UnknownDataConstructor _ -> ok
+          _ -> crash "Error wasn't UnknownDataConstructor"
 
   builtins :: Names
   builtins = Names.fromTerms
