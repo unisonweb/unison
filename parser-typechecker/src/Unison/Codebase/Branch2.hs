4c9d048702457bbc27c3e7d8ec67398d900ea932
Arya Irani <arya.irani@gmail.com>
2019-04-25 22:54:29 -0400

    use Branch.headHash or Causal.currentHash b, not Causal.hash (head b), to construct Branch0 correctly

--- parser-typechecker/src/Unison/Codebase/Branch2.hs
+++ parser-typechecker/src/Unison/Codebase/Branch2.hs
@@ -7,7 +7,7 @@ module Unison.Codebase.Branch2 where
 
 import           Prelude                  hiding (head,subtract)
 
-import           Control.Lens
+import           Control.Lens             hiding (children)
 -- import           Control.Monad            (join)
 -- import           Data.GUID                (genText)
 import           Data.List                (intercalate)
@@ -30,16 +30,16 @@ import           Unison.Referent          (Referent)
 import qualified Unison.Util.Relation     as R
 import           Unison.Util.Relation     (Relation)
 
--- data RepoRef
---   = Local
---   | Github { username :: Text, repo :: Text, commit :: Text }
---   deriving (Eq, Ord, Show)
---
+data RepoRef
+  = Local
+  | Github { username :: Text, repo :: Text, commit :: Text }
+  deriving (Eq, Ord, Show)
+
 -- type EditGuid = Text
---
--- data RepoLink a = RepoLink RepoRef a
---   deriving (Eq, Ord, Show)
---
+
+data RepoLink a = RepoLink RepoRef a
+  deriving (Eq, Ord, Show)
+
 -- type Link = RepoLink Hash
 -- type EditLink = RepoLink EditGuid
 --
@@ -60,35 +60,33 @@ import           Unison.Util.Relation     (Relation)
 --
 -- type FriendlyEditNames = Relation Text EditGuid
 
--- newtype Branch = Branch
---   { _history :: Causal Identity Branch0 }
---
--- data Branch0 = Branch0
---   { _terms :: Relation NameSegment Referent
---   , _types :: Relation NameSegment Reference
---   , _children :: Map NameSegment Link
---   }
-
 -- data Codebase' = Codebase'
 --   { namespaceRoot :: Branch
 --   , edits :: ???
 --   }
 
-newtype Branch m = Branch (Causal m (Branch0 m))
+newtype Branch m = Branch { _history :: Causal m (Branch0 m) }
 
 head :: Branch m -> Branch0 m
 head (Branch c) = Causal.head c
 
-data Branch0 m = Branch0
-  { _terms' :: Relation NameSegment Reference
-  , _types' :: Relation NameSegment Reference
-  , _children' :: Map NameSegment (Hash, m (Branch m))
-  }
+headHash :: Branch m -> Hash
+headHash (Branch c) = Causal.currentHash c
 
+data Branch0 m = Branch0
+  { _terms :: Relation NameSegment Reference
+  , _types :: Relation NameSegment Reference
+  -- Q: How will we handle merges and conflicts for `children`?
+  --    Should this be a relation?
+  --    What is the UX to resolve conflicts?
+  , _children :: Map NameSegment (Hash, m (Branch m))
+  }
 makeLenses ''Branch0
+makeLenses ''Branch
+
+data ForkFailure = SrcNotFound | DestExists
 
 -- copy a path to another path
-data ForkFailure = SrcNotFound | DestExists
 fork
   :: Monad m => Branch m -> Path -> Path -> m (Either ForkFailure (Branch m))
 fork root src dest = do
@@ -99,12 +97,36 @@ fork root src dest = do
       Nothing -> pure $ Left DestExists
       Just root' -> pure $ Right root'
 
+-- Move the node at src to dest.
+-- It's okay if `dest` is inside `src`, just create empty levels.
+-- Try not to `step` more than once at each node.
+move
+  :: Monad m => Branch m -> Path -> Path -> m (Either ForkFailure (Branch m))
+move root src dest = do
+  getAt root src >>= \case
+    Nothing -> pure $ Left SrcNotFound
+    Just src' ->
+      -- make sure dest doesn't already exist
+      getAt root dest >>= \case
+        Just _destExists -> pure $ Left DestExists
+        -- modify last shared ancestor of `src` and `dest`:
+        -- if lsa = src then setAt empty dest src'
+        -- else
+        --    in src branch of lsa, delete src, removing newly empty parents.
+        --      (is it ok to throw away this history?)
+        --    in dest branch of lsa, add src at dest
+        --    update lsa, simultaneously update src branch and dest branch
+        Nothing -> error "todo"
+
 setIfNotExists
   :: Monad m => Branch m -> Path -> Branch m -> m (Maybe (Branch m))
 setIfNotExists root dest b =
   getAt root dest >>= \case
     Just _destExists -> pure Nothing
-    Nothing -> Just <$> modifyAt root dest (const . pure $ b)
+    Nothing -> Just <$> setAt root dest b
+
+setAt :: Monad m => Branch m -> Path -> Branch m -> m (Branch m)
+setAt root dest b = modifyAt root dest (const b)
 
 getAt :: Monad m
       => Branch m
@@ -113,7 +135,7 @@ getAt :: Monad m
 -- todo: return Nothing if exists but is empty
 getAt root path = case Path.toList path of
   [] -> pure $ Just root
-  seg : path -> case Map.lookup seg (_children' . head $ root) of
+  seg : path -> case Map.lookup seg (_children $ head root) of
     Nothing -> pure Nothing
     Just (_h, m) -> do
       root <- m
@@ -122,29 +144,65 @@ getAt root path = case Path.toList path of
 empty :: Branch m
 empty = Branch $ Causal.one (Branch0 mempty mempty mempty)
 
---
-modifyAt :: Monad m
-         => Branch m
-         -> Path
-         -> (Branch m -> m (Branch m))
-         -> m (Branch m)
-modifyAt root path f = case Path.toList path of
-  [] -> f root
+-- Modify the branch0 at the head of at `path` with `f`,
+-- after creating it if necessary.  Preserves history.
+stepAt :: Monad m
+       => Branch m
+       -> Path
+       -> (Branch0 m -> Branch0 m)
+       -> m (Branch m)
+stepAt b path f = stepAtM b path (pure . f)
+
+-- Modify the branch0 at the head of at `path` with `f`,
+-- after creating it if necessary.  Preserves history.
+stepAtM :: Monad m
+        => Branch m
+        -> Path
+        -> (Branch0 m -> m (Branch0 m))
+        -> m (Branch m)
+stepAtM (Branch c) path f = case Path.toList path of
+  [] -> do
+    let b0 = Causal.head c
+    b0' <- f b0
+    let c' = Causal.cons b0' c
+    pure (Branch c')
   seg : path ->
-    let recurse root'@(Branch c) = do
-          b <- modifyAt root' (Path path) f
-          let h = Causal.hash (head b)
-          pure . Branch . Causal.step
-            (over children' $ Map.insert seg (h, pure b)) $ c
-    in case Map.lookup seg (_children' . head $ root) of
+    let recurse b@(Branch c) = do
+          b' <- stepAtM b (Path path) f
+          let c' = Causal.step
+               (over children $ Map.insert seg (headHash b', pure b')) c
+          pure (Branch c')
+    in case Map.lookup seg (_children $ Causal.head c) of
       Nothing -> recurse empty
       Just (_h, m) -> m >>= recurse
 
+-- Modify the Branch at `path` with `f`, after creating it if necessary.
+-- Because it's a `Branch`, it overwrites the history at `path`.
+modifyAt :: Monad m
+  => Branch m -> Path -> (Branch m -> Branch m) -> m (Branch m)
+modifyAt b path f = modifyAtM b path (pure . f)
+
+-- Modify the Branch at `path` with `f`, after creating it if necessary.
+-- Because it's a `Branch`, it overwrites the history at `path`.
+modifyAtM :: Monad m
+  => Branch m -> Path -> (Branch m -> m (Branch m)) -> m (Branch m)
+modifyAtM b path f = case Path.toList path of
+  [] -> f b
+  seg : path ->
+    let recurse b@(Branch c) = do
+          b' <- modifyAtM b (Path path) f
+          let c' = Causal.step
+               (over children $ Map.insert seg (headHash b', pure b')) c
+          pure (Branch c')
+    in case Map.lookup seg (_children $ head b) of
+        Nothing -> recurse empty
+        Just (_h, m) -> m >>= recurse
+
 instance Hashable (Branch0 m) where
   tokens b =
-    [ H.accumulateToken . R.toList $ (_terms' b)
-    , H.accumulateToken . R.toList $ (_types' b)
-    , H.accumulateToken (fst <$> _children' b)
+    [ H.accumulateToken . R.toList $ (_terms b)
+    , H.accumulateToken . R.toList $ (_types b)
+    , H.accumulateToken (fst <$> _children b)
     ]
 
 -- getLocalBranch :: Hash -> IO Branch
@@ -153,5 +211,4 @@ instance Hashable (Branch0 m) where
 
 -- makeLenses ''Namespace
 -- makeLenses ''Edits
--- makeLenses ''Branch
 -- makeLenses ''Causal
