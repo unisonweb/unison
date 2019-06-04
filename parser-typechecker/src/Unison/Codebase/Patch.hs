b1f79aee2662970e05d0ab5cb22c87fb1a7a9053
Arya Irani <arya.irani@gmail.com>
2019-06-04 13:39:38 -0400

    Patch.updateTerm

--- parser-typechecker/src/Unison/Codebase/Patch.hs
+++ parser-typechecker/src/Unison/Codebase/Patch.hs
@@ -8,7 +8,8 @@ import           Prelude                  hiding (head,read,subtract)
 import           Control.Lens            hiding ( children, cons, transform )
 --import           Control.Monad.Extra            ( whenM )
 -- import           Data.GUID                (genText)
-import           Unison.Codebase.TermEdit       ( TermEdit )
+import           Unison.Codebase.TermEdit       ( TermEdit, Typing(Same) )
+import qualified Unison.Codebase.TermEdit      as TermEdit
 import           Unison.Codebase.TypeEdit       ( TypeEdit )
 import           Unison.Hashable                ( Hashable )
 import qualified Unison.Hashable               as H
@@ -17,7 +18,7 @@ import qualified Unison.Hashable               as H
 import           Unison.Reference               ( Reference )
 --import qualified Unison.Reference             as Reference
 
---import qualified Unison.Util.Relation          as R
+import qualified Unison.Util.Relation          as R
 import           Unison.Util.Relation           ( Relation )
 
 data Patch = Patch
@@ -27,6 +28,28 @@ data Patch = Patch
 
 makeLenses ''Patch
 
+updateTerm :: (Reference -> Reference -> Typing)
+           -> Reference -> TermEdit -> Patch -> Patch
+updateTerm typing r edit p =
+  -- get D ~= lookupRan r
+  -- for each d ∈ D, remove (d, r) and add (d, r')
+  -- add (r, r') and remove (r', r')
+  let deleteCycle = case edit of
+        TermEdit.Deprecate -> id
+        TermEdit.Replace r' _ -> R.delete r' (TermEdit.Replace r' Same)
+      edits' :: Relation Reference TermEdit
+      edits' = deleteCycle
+             . R.insert r edit
+             . R.map f
+             $ _termEdits p
+      f (x, TermEdit.Replace y _) | y == r = case edit of
+        TermEdit.Replace r' _ -> (x, TermEdit.Replace r' (typing x r'))
+        TermEdit.Deprecate -> (x, TermEdit.Deprecate)
+      f p = p
+  in p { _termEdits = edits' }
+
+
+-- todo: replace with monoid for patch diff for 3-way merge
 instance Semigroup Patch where
   a <> b = Patch (_termEdits a <> _termEdits b)
                  (_typeEdits a <> _typeEdits b)
