87ce668dfce72d5c38715ab53bcfe45f23520636
Arya Irani <arya.irani@gmail.com>
2019-07-21 06:33:25 -1000

    more pending tests #586

--- parser-typechecker/tests/Unison/Test/Codebase/FileCodebase.hs
+++ parser-typechecker/tests/Unison/Test/Codebase/FileCodebase.hs
@@ -3,6 +3,8 @@ module Unison.Test.Codebase.FileCodebase where
 import EasyTest
 import Unison.Codebase.FileCodebase
 import qualified Data.Set as Set
+import qualified Unison.Lexer as L
+import Data.Foldable (toList)
 
 test :: Test ()
 test = scope "FileCodebase" . tests $
@@ -10,10 +12,21 @@ test = scope "FileCodebase" . tests $
     [ encodeDecode "abc"
     , encodeDecode "👍"
     , encodeDecode "\xfff"
-    , pending $ encodeDecode "."
     , pending $ encodeDecode ['!'..'~']
+    , pending $ specialEncode "."
+    , pending $ specialEncode ".."
+    , pending $
+        tests $ map specialEncodeChar (toList $ Set.delete '.' L.symbolyIdChars)
+    , pending $ tests $ map specialEncodeChar unsafeChars
     ]
   ]
+
+specialEncode :: String -> Test ()
+specialEncode s =
+  scope (s <> " gets special encoding") $ expect (encodeFileName s /= s)
+
+specialEncodeChar :: Char -> Test ()
+specialEncodeChar = specialEncode . pure
 
 encodeDecode :: String -> Test ()
 encodeDecode s =
@@ -24,3 +37,6 @@ encodeDecode s =
 safeChars :: Set.Set Char
 safeChars = Set.fromList $
   ['0'..'9'] ++ ['a'..'z'] ++ ['A'..'Z'] ++ "-._$"
+
+unsafeChars :: [Char]
+unsafeChars = toList $ (Set.fromList ['!'..'~'] `Set.difference` safeChars)
