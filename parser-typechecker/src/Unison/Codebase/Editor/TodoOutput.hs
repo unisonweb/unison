97fb8343821487fcf51e86ca521047e7a5be41e6
Arya Irani <arya.irani@gmail.com>
2019-07-29 15:22:08 -0400

    fix #608 by postponing the pretty-printing of todo output until OutputMessages

--- parser-typechecker/src/Unison/Codebase/Editor/TodoOutput.hs
+++ parser-typechecker/src/Unison/Codebase/Editor/TodoOutput.hs
@@ -7,7 +7,6 @@ import qualified Unison.Util.Relation as R
 import qualified Unison.Codebase.Patch as Patch
 import qualified Data.Set as Set
 import qualified Unison.DataDeclaration as DD
-import qualified Unison.HashQualified' as HQ'
 import Unison.Reference (Reference)
 import Unison.Names3 (Names0)
 import Unison.Codebase.Patch (Patch)
@@ -24,11 +23,11 @@ type Score = Int
 data TodoOutput v a = TodoOutput
   { todoScore :: Score
   , todoFrontier ::
-        ( [(HQ'.HashQualified, Reference, Maybe (Type v a))]
-        , [(HQ'.HashQualified, Reference, DisplayThing (Decl v a))])
+        ( [(Reference, Maybe (Type v a))]
+        , [(Reference, DisplayThing (Decl v a))])
   , todoFrontierDependents ::
-        ( [(Score, HQ'.HashQualified, Reference, Maybe (Type v a))]
-        , [(Score, HQ'.HashQualified, Reference, DisplayThing (Decl v a))])
+        ( [(Score, Reference, Maybe (Type v a))]
+        , [(Score, Reference, DisplayThing (Decl v a))])
   , nameConflicts :: Names0
   , editConflicts :: Patch
   } deriving (Show)
@@ -36,19 +35,19 @@ data TodoOutput v a = TodoOutput
 labeledDependencies :: Ord v => TodoOutput v a -> Set LabeledDependency
 labeledDependencies TodoOutput{..} = Set.fromList (
   -- term refs
-  [LD.termRef r | (_, r, _) <- fst todoFrontier] <>
-  [LD.termRef r | (_, _, r, _) <- fst todoFrontierDependents] <>
-  [LD.typeRef r | (_, r, _) <- snd todoFrontier] <>
-  [LD.typeRef r | (_, _, r, _) <- snd todoFrontierDependents] <>
+  [LD.termRef r | (r, _) <- fst todoFrontier] <>
+  [LD.termRef r | (_, r, _) <- fst todoFrontierDependents] <>
+  [LD.typeRef r | (r, _) <- snd todoFrontier] <>
+  [LD.typeRef r | (_, r, _) <- snd todoFrontierDependents] <>
   -- types of term refs
-  [LD.typeRef r | (_, _, Just t) <- fst todoFrontier
+  [LD.typeRef r | (_, Just t) <- fst todoFrontier
             , r <- toList (Type.dependencies t)] <>
-  [LD.typeRef r | (_, _, _, Just t) <- fst todoFrontierDependents
+  [LD.typeRef r | (_, _, Just t) <- fst todoFrontierDependents
             , r <- toList (Type.dependencies t)] <>
   -- and decls of type refs
-  [LD.typeRef r | (_, _, RegularThing d) <- snd todoFrontier
+  [LD.typeRef r | (_, RegularThing d) <- snd todoFrontier
            , r <- toList (DD.declDependencies d)] <>
-  [LD.typeRef r | (_, _, _, RegularThing d) <- snd todoFrontierDependents
+  [LD.typeRef r | (_, _, RegularThing d) <- snd todoFrontierDependents
            , r <- toList (DD.declDependencies d)]) <>
   -- name conflicts
   Set.map LD.referent (R.ran (Names.terms0 nameConflicts)) <>
