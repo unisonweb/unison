8b6552b3de48c352e86988e23957131dd35fe858
Arya Irani <arya.irani@gmail.com>
2019-08-06 17:28:09 -0400

    Fix #668, accidental infinite recursion in `Names2.hqTypeName`

--- parser-typechecker/src/Unison/Names2.hs
+++ parser-typechecker/src/Unison/Names2.hs
@@ -230,7 +230,7 @@ hqTermName b n r = if Set.size (termsNamed b n) > 1
 
 hqTypeName :: Ord n => Names' n -> n -> Reference -> HQ.HashQualified' n
 hqTypeName b n r = if Set.size (typesNamed b n) > 1
-  then hqTypeName b n r
+  then hqTypeName' b n r
   else HQ.fromName n
 
 hqTypeAliases ::
@@ -245,6 +245,10 @@ hqTermAliases b n r = Set.map (flip (hqTermName b) r) (termAliases b n r)
 hqTermName' :: Names' n -> n -> Referent -> HQ.HashQualified' n
 hqTermName' b n r =
   HQ.take (numHashChars b) $ HQ.fromNamedReferent n r
+
+hqTypeName' :: Names' n -> n -> Reference -> HQ.HashQualified' n
+hqTypeName' b n r =
+  HQ.take (numHashChars b) $ HQ.fromNamedReference n r
 
 fromTerms :: Ord n => [(n, Referent)] -> Names' n
 fromTerms ts = Names (R.fromList ts) mempty
