8d5429bb86a163da583763199d9868138a47e5ed
SunriseM <sunrisem.js@gmail.com>
2019-10-03 18:42:39 -0400

    Update Input.hs

--- parser-typechecker/src/Unison/Codebase/Editor/Input.hs
+++ parser-typechecker/src/Unison/Codebase/Editor/Input.hs
@@ -103,7 +103,7 @@ data Input
   | LinksI Path.HQSplit' (Maybe String)
   -- other
   | SearchByNameI Bool Bool [String] -- SearchByName isVerbose showAll query
-  | FindShallowl Path'
+  | FindShallowL Path'
   | FindPatchI
   | ShowDefinitionI OutputLocation [String]
   | ShowDefinitionByPrefixI OutputLocation [String]
