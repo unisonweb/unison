00ce1a06ca23a0e52d14a521b50ccc0e7e7f8ef3
Paul Chiusano <paul.chiusano@gmail.com>
2019-10-28 21:05:49 -0400

    nice error message if type/term can't be resolved

--- parser-typechecker/src/Unison/PrintError.hs
+++ parser-typechecker/src/Unison/PrintError.hs
@@ -1036,16 +1036,30 @@ prettyParseError s = \case
     "I expected a non-empty watch expression and not just \">\""
   go (Parser.UnknownAbilityConstructor tok _referents) = unknownConstructor "ability" tok
   go (Parser.UnknownDataConstructor    tok _referents) = unknownConstructor "data" tok
-  go (Parser.UnknownTerm               tok _referents) = mconcat
-    [ "I couldn't find a term for "
+  go (Parser.UnknownTerm               tok referents) = Pr.lines
+    [ if Set.null referents then 
+        "I couldn't find a term for " <> style ErrorSite (HQ.toString (L.payload tok)) <> "."
+      else
+        "The term reference " <> style ErrorSite (HQ.toString (L.payload tok)) <> " was ambiguous."
+    , ""
     , tokenAsErrorSite s $ HQ.toString <$> tok
-    , ". Make sure it's spelled correctly and that you have the right hash."
+    , if missing then "Make sure it's spelled correctly."
+      else "Try hash-qualifying the term you meant to reference."
     ]
-  go (Parser.UnknownType               tok _referents) = mconcat
-    [ "I couldn't find a type for "
+    where
+    missing = Set.null referents
+  go (Parser.UnknownType               tok referents) = Pr.lines
+    [ if Set.null referents then 
+        "I couldn't find a type for " <> style ErrorSite (HQ.toString (L.payload tok)) <> "."
+      else
+        "The type reference " <> style ErrorSite (HQ.toString (L.payload tok)) <> " was ambiguous."
+    , ""
     , tokenAsErrorSite s $ HQ.toString <$> tok
-    , ". Make sure it's spelled correctly and that you have the right hash."
+    , if missing then "Make sure it's spelled correctly."
+      else "Try hash-qualifying the type you meant to reference."
     ]
+    where
+    missing = Set.null referents
   go (Parser.ResolutionFailures        failures) =
     Pr.border 2 . prettyResolutionFailures s $ failures
   unknownConstructor
