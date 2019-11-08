8d7da9ef7d6dec256e67b7899eba542dc205a238
Arya Irani <arya.irani@gmail.com>
2019-11-08 09:51:29 -0500

    delete UriParser.webRepoParser in favor of autocompletes

--- parser-typechecker/src/Unison/Codebase/Editor/UriParser.hs
+++ parser-typechecker/src/Unison/Codebase/Editor/UriParser.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-unused-local-binds -Wno-unused-imports #-}
 
-module Unison.Codebase.Editor.UriParser (repoPath, webRepoPath) where
+module Unison.Codebase.Editor.UriParser (repoPath) where
 
 import qualified Text.Megaparsec as P
 import qualified Text.Megaparsec.Char.Lexer as L
@@ -12,8 +12,6 @@ import Data.Text (Text)
 import Unison.Codebase.Path (Path(..))
 import qualified Unison.Codebase.Path as Path
 import Unison.Codebase.Editor.RemoteRepo (RemoteRepo(..))
-import Unison.Codebase.Editor.EncodedRepo (EncodedRepo(..))
-import qualified Unison.Codebase.Editor.EncodedRepo as ER
 import Unison.Codebase.ShortBranchHash (ShortBranchHash(..))
 import Unison.Prelude
 import qualified Unison.Hash as Hash
@@ -24,41 +22,29 @@ import Data.Char (isAlphaNum, isSpace, isDigit)
 
 type P = P.Parsec () Text
 
---Sidebar on Unison URLs:
---
---Frustratingly, git branches may include /, ., #, =, etc. which make them hard or impossible to parse a branch name among /-separated elements (as is needed to parse branches out of Github urls) without using IO to check whether a given parse corresponds to an actual branch. At least : isn't allowed in a branch name, though it is unfortunately allowed in scp-style git repo addresses (e.g. git@github.com:foo/bar). Saving grace: Github doesn't allow # or : or / in repo names, so that's something.
---
---gh:<user>/<repo>[/<treeish>][:[<nshash>[.<path>] | [.]<path>]]
---
---Examples:
---
---gh:aryairani/unisonbase
---gh:aryairani/unisonbase:libs.v1
---gh:aryairani/unisonbase:#abc123sbh
---gh:aryairani/unisonbase/mybranch
---gh:aryairani/unisonbase:#abc123sbh.path
---gh:aryairani/unisonbase/dev
---gh:aryairani/unisonbase/dev:mybranch
+-- Here are the git protocols that we know how to parse
+-- Local Protocol
+-- $ git clone /srv/git/project.git
+-- $ git clone /srv/git/project.git[:treeish][:[#hash][.path]]
+-- File Protocol
+-- $ git clone file:///srv/git/project.git[:treeish][:[#hash][.path]]
+-- Smart / Dumb HTTP protocol
+-- $ git clone https://example.com/gitproject.git[:treeish][:[#hash][.path]]
+-- SSH Protocol
+-- $ git clone ssh://[user@]server/project.git[:treeish][:[#hash][.path]]
+-- $ git clone [user@]server:project.git[:treeish][:[#hash][.path]]
+-- Git Protocol (obsolete)
+repoPath :: P (RemoteRepo, Maybe ShortBranchHash, Path)
+repoPath = P.label "generic git repo" $ do
+  protocol <- parseProtocol
+  treeish <- P.optional treeishSuffix
+  let repo = GitRepo (printProtocol protocol) treeish
+  nshashPath <- P.optional (C.char ':' *> namespaceHashPath)
+  case nshashPath of
+    Nothing -> pure (repo, Nothing, Path.empty)
+    Just (sbh, p) -> pure (repo, sbh, p)
 
--- colon alternative:
---gh:<user>/<repo>[:<treeish>][':' ( '#' <nshash> ['.' <path>] | '.' <path> )]
-
---gh:aryairani/unisonbase
---gh:aryairani/unisonbase:.libs.v1
---gh:aryairani/unisonbase:#abc123sbh
---gh:aryairani/unisonbase:mybranch
---gh:aryairani/unisonbase:#abc123sbh.path
---gh:aryairani/unisonbase:dev
---gh:aryairani/unisonbase:dev:.mybranch
-
---gh:<user>/<repo>[/<treeish>]:namespace:[<nshash>.<path>|<path>]]
---gh:<user>/<repo>[/<treeish>]:term:[<displayname>#]<hash>
---gh:<user>/<repo>[/<treeish>]:type:[<displayname>#]<hash>
-
---type Repo = GitCommit { url :: Text, commit :: Text }
-
--- gh:<user>/<repo>[/<treeish]
-
+-- does this not exist somewhere in megaparsec? yes in 7.0
 symbol :: Text -> P Text
 symbol = L.symbol (pure ())
 
@@ -153,37 +139,6 @@ parseProtocol = P.label "parseProtocol" $
       pure $ Text.pack $ o1 <> "." <> o2 <> "." <> o3 <> "." <> o4
     decOctet = P.count' 1 3 C.digitChar
 
-repoPath :: P (RemoteRepo, Maybe ShortBranchHash, Path)
-repoPath = P.label "generic git repo" $ do
-  protocol <- parseProtocol
-  treeish <- P.optional treeishSuffix
-  let repo = GitRepo (printProtocol protocol) treeish
-  nshashPath <- P.optional (C.char ':' *> namespaceHashPath)
-  case nshashPath of
-    Nothing -> pure (repo, Nothing, Path.empty)
-    Just (sbh, p) -> pure (repo, sbh, p)
-
--- Local Protocol
--- $ git clone /srv/git/project.git
--- $ git clone /srv/git/project.git[:treeish][:[#hash][.path]]
--- File Protocol
--- $ git clone file:///srv/git/project.git[:treeish][:[#hash][.path]] <- imagined
--- Smart / Dumb HTTP protocol
--- $ git clone https://example.com/gitproject.git[:treeish][:[#hash][.path]] <- imagined
--- SSH Protocol
--- $ git clone ssh://[user@]server/project.git[:treeish][:[#hash][.path]]
--- $ git clone [user@]server:project.git[:treeish][:[#hash][.path]]
--- Git Protocol (obsolete)
-
-
-webRepoPath :: P (EncodedRepo, Maybe ShortBranchHash, Path)
-webRepoPath = P.label "hosted git repo path" $ do
-  repo <- webRepoTreeish
-  nshashPath <- P.optional (C.char ':' *> namespaceHashPath)
-  case nshashPath of
-    Nothing -> pure (repo, Nothing, Path.empty)
-    Just (sbh, p) -> pure (repo, sbh, p)
-
 -- #nshashabc.path.foo.bar or .path.foo.bar
 namespaceHashPath :: P (Maybe ShortBranchHash, Path)
 namespaceHashPath = do
@@ -199,42 +154,12 @@ namespaceHashPath = do
     Just p  -> pure (sbh, makePath p)
   where makePath = Path . Seq.fromList . fmap (NameSegment . Text.pack)
 
--- double-check me
 treeishSuffix :: P Text
 treeishSuffix = P.label "git treeish" . P.try $ do
   void $ C.char ':'
   notdothash <- C.noneOf @[] ".#:"
   rest <- P.takeWhileP (Just "not colon") (/= ':')
   pure $ Text.cons notdothash rest
-
-webRepoTreeish :: P EncodedRepo
-webRepoTreeish = P.label "hosted git repo + treeish" $ do
-  svc <- service
-  void $ C.char ':'
-  user <- gitwebUser
-  void $ C.char '/'
-  repo <- gitwebSlug
-  treeish <- P.optional treeishSuffix
-  pure $ ER.UserSlugRepo svc user repo treeish
-  where
-  service = ((symbol "gh" <|> symbol "github") $> ER.Github)
-        <|> ((symbol "gl" <|> symbol "gitlab") $> ER.Gitlab)
-        <|> ((symbol "bb" <|> symbol "bitbucket") $> ER.Bitbucket)
-  -- Github: "Username may only contain alphanumeric characters or single hyphens,
-  --          and cannot begin or end with a hyphen."
-  -- Gitlab: [a-zA-Z0-9_\.][a-zA-Z0-9_\-\.]*[a-zA-Z0-9_\-]|[a-zA-Z0-9_]
-  gitwebUser:: P Text
-  gitwebUser =
-    -- permissive approximation
-    P.takeWhile1P (Just "web username character")
-      (\c -> isAlphaNum c || elem @[] c "_-.")
-
-  -- Gitlab: "Path can contain only letters, digits, '_', '-' and '.'.
-  --          Cannot start with '-', end in '.git' or end in '.atom'"
-  gitwebSlug = gitwebUser
-
--- <nshash>[.<path>] | [.]<path>
---remoteNameSpace ::
 
 shortBranchHash :: P ShortBranchHash
 shortBranchHash = P.label "short branch hash" $ do
--- parser-typechecker/tests/Unison/Test/UriParser.hs
+++ parser-typechecker/tests/Unison/Test/UriParser.hs
@@ -4,7 +4,6 @@ module Unison.Test.UriParser where
 
 import EasyTest
 import Unison.Codebase.Editor.RemoteRepo (RemoteRepo(..))
-import Unison.Codebase.Editor.EncodedRepo (EncodedRepo(..), UserSlugService(..))
 import Unison.Codebase.Path (Path(..))
 import qualified Unison.Codebase.Path as Path
 import qualified Text.Megaparsec as P
@@ -16,7 +15,7 @@ import Unison.Codebase.NameSegment (NameSegment(..))
 import qualified Data.Text as Text
 
 test :: Test ()
-test = scope "uriparser" . tests $ [ testEncoded, testAugmented ]
+test = scope "uriparser" . tests $ [ testAugmented ]
 
 testAugmented:: Test ()
 testAugmented = scope "augmented" . tests $
@@ -32,7 +31,7 @@ testAugmented = scope "augmented" . tests $
       (GitRepo "srv/git/project.git" Nothing, Nothing, Path.empty))
     , ("srv/git/project.git:abc:#def.hij.klm",
       (GitRepo "srv/git/project.git" (Just "abc"), sbh "def", path ["hij", "klm"]))
-    ], 
+    ],
 -- File Protocol
 -- $ git clone file:///srv/git/project.git[:treeish][:[#hash][.path]] <- imagined
     scope "file-protocol" . tests . map parseAugmented $
@@ -44,7 +43,7 @@ testAugmented = scope "augmented" . tests $
       (GitRepo "file://srv/git/project.git" Nothing, Nothing, Path.empty))
     , ("file://srv/git/project.git:abc:#def.hij.klm",
       (GitRepo "file://srv/git/project.git" (Just "abc"), sbh "def", path ["hij", "klm"]))
-    ],    
+    ],
 -- Smart / Dumb HTTP protocol
 -- $ git clone https://example.com/gitproject.git[:treeish][:[#hash][.path]] <- imagined
     scope "http-protocol" . tests . map parseAugmented $
@@ -72,40 +71,14 @@ testAugmented = scope "augmented" . tests $
     ]
   ]
 
-testEncoded :: Test ()
-testEncoded = scope "encoded" . tests . map parseEncoded $
-  [ ("gh:aryairani/unisonbase",
-      (UserSlugRepo Github "aryairani" "unisonbase" Nothing, Nothing, Path.empty)),
-    ("gl:aryairani/unisonbase:.libs.v1",
-      (UserSlugRepo Gitlab "aryairani" "unisonbase" Nothing, Nothing, path ["libs", "v1"])),
-    ("bb:aryairani/unisonbase:#abc123sbh",
-      (UserSlugRepo Bitbucket "aryairani" "unisonbase" Nothing, sbh "abc123sbh", Path.empty)),
-    ("github:aryairani/unisonbase:.mybranch",
-      (UserSlugRepo Github "aryairani" "unisonbase" Nothing, Nothing, path ["mybranch"])),
-    ("gitlab:aryairani/unisonbase:#abc123sbh.path",
-      (UserSlugRepo Gitlab "aryairani" "unisonbase" Nothing, sbh "abc123sbh", path ["path"])),
-    ("bitbucket:aryairani/unisonbase:dev",
-      (UserSlugRepo Bitbucket "aryairani" "unisonbase" (Just "dev"), Nothing, Path.empty)),
-    ("gh:aryairani/unisonbase:dev:.mybranch",
-      (UserSlugRepo Github "aryairani" "unisonbase" (Just "dev"), Nothing, path ["mybranch"])),
-    ("gh:aryairani/unisonbase:dev:#abc123sbh.path",
-      (UserSlugRepo Github "aryairani" "unisonbase" (Just "dev"), sbh "abc123sbh", path ["path"]))
-  ]
-
-parseEncoded :: (Text, (EncodedRepo, Maybe ShortBranchHash, Path)) -> Test ()
-parseEncoded (s, r) = scope (Text.unpack s) $ 
-  case P.parse UriParser.webRepoPath "test case" s of
-    Left x -> crash $ show x
-    Right x -> expectEqual x r
-    
 parseAugmented :: (Text, (RemoteRepo, Maybe ShortBranchHash, Path)) -> Test ()
-parseAugmented (s, r) = scope (Text.unpack s) $ 
+parseAugmented (s, r) = scope (Text.unpack s) $
   case P.parse UriParser.repoPath "test case" s of
     Left x -> crash $ show x
     Right x -> expectEqual x r
 
 path :: [Text] -> Path
-path = Path . Seq.fromList . fmap NameSegment 
+path = Path . Seq.fromList . fmap NameSegment
 
 sbh :: Text -> Maybe ShortBranchHash
 sbh = Just . ShortBranchHash
