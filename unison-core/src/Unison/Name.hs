2a4abced527bbdf4c06ba670681eccafc43e08a6
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-12-26 14:43:29 -0500

    Fill in a few name functions

--- unison-core/src/Unison/Name.hs
+++ unison-core/src/Unison/Name.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE BlockArguments      #-}
 {-# LANGUAGE OverloadedStrings   #-}
+
+{-# OPTIONS_GHC -Wwarn #-} -- temporary
 
 module Unison.Name
   ( Name
@@ -8,18 +11,14 @@ module Unison.Name
   , joinDot
   , makeAbsolute
   , parent
-  , segments
-  , sortNames
   , sortNamed
   , sortNamed'
   , stripNamePrefix
-  , stripPrefixes
   , suffixes
   , toString
   , toText
   , toVar
   , unqualified
-  , unqualified'
   , unsafeFromText
   , unsafeFromString
   , fromVar
@@ -29,9 +28,11 @@ where
 import Unison.Prelude
 
 import           Control.Lens                   ( unsnoc )
-import           Data.List.NonEmpty             ( NonEmpty )
+import qualified Data.List                     as List
+import           Data.List.NonEmpty             ( NonEmpty((:|)) )
+import qualified Data.List.NonEmpty            as NonEmpty
 import qualified Data.Text                     as Text
-import           Unison.Codebase.NameSegment    ( NameSegment(NameSegment) )
+import           Unison.Codebase.NameSegment    ( NameSegment )
 import qualified Unison.Codebase.NameSegment   as NameSegment
 import qualified Unison.Hashable               as H
 import           Unison.Var                     ( Var )
@@ -46,15 +47,11 @@ data Name
 
 toText :: Name -> Text
 toText (Name name) = name
+toText (Name' names) =
+  Text.intercalate "." (map NameSegment.toText (toList names))
 
 fromNameSegment :: NameSegment -> Name
 fromNameSegment = unsafeFromText . NameSegment.toText
-
-segments :: Name -> [NameSegment]
-segments name = NameSegment <$> Text.splitOn "." (toText name)
-
-sortNames :: [Name] -> [Name]
-sortNames = sortNamed id
 
 sortNamed :: (a -> Name) -> [a] -> [a]
 sortNamed by as = let
@@ -87,15 +84,8 @@ toString :: Name -> String
 toString = Text.unpack . toText
 
 isPrefixOf :: Name -> Name -> Bool
-a `isPrefixOf` b = toText a `Text.isPrefixOf` toText b
-
--- stripTextPrefix a.b. a.b.c = Just c
--- stripTextPrefix a.b  a.b.c = Just .c;  you probably don't want to do this
--- stripTextPrefix x.y. a.b.c = Nothing
--- stripTextPrefix "" a.b.c = undefined
-_stripTextPrefix :: Text -> Name -> Maybe Name
-_stripTextPrefix prefix name =
-  Name <$> Text.stripPrefix prefix (toText name)
+Name a `isPrefixOf` Name b = a `Text.isPrefixOf` b
+Name' a `isPrefixOf` Name' b = toList a `NonEmpty.isPrefixOf` b
 
 -- stripNamePrefix a.b  a.b.c = Just c
 -- stripNamePrefix a.b. a.b.c = undefined, "a.b." isn't a valid name IMO
@@ -103,22 +93,24 @@ _stripTextPrefix prefix name =
 -- stripNamePrefix "" a.b.c = undefined, "" isn't a valid name IMO
 -- stripNamePrefix . .Nat = Just Nat
 stripNamePrefix :: Name -> Name -> Maybe Name
-stripNamePrefix prefix name =
+stripNamePrefix prefix@Name{} name@Name{} =
   Name <$> Text.stripPrefix (toText prefix <> mid) (toText name)
   where
   mid = if toText prefix == "." then "" else "."
-
--- a.b.c.d -> d
-stripPrefixes :: Name -> Name
-stripPrefixes = unsafeFromText . last . Text.splitOn "." . toText
+stripNamePrefix (Name' prefix) (Name' name) =
+  fmap Name' do
+    suffix <- List.stripPrefix (toList prefix) (toList name)
+    NonEmpty.nonEmpty suffix
 
 joinDot :: Name -> Name -> Name
-joinDot prefix suffix =
+joinDot prefix@Name{} suffix@Name{} =
   if toText prefix == "." then Name (toText prefix <> toText suffix)
   else Name (toText prefix <> "." <> toText suffix)
+joinDot (Name' prefix) (Name' suffix) = Name' (prefix <> suffix)
 
 unqualified :: Name -> Name
-unqualified = unsafeFromText . unqualified' . toText
+unqualified (Name name) = unsafeFromText (last (Text.splitOn "." name))
+unqualified (Name' names) = Name' (NonEmpty.last names :| [])
 
 -- parent . -> Nothing
 -- parent + -> Nothing
@@ -130,15 +122,16 @@ parent (Name txt) = case unsnoc (Text.splitOn "." txt) of
   Nothing -> Nothing
   Just ([],_) -> Nothing
   Just (init,_) -> Just $ Name (Text.intercalate "." init)
+parent (Name' names) =
+  Name' <$> NonEmpty.nonEmpty (NonEmpty.init names)
 
 suffixes :: Name -> [Name]
 suffixes (Name n) =
   fmap up . tails . dropWhile (== "") $ Text.splitOn "." n
   where
   up ns = Name (Text.intercalate "." ns)
-
-unqualified' :: Text -> Text
-unqualified' = last . Text.splitOn "."
+suffixes (Name' names) =
+  map (Name' . NonEmpty.fromList) (NonEmpty.init (NonEmpty.tails names))
 
 makeAbsolute :: Name -> Name
 makeAbsolute n | toText n == "."                = Name ".."
