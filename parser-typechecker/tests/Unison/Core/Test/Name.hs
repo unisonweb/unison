52965d2ddf6bfb81e23c67a38360abbf99907936
pete-ts <pete_t@tutanota.com>
2020-03-19 20:05:14 +0000

    suffixes no longer adds "" at the end of the list

--- parser-typechecker/tests/Unison/Core/Test/Name.hs
+++ parser-typechecker/tests/Unison/Core/Test/Name.hs
@@ -6,14 +6,14 @@ import           EasyTest
 import           Unison.Name                   as Name
 
 test :: Test ()
-test = scope "name" $ tests [ 
-  scope "suffixes" $ 
+test = scope "name" $ tests [
+  scope "suffixes" $
     tests
-      [ scope "empty" $ expectEqual (suffixes "") [""]
-      , scope "one namespace" $ expectEqual (suffixes "bar") ["bar", ""]
+      [ scope "empty" $ expectEqual (suffixes "") []
+      , scope "one namespace" $ expectEqual (suffixes "bar") ["bar"]
       , scope "two namespaces"
-        $ expectEqual (suffixes "foo.bar") ["foo.bar", "bar", ""]
-      , scope "multiple namespaces" 
-        $ expectEqual (suffixes "foo.bar.baz") ["foo.bar.baz", "bar.baz", "baz", ""]
+        $ expectEqual (suffixes "foo.bar") ["foo.bar", "bar"]
+      , scope "multiple namespaces"
+        $ expectEqual (suffixes "foo.bar.baz") ["foo.bar.baz", "bar.baz", "baz"]
       ]
   ]
