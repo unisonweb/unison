dcd0d331f3066f2dc9b8fc95f77e8d6a4d83f0ae
Paul Chiusano <paul.chiusano@gmail.com>
2020-03-31 20:32:11 -0400

    Added unit test which fails without this fix

--- parser-typechecker/tests/Unison/Test/ABT.hs
+++ parser-typechecker/tests/Unison/Test/ABT.hs
@@ -7,6 +7,8 @@ import EasyTest
 import Unison.ABT as ABT
 import Unison.Symbol (Symbol(..))
 import Unison.Var as Var
+import           Unison.Codebase.Serialization    ( getFromBytes, putBytes )
+import qualified Unison.Codebase.Serialization.V1 as V1
 
 test :: Test ()
 test = scope "abt" $ tests [
@@ -20,5 +22,11 @@ test = scope "abt" $ tests [
     in tests
       [ scope "first"  $ expect (not $ Set.member fresh (ABT.freeVars t1))
       , scope "second" $ expect (not $ Set.member fresh (ABT.freeVars t2))
-      ]
+      ],
+  -- confirmation of fix for https://github.com/unisonweb/unison/issues/1388
+  -- where symbols with nonzero freshIds did not round trip
+  scope "putSymbol" $ let
+    v = Symbol 10 (User "hi")
+    v' = getFromBytes V1.getSymbol (putBytes V1.putSymbol v)
+    in expectEqual (Just v) v'
   ]
