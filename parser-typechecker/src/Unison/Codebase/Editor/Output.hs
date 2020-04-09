3042a36500cede0395bf6a709e37e4700a1e6a3e
pete-ts <pete_t@tutanota.com>
2020-04-09 22:47:34 +0100

    Adds the LinkDefnsNotFound constructor and relevant output messages

--- parser-typechecker/src/Unison/Codebase/Editor/Output.hs
+++ parser-typechecker/src/Unison/Codebase/Editor/Output.hs
@@ -174,6 +174,7 @@ data Output v
   | DisplayLinks PPE.PrettyPrintEnvDecl Metadata.Metadata
                (Map Reference (DisplayThing (Decl v Ann)))
                (Map Reference (DisplayThing (Term v Ann)))
+  | LinkDefnsNotFound [Path']
   | MetadataMissingType PPE.PrettyPrintEnv Referent
   | MetadataNotFound (Maybe Name)
   | MetadataAmbiguous PPE.PrettyPrintEnv [Referent]
@@ -304,6 +305,7 @@ isFailure o = case o of
   ConfiguredGitUrlParseError{} -> True
   ConfiguredGitUrlIncludesShortBranchHash{} -> True
   DisplayLinks{} -> False
+  LinkDefnsNotFound{} -> True
   MetadataMissingType{} -> True
   MetadataNotFound{} -> True
   MetadataAmbiguous{} -> True
