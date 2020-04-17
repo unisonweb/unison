3bfe9ee3f383b5fc9720e17a2210eaab0b610b6f
pete-ts <pete_t@tutanota.com>
2020-04-17 19:02:52 +0100

    Formats Help

--- parser-typechecker/src/Unison/Codebase/Editor/Help.hs
+++ parser-typechecker/src/Unison/Codebase/Editor/Help.hs
@@ -2,30 +2,38 @@
 
 module Unison.Codebase.Editor.Help where
 
-import Unison.Prelude
+import           Unison.Prelude
 
-import           Data.List (intercalate, find, sortOn)
-import qualified Unison.CommandLine.InputPattern as I
-import qualified Unison.CommandLine.InputPatterns as IP
-import qualified Unison.Util.ColorText as CT
-import qualified Unison.Util.Pretty as P
-import           Unison.Util.Monoid (intercalateMap)
+import           Data.List                      ( intercalate
+                                                , find
+                                                , sortOn
+                                                )
+import qualified Unison.CommandLine.InputPattern
+                                               as I
+import qualified Unison.CommandLine.InputPatterns
+                                               as IP
+import qualified Unison.Util.ColorText         as CT
+import qualified Unison.Util.Pretty            as P
+import           Unison.Util.Monoid             ( intercalateMap )
 
 help :: P.Pretty CT.ColorText
-help = intercalateMap "\n\n" showPatternHelp (sortOn I.patternName IP.validInputs)
+help =
+  intercalateMap "\n\n" showPatternHelp (sortOn I.patternName IP.validInputs)
 
 helpFor :: String -> P.Pretty CT.ColorText
 helpFor command = case findByName command of
   Just pat -> showPatternHelp pat
-  Nothing -> help
+  Nothing  -> help
 
 findByName :: String -> Maybe I.InputPattern
-findByName c = find (\pattern -> (I.patternName pattern) == c) IP.validInputs
+findByName c = find (\p -> c == I.patternName p) IP.validInputs
 
 showPatternHelp :: I.InputPattern -> P.Pretty CT.ColorText
-showPatternHelp i = P.lines [
-  P.bold (fromString $ I.patternName i) <> fromString
+showPatternHelp i = P.lines
+  [ P.bold (fromString $ I.patternName i) <> fromString
     (if not . null $ I.aliases i
-     then " (or " <> intercalate ", " (I.aliases i) <> ")"
-     else ""),
-  P.wrap $ I.help i ]
+      then " (or " <> intercalate ", " (I.aliases i) <> ")"
+      else ""
+    )
+  , P.wrap $ I.help i
+  ]
