b28308f02a188424e23cf4b2f020ccdee71a4cfa
Runar Bjarnason <runarorama@gmail.com>
2020-04-20 15:30:39 -0400

    Make "no-op" message less confusing

--- parser-typechecker/src/Unison/CommandLine/OutputMessages.hs
+++ parser-typechecker/src/Unison/CommandLine/OutputMessages.hs
@@ -966,7 +966,7 @@ notifyUser dir o = case o of
   DumpNumberedArgs args -> pure . P.numberedList $ fmap P.string args
   NoConflictsOrEdits ->
     pure (P.okCallout "No conflicts or edits in progress.")
-  NoOp -> pure $ P.okCallout "That didn't do anything."
+  NoOp -> pure $ P.string "I didn't make any changes."
   DefaultMetadataNotification -> pure $ P.wrap "Adding default metadata..."
   DumpBitBooster head map -> let
     go output []          = output
