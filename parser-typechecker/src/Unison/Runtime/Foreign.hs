0b1c3aa3f4ea047735c5d8065276a598c7a4c189
Dan Doel <dan.doel@gmail.com>
2020-05-12 16:41:13 -0400

    Flesh out foreign module with maybe calling convention

--- parser-typechecker/src/Unison/Runtime/Foreign.hs
+++ parser-typechecker/src/Unison/Runtime/Foreign.hs
@@ -1,5 +1,6 @@
 {-# language GADTs #-}
 {-# language BangPatterns #-}
+{-# language PatternGuards #-}
 
 module Unison.Runtime.Foreign
   ( Foreign(..)
@@ -11,6 +12,9 @@ module Unison.Runtime.Foreign
   , foreign1
   , foreign2
   , foreign3
+  , foreign1m1
+  , foreign1m2
+  , foreign1m3
   ) where
 
 import Unsafe.Coerce
@@ -38,7 +42,7 @@ foreign0 e = FF $ \[] -> e
 foreign1 :: (a -> IO [Either Int Foreign]) -> ForeignFunc
 foreign1 f = FF $ \[x] -> f $ unwrapForeign x
 
-foreign2 :: (a ->b -> IO [Either Int Foreign]) -> ForeignFunc
+foreign2 :: (a -> b -> IO [Either Int Foreign]) -> ForeignFunc
 foreign2 f = FF $ \[x,y] -> f (unwrapForeign x) (unwrapForeign y)
 
 foreign3 :: (a -> b -> c -> IO [Either Int Foreign]) -> ForeignFunc
@@ -46,3 +50,35 @@ foreign3 f
    = FF $ \[x,y,z]
   -> f (unwrapForeign x) (unwrapForeign y) (unwrapForeign z)
 
+foreign1m1 :: (Maybe a -> IO [Either Int Foreign]) -> ForeignFunc
+foreign1m1 f = FF $ \case
+  t:xs
+    | 0 <- unwrapForeign t
+    -> f Nothing
+    | 1 <- unwrapForeign t
+    , [x] <- xs
+    -> f (Just $ unwrapForeign x)
+  _ -> error "mismatched foreign calling convention"
+
+foreign1m2 :: (Maybe a -> b -> IO [Either Int Foreign]) -> ForeignFunc
+foreign1m2 f = FF $ \case
+  t:xs
+    | 0 <- unwrapForeign t
+    , [y] <- xs
+    -> f Nothing (unwrapForeign y)
+    | 1 <- unwrapForeign t
+    , [x,y] <- xs
+    -> f (Just $ unwrapForeign x) (unwrapForeign y)
+  _ -> error "mismatched foreign calling convention"
+
+foreign1m3
+  :: (Maybe a -> b -> c -> IO [Either Int Foreign]) -> ForeignFunc
+foreign1m3 f = FF $ \case
+  t:xs
+    | 0 <- unwrapForeign t
+    , [y,z] <- xs
+    -> f Nothing (unwrapForeign y) (unwrapForeign z)
+    | 1 <- unwrapForeign t
+    , [x,y,z] <- xs
+    -> f (Just $ unwrapForeign x) (unwrapForeign y) (unwrapForeign z)
+  _ -> error "mismatched foreign calling convention"
