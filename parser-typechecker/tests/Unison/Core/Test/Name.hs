901032cc276d9eaff031f104f3ee4f5866c11400
Runar Bjarnason <runarorama@gmail.com>
2020-05-28 20:53:41 -0400

    Added a test

--- parser-typechecker/tests/Unison/Core/Test/Name.hs
+++ parser-typechecker/tests/Unison/Core/Test/Name.hs
@@ -4,18 +4,25 @@ module Unison.Core.Test.Name where
 
 import           EasyTest
 import           Unison.Name                   as Name
+import           Data.List                      ( intercalate )
+import           Data.Text                      ( pack )
 
 test :: Test ()
-test = scope "name" $ tests [
-  scope "suffixes" $
-    tests
-      [ scope "empty" $ expectEqual (suffixes "") []
-      , scope "one namespace" $ expectEqual (suffixes "bar") ["bar"]
-      , scope "two namespaces"
-        $ expectEqual (suffixes "foo.bar") ["foo.bar", "bar"]
-      , scope "multiple namespaces"
-        $ expectEqual (suffixes "foo.bar.baz") ["foo.bar.baz", "bar.baz", "baz"]
-      , scope "terms named `.`"
-        $ expectEqual (suffixes "base..") ["base..", "."]
-      ]
+test = scope "name" $ tests
+  [ scope "suffixes" $ tests
+    [ scope "empty" $ expectEqual (suffixes "") []
+    , scope "one namespace" $ expectEqual (suffixes "bar") ["bar"]
+    , scope "two namespaces"
+      $ expectEqual (suffixes "foo.bar") ["foo.bar", "bar"]
+    , scope "multiple namespaces"
+      $ expectEqual (suffixes "foo.bar.baz") ["foo.bar.baz", "bar.baz", "baz"]
+    , scope "terms named `.`" $ expectEqual (suffixes "base..") ["base..", "."]
+    ]
+  , scope "segments" $ do
+    numDots <- int' 0 10
+    numSegs <- int' 0 10
+    n       <- int' 0 10
+    segs <- listOf n . pick $ replicate numDots "." ++ replicate numSegs "foo"
+    expectEqual (segments $ Name . pack $ intercalate "." segs)
+                (Name . pack <$> segs)
   ]
