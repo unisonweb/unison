bc5052382710e128471a2a044f4eca3e6e3ba0bb
Runar Bjarnason <runarorama@gmail.com>
2020-06-26 14:43:27 -0400

    Delete dead code

--- parser-typechecker/src/Unison/Builtin/Decls.hs
+++ parser-typechecker/src/Unison/Builtin/Decls.hs
@@ -67,11 +67,6 @@ okConstructorReferent, failConstructorReferent :: Referent.Referent
 okConstructorReferent = Referent.Con testResultRef okConstructorId CT.Data
 failConstructorReferent = Referent.Con testResultRef failConstructorId CT.Data
 
-failResult :: (Ord v, Monoid a) => a -> Text -> Term v a
-failResult ann msg =
-  Term.app ann (Term.constructor ann testResultRef failConstructorId)
-               (Term.text ann msg)
-
 -- | parse some builtin data types, and resolve their free variables using
 -- | builtinTypes' and those types defined herein
 builtinDataDecls :: Var v => [(v, Reference.Id, DataDeclaration' v ())]
