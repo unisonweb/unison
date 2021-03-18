9ceead2d6ca73fbacbdd4263091f64b79b9bd044
Dan Doel <dan.doel@gmail.com>
2021-03-18 17:07:52 -0400

    Improve the error message for bug/todo in the new runtime

--- parser-typechecker/src/Unison/Runtime/Exception.hs
+++ parser-typechecker/src/Unison/Runtime/Exception.hs
@@ -3,13 +3,14 @@ module Unison.Runtime.Exception where
 
 import Control.Exception
 import Data.String (fromString)
+import Data.Text
 
 import Unison.Runtime.Stack
 import Unison.Util.Pretty as P
 
 data RuntimeExn
   = PE (P.Pretty P.ColorText)
-  | BU Closure
+  | BU Text Closure
   deriving (Show)
 instance Exception RuntimeExn
 
