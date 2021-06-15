e94223073a8e54465dfb5b50d827f8803ddbd865
Paul Chiusano <paul.chiusano@gmail.com>
2021-06-15 11:53:57 -0500

    Filled in `Unison.Server.Doc`, sans json-ification

--- parser-typechecker/src/Unison/Server/Doc.hs
+++ parser-typechecker/src/Unison/Server/Doc.hs
@@ -1,3 +1,53 @@
 module Unison.Server.Doc where
 
-type Doc = Int
+import Data.Text (Text)
+import Data.Word
+import Unison.Codebase.Editor.DisplayObject (DisplayObject)
+import Unison.Server.Syntax (SyntaxText)
+
+type Nat = Word64
+
+data Doc
+  = Word Text
+  | Code Doc
+  | CodeBlock Text Doc
+  | Bold Doc
+  | Italic Doc
+  | Strikethrough Doc
+  | Style Text Doc
+  | Anchor Text Doc
+  | Blockquote Doc
+  | Blankline
+  | Linebreak
+  | SectionBreak
+  | Tooltip Doc Doc
+  | Aside Doc
+  | Callout (Maybe Doc) Doc
+  | Table [[Doc]]
+  | Folded Bool Doc Doc
+  | Paragraph [Doc]
+  | BulletedList [Doc]
+  | NumberedList Nat [Doc]
+  | Section Doc [Doc]
+  | NamedLink Doc Doc
+  | Image Doc Doc (Maybe Doc)
+  | Special SpecialForm
+  | Join [Doc]
+  | UntitledSection [Doc]
+  | Column [Doc]
+  | Group Doc
+  deriving (Eq,Show)
+
+data SpecialForm
+  = Source [DisplayObject (SyntaxText,SyntaxText)] -- unfolded, folded
+  | FoldedSource [DisplayObject (SyntaxText, SyntaxText)]
+  | Example SyntaxText
+  | ExampleBlock SyntaxText
+  | Link SyntaxText
+  | Signature [SyntaxText]
+  | SignatureInline SyntaxText
+  | Eval SyntaxText SyntaxText
+  | EvalInline SyntaxText SyntaxText
+  | Embed SyntaxText
+  | EmbedInline SyntaxText
+  deriving (Eq,Show)
