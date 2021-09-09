a8c595ad65d7122c737235cc2caf4ed822c4ef56
Arya Irani <arya.irani@gmail.com>
2021-09-09 17:22:11 -0400

    Hashing.V1.Convert.hashTypeComponents

--- parser-typechecker/src/Unison/Builtin/Decls.hs
+++ parser-typechecker/src/Unison/Builtin/Decls.hs
@@ -28,6 +28,7 @@ import Unison.Type (Type)
 import qualified Unison.Type as Type
 import Unison.Var (Var)
 import qualified Unison.Var as Var
+import Control.Monad.Validate (runValidate)
 
 lookupDeclRef :: Text -> Reference
 lookupDeclRef str
@@ -117,10 +118,10 @@ failConstructorReferent = Referent.Con testResultRef failConstructorId CT.Data
 builtinDataDecls :: Var v => [(v, Reference.Id, DataDeclaration v ())]
 builtinDataDecls = rs1 ++ rs
  where
-  rs1 = case hashDecls (const $ Just 1) $ Map.fromList
+  rs1 = case runValidate . hashDecls (pure $ pure 1) $ Map.fromList
     [ (v "Link"                , link)
     ] of Right a -> a; Left e -> error $ "builtinDataDecls: " <> show e
-  rs = case hashDecls (const $ Just 1) $ Map.fromList
+  rs = case runValidate . hashDecls (pure $ pure 1) $ Map.fromList
     [ (v "Unit"               , unit)
     , (v "Tuple"              , tuple)
     , (v "Optional"           , opt)
@@ -306,7 +307,7 @@ builtinDataDecls = rs1 ++ rs
 
 builtinEffectDecls :: Var v => [(v, Reference.Id, DD.EffectDeclaration v ())]
 builtinEffectDecls =
-  case hashDecls $ Map.fromList [ (v "Exception", exception) ] of
+  case runValidate . hashDecls (pure $ pure 1) $ Map.fromList [ (v "Exception", exception) ] of
     Right a -> over _3 DD.EffectDeclaration <$> a
     Left e -> error $ "builtinEffectDecls: " <> show e
   where
--- parser-typechecker/src/Unison/Hashing/V1/Convert.hs
+++ parser-typechecker/src/Unison/Hashing/V1/Convert.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ViewPatterns #-}
 
-module Unison.Hashing.V1.Convert where
+module Unison.Hashing.V1.Convert (hashDecls) where
 
 import Control.Lens (over, _3)
 import qualified Control.Lens as Lens
@@ -18,6 +18,7 @@ import qualified Unison.Hashing.V1.Type as Hashing.Type
 import qualified Unison.Names.ResolutionResult as Names
 import qualified Unison.Reference as Memory.Reference
 import qualified Unison.Referent as Memory.Referent
+import qualified Unison.Term as Memory.Term
 import qualified Unison.Type as Memory.Type
 import Unison.Var (Var)
 
@@ -38,44 +39,68 @@ convertResolutionResult = \case
       Names.TermResolutionFailure v a rs -> TermResolutionFailure v a rs
       Names.TypeResolutionFailure v a rs -> TypeResolutionFailure v a rs
 
+hashTypeComponents
+  :: Var v => (Hash -> Maybe Hashing.Reference.Size) -> Map v (Memory.Type.Type v a) -> Validate (Seq Hash) (Map v (Memory.Reference.Id, Memory.Type.Type v a))
+hashTypeComponents f memTypes = do
+  hashingTypes <- traverse (m2hType f) memTypes
+  let hashingResult = Hashing.Type.hashComponents hashingTypes
+  pure $ fmap h2mTypeResult hashingResult
+
+-- hashTermComponents :: Var v => (Hash -> Maybe Hashing.Reference.Size) -> Map v (Memory.Term.Term v a) -> Map v (Memory.Reference.Id, Memory.Term.Term v a)
+-- hashTermComponents f memTerms = undefined
+
 hashDecls ::
-  (Eq v, Var v) =>
+  Var v =>
   (Hash -> Maybe Hashing.Reference.Size) ->
   Map v (Memory.DD.DataDeclaration v a) ->
   ResolutionResult v a [(v, Memory.Reference.Id, Memory.DD.DataDeclaration v a)]
 hashDecls f memDecls = do
-  hashingDecls <- traverse m2hDecl memDecls
+  hashingDecls <- Validate.mapErrors (fmap CycleResolutionFailure) $ traverse (m2hDecl f) memDecls
   hashingResult <- convertResolutionResult $ Hashing.DD.hashDecls hashingDecls
   pure $ map h2mDeclResult hashingResult
-  where
-    lookupHash :: Hash -> ResolutionResult v a Hashing.Reference.Size
-    lookupHash h = case f h of
-      Just size -> pure size
-      Nothing -> Validate.refute . pure $ CycleResolutionFailure h
 
-    m2hDecl :: Ord v => Memory.DD.DataDeclaration v a -> ResolutionResult v a (Hashing.DD.DataDeclaration v a)
-    m2hDecl (Memory.DD.DataDeclaration mod ann bound ctors) =
-      Hashing.DD.DataDeclaration (m2hModifier mod) ann bound
-        <$> traverse (Lens.mapMOf _3 m2hType) ctors
+m2hDecl ::
+  Ord v =>
+  (Hash -> Maybe Hashing.Reference.Size) ->
+  Memory.DD.DataDeclaration v a ->
+  Validate (Seq Hash) (Hashing.DD.DataDeclaration v a)
+m2hDecl f (Memory.DD.DataDeclaration mod ann bound ctors) =
+  Hashing.DD.DataDeclaration (m2hModifier mod) ann bound
+    <$> traverse (Lens.mapMOf _3 (m2hType f)) ctors
 
-    m2hType :: Ord v => Memory.Type.Type v a -> ResolutionResult v a (Hashing.Type.Type v a)
-    m2hType = ABT.transformM \case
-      Memory.Type.Ref ref -> Hashing.Type.Ref <$> m2hReference ref
-      Memory.Type.Arrow a1 a1' -> pure $ Hashing.Type.Arrow a1 a1'
-      Memory.Type.Ann a1 ki -> pure $ Hashing.Type.Ann a1 ki
-      Memory.Type.App a1 a1' -> pure $ Hashing.Type.App a1 a1'
-      Memory.Type.Effect a1 a1' -> pure $ Hashing.Type.Effect a1 a1'
-      Memory.Type.Effects a1s -> pure $ Hashing.Type.Effects a1s
-      Memory.Type.Forall a1 -> pure $ Hashing.Type.Forall a1
-      Memory.Type.IntroOuter a1 -> pure $ Hashing.Type.IntroOuter a1
+lookupHash :: (Hash -> Maybe Hashing.Reference.Size) -> Hash -> Validate (Seq Hash) Hashing.Reference.Size
+lookupHash f h = case f h of
+  Just size -> pure size
+  Nothing -> Validate.refute $ pure h
 
-    m2hReference :: Memory.Reference.Reference -> ResolutionResult v a (Hashing.Reference.Reference)
-    m2hReference = \case
-      Memory.Reference.Builtin t -> pure $ Hashing.Reference.Builtin t
-      Memory.Reference.DerivedId d -> Hashing.Reference.DerivedId <$> m2hReferenceId d
+m2hType ::
+  Ord v =>
+  (Hash -> Maybe Hashing.Reference.Size) ->
+  Memory.Type.Type v a ->
+  Validate (Seq Hash) (Hashing.Type.Type v a)
+m2hType f = ABT.transformM \case
+  Memory.Type.Ref ref -> Hashing.Type.Ref <$> m2hReference f ref
+  Memory.Type.Arrow a1 a1' -> pure $ Hashing.Type.Arrow a1 a1'
+  Memory.Type.Ann a1 ki -> pure $ Hashing.Type.Ann a1 ki
+  Memory.Type.App a1 a1' -> pure $ Hashing.Type.App a1 a1'
+  Memory.Type.Effect a1 a1' -> pure $ Hashing.Type.Effect a1 a1'
+  Memory.Type.Effects a1s -> pure $ Hashing.Type.Effects a1s
+  Memory.Type.Forall a1 -> pure $ Hashing.Type.Forall a1
+  Memory.Type.IntroOuter a1 -> pure $ Hashing.Type.IntroOuter a1
 
-    m2hReferenceId :: Memory.Reference.Id -> ResolutionResult v a (Hashing.Reference.Id)
-    m2hReferenceId (Memory.Reference.Id h i _n) = Hashing.Reference.Id h i <$> lookupHash h
+m2hReference ::
+  (Hash -> Maybe Hashing.Reference.Size) ->
+  Memory.Reference.Reference ->
+  Validate (Seq Hash) Hashing.Reference.Reference
+m2hReference f = \case
+  Memory.Reference.Builtin t -> pure $ Hashing.Reference.Builtin t
+  Memory.Reference.DerivedId d -> Hashing.Reference.DerivedId <$> m2hReferenceId f d
+
+m2hReferenceId ::
+  (Hash -> Maybe Hashing.Reference.Size) ->
+  Memory.Reference.Id ->
+  Validate (Seq Hash) Hashing.Reference.Id
+m2hReferenceId f (Memory.Reference.Id h i _n) = Hashing.Reference.Id h i <$> lookupHash f h
 
 h2mModifier :: Hashing.DD.Modifier -> Memory.DD.Modifier
 h2mModifier = \case
@@ -89,6 +114,9 @@ m2hModifier = \case
 
 h2mDeclResult :: Ord v => (v, Hashing.Reference.Id, Hashing.DD.DataDeclaration v a) -> (v, Memory.Reference.Id, Memory.DD.DataDeclaration v a)
 h2mDeclResult (v, id, dd) = (v, h2mReferenceId id, h2mDecl dd)
+
+h2mTypeResult :: Ord v => (Hashing.Reference.Id, Hashing.Type.Type v a) -> (Memory.Reference.Id, Memory.Type.Type v a)
+h2mTypeResult (id, dd) = (h2mReferenceId id, h2mType dd)
 
 h2mDecl :: Ord v => Hashing.DD.DataDeclaration v a -> Memory.DD.DataDeclaration v a
 h2mDecl (Hashing.DD.DataDeclaration mod ann bound ctors) =
@@ -112,5 +140,3 @@ h2mReference = \case
 
 h2mReferenceId :: Hashing.Reference.Id -> Memory.Reference.Id
 h2mReferenceId (Hashing.Reference.Id h i n) = Memory.Reference.Id h i n
-
-
