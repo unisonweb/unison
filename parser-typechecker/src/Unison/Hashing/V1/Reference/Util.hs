065ced0c18b15875a885412d3135bc9fcc1a8258
Arya Irani <arya.irani@gmail.com>
2021-09-09 09:09:24 -0400

    copying out definition-related classes for hashing purposes
    copied more than necessary temporarily, to avoid broken project nightmare

---
+++ parser-typechecker/src/Unison/Hashing/V1/Reference/Util.hs
@@ -0,0 +1,21 @@
+module Unison.Hashing.V1.Reference.Util where
+
+import Unison.Prelude
+
+import qualified Unison.Hashing.V1.Reference as Reference
+import Unison.Hashable (Hashable1)
+import Unison.ABT (Var)
+import qualified Unison.ABT as ABT
+import qualified Data.Map as Map
+
+hashComponents ::
+     (Functor f, Hashable1 f, Foldable f, Eq v, Show v, Var v)
+  => (Reference.Id -> ABT.Term f v ())
+  -> Map v (ABT.Term f v a)
+  -> Map v (Reference.Id, ABT.Term f v a)
+hashComponents embedRef tms =
+  Map.fromList [ (v, (r,e)) | ((v,e), r) <- cs ]
+  where cs = Reference.components $ ABT.hashComponents ref tms
+        ref h i n = embedRef (Reference.Id h i n)
+
+
