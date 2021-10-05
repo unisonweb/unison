1462a9ebf0e2774954e0a6cd63d12c42abc21a04
Mitchell Rosen <mitchellwrosen@gmail.com>
2021-10-05 11:12:22 -0400

    ormolu

--- lib/unison-prelude/src/Unison/Prelude.hs
+++ lib/unison-prelude/src/Unison/Prelude.hs
@@ -1,15 +1,22 @@
 module Unison.Prelude
-  ( module X, readUtf8, safeReadUtf8, safeReadUtf8StdIn, writeUtf8, reportBug
-  ) where
+  ( module X,
+    readUtf8,
+    safeReadUtf8,
+    safeReadUtf8StdIn,
+    writeUtf8,
+    reportBug,
+  )
+where
 
 import Control.Applicative as X
-import Control.Exception as X (Exception, SomeException, IOException, try)
+import Control.Exception as X (Exception, IOException, SomeException, try)
 import Control.Monad as X
 import Control.Monad.Extra as X (ifM, mapMaybeM, unlessM, whenM)
-import Control.Monad.IO.Class as X (MonadIO(liftIO))
-import Control.Monad.Trans as X (MonadTrans(lift))
-import Control.Monad.Trans.Maybe as X (MaybeT(MaybeT, runMaybeT))
+import Control.Monad.IO.Class as X (MonadIO (liftIO))
+import Control.Monad.Trans as X (MonadTrans (lift))
+import Control.Monad.Trans.Maybe as X (MaybeT (MaybeT, runMaybeT))
 import Data.ByteString as X (ByteString)
+import qualified Data.ByteString as BS
 import Data.Either as X
 import Data.Either.Combinators as X (mapLeft, maybeToRight)
 import Data.Foldable as X (asum, fold, foldl', for_, toList, traverse_)
@@ -22,15 +29,13 @@ import Data.Sequence as X (Seq)
 import Data.Set as X (Set)
 import Data.String as X (IsString, fromString)
 import Data.Text as X (Text)
-import Data.Text.Encoding as X (encodeUtf8, decodeUtf8)
+import Data.Text.Encoding as X (decodeUtf8, encodeUtf8)
 import Data.Traversable as X (for)
 import Data.Word as X
 import Debug.Trace as X
 import GHC.Generics as X (Generic, Generic1)
 import Safe as X (atMay, headMay, lastMay, readMay)
 import Text.Read as X (readMaybe)
-
-import qualified Data.ByteString as BS
 
 -- Read an entire file strictly assuming UTF8
 readUtf8 :: FilePath -> IO Text
@@ -46,17 +51,19 @@ writeUtf8 :: FilePath -> Text -> IO ()
 writeUtf8 p txt = BS.writeFile p (encodeUtf8 txt)
 
 reportBug :: String -> String -> String
-reportBug bugId msg = unlines [
-  "🐞",
-  "",
-  msg,
-  "",
-  "This is a Unison bug and you can report it here:", "",
-     "https://github.com/unisonweb/unison/issues?utf8=%E2%9C%93&q=is%3Aissue+is%3Aopen+" <> bugId <> "+",
-  "",
-  "Bug reference: " <> bugId, "",
-  "If there's already an issue with this reference, you can give a 👍",
-  "on the issue to let the team know you encountered it, and you can add",
-  "any additional details you know of to the issue."
-  ]
-
+reportBug bugId msg =
+  unlines
+    [ "🐞",
+      "",
+      msg,
+      "",
+      "This is a Unison bug and you can report it here:",
+      "",
+      "https://github.com/unisonweb/unison/issues?utf8=%E2%9C%93&q=is%3Aissue+is%3Aopen+" <> bugId <> "+",
+      "",
+      "Bug reference: " <> bugId,
+      "",
+      "If there's already an issue with this reference, you can give a 👍",
+      "on the issue to let the team know you encountered it, and you can add",
+      "any additional details you know of to the issue."
+    ]
