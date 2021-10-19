556ce1bfcff4cab0eb839413ba4af6d3acf1e0df
Chris Penner <christopher.penner@gmail.com>
2021-10-18 19:16:18 -0600

    Cleanups

--- parser-typechecker/src/Unison/CommandLine/Globbing.hs
+++ parser-typechecker/src/Unison/CommandLine/Globbing.hs
@@ -1,9 +1,8 @@
-{-| Provides Globbing for selecting types, terms and namespaces using wildcards.
--}
-
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ViewPatterns #-}
+
+{-| Provides Globbing for selecting types, terms and namespaces using wildcards.  -}
 module Unison.CommandLine.Globbing
   ( expandGlobs
   , TargetType(..)
@@ -56,35 +55,43 @@ globPredicate globArg (NameSegment.toText -> ns') =
 
 -- | Expands a glob into a list of paths which lead to valid targets.
 expandGlobToPaths :: Set TargetType -> GlobPath -> Branch0 m -> [Path.Relative]
-expandGlobToPaths targets gp branch = (Path.Relative . Path.fromList) <$> expandGlobToNameSegments targets gp branch
+expandGlobToPaths targets globPath branch =
+  (Path.Relative . Path.fromList) <$> expandGlobToNameSegments targets branch globPath
 
 -- | Helper for 'expandGlobToPaths'
-expandGlobToNameSegments :: forall m. Set TargetType -> GlobPath -> Branch0 m -> [[NameSegment]]
-expandGlobToNameSegments _targets [] _branch = []
-expandGlobToNameSegments targets [segment] branch =
-         Monoid.whenM (Set.member Term targets) matchingTerms
-      <> Monoid.whenM (Set.member Type targets) matchingTypes
-      <> Monoid.whenM (Set.member Namespace targets) matchingNamespaces
-  where
-    predicate = globPredicate segment
-    matchingNamespaces, matchingTerms, matchingTypes :: [[NameSegment]]
-    matchingNamespaces = branch ^.. matchingChildBranches predicate . asIndex . to (pure @[])
-    matchingTerms = matchingNamesInStar predicate (Branch._terms branch)
-    matchingTypes = matchingNamesInStar predicate (Branch._types branch)
-    matchingNamesInStar :: (NameSegment -> Bool) -> Branch.Star a NameSegment -> [[NameSegment]]
-    matchingNamesInStar predicate star =
-      star & Star3.d1
-           & Relation.ran
-           & Set.toList
-           & filter predicate
-           & fmap (pure @[])
-expandGlobToNameSegments targets (x:xs) b = recursiveMatches
-  where
-    nextBranches :: [(NameSegment, (Branch0 m))]
-    nextBranches = b ^@.. matchingChildBranches (globPredicate x)
-    recursiveMatches :: ([[NameSegment]])
-    recursiveMatches =
-      (foldMap (\(ns, b) -> (ns:) <$> expandGlobToNameSegments targets xs b) nextBranches)
+expandGlobToNameSegments :: forall m. Set TargetType -> Branch0 m -> GlobPath -> [[NameSegment]]
+expandGlobToNameSegments targets branch globPath =
+  case globPath of
+    -- The glob path was empty; it yields no matches.
+    [] -> []
+    -- If we're at the end of the path, add any targets which match.
+    [segment] ->
+           Monoid.whenM (Set.member Term targets)      matchingTerms
+        <> Monoid.whenM (Set.member Type targets)      matchingTypes
+        <> Monoid.whenM (Set.member Namespace targets) matchingNamespaces
+      where
+        predicate :: NameSegment -> Bool
+        predicate = globPredicate segment
+        matchingNamespaces, matchingTerms, matchingTypes :: [[NameSegment]]
+        matchingNamespaces = branch ^.. matchingChildBranches predicate . asIndex . to (pure @[])
+        matchingTerms = matchingNamesInStar predicate (Branch._terms branch)
+        matchingTypes = matchingNamesInStar predicate (Branch._types branch)
+        matchingNamesInStar :: (NameSegment -> Bool) -> Branch.Star a NameSegment -> [[NameSegment]]
+        matchingNamesInStar predicate star =
+          star & Star3.d1
+               & Relation.ran
+               & Set.toList
+               & filter predicate
+               & fmap (pure @[]) -- Embed each name segment into a path.
+    -- If we have multiple remaining segments, descend into any children matching the current
+    -- segment, then keep matching on the remainder of the path.
+    (segment:rest) -> recursiveMatches
+      where
+        nextBranches :: [(NameSegment, (Branch0 m))]
+        nextBranches = branch ^@.. matchingChildBranches (globPredicate segment)
+        recursiveMatches :: [[NameSegment]]
+        recursiveMatches =
+          foldMap (\(ns, b) -> (ns:) <$> expandGlobToNameSegments targets b rest) nextBranches
 
 -- | Find all child branches whose name matches a predicate.
 matchingChildBranches :: (NameSegment -> Bool) -> IndexedTraversal' NameSegment (Branch0 m) (Branch0 m)
