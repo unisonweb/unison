6f080b2eb61985fd8b5b61b87bf21a45da02ba88
Chris Penner <christopher.penner@gmail.com>
2021-10-21 12:34:52 -0600

    Pseudocode

--- parser-typechecker/src/Unison/Codebase/SqliteCodebase/MigrateSchema12.hs
+++ parser-typechecker/src/Unison/Codebase/SqliteCodebase/MigrateSchema12.hs
@@ -18,6 +18,7 @@ import Unison.Prelude (ByteString, Map, MonadIO)
 import Unison.Reference (Pos)
 import Unison.Referent (ConstructorId)
 import Data.Set (Set)
+import qualified U.Codebase.Sqlite.Operations as Ops
 
 -- lookupCtor :: ConstructorMapping -> ObjectId -> Pos -> ConstructorId -> Maybe (Pos, ConstructorId)
 -- lookupCtor (ConstructorMapping cm) oid pos cid =
@@ -41,9 +42,11 @@ type Old a = a
 type New a = a
 data MigrationState = MigrationState
   -- Mapping between old cycle-position -> new cycle-position for a given Decl object.
-  { declLookup :: Map (Old ObjectId) (Map (Old Pos) (New Pos)),
+  { -- declLookup :: Map (Old ObjectId) (Map (Old Pos) (New Pos)),
+    declLookup :: Map (Old Reference.Id) (New Reference.Id),
     -- Mapping between contructor indexes for the type identified by (ObjectId, Pos)
     ctorLookup :: Map (Old TypeIdentifier) (Map (Old ConstructorId) (New ConstructorId)),
+    ctorLookup' :: Map (Old Referent.Id) (New Referent.Id),
     -- This provides the info needed for rewriting a term.  You'll access it with a function :: Old
     termLookup :: Map (Old ObjectId) (New ObjectId, Map (Old Pos) (New Pos)),
     objLookup :: Map (Old ObjectId) (New ObjectId),
@@ -53,7 +56,7 @@ data MigrationState = MigrationState
     componentPositionMapping :: Map ObjectId (Map (Old Pos) (New Pos)),
     constructorIDMapping :: Map ObjectId (Map (Old ConstructorId) (New ConstructorId)),
     completed :: Set ObjectId
-  }
+  } deriving Generic
 
   -- declLookup :: Map ObjectId (Map Pos (Pos, Map ConstructorId ConstructorId)),
 
@@ -176,12 +179,128 @@ migrateNamespace = error "not implemented"
 migrateTermComponent :: HashId -> ByteString -> m _
 migrateTermComponent = error "not implemented"
 
-migrateDeclComponent :: HashId -> ByteString -> m _
-migrateDeclComponent hashId declFormatBytes = do
-  let DeclFormat locallyIndexedComponent = case runGetS S.getDeclFormat declFormatBytes of
-    Left err -> error "something went wrong"
-    Right declFormat -> declFormat
-  let unhashed = Decl.unhashComponent
+migrateDeclComponent :: EDB m => Codebase m v a -> HashId -> m _
+migrateDeclComponent Codebase{..} hashId = do
+  hash <- Ops.loadHashByHashId hashId
+  declComponent :: [Decl v a] <- Codebase.getDeclComponent (Cv.hash2to1 hash) >>= \case
+    Nothing -> error "handle this" -- not non-fatal! 
+    Just dc -> pure dc
+  
+  -- type Decl = Either EffectDeclaration DataDeclaration
+  let componentIDMap :: Map Reference.Id (Decl v a)
+      componentIDMap = Map.fromList $ Reference.componentFor hash declComponent
+
+  let unhashed :: Map Reference.Id (v, Decl v a)
+      unhashed = DD.unhashComponent componentIDMap
+--  data DataDeclaration v a = DataDeclaration {
+--   modifier :: Modifier,
+--   annotation :: a,
+--   bound :: [v],
+--   constructors' :: [(a, v, Type v a)]
+-- } deriving (Eq, Show, Functor)
+ 
+  let allTypes :: [Type v a]
+      allTypes = (\(_, _, typ) -> typ) . concatMap (constructors' . snd) . Map.values $ unhashed
+  let allContainedReferences :: [Reference.Id]
+      allContainedReferences = foldMap (ABT.find findReferenceIds) (constructors)
+  
+
+
+-- get references:
+--
+--   references :: Term f v a -> [Reference.Id]
+--
+-- are all those references keys in our skymap?
+--   yes => migrate term
+--   no => returh those references (as Entity, though) as more work to do 
+
+-- how to turn Reference.Id into Entity?
+--   need its ObjectId,
+
+-- Term f v a -> ValidateT (Seq Reference.Id) m (Term f v a)
+-- 
+recordRefsInType :: MonadState MigrationState m => Type v a -> WriterT [Reference.Id] m (Type v a)
+recordRefsInType = _
+
+findReferenceIds :: Type v a -> FindAction Reference.Id
+findReferenceIds = Term.out >>> \case
+  Tm (Ref (Reference.DerivedId r)) -> Found r
+  x -> Continue
+
+
+
+
+-- data DataDeclaration v a = DataDeclaration {
+--   modifier :: Modifier,
+--   annotation :: a,
+--   bound :: [v],
+--   constructors' :: [(a, v, Type v a)]
+-- } deriving (Eq, Show, Functor)
+
+
+-- compute correspondence between `v`s in `fst <$> named` compared to `fst <$> new_references` to get a Reference.Id -> Reference.Id mapping
+-- mitchell tapped out before understanding the following line
+-- compute correspondence between constructors names & constructor indices in corresponding decls
+-- submit/mappend these two correspondences to sky mapping
+
+-- Swap the Reference positions according to our map of already computed swaps
+-- Hydrate into the parser-typechecker version, get the new hash
+-- reserialize it into the sqlite format
+-- Compare the old and new sqlite versions to add those ConstructorID/Pos mappings to our context.
+
+-- unrelated Q:
+--   do we kinda have circular dependency issues here?
+--   parser-typechecker depends on codebase2, but we are talking about doing things at the parser-typechecker level in this migration
+--   answer: no
+  
+  -- unhashComponent
+  -- :: forall v a. Var v => Map Reference.Id (Decl v a) -> Map Reference.Id (v, Decl v a)
+
+  let dataDecls = fmap (either toDataDecl id) declComponent
+  -- DD.unhashComponent
+  
+  
+  -- [OldDecl] ==map==> [NewDecl] ==number==> [(NewDecl, Int)] ==sort==> [(NewDecl, Int)] ==> permutation is map snd of that
+
+
+-- type List a = Nil | Cons (List a)
+
+-- unique type Thunk = Thunk (Int ->{MakeThunk} Int)
+-- ability MakeThunk where go : (Int -> Int) -> Thunk
+
+-- What mitchell thinks unhashComponent is doing:
+--
+--  Take a recursive type like
+--
+--     Fix \myself -> Alternatives [Nil, Cons a myself]
+--
+--  And write it with variables in place of recursive mentions like
+--
+--     (Var 1, Alternatives [Nil, Cons a (Var 1)]  
+
+-- can derive `original` from Hash + [OldDecl]
+-- original :: Map Reference.Id (Decl v a)
+
+-- named, rewritten_dependencies :: Map (Reference.Id {old}) (v, Decl v a {old pos in references})
+-- named = Decl.unhashComponent original
+
+-- Mapping from the sky: (Reference.Id -> Reference.Id)
+
+-- rewritten_dependencies = replace_dependency_pos's skymap named
+
+-- new_references :: Map v (Reference.Id {new}, DataDeclaration v a)
+-- new_references = Unison.Hashing.V2.Convert.hashDecls $ Map.toList $ Foldable.toList rewritten_dependencies
+
+  
+  
+
+
+  -- let DeclFormat locallyIndexedComponent = case runGetS S.getDeclFormat declFormatBytes of
+  --   Left err -> error "something went wrong"
+  --   Right declFormat -> declFormat
+
+  -- Operations.hs converts from S level to C level
+  -- SqliteCodebase.hs converts from C level to 
   
 
 -- | migrate sqlite codebase from version 1 to 2, return False and rollback on failure
