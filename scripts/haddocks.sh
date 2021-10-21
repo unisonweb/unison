4ef26b056250fcd289ff5f36e445ec783586f560
Chris Penner <christopher.penner@gmail.com>
2021-10-21 17:53:59 -0600

    Add haddocks script and workflow

---
+++ scripts/haddocks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Early exit on failure
+set -e
+
+stack haddock --fast
+docs_root="$(stack path --local-doc-root)"
+
+git checkout --force haddocks
+rm -r ./*
+cp -r "${docs_root}/*" ./
+git add -a
+git commit -m "Regenerate haddocks based on ${GITHUB_SHA}"
+git push
