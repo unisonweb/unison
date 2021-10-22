306d3fcf7a8625f6270d364d14b15889c1520ef3
Chris Penner <christopher.penner@gmail.com>
2021-10-21 18:56:09 -0600

    Properly copy docs dirs

--- scripts/haddocks.sh
+++ scripts/haddocks.sh
@@ -8,7 +8,7 @@ docs_root="$(stack path --local-doc-root)"
 
 git checkout --force haddocks
 rm -r ./*
-cp -r "${docs_root}/*" ./
-git add -a
+cp -r "${docs_root}"/* ./
+git add .
 git commit -m "Regenerate haddocks based on ${GITHUB_SHA}"
 git push
