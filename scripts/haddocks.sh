82374bf5ff76d5e17af998adcb888a080606b99e
Chris Penner <christopher.penner@gmail.com>
2021-10-21 19:08:32 -0600

    Comments on haddocks script

--- scripts/haddocks.sh
+++ scripts/haddocks.sh
@@ -6,9 +6,13 @@ set -e
 stack haddock --fast
 docs_root="$(stack path --local-doc-root)"
 
+# Since the docs_root is a hidden & gitignored directory, these steps don't wipe it out.
 git checkout --force haddocks
+# Clear all current docs.
 rm -r ./*
+# Replace with new docs
 cp -r "${docs_root}"/* ./
+
 git add .
 git commit -m "Regenerate haddocks based on ${GITHUB_SHA}"
 git push
