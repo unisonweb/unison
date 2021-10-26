378478d08f08bc3ea5e3230332ed8c4fbc8f1c79
satotake <doublequotation@gmail.com>
2021-10-26 12:04:30 +0000

    make tests less abstract

--- unison-cli/integration-tests/IntegrationTests/ArgumentParsing.hs
+++ unison-cli/integration-tests/IntegrationTests/ArgumentParsing.hs
@@ -16,6 +16,12 @@ uFile = "cli/integration-tests/IntegrationTests/print.u"
 transcriptFile :: String
 transcriptFile = "cli/integration-tests/IntegrationTests/transcript.md"
 
+unisonCmdString :: String
+unisonCmdString = unlines 
+  [ "print : '{IO, Exception} ()"
+  , "print _ = base.io.printLine \"ok\""
+  ]
+
 tempCodebase :: String
 tempCodebase = "tempcodebase"
 
@@ -23,64 +29,47 @@ test :: Test ()
 test =
   EasyTest.using (pure ()) clearTempCodebase \_ ->
      scope "argument-parsing" . tests $
-      [ expectExitCode ExitSuccess ["--help"]
-      , expectExitCode ExitSuccess ["-h"]
-      , expectExitCode ExitSuccess ["version", "--help"]
-      , expectExitCode ExitSuccess ["init", "--help"]
-      , expectExitCode ExitSuccess ["run", "--help"]
-      -- , expectExitCode ExitSuccess ["run.compile", "--help"] -- Invalid Argument?
-      , expectExitCode ExitSuccess ["run.file", "--help"]
-      , expectExitCode ExitSuccess ["run.pipe", "--help"]
-      , expectExitCode ExitSuccess ["transcript", "--help"]
-      , expectExitCode ExitSuccess ["transcript.fork", "--help"]
-      , expectExitCode ExitSuccess ["headless", "--help"]
-      , expectExitCode ExitSuccess ["version"]
-      , expectExitCode ExitSuccess ["init"] -- removed
-      -- , expectExitCode ExitSuccess ["run"] -- how?
-      , expectExitCode ExitSuccess ["run.file", uFile, "print"]
-      , expectExitCode ExitSuccess ["transcript", transcriptFile]
-      , expectExitCode ExitSuccess ["transcript.fork", transcriptFile]
-      -- , expectExitCode ExitSuccess ["headless"] -- ?
+      [ expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "--help"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "-h"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "version", "--help"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "init", "--help"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "run", "--help"] ""
+      -- , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "run.compile", "--help"] "" -- Invalid Argument?
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "run.file", "--help"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "run.pipe", "--help"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "transcript", "--help"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "transcript.fork", "--help"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "headless", "--help"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "version"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "init"] "" -- removed
+      -- , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "run"] "" -- how?
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "run.file", uFile, "print"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "run.pipe", "print"] unisonCmdString
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "transcript", transcriptFile] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "transcript.fork", transcriptFile] ""
+      -- , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "headless"] "" -- ?
       -- options
-      , expectExitCode ExitSuccess ["--port", "8000"]
-      , expectExitCode ExitSuccess ["--host", "localhost"]
-      , expectExitCode ExitSuccess ["--token", "MY_TOKEN"] -- ?
-      , expectExitCode ExitSuccess ["--codebase-create", tempCodebase]
-      , expectExitCode ExitSuccess ["--codebase", tempCodebase]
-      , expectExitCode ExitSuccess ["--ui", tempCodebase]
-      , expectExitCode ExitSuccess ["--no-base"]
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "--port", "8000"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "--host", "localhost"] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "--token", "MY_TOKEN"] "" -- ?
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "--codebase-create", tempCodebase] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "--codebase", tempCodebase] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "--ui", tempCodebase] ""
+      , expectExitCode ExitSuccess "stack" ["exec", "--", "unison", "--no-base"] ""
 
       -- Failure
-      , expectExitCode (ExitFailure 1) ["--port", "x"] -- must be number
-      , expectExitCode (ExitFailure 1) ["run.compile", "--help"] -- ?
-      , expectExitCode (ExitFailure 1) ["run.file"] -- without file
-      , expectExitCode (ExitFailure 1) ["run.file", uFile] -- without SYMBOL
-      , expectExitCode (ExitFailure 1) ["transcript"] -- without file
-      , expectExitCode (ExitFailure 1) ["transcript.fork"] -- without file
-
-      -- others
-      , testRunPipe
+      , expectExitCode (ExitFailure 1) "stack" ["exec", "--", "unison", "--port", "x"] "" -- must be number
+      , expectExitCode (ExitFailure 1) "stack" ["exec", "--", "unison", "run.compile", "--help"] "" -- ?
+      , expectExitCode (ExitFailure 1) "stack" ["exec", "--", "unison", "run.file"] "" -- without file
+      , expectExitCode (ExitFailure 1) "stack" ["exec", "--", "unison", "run.file", uFile] "" -- without SYMBOL
+      , expectExitCode (ExitFailure 1) "stack" ["exec", "--", "unison", "transcript"] "" -- without file
+      , expectExitCode (ExitFailure 1) "stack" ["exec", "--", "unison", "transcript.fork"] "" -- without file
       ]
 
-expectExitCode :: ExitCode -> [String] -> Test ()
-expectExitCode expected args = scope (intercalate " " args) do
-  (code, _, _) <- io $ readProcessWithExitCode "stack" fullargs ""
+expectExitCode :: ExitCode -> FilePath -> [String] -> String -> Test ()
+expectExitCode expected cmd args stdin = scope (intercalate " " (cmd : args)) do
+  (code, _, _) <- io $ readProcessWithExitCode cmd args stdin
   expectEqual code expected
-  where
-    fullargs :: [String]
-    fullargs = ["exec", "--", "unison"] ++ args
-
-testRunPipe :: Test ()
-testRunPipe = scope "run.pipe print" do
-  (code, _, _) <- io $ readProcessWithExitCode "stack" fullargs stdin
-  expectEqual code ExitSuccess
-  where
-    stdin = unlines [ "print : '{IO, Exception} ()"
-                    , "print _ = base.io.printLine \"ok\""
-                    ]
-
-    fullargs :: [String]
-    fullargs = ["exec", "--", "unison", "run.pipe", "print"]
 
 clearTempCodebase :: () -> IO()
 clearTempCodebase _ = do
