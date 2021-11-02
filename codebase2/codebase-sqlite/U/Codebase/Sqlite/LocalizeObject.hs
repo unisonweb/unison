303ba0faef6b89520afc1147336383373cb94138
Mitchell Rosen <mitchellwrosen@gmail.com>
2021-11-01 21:32:49 -0400

    move patch localization to U.Codebase.Sqlite.LocalizeObject

--- codebase2/codebase-sqlite/U/Codebase/Sqlite/LocalizeObject.hs
+++ codebase2/codebase-sqlite/U/Codebase/Sqlite/LocalizeObject.hs
@@ -2,19 +2,25 @@
 module U.Codebase.Sqlite.LocalizeObject
   ( -- * High-level localization
     localizeBranch,
+    localizePatch,
 
     -- * General-purpose localization
     LocalizeBranchT,
     runLocalizeBranchT,
 
     -- ** Helpers
-    localizeBranchChild,
+    localizeBranchReference,
     localizeBranchMetadata,
     localizeDefn,
-    localizePatch,
+    localizeHash,
+    localizePatchReference,
     localizeReference,
+    localizeReferenceH,
     localizeReferent,
+    localizeReferentH,
+    localizeTermEdit,
     localizeText,
+    localizeTypeEdit,
 
     -- * @Contains@ constraints
     ContainsBranches,
@@ -35,15 +41,21 @@ import U.Codebase.Sqlite.Branch.Format (BranchLocalIds)
 import qualified U.Codebase.Sqlite.Branch.Format as Branch
 import U.Codebase.Sqlite.Branch.Full (Branch' (..), DbBranch, LocalBranch)
 import qualified U.Codebase.Sqlite.Branch.Full as Branch
-import U.Codebase.Sqlite.DbId (BranchObjectId, CausalHashId, ObjectId, PatchObjectId, TextId)
+import U.Codebase.Sqlite.DbId (BranchObjectId, CausalHashId, HashId, ObjectId, PatchObjectId, TextId)
 import U.Codebase.Sqlite.LocalIds
   ( LocalBranchChildId (..),
     LocalDefnId (..),
+    LocalHashId (..),
     LocalPatchObjectId (..),
     LocalTextId (..),
   )
-import U.Codebase.Sqlite.Reference (LocalReference, Reference)
-import U.Codebase.Sqlite.Referent (LocalReferent, Referent)
+import U.Codebase.Sqlite.Patch.Format (PatchLocalIds)
+import qualified U.Codebase.Sqlite.Patch.Format as Patch
+import U.Codebase.Sqlite.Patch.Full (LocalPatch, Patch, Patch' (..))
+import U.Codebase.Sqlite.Patch.TermEdit (LocalTermEdit, TermEdit)
+import U.Codebase.Sqlite.Patch.TypeEdit (LocalTypeEdit, TypeEdit)
+import U.Codebase.Sqlite.Reference (LocalReference, LocalReferenceH, Reference, ReferenceH)
+import U.Codebase.Sqlite.Referent (LocalReferent, LocalReferentH, Referent, ReferentH)
 import qualified U.Util.Map as Map
 import qualified U.Util.Set as Set
 import Unison.Prelude
@@ -57,8 +69,15 @@ localizeBranch (Branch terms types patches children) =
     Branch
       <$> Map.bitraverse localizeText (Map.bitraverse localizeReferent localizeBranchMetadata) terms
       <*> Map.bitraverse localizeText (Map.bitraverse localizeReference localizeBranchMetadata) types
-      <*> Map.bitraverse localizeText localizePatch patches
-      <*> Map.bitraverse localizeText localizeBranchChild children
+      <*> Map.bitraverse localizeText localizePatchReference patches
+      <*> Map.bitraverse localizeText localizeBranchReference children
+
+localizePatch :: Patch -> (PatchLocalIds, LocalPatch)
+localizePatch (Patch termEdits typeEdits) =
+  (runIdentity . runLocalizePatchT) do
+    Patch
+      <$> Map.bitraverse localizeReferentH (Set.traverse localizeTermEdit) termEdits
+      <*> Map.bitraverse localizeReferenceH (Set.traverse localizeTypeEdit) typeEdits
 
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- General-purpose localization
@@ -69,14 +88,17 @@ type ContainsBranches =
 type ContainsDefns =
   HasType (Map ObjectId LocalDefnId)
 
+type ContainsHashes =
+  HasType (Map HashId LocalHashId)
+
 type ContainsPatches =
   HasType (Map PatchObjectId LocalPatchObjectId)
 
 type ContainsText =
   HasType (Map TextId LocalTextId)
 
-type LocalizeBranchT m a =
-  StateT LocalizeBranchState m a
+type LocalizeBranchT =
+  StateT LocalizeBranchState
 
 type LocalizeBranchState =
   ( Map TextId LocalTextId,
@@ -98,8 +120,29 @@ runLocalizeBranchT action = do
           }
   pure (branchLocalIds, result)
 
-localizeBranchChild :: (ContainsBranches s, Monad m) => (BranchObjectId, CausalHashId) -> StateT s m LocalBranchChildId
-localizeBranchChild =
+type LocalizePatchState =
+  ( Map TextId LocalTextId,
+    Map HashId LocalHashId,
+    Map ObjectId LocalDefnId
+  )
+
+type LocalizePatchT =
+  StateT LocalizePatchState
+
+runLocalizePatchT :: Monad m => LocalizePatchT m a -> m (PatchLocalIds, a)
+runLocalizePatchT action = do
+  (result, (localTexts, localHashes, localDefns)) <- State.runStateT action (mempty @LocalizePatchState)
+  let patchLocalIds :: PatchLocalIds
+      patchLocalIds =
+        Patch.LocalIds
+          { Patch.patchTextLookup = Map.valuesVector (Map.swap localTexts),
+            Patch.patchHashLookup = Map.valuesVector (Map.swap localHashes),
+            Patch.patchDefnLookup = Map.valuesVector (Map.swap localDefns)
+          }
+  pure (patchLocalIds, result)
+
+localizeBranchReference :: (ContainsBranches s, Monad m) => (BranchObjectId, CausalHashId) -> StateT s m LocalBranchChildId
+localizeBranchReference =
   zoom typed . localize
 
 localizeBranchMetadata :: (ContainsDefns s, ContainsText s, Monad m) => Branch.DbMetadataSet -> StateT s m Branch.LocalMetadataSet
@@ -110,21 +153,41 @@ localizeDefn :: (ContainsDefns s, Monad m) => ObjectId -> StateT s m LocalDefnId
 localizeDefn =
   zoom typed . localize
 
-localizePatch :: (ContainsPatches s, Monad m) => PatchObjectId -> StateT s m LocalPatchObjectId
-localizePatch =
+localizeHash :: (ContainsHashes s, Monad m) => HashId -> StateT s m LocalHashId
+localizeHash =
+  zoom typed . localize
+
+localizePatchReference :: (ContainsPatches s, Monad m) => PatchObjectId -> StateT s m LocalPatchObjectId
+localizePatchReference =
   zoom typed . localize
 
 localizeReference :: (ContainsDefns s, ContainsText s, Monad m) => Reference -> StateT s m LocalReference
 localizeReference =
   bitraverse localizeText localizeDefn
 
+localizeReferenceH :: (ContainsHashes s, ContainsText s, Monad m) => ReferenceH -> StateT s m LocalReferenceH
+localizeReferenceH =
+  bitraverse localizeText localizeHash
+
 localizeReferent :: (ContainsDefns s, ContainsText s, Monad m) => Referent -> StateT s m LocalReferent
 localizeReferent =
   bitraverse localizeReference localizeReference
 
+localizeReferentH :: (ContainsHashes s, ContainsText s, Monad m) => ReferentH -> StateT s m LocalReferentH
+localizeReferentH =
+  bitraverse localizeReferenceH localizeReferenceH
+
+localizeTermEdit :: (ContainsText s, ContainsDefns s, Monad m) => TermEdit -> StateT s m LocalTermEdit
+localizeTermEdit =
+  bitraverse localizeText localizeDefn
+
 localizeText :: (ContainsText s, Monad m) => TextId -> StateT s m LocalTextId
 localizeText =
   zoom typed . localize
+
+localizeTypeEdit :: (ContainsText s, ContainsDefns s, Monad m) => TypeEdit -> StateT s m LocalTypeEdit
+localizeTypeEdit =
+  bitraverse localizeText localizeDefn
 
 localize :: (Coercible localId Word64, Monad m, Ord realId) => realId -> StateT (Map realId localId) m localId
 localize realId = do
--- codebase2/codebase-sqlite/U/Codebase/Sqlite/Patch/Format.hs
+++ codebase2/codebase-sqlite/U/Codebase/Sqlite/Patch/Format.hs
@@ -3,16 +3,9 @@ module U.Codebase.Sqlite.Patch.Format
     PatchLocalIds (..),
     SyncPatchFormat (..),
     localPatchToPatch,
-    patchToLocalPatch,
   )
 where
 
-import Control.Lens (Lens', zoom, _1, _2)
-import Control.Monad.Trans.State.Strict (State)
-import qualified Control.Monad.Trans.State.Strict as State
-import Data.Bitraversable (bitraverse)
-import Data.Coerce (Coercible, coerce)
-import qualified Data.Map.Strict as Map
 import Data.Vector (Vector)
 import qualified Data.Vector as Vector
 import U.Codebase.Sqlite.DbId (HashId, ObjectId, PatchObjectId, TextId)
@@ -20,12 +13,6 @@ import U.Codebase.Sqlite.LocalIds (LocalDefnId (LocalDefnId), LocalHashId (Local
 import U.Codebase.Sqlite.Patch.Diff (LocalPatchDiff)
 import U.Codebase.Sqlite.Patch.Full (LocalPatch, Patch)
 import qualified U.Codebase.Sqlite.Patch.Full as Patch.Full
-import U.Codebase.Sqlite.Patch.TermEdit (LocalTermEdit, TermEdit)
-import U.Codebase.Sqlite.Patch.TypeEdit (LocalTypeEdit, TypeEdit)
-import U.Codebase.Sqlite.Reference (LocalReferenceH, ReferenceH)
-import U.Codebase.Sqlite.Referent (LocalReferentH, ReferentH)
-import qualified U.Util.Map as Map
-import qualified U.Util.Set as Set
 import Unison.Prelude
 
 data PatchFormat
@@ -54,64 +41,3 @@ localPatchToPatch li =
 
     lookupPatchLocalDefn :: PatchLocalIds -> LocalDefnId -> ObjectId
     lookupPatchLocalDefn li (LocalDefnId w) = patchDefnLookup li Vector.! fromIntegral w
-
-type PatchToLocalPatchState =
-  ( Map TextId LocalTextId,
-    Map HashId LocalHashId,
-    Map ObjectId LocalDefnId
-  )
-
-patchToLocalPatch :: Patch -> (PatchLocalIds, LocalPatch)
-patchToLocalPatch (Patch.Full.Patch termEdits typeEdits) =
-  let (localPatch, (localTexts, localHashes, localDefns)) =
-        (`State.runState` (mempty @PatchToLocalPatchState)) do
-          Patch.Full.Patch
-            <$> Map.bitraverse (zoom _1_2 . localReferentH) (Set.traverse (zoom _1_3 . localTermEdit)) termEdits
-            <*> Map.bitraverse (zoom _1_2 . localReferenceH) (Set.traverse (zoom _1_3 . localTypeEdit)) typeEdits
-      patchLocalIds :: PatchLocalIds
-      patchLocalIds =
-        LocalIds
-          { patchTextLookup = Map.valuesVector (Map.swap localTexts),
-            patchHashLookup = Map.valuesVector (Map.swap localHashes),
-            patchDefnLookup = Map.valuesVector (Map.swap localDefns)
-          }
-   in (patchLocalIds, localPatch)
-  where
-    localDefn :: ObjectId -> State (Map ObjectId LocalDefnId) LocalDefnId
-    localDefn = localize
-
-    localText :: TextId -> State (Map TextId LocalTextId) LocalTextId
-    localText = localize
-
-    localHash :: HashId -> State (Map HashId LocalHashId) LocalHashId
-    localHash = localize
-
-    localReferenceH :: ReferenceH -> State (Map TextId LocalTextId, Map HashId LocalHashId) LocalReferenceH
-    localReferenceH = bitraverse (zoom _1 . localText) (zoom _2 . localHash)
-
-    localReferentH :: ReferentH -> State (Map TextId LocalTextId, Map HashId LocalHashId) LocalReferentH
-    localReferentH = bitraverse localReferenceH localReferenceH
-
-    localTermEdit :: TermEdit -> State (Map TextId LocalTextId, Map ObjectId LocalDefnId) LocalTermEdit
-    localTermEdit = bitraverse (zoom _1 . localText) (zoom _2 . localDefn)
-
-    localTypeEdit :: TypeEdit -> State (Map TextId LocalTextId, Map ObjectId LocalDefnId) LocalTypeEdit
-    localTypeEdit = bitraverse (zoom _1 . localText) (zoom _2 . localDefn)
-
-localize :: (Coercible local Word64, Ord real) => real -> State (Map real local) local
-localize real = do
-  mapping <- State.get
-  case Map.lookup real mapping of
-    Nothing -> do
-      let nextLocal = coerce @Word64 (fromIntegral (Map.size mapping))
-      State.put $! Map.insert real nextLocal mapping
-      pure nextLocal
-    Just local -> pure local
-
-_1_2 :: Lens' (a, b, c) (a, b)
-_1_2 f (a0, b0, c) =
-  (\(a, b) -> (a, b, c)) <$> f (a0, b0)
-
-_1_3 :: Lens' (a, b, c) (a, c)
-_1_3 f (a0, b, c0) =
-  (\(a, c) -> (a, b, c)) <$> f (a0, c0)
