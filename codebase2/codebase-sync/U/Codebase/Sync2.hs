9e847011d4bc674d2ba8538387fdafb65b1b1c97
Mitchell Rosen <mitchellwrosen@gmail.com>
2021-11-19 14:03:50 -0500

    add thread safe variants

--- codebase2/codebase-sync/U/Codebase/Sync2.hs
+++ codebase2/codebase-sync/U/Codebase/Sync2.hs
@@ -18,6 +18,7 @@ module U.Codebase.Sync2
   )
 where
 
+import Control.Concurrent.MVar
 import qualified Data.Dependent.Map as Dependent.Map
 import Data.Functor.Identity
 import Data.GADT.Compare (GCompare)
@@ -56,71 +57,106 @@ runSync :: Applicative m => Sync m a -> m a
 runSync (Sync action) =
   action pure
 
-sync :: forall k m v. (Ord k, MonadIO m) => Store m k v -> ((k -> Sync m v) -> k -> Sync m v) -> k -> m v
-sync Store {readStore, writeStore} f k0 =
-  runSync do
-    let go :: k -> Sync m v
-        go k =
-          lift (readStore k) >>= \case
-            Nothing -> do
-              v <- f go k
-              lift (writeStore k v)
-              pure v
-            Just v -> pure v
-    go k0
+sync ::
+  forall k m v.
+  (Ord k, MonadIO m) =>
+  Store k v ->
+  ((k -> Sync m v) -> k -> Sync m v) ->
+  k ->
+  m v
+sync (Store fetch) f =
+  let go :: k -> Sync m v
+      go k = fetch k (f go k)
+   in runSync . go
 
 syncD ::
   forall k m v.
   (GCompare k, MonadIO m) =>
-  StoreD m k ->
+  StoreD k ->
   ((forall v. k v -> Sync m v) -> (forall v. k v -> Sync m v)) ->
   k v ->
   m v
-syncD StoreD {readStoreD, writeStoreD} f k0 =
-  runSync do
-    let go :: forall v. k v -> Sync m v
-        go k =
-          lift (readStoreD k) >>= \case
-            Nothing -> do
-              v <- f go k
-              lift (writeStoreD k v)
-              pure v
-            Just v -> pure v
-    go k0
+syncD (StoreD fetch) f =
+  let go :: forall v. k v -> Sync m v
+      go k = fetch k (f go k)
+   in runSync . go
 
-data Store m k v = Store
-  { readStore :: k -> m (Maybe v),
-    writeStore :: k -> v -> m ()
-  }
+newtype Store k v
+  = Store (forall m. MonadIO m => k -> m v -> m v)
 
-data StoreD m k = StoreD
-  { readStoreD :: forall v. k v -> m (Maybe v),
-    writeStoreD :: forall v. k v -> v -> m ()
-  }
+newtype StoreD k
+  = StoreD (forall m v. MonadIO m => k v -> m v -> m v)
 
 data ThreadSafety
   = NotThreadSafe
+  | ThreadSafe
 
-makeMapStore :: forall k m v. (MonadIO m, Ord k) => ThreadSafety -> m (Store m k v)
-makeMapStore NotThreadSafe = do
-  storeRef <- liftIO (newIORef Map.empty)
-  let readStore :: k -> m (Maybe v)
-      readStore k = do
-        store <- liftIO (readIORef storeRef)
-        pure (Map.lookup k store)
-  let writeStore :: k -> v -> m ()
-      writeStore k v =
-        liftIO (modifyIORef' storeRef (Map.insert k v))
-  pure Store {readStore, writeStore}
+makeMapStore :: forall k m v. (MonadIO m, Ord k) => ThreadSafety -> m (Store k v)
+makeMapStore = \case
+  NotThreadSafe -> do
+    storeRef <- liftIO (newIORef Map.empty)
+    let fetch :: forall m. MonadIO m => k -> m v -> m v
+        fetch k mv = do
+          store <- liftIO (readIORef storeRef)
+          case Map.lookup k store of
+            Nothing -> do
+              v <- mv
+              liftIO (writeIORef storeRef $! Map.insert k v store)
+              pure v
+            Just v -> pure v
+    pure (Store fetch)
+  ThreadSafe -> do
+    storeVar <- liftIO (newMVar Map.empty)
+    let fetch :: forall m. MonadIO m => k -> m v -> m v
+        fetch k mv = do
+          store0 <- liftIO (readMVar storeVar)
+          case Map.lookup k store0 of
+            Nothing -> do
+              store0 <- liftIO (takeMVar storeVar)
+              case Map.lookup k store0 of
+                Nothing -> do
+                  vvar <- liftIO newEmptyMVar
+                  liftIO (putMVar storeVar $! Map.insert k vvar store0)
+                  v <- mv
+                  liftIO (putMVar vvar v)
+                  pure v
+                Just vvar -> liftIO do
+                  putMVar storeVar store0
+                  readMVar vvar
+            Just v -> liftIO (readMVar v)
+    pure (Store fetch)
 
-makeDependentMapStore :: forall k m. (GCompare k, MonadIO m) => ThreadSafety -> m (StoreD m k)
-makeDependentMapStore NotThreadSafe = do
-  storeRef <- liftIO (newIORef Dependent.Map.empty)
-  let readStoreD :: k v -> m (Maybe v)
-      readStoreD k = do
-        store <- liftIO (readIORef storeRef)
-        pure (runIdentity <$> Dependent.Map.lookup k store)
-  let writeStoreD :: k v -> v -> m ()
-      writeStoreD k v =
-        liftIO (modifyIORef' storeRef (Dependent.Map.insert k (Identity v)))
-  pure StoreD {readStoreD, writeStoreD}
+makeDependentMapStore :: forall k m. (GCompare k, MonadIO m) => ThreadSafety -> m (StoreD k)
+makeDependentMapStore = \case
+  NotThreadSafe -> do
+    storeRef <- liftIO (newIORef Dependent.Map.empty)
+    let fetch :: forall m v. MonadIO m => k v -> m v -> m v
+        fetch k mv = do
+          store <- liftIO (readIORef storeRef)
+          case Dependent.Map.lookup k store of
+            Nothing -> do
+              v <- mv
+              liftIO (writeIORef storeRef $! Dependent.Map.insert k (Identity v) store)
+              pure v
+            Just (Identity v) -> pure v
+    pure (StoreD fetch)
+  ThreadSafe -> do
+    storeVar <- liftIO (newMVar Dependent.Map.empty)
+    let fetch :: forall m v. MonadIO m => k v -> m v -> m v
+        fetch k mv = do
+          store0 <- liftIO (readMVar storeVar)
+          case Dependent.Map.lookup k store0 of
+            Nothing -> do
+              store0 <- liftIO (takeMVar storeVar)
+              case Dependent.Map.lookup k store0 of
+                Nothing -> do
+                  vvar <- liftIO newEmptyMVar
+                  liftIO (putMVar storeVar $! Dependent.Map.insert k vvar store0)
+                  v <- mv
+                  liftIO (putMVar vvar v)
+                  pure v
+                Just vvar -> liftIO do
+                  putMVar storeVar store0
+                  readMVar vvar
+            Just v -> liftIO (readMVar v)
+    pure (StoreD fetch)
