bf54fc40e263932a52a6b6eaeef01bf221371c86
Simon Højberg <r.hackr@gmail.com>
2021-11-29 10:30:22 -0500

    Match UI release zip file of unisonLocal.zip
    The Codebase UI changed its main artifact of ucm.zip to unisonLocal.zip,
    make sure we build using the right artifcat.

--- dev-ui-install.sh
+++ dev-ui-install.sh
@@ -1,10 +1,10 @@
-echo "This script downloads the latest codebase UI release"
+echo "This script downloads the latest Unison Local UI release"
 echo "and puts it in the correct spot next to the unison"
 echo "executable built by stack."
 echo ""
 
 stack build
-curl -L https://github.com/unisonweb/codebase-ui/releases/download/latest/ucm.zip --output ucm.zip
+curl -L https://github.com/unisonweb/codebase-ui/releases/download/latest/unisonLocal.zip --output unisonLocal.zip
 parent_dir="$(dirname -- $(stack exec which unison))"
 mkdir -p "$parent_dir/ui"
-unzip -o ucm.zip -d "$parent_dir/ui"
+unzip -o unisonLocal.zip -d "$parent_dir/ui"
