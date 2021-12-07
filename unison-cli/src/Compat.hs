82eedc5a5b10647210ea579f24f5f1b19163cc77
Chris Penner <christopher.penner@gmail.com>
2021-12-06 19:14:42 -0600

    Add new interrupt handling methods

--- unison-cli/src/Compat.hs
+++ unison-cli/src/Compat.hs
@@ -1,10 +1,12 @@
-{- ORMOLU_DISABLE -} -- Remove this when the file is ready to be auto-formatted
 {-# LANGUAGE CPP #-}
+
 module Compat where
 
 import Control.Concurrent (mkWeakThreadId, myThreadId)
 import Control.Exception (AsyncException (UserInterrupt), throwTo)
 import System.Mem.Weak (deRefWeak)
+import Unison.Prelude
+import qualified UnliftIO
 
 #if defined(mingw32_HOST_OS)
 import qualified GHC.ConsoleHandler as WinSig
@@ -12,23 +14,32 @@ import qualified GHC.ConsoleHandler as WinSig
 import qualified System.Posix.Signals as Sig
 #endif
 
-installSignalHandlers :: IO ()
-installSignalHandlers = do
+defaultInterruptHandler :: IO (IO ())
+defaultInterruptHandler = do
   main_thread <- myThreadId
   wtid <- mkWeakThreadId main_thread
   let interrupt = do
         r <- deRefWeak wtid
         case r of
           Nothing -> return ()
-          Just t  -> throwTo t UserInterrupt
+          Just t -> throwTo t UserInterrupt
+  pure interrupt
 
+-- | Replaces any existing interrupt handlers with the provided IO action while the provided
+-- action is running, restoring any existing handlers afterwards.
+withInterruptHandler :: IO () -> IO a -> IO a
+withInterruptHandler handler action = do
 #if defined(mingw32_HOST_OS)
-  let sig_handler WinSig.ControlC = interrupt
-      sig_handler WinSig.Break    = interrupt
+  let sig_handler WinSig.ControlC = handler
+      sig_handler WinSig.Break    = handler
       sig_handler _               = return ()
-  _ <- WinSig.installHandler (WinSig.Catch sig_handler)
+  oldHandler <- WinSig.installHandler (WinSig.Catch sig_handler)
+  a <- action `UnliftIO.finally` (void $ WinSig.installHandler oldHandler)
 #else
-  _ <- Sig.installHandler Sig.sigQUIT  (Sig.Catch interrupt) Nothing
-  _ <- Sig.installHandler Sig.sigINT   (Sig.Catch interrupt) Nothing
+  oldQuitHandler <- Sig.installHandler Sig.sigQUIT  (Sig.Catch handler) Nothing
+  oldInterruptHandler <- Sig.installHandler Sig.sigINT   (Sig.Catch handler) Nothing
+  a <- action `UnliftIO.finally` do
+    void $ Sig.installHandler Sig.sigQUIT oldQuitHandler Nothing
+    void $ Sig.installHandler Sig.sigINT oldInterruptHandler Nothing
 #endif
-  return ()
+  pure a
