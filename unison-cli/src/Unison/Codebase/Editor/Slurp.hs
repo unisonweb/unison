191ad5be40855201f6d385889c8b18c249678536
Chris Penner <christopher.penner@gmail.com>
2022-01-13 15:38:24 -0600

    WIP

--- unison-cli/src/Unison/Codebase/Editor/Slurp.hs
+++ unison-cli/src/Unison/Codebase/Editor/Slurp.hs
@@ -6,7 +6,6 @@ import Control.Lens
 import Control.Monad.State
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-import qualified Unison.ABT as ABT
 import qualified Unison.DataDeclaration as DD
 import Unison.Hash (Hash)
 import qualified Unison.LabeledDependency as LD
@@ -23,7 +22,6 @@ import Unison.Term (Term)
 import qualified Unison.Term as Term
 import qualified Unison.UnisonFile as UF
 import qualified Unison.UnisonFile.Names as UF
-import qualified Unison.UnisonFile.Type as UF
 import qualified Unison.Util.Relation as Rel
 import qualified Unison.Util.Relation3 as Rel3
 import qualified Unison.Util.Set as Set
@@ -45,14 +43,13 @@ data LabeledVar v = LabeledVar v LD.LabeledDependency
   deriving (Eq, Ord)
 
 data SlurpStatus = New | Updated | Duplicate
+  deriving (Eq, Ord, Show)
 
 data SlurpOp = Add | Update
+  deriving (Eq, Ord, Show)
 
 data TypeOrTermVar v = TypeVar v | TermVar v
-
--- componentHashForLabeledVar :: LabeledVar v -> ComponentHash
--- componentHashForLabeledVar (LabeledVar _ ld) =
---   labeledDepToComponentHash ld
+  deriving (Eq, Ord, Show)
 
 labeledDepToComponentHash :: LD.LabeledDependency -> ComponentHash
 labeledDepToComponentHash ld =
@@ -77,12 +74,14 @@ data SlurpPrintout v = SlurpPrintout
   { notOk :: Map v SlurpErr,
     ok :: Map v SlurpStatus
   }
+  deriving (Eq, Ord, Show)
 
 data SlurpErr
   = TermCtorCollision
   | CtorTermCollision
+  deriving (Eq, Ord, Show)
 
-data SlurpComponent v = SlurpComponent {types :: Set v, terms :: Set v}
+data SlurpComponent v = SlurpComponent {types :: Set v, terms :: Set v, errs :: Map v SlurpErr}
   deriving (Eq, Ord, Show)
 
 data DefinitionNotes
@@ -100,18 +99,6 @@ data Components v = Components
   { termComponents :: Map Hash (Set v),
     typeComponents :: Map Hash (Set v)
   }
-
-toSlurpPrintout :: SlurpResult v -> SlurpPrintout v
-toSlurpPrintout = undefined
-
-collectComponents :: UF.TypecheckedUnisonFile v Ann -> Components v
-collectComponents _uf = Components {termComponents, typeComponents}
-  where
-    termComponents = undefined
-    typeComponents = undefined
-
-computeComponentDependencies :: Components v -> Map Hash (Set Hash)
-computeComponentDependencies = undefined
 
 analyzeTypecheckedUnisonFile ::
   forall v.
@@ -135,7 +122,8 @@ analyzeTypecheckedUnisonFile uf unalteredCodebaseNames maybeDefsToConsider =
           & Map.fromList
           & Map.mapEitherWithKey
             ( \lv x -> case lv of
-                LabeledVar v ld -> _
+                LabeledVar _ (LD.TypeReference {}) -> Left x
+                LabeledVar _ (LD.TermReferent {}) -> Right x
             )
           & over both (Map.mapKeys (\(LabeledVar v _) -> v))
    in -- & Map.mapEitherWithKey _
@@ -181,73 +169,29 @@ analyzeTypecheckedUnisonFile uf unalteredCodebaseNames maybeDefsToConsider =
           & Rel3.d1s
       Just vs -> vs
 
-    -- labeledVars :: Map v (LabeledVar v)
-    -- labeledVars = undefined
-
     definitionStatus :: LabeledVar v -> DefinitionNotes
-    definitionStatus (undefined -> tv) =
-      let v = unlabeled tv
-          existingTypesAtName = Names.typesNamed codebaseNames (Name.unsafeFromVar v)
+    definitionStatus (LabeledVar v ld) =
+      let existingTypesAtName = Names.typesNamed codebaseNames (Name.unsafeFromVar v)
           existingTermsAtName = Names.termsNamed codebaseNames (Name.unsafeFromVar v)
           existingTermsMatchingReference = Names.termsNamed codebaseNames (Name.unsafeFromVar v)
-          varRef = varReferences Map.! v
-       in case tv of
-            TermVar {} ->
+       in case ld of
+            LD.TypeReference {} ->
               case Set.toList existingTermsAtName of
                 [] -> DefStatus New
-                [r] | LD.referent r == varRef -> DefStatus Duplicate
-                [Referent.Con {}] | LD.ConReference {} <- varRef -> DefErr TermCtorCollision
-                [Referent.Ref {}] | LD.ConReference {} <- varRef -> DefErr CtorTermCollision
+                [r] | LD.referent r == ld -> DefStatus Duplicate
+                [Referent.Con {}] | LD.ConReference {} <- ld -> DefErr TermCtorCollision
+                [Referent.Ref {}] | LD.ConReference {} <- ld -> DefErr CtorTermCollision
                 -- This allows us to resolve conflicts with an update.
                 _ -> DefStatus Updated
             -- [r] -> DefStatus Updated
             -- _ -> DefStatus Conflicted
-            TypeVar {} -> undefined
-    varReferences :: Map v LD.LabeledDependency
-    varReferences = UF.referencesMap uf
+            LD.TermReference {} -> undefined
+            LD.ConReference {} -> undefined
 
     varRelation :: Rel3.Relation3 v (LabeledVar v) ComponentHash
     varRelation = labelling uf
 
     -- Get the set of all DIRECT definitions in the file which a definition depends on.
-    -- directDeps lv = do
-    -- let vComponentHash = componentHashForLabeledVar lv
-    -- componentPeers = Rel.ran $ Rel3.lookupD3 vComponentHash varRelation
-    -- undefined
-    -- let varComponentHash = varToComponentHash Map.! v
-    --     componentPeers = componentMapping Map.! varComponentHash
-    --     directDeps = case UF.hashTermsId uf Map.!? v of
-    --       Nothing -> mempty
-    --       Just (_, _, term, _) -> ABT.freeVars term
-    --  in Set.delete v (componentPeers <> directDeps)
-
-    -- transitiveVarDeps :: Set v -> v -> Set v
-    -- transitiveVarDeps resolved v =
-    --   let directDeps = varDeps v
-    --    in Foldable.foldl' transitiveVarDeps (Set.insert v resolved) directDeps
-    --   where
-    --     go resolved nextV =
-    --       if Set.member nextV resolved
-    --         then resolved
-    --         else resolved <> transitiveVarDeps resolved nextV
-
-    -- transitiveVarDeps :: Set (LabeledVar v) -> LabeledVar v -> Set (LabeledVar v)
-    -- transitiveVarDeps resolved v =
-    --   let directDeps = varDeps v
-    --    in Foldable.foldl' transitiveVarDeps (Set.insert v resolved) directDeps
-    --   where
-    --     go resolved nextV =
-    --       if Set.member nextV resolved
-    --         then resolved
-    --         else resolved <> transitiveVarDeps resolved nextV
-
-    -- varToComponentHash :: Map v ComponentHash
-    -- varToComponentHash = Map.fromList $ do
-    --   -- List monad
-    --   (hash, vars) <- Map.toList componentMapping
-    --   v <- Set.toList vars
-    --   pure (v, hash)
-
     codebaseNames :: Names
     codebaseNames =
       -- TODO: make faster
@@ -289,25 +233,27 @@ slurpErrs (SlurpResult defs _) =
 
 slurpOp ::
   forall v.
+  Ord v =>
   SlurpResult v ->
-  (SlurpComponent v, SlurpComponent v, Map v SlurpErr)
-slurpOp (SlurpResult sr _) = do
-  let (adds, updates, errs) =
-        flip execState mempty $
-          for (Map.toList sr) $ \(v, (dn, _)) -> do
-            case dn of
-              DefStatus New -> _1 %= Set.insert v
-              DefStatus Updated -> _2 %= Set.insert v
-              DefStatus Duplicate -> pure ()
-              DefErr err -> _3 . at v ?= err
-      adds' = partitionTypesAndTerms adds
-      updates' = partitionTypesAndTerms updates
-   in (adds', updates', errs)
+  (SlurpComponent v, SlurpComponent v)
+slurpOp (SlurpResult terms types) =
+  let (termAdds, termUpdates, termErrs) = partition terms
+      (typeAdds, typeUpdates, typeErrs) = partition types
+   in (SlurpComponent termAdds termUpdates termErrs, SlurpComponent typeAdds typeUpdates typeErrs)
+  where
+    partition :: (Map v (DefinitionNotes, Set (LabeledVar v))) -> (Set v, Set v, Map v SlurpErr)
+    partition sr =
+      let (adds, updates, errs) =
+            flip execState mempty $
+              for (Map.toList sr) $ \(v, (dn, _)) -> do
+                case dn of
+                  DefStatus New -> _1 %= Set.insert v
+                  DefStatus Updated -> _2 %= Set.insert v
+                  DefStatus Duplicate -> pure ()
+                  DefErr err -> _3 . at v ?= err
+       in (adds, updates, errs)
 
-partitionTypesAndTerms :: Set v -> SlurpComponent v
-partitionTypesAndTerms = undefined
-
-componentTransitiveDeps :: UF.TypecheckedUnisonFile v a -> Map ComponentHash (Set ComponentHash)
+componentTransitiveDeps :: Ord v => UF.TypecheckedUnisonFile v a -> Map ComponentHash (Set ComponentHash)
 componentTransitiveDeps uf =
   let deps = Map.unionsWith (<>) [termDeps, dataDeps, effectDeps]
       filteredDeps :: Map ComponentHash (Set ComponentHash)
@@ -363,27 +309,27 @@ dataDeclRefs decl =
     & Set.mapMaybe componentHashForReference
 
 -- Does not include constructors
-labelling :: forall v a. UF.TypecheckedUnisonFile v a -> Rel3.Relation3 v (LabeledVar v) ComponentHash
-labelling uf = _ $ decls <> effects <> terms
+labelling :: forall v a. Ord v => UF.TypecheckedUnisonFile v a -> Rel3.Relation3 v (LabeledVar v) ComponentHash
+labelling uf = decls <> effects <> terms
   where
-    terms :: Rel3.Relation3 v LD.LabeledDependency ComponentHash
+    terms :: Rel3.Relation3 v (LabeledVar v) ComponentHash
     terms =
       UF.hashTermsId uf
         & Map.toList
-        & fmap (\(v, (refId, _, _, _)) -> (v, LD.derivedTerm refId, idToComponentHash refId))
+        & fmap (\(v, (refId, _, _, _)) -> (v, LabeledVar v (LD.derivedTerm refId), idToComponentHash refId))
         & Rel3.fromList
-    decls :: Rel3.Relation3 v LD.LabeledDependency ComponentHash
+    decls :: Rel3.Relation3 v (LabeledVar v) ComponentHash
     decls =
       UF.dataDeclarationsId' uf
         & Map.toList
-        & fmap (\(v, (refId, _)) -> (v, LD.derivedType refId, idToComponentHash refId))
+        & fmap (\(v, (refId, _)) -> (v, LabeledVar v (LD.derivedType refId), idToComponentHash refId))
         & Rel3.fromList
 
-    effects :: Rel3.Relation3 v LD.LabeledDependency ComponentHash
+    effects :: Rel3.Relation3 v (LabeledVar v) ComponentHash
     effects =
       UF.effectDeclarationsId' uf
         & Map.toList
-        & fmap (\(v, (refId, _)) -> (v, LD.derivedType refId, idToComponentHash refId))
+        & fmap (\(v, (refId, _)) -> (v, LabeledVar v (LD.derivedType refId), idToComponentHash refId))
         & Rel3.fromList
 
 idToComponentHash :: Ref.Id -> ComponentHash
