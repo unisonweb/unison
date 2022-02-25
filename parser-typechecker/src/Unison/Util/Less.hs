c2aa0e637fdb804a6c9d2e275356e6f90502be03
Chris Penner <christopher.penner@gmail.com>
2022-02-25 15:49:48 -0600

    Fallback to more

--- parser-typechecker/src/Unison/Util/Less.hs
+++ parser-typechecker/src/Unison/Util/Less.hs
@@ -5,27 +5,50 @@ import System.Environment (lookupEnv)
 import System.Process
 import System.IO (hPutStr, hClose)
 import Control.Exception.Extra (ignore)
-import Unison.Prelude (void)
+import Unison.Prelude
+import UnliftIO.Directory (findExecutable)
+import qualified UnliftIO
 
 less :: String -> IO ()
 less str = do
-  inEmacs <- lookupEnv "INSIDE_EMACS"
-  case inEmacs of
-    Just _ -> putStr str
-    Nothing -> do
-      let args = ["--no-init"            -- don't clear the screen on exit
-                 ,"--raw-control-chars"  -- pass through colors and stuff
-                 ,"--prompt=[less] Use space/arrow keys to navigate, or 'q' to return to ucm:"
-                 ,"--quit-if-one-screen" -- self-explanatory
-                 ]
-      (Just stdin, _stdout, _stderr, pid)
-        <- createProcess (proc "less" args) { std_in = CreatePipe }
+  isInteractive <- lookupEnv "INSIDE_EMACS" >>= \case
+                     Just _ -> pure False
+                     Nothing -> UnliftIO.hIsTerminalDevice UnliftIO.stdin
+  if isInteractive
+     then usePager
+     else noPager
+  where
+    noPager :: IO ()
+    noPager = putStr str
+    usePager :: IO ()
+    usePager = do
+      pager <- runMaybeT $ msum
+                [ shell <$> MaybeT (lookupEnv "PAGER")
+                , MaybeT (findExecutable "less") <&> \less -> proc less lessArgs
+                -- most windows machines have 'more'.
+                , MaybeT (findExecutable "more") <&> \more -> proc more moreArgs
+                ]
+      case pager of
+        Nothing -> noPager
+        Just process -> do
+          (Just stdin, _stdout, _stderr, pid)
+            <- createProcess process { std_in = CreatePipe }
 
-      -- If `less` exits before consuming all of stdin, `hPutStr` will crash.
-      ignore $ hPutStr stdin str
+          -- If pager exits before consuming all of stdin, `hPutStr` will crash.
+          ignore $ hPutStr stdin str
 
-      -- If `less` has already exited, hClose throws an exception.
-      ignore $ hClose stdin
+          -- If pager has already exited, hClose throws an exception.
+          ignore $ hClose stdin
 
-      -- Wait for `less` to exit.
-      void $ waitForProcess pid
+          -- Wait for pager to exit.
+          void $ waitForProcess pid
+
+    lessArgs :: [String]
+    lessArgs = ["--no-init"            -- don't clear the screen on exit
+               ,"--raw-control-chars"  -- pass through colors and stuff
+               ,"--prompt=[less] Use space/arrow keys to navigate, or 'q' to return to ucm:"
+               ,"--quit-if-one-screen" -- self-explanatory
+                           ]
+
+    moreArgs :: [String]
+    moreArgs = []
