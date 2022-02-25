587246365d3ddf658e43f6fde7f2e7b38ec207d3
Chris Penner <christopher.penner@gmail.com>
2022-02-25 16:15:03 -0600

    ormolu

--- parser-typechecker/src/Unison/Util/Less.hs
+++ parser-typechecker/src/Unison/Util/Less.hs
@@ -1,38 +1,40 @@
-{- ORMOLU_DISABLE -} -- Remove this when the file is ready to be auto-formatted
 module Unison.Util.Less where
 
-import System.Environment (lookupEnv)
-import System.Process
-import System.IO (hPutStr, hClose)
 import Control.Exception.Extra (ignore)
+import System.Environment (lookupEnv)
+import System.IO (hClose, hPutStr)
+import System.Process
 import Unison.Prelude
-import UnliftIO.Directory (findExecutable)
 import qualified UnliftIO
+import UnliftIO.Directory (findExecutable)
 
 less :: String -> IO ()
 less str = do
-  isInteractive <- lookupEnv "INSIDE_EMACS" >>= \case
-                     Just _ -> pure False
-                     Nothing -> UnliftIO.hIsTerminalDevice UnliftIO.stdin
+  isInteractive <-
+    lookupEnv "INSIDE_EMACS" >>= \case
+      Just _ -> pure False
+      Nothing -> UnliftIO.hIsTerminalDevice UnliftIO.stdin
   if isInteractive
-     then usePager
-     else noPager
+    then usePager
+    else noPager
   where
     noPager :: IO ()
     noPager = putStr str
     usePager :: IO ()
     usePager = do
-      pager <- runMaybeT $ msum
-                [ shell <$> MaybeT (lookupEnv "PAGER")
-                , MaybeT (findExecutable "less") <&> \less -> proc less lessArgs
-                -- most windows machines have 'more'.
-                , MaybeT (findExecutable "more") <&> \more -> proc more moreArgs
-                ]
+      pager <-
+        runMaybeT $
+          msum
+            [ shell <$> MaybeT (lookupEnv "PAGER"),
+              MaybeT (findExecutable "less") <&> \less -> proc less lessArgs,
+              -- most windows machines have 'more'.
+              MaybeT (findExecutable "more") <&> \more -> proc more []
+            ]
       case pager of
         Nothing -> noPager
         Just process -> do
-          (Just stdin, _stdout, _stderr, pid)
-            <- createProcess process { std_in = CreatePipe }
+          (Just stdin, _stdout, _stderr, pid) <-
+            createProcess process {std_in = CreatePipe}
 
           -- If pager exits before consuming all of stdin, `hPutStr` will crash.
           ignore $ hPutStr stdin str
@@ -44,11 +46,9 @@ less str = do
           void $ waitForProcess pid
 
     lessArgs :: [String]
-    lessArgs = ["--no-init"            -- don't clear the screen on exit
-               ,"--raw-control-chars"  -- pass through colors and stuff
-               ,"--prompt=[less] Use space/arrow keys to navigate, or 'q' to return to ucm:"
-               ,"--quit-if-one-screen" -- self-explanatory
-                           ]
-
-    moreArgs :: [String]
-    moreArgs = []
+    lessArgs =
+      [ "--no-init", -- don't clear the screen on exit
+        "--raw-control-chars", -- pass through colors and stuff
+        "--prompt=[less] Use space/arrow keys to navigate, or 'q' to return to ucm:",
+        "--quit-if-one-screen" -- self-explanatory
+      ]
