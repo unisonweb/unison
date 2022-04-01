52da285bf67d0afee09f5d58ab76189640969a18
Chris Penner <christopher.penner@gmail.com>
2022-04-01 10:14:01 -0600

    Add unison-share-api package with Sync APIs (#3018)

---
+++ unison-share-api/src/Unison/Sync/API.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DataKinds #-}
+
+module Unison.Sync.API (API) where
+
+import Servant.API
+import Unison.Sync.Types
+
+type API =
+  "path" :> "get" :> GetCausalHashByPathEndpoint
+    :<|> "path" :> "update" :> UpdatePathEndpoint
+    :<|> "entities" :> "download" :> DownloadEntitiesEndpoint
+    :<|> "entities" :> "upload" :> UploadEntitiesEndpoint
+
+type GetCausalHashByPathEndpoint =
+  ReqBody '[JSON] GetCausalHashByPathRequest
+    :> Post '[JSON] GetCausalHashByPathResponse
+
+type UpdatePathEndpoint =
+  ReqBody '[JSON] UpdatePathRequest
+    :> UVerb 'POST '[JSON] '[WithStatus 204 NoContent, WithStatus 404 (NeedDependencies HashJWT), WithStatus 412 HashMismatch]
+
+type DownloadEntitiesEndpoint =
+  ReqBody '[JSON] DownloadEntitiesRequest
+    :> Post '[JSON] DownloadEntitiesResponse
+
+type UploadEntitiesEndpoint =
+  ReqBody '[JSON] UploadEntitiesRequest
+    :> UVerb 'POST '[JSON] '[WithStatus 200 NoContent, WithStatus 202 (NeedDependencies Hash)]
