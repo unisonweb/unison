68e47963b6ac6738ca775b563d4c1f3606db151c
Chris Penner <christopher.penner@gmail.com>
2022-04-28 15:20:16 -0600

    Disable names index in main

--- unison-cli/unison/Main.hs
+++ unison-cli/unison/Main.hs
@@ -69,8 +69,8 @@ import Unison.Prelude
 import qualified Unison.PrettyTerminal as PT
 import Unison.Runtime.Exception (RuntimeExn (..))
 import qualified Unison.Runtime.Interface as RTI
-import qualified Unison.Server.CodebaseServer as Server
 import qualified Unison.Server.Backend as Backend
+import qualified Unison.Server.CodebaseServer as Server
 import Unison.Symbol (Symbol)
 import qualified Unison.Util.Pretty as P
 import UnliftIO.Directory (getHomeDirectory)
@@ -210,7 +210,7 @@ main = withCP65001 do
       Launch isHeadless codebaseServerOpts downloadBase -> do
         getCodebaseOrExit mCodePathOption \(initRes, _, theCodebase) -> do
           runtime <- RTI.startRuntime RTI.Persistent Version.gitDescribeWithDate
-          Server.startServer (Backend.BackendEnv {Backend.useNamesIndex=True}) codebaseServerOpts runtime theCodebase $ \baseUrl -> do
+          Server.startServer (Backend.BackendEnv {Backend.useNamesIndex = False}) codebaseServerOpts runtime theCodebase $ \baseUrl -> do
             case isHeadless of
               Headless -> do
                 PT.putPrettyLn $
