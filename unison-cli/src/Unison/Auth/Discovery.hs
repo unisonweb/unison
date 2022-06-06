eca50efd4d8a84360c5395d2c80bf732a8463ed0
Chris Penner <christopher.penner@gmail.com>
2022-06-06 17:40:55 -0600

    Implement token refresh (#3095)

--- unison-cli/src/Unison/Auth/Discovery.hs
+++ unison-cli/src/Unison/Auth/Discovery.hs
@@ -3,22 +3,23 @@ module Unison.Auth.Discovery where
 import qualified Data.Aeson as Aeson
 import qualified Data.Text as Text
 import qualified Network.HTTP.Client as HTTP
+import qualified Network.HTTP.Client.TLS as HTTP
 import Network.URI
 import Unison.Auth.Types
 import Unison.Prelude
 import Unison.Share.Types (CodeserverURI (..), codeserverToURI)
 import qualified UnliftIO
 
-discoveryURI :: CodeserverURI -> URI
-discoveryURI cs =
+discoveryURIForCodeserver :: CodeserverURI -> URI
+discoveryURIForCodeserver cs =
   let uri = codeserverToURI cs
    in uri {uriPath = uriPath uri <> "/.well-known/openid-configuration"}
 
-discoveryForCodeserver :: MonadIO m => HTTP.Manager -> CodeserverURI -> m (Either CredentialFailure DiscoveryDoc)
-discoveryForCodeserver httpClient host = liftIO . UnliftIO.try @_ @CredentialFailure $ do
-  let uri = discoveryURI host
-  req <- HTTP.requestFromURI uri
-  resp <- HTTP.httpLbs req httpClient
+fetchDiscoveryDoc :: MonadIO m => URI -> m (Either CredentialFailure DiscoveryDoc)
+fetchDiscoveryDoc discoveryURI = liftIO . UnliftIO.try @_ @CredentialFailure $ do
+  unauthenticatedHttpClient <- HTTP.getGlobalManager
+  req <- HTTP.requestFromURI discoveryURI
+  resp <- HTTP.httpLbs req unauthenticatedHttpClient
   case Aeson.eitherDecode (HTTP.responseBody $ resp) of
-    Left err -> UnliftIO.throwIO $ InvalidDiscoveryDocument uri (Text.pack err)
+    Left err -> UnliftIO.throwIO $ InvalidDiscoveryDocument discoveryURI (Text.pack err)
     Right doc -> pure doc
