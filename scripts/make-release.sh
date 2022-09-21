b6c2363516c33867af5b0eaabeb1fbd3e934246b
Chris Penner <christopher.penner@gmail.com>
2022-09-21 11:37:24 -0600

    Update scripts

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -2,31 +2,50 @@
 
 set -e
 
+usage() {
+    echo "Usage:"
+    echo "$0 <version>"
+    echo "E.g."
+    echo "$0 M4a"
+}
+
+if [[ $? -ne 1  ]] ; then
+  usage
+  exit 1
+fi
+
 if ! command -V "gh" >/dev/null 2>&1; then
    echo "Required command \`gh\` not found, find installation instructions here: https://cli.github.com/manual/installation"
    exit 1
 fi
 
 if ! [[ "$1" =~ ^M[0-9]+[a-z]?$ ]] ; then
- echo "Version tag be of the form 'M4' or 'M4a'"
- echo "E.g. "
- echo "$0 M4a"
+ echo "Version tag must be of the form 'M4' or 'M4a'"
+ usage
  exit 1
 fi
 
 version="${1}"
 committish=${2:trunk}
 
+prereleaseFlag=""
+
+if [[ "$1" =~ ^M[0-9]+[a-z]$ ]] ; then
+    prereleaseFlag="--prerelease"
+fi
+
+gh workflow run release --repo unisonweb/unison --field "version=${version}" --ref cp/automate-minor-releases
+
+
 echo "Tagging current unison-local-ui revision for this release..."
-gh release create "release/${version}" --repo unisonweb/unison-local-ui --target main --generate-notes
+gh release create "release/${version}" --repo unisonweb/unison-local-ui --target main --generate-notes $prereleaseFlag
 
 echo "Tagging ${committish} with ${version} in the unison repo"
 git tag "${version}" "${committish}"
 git push origin "${version}"
 
-
 echo "Kicking off Homebrew update task"
-gh workflow run release_version --repo unisonweb/homebrew-unison --field "version=${version}"
+gh workflow run release_version --repo unisonweb/homebrew-unison --field "version=${version}" --ref cp/automate-brew-upgrade
 
 echo "Opening relevant workflows in browser"
 gh workflow view release_version --web --repo unisonweb/homebrew-unison || true
