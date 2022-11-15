216406938348105dd8e5d70c4ac18d3853f31c59
Dan Doel <dan.doel@gmail.com>
2022-11-15 11:09:37 -0500

    Make universal compare do lexocographic comparison on lists
    - Eq still checks length first, because that works

--- parser-typechecker/src/Unison/Runtime/Machine.hs
+++ parser-typechecker/src/Unison/Runtime/Machine.hs
@@ -2159,7 +2159,7 @@ universalCompare frn = cmpc False
     cmpc tyEq (Foreign fl) (Foreign fr)
       | Just sl <- maybeUnwrapForeign Rf.listRef fl,
         Just sr <- maybeUnwrapForeign Rf.listRef fr =
-          comparing Sq.length sl sr <> fold (Sq.zipWith (cmpc tyEq) sl sr)
+          fold (Sq.zipWith (cmpc tyEq) sl sr)
       | Just al <- maybeUnwrapForeign Rf.iarrayRef fl,
         Just ar <- maybeUnwrapForeign Rf.iarrayRef fr =
           arrayCmp (cmpc tyEq) al ar
