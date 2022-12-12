a7ea80049304887ef38cb26df1a5359575b4cfde
Chris Penner <christopher.penner@gmail.com>
2022-12-12 15:49:31 -0600

    Add instance for DisplayObject

--- parser-typechecker/src/Unison/Codebase/Editor/DisplayObject.hs
+++ parser-typechecker/src/Unison/Codebase/Editor/DisplayObject.hs
@@ -2,7 +2,9 @@
 
 module Unison.Codebase.Editor.DisplayObject where
 
+import Data.Bifoldable
 import Data.Bifunctor
+import Data.Bitraversable
 import Unison.Prelude
 import Unison.ShortHash
 
@@ -13,6 +15,14 @@ instance Bifunctor DisplayObject where
   bimap _ _ (MissingObject sh) = MissingObject sh
   bimap f _ (BuiltinObject b) = BuiltinObject (f b)
   bimap _ f (UserObject a) = UserObject (f a)
+
+instance Bitraversable DisplayObject where
+  bitraverse f _ (BuiltinObject b) = BuiltinObject <$> f b
+  bitraverse _ _ (MissingObject sh) = pure (MissingObject sh)
+  bitraverse _ g (UserObject a) = UserObject <$> g a
+
+instance Bifoldable DisplayObject where
+  bifoldMap = bifoldMapDefault
 
 toMaybe :: DisplayObject b a -> Maybe a
 toMaybe = \case
