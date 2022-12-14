3e48cabaad793e3fd70876c18cdb89a4a03d98e2
Mitchell Rosen <mitchellwrosen@gmail.com>
2022-12-14 18:37:51 -0500

    Add Unison.ContentAddressable class

---
+++ codebase2/core/Unison/ContentAddressable.hs
@@ -0,0 +1,12 @@
+module Unison.ContentAddressable
+  ( ContentAddressable (..),
+  )
+where
+
+import U.Util.Hash (Hash)
+
+-- | A type class that is inhabited by types that can compute a hash of their content.
+--
+-- Instances of this class should only live in dedicated "hashing packages" such as @unison-hashing-v2@.
+class ContentAddressable a where
+  hash :: a -> Hash
