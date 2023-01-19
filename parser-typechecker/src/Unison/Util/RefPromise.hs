38e79afbe269cb87c75d7cb2718fed66c1894baa
Fabio Labella <fabio.labella2@gmail.com>
2023-01-19 13:14:52 +0000

    casIORef works in haskell, bus error is in the unison bindings

--- parser-typechecker/src/Unison/Util/RefPromise.hs
+++ parser-typechecker/src/Unison/Util/RefPromise.hs
@@ -7,12 +7,14 @@ module Unison.Util.RefPromise
     newPromise,
     readPromise,
     tryReadPromise,
-    writePromise
+    writePromise,
+    foo -- TODO remove
   )
 where
 
 import Control.Concurrent.MVar (MVar, newEmptyMVar, readMVar, tryReadMVar, tryPutMVar)
 import Data.Atomics (Ticket, peekTicket, readForCAS, casIORef)
+import Data.IORef -- TODO remove
 
 newtype Promise a = Promise { state :: MVar a}
 
@@ -34,3 +36,13 @@ tryReadPromise Promise { state } = tryReadMVar state
 -- if full, ignore the write and return False
 writePromise :: a -> Promise a -> IO Bool
 writePromise value Promise { state } = tryPutMVar state value
+
+-- TODO remove
+foo :: IO (Bool, Bool)
+foo = do
+  state <- newIORef (0 :: Integer)
+  ticket <- readForCAS state
+  success <- fmap fst $ casIORef state ticket 5
+  writeIORef state 10
+  failure <- fmap fst $ casIORef state ticket 5
+  pure (success, failure)
