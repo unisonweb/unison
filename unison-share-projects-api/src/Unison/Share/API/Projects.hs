473b859d29765ade99b712a1374912a90317cfb2
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-01-24 16:40:20 -0500

    add get project endpoint

--- unison-share-projects-api/src/Unison/Share/API/Projects.hs
+++ unison-share-projects-api/src/Unison/Share/API/Projects.hs
@@ -1,3 +1,37 @@
-module Unison.Share.API.Projects where
+{-# LANGUAGE DataKinds #-}
 
-import Servant
+module Unison.Share.API.Projects
+  ( ProjectsAPI,
+
+    -- * Types
+    GetProjectResponse (..),
+    Project (..),
+  )
+where
+
+import Data.Text (Text)
+import GHC.Generics (Generic)
+import Servant.API
+
+-- |
+--
+-- [@GET /project?id=XXX@]: Get a project by id.
+--
+-- [@GET /project?name=XXX@]: Get a project by name.
+type ProjectsAPI =
+  "project"
+    :> QueryParam "id" Text
+    :> QueryParam "name" Text
+    :> Verb Get 200 '[JSON] GetProjectResponse
+
+-- | @GET /project@ response.
+data GetProjectResponse
+  = -- | 404 not found.
+    GetProjectResponseNotFound
+  | GetProjectResponseSuccess !Project
+
+data Project = Project
+  { id :: Text,
+    name :: Text
+  }
+  deriving stock (Eq, Generic, Show)
