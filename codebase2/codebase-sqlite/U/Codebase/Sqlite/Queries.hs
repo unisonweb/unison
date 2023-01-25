78aa94370f0ca7fb9deba10819a99e3d69ffcf37
Chris Penner <christopher.penner@gmail.com>
2023-01-25 11:28:11 -0600

    Scope indexes too.

--- codebase2/codebase-sqlite/U/Codebase/Sqlite/Queries.hs
+++ codebase2/codebase-sqlite/U/Codebase/Sqlite/Queries.hs
@@ -1700,12 +1700,12 @@ ensureScopedNameLookupTables = do
   -- reversed_name over rows with a matching last segment without using an index and should be plenty fast.
   execute_
     [here|
-      CREATE INDEX IF NOT EXISTS scoped_term_names_by_namespace_and_last_name_segment ON term_name_lookup(root_branch_hash_id, last_name_segment, namespace)
+      CREATE INDEX IF NOT EXISTS scoped_term_names_by_namespace_and_last_name_segment ON scoped_term_name_lookup(root_branch_hash_id, last_name_segment, namespace)
     |]
   -- This index allows us to find all names with a given ref within a specific namespace
   execute_
     [here|
-      CREATE INDEX IF NOT EXISTS scoped_term_name_by_referent_lookup ON term_name_lookup(root_branch_hash_id, referent_builtin, referent_component_hash, referent_component_index, referent_constructor_index, namespace)
+      CREATE INDEX IF NOT EXISTS scoped_term_name_by_referent_lookup ON scoped_term_name_lookup(root_branch_hash_id, referent_builtin, referent_component_hash, referent_component_index, referent_constructor_index, namespace)
     |]
 
   -- Allows fetching ALL names within a specific namespace prefix. We currently use this to
@@ -1747,20 +1747,20 @@ ensureScopedNameLookupTables = do
   -- reversed_name over rows with a matching last segment without using an index and should be plenty fast.
   execute_
     [here|
-      CREATE INDEX IF NOT EXISTS scoped_type_names_by_namespace_and_last_name_segment ON type_name_lookup(root_branch_hash_id, last_name_segment, namespace)
+      CREATE INDEX IF NOT EXISTS scoped_type_names_by_namespace_and_last_name_segment ON scoped_type_name_lookup(root_branch_hash_id, last_name_segment, namespace)
     |]
 
   -- This index allows us to find all names with a given ref within a specific namespace.
   execute_
     [here|
-      CREATE INDEX IF NOT EXISTS scoped_type_name_by_reference_lookup ON type_name_lookup(root_branch_hash_id, reference_builtin, reference_component_hash, reference_component_index, namespace)
+      CREATE INDEX IF NOT EXISTS scoped_type_name_by_reference_lookup ON scoped_type_name_lookup(root_branch_hash_id, reference_builtin, reference_component_hash, reference_component_index, namespace)
     |]
 
   -- Allows fetching ALL names within a specific namespace prefix. We currently use this to
   -- pretty-print on share, but will be replaced with a more precise set of queries soon.
   execute_
     [here|
-      CREATE INDEX IF NOT EXISTS scoped_type_names_by_namespace ON type_name_lookup(root_branch_hash_id, namespace)
+      CREATE INDEX IF NOT EXISTS scoped_type_names_by_namespace ON scoped_type_name_lookup(root_branch_hash_id, namespace)
     |]
 
 -- | Copies existing name lookup rows but replaces their branch hash id;
