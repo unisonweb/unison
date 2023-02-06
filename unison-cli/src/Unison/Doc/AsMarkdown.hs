155af3f4ffef39616e1c53db6401fd1d53121e04
Chris Penner <christopher.penner@gmail.com>
2023-02-06 12:48:06 -0600

    Add Markdown Newtype

--- unison-cli/src/Unison/Doc/AsMarkdown.hs
+++ unison-cli/src/Unison/Doc/AsMarkdown.hs
@@ -76,14 +76,13 @@ normalizeHref docNamesByRef = go InvalidHref
         _ ->
           href
 
-toMarkdownSource :: EmbeddedSource -> [Markdown]
+toMarkdownSource :: EmbeddedSource -> Markdown
 toMarkdownSource source =
   case source of
     Builtin summary ->
-      [ codeBlockText "unison" $ Syntax.toPlainText summary,
-        text "Built-in provided by the Unison runtime"
-      ]
-    EmbeddedSource _summary details -> [codeBlockText "unison" $ Syntax.toPlainText details]
+      codeBlockText "unison" (Syntax.toPlainText summary)
+        <> text "Built-in provided by the Unison runtime"
+    EmbeddedSource _summary details -> codeBlockText "unison" $ Syntax.toPlainText details
 
 -- | Merge down Doc to Text by merging Paragraphs and Words.
 -- Used for things like extract an src of an image. I.e something that has to
@@ -125,13 +124,29 @@ toText sep doc =
         . map (toText sep)
 
 -- | Markdown node
-type Markdown = M.Node
+newtype Markdown = Markdown (Seq M.Node)
+
+toNodes :: Markdown -> [M.Node]
+toNodes (Markdown ns) = toList ns
+
+markdownNode :: M.Node -> Markdown
+markdownNode = Markdown . Seq.singleton
+
+instance Semigroup Markdown where
+  -- We represent words in our AST, but CMark doesn't, so we have to manually add spaces when
+  -- joining plain text nodes.
+  Markdown (a Seq.:|> M.Node _ (M.TEXT txt) _) <> Markdown (M.Node _ (M.TEXT txt') _ Seq.:<| b) =
+    Markdown (a <> Seq.singleton (M.Node Nothing (M.TEXT (Text.unwords [txt, txt'])) []) <> b)
+  Markdown a <> Markdown b = Markdown (a <> b)
+
+instance Monoid Markdown where
+  mempty = Markdown mempty
 
 type MarkdownText = Text
 
 data SideContent
   = FrontMatterContent (Map Text [Text])
-  | TooltipContent (Markdown)
+  | TooltipContent Markdown
 
 newtype FrontMatterData = FrontMatterData (Map Text [Text])
 
@@ -141,7 +156,7 @@ type MarkdownM = ReaderT Word64 (Writer (Seq SideContent))
 toMarkdown :: Map Referent Name -> Doc -> (FrontMatterData, MarkdownText, [MarkdownText])
 toMarkdown docNamesByRef doc =
   ( FrontMatterData frontMatterContent,
-    nodeToMarkdownText [document content],
+    markdownToText $ document content,
     tooltips
   )
   where
@@ -151,20 +166,20 @@ toMarkdown docNamesByRef doc =
       sideContent
         & foldMap \case
           FrontMatterContent map -> (mempty, map)
-          TooltipContent tc -> ([nodeToMarkdownText [tc]], mempty)
+          TooltipContent tc -> ([markdownToText tc], mempty)
 
-    content :: [Markdown]
+    content :: Markdown
     sideContent :: Seq SideContent
     (content, sideContent) = runWriter (runReaderT (toMarkdown_ doc) 1)
 
     toMarkdown_ ::
       Doc ->
-      MarkdownM [Markdown]
+      MarkdownM Markdown
     toMarkdown_ doc =
       let sectionContentToMarkdown ::
-            (Doc -> MarkdownM [Markdown]) ->
+            (Doc -> MarkdownM Markdown) ->
             Doc ->
-            MarkdownM [Markdown]
+            MarkdownM Markdown
           sectionContentToMarkdown renderer doc_ =
             -- Block elements can't be children for <p> elements
             case doc_ of
@@ -187,7 +202,7 @@ toMarkdown docNamesByRef doc =
               Paragraph [Special (Embed _)] -> renderer doc_
               Paragraph [UntitledSection ds] -> do
                 contents <- foldMapM renderer ds
-                pure [paragraph contents]
+                pure $ paragraph contents
               Paragraph [Column _] -> renderer doc_
               Paragraph _ -> renderer doc_
               _ ->
@@ -195,43 +210,43 @@ toMarkdown docNamesByRef doc =
        in case doc of
             Tooltip triggerContent tooltipContent -> do
               tooltip <- toMarkdown_ tooltipContent
-              lift $ Writer.tell (Seq.fromList . fmap TooltipContent $ tooltip)
+              lift $ Writer.tell (Seq.singleton . TooltipContent $ tooltip)
               toMarkdown_ triggerContent
-            Word word -> pure [text word]
+            Word word -> pure $ text word
             Code contents -> do
               result <- toMarkdown_ contents
-              pure [inlineCode result]
+              pure $ inlineCode result
             CodeBlock lang contents -> do
               result <- toMarkdown_ contents
-              pure [codeBlock lang result]
+              pure $ codeBlock lang result
             Bold d -> do
               result <- toMarkdown_ d
-              pure [bold result]
+              pure $ bold result
             Italic d -> do
               result <- toMarkdown_ d
-              pure [italic result]
+              pure $ italic result
             Strikethrough d -> do
               result <- toMarkdown_ d
-              pure [strikethrough result]
-            Style {} -> pure []
+              pure $ strikethrough result
+            Style {} -> pure mempty
             Anchor id' d -> do
-              label <- nodeToMarkdownText <$> toMarkdown_ d
-              pure [link id' label]
+              label <- markdownToText <$> toMarkdown_ d
+              pure $ link id' label
             Blockquote d -> do
               contents <- toMarkdown_ d
-              pure [blockquote contents]
+              pure $ blockquote contents
             Blankline ->
-              pure [linebreak, linebreak]
+              pure $ linebreak <> linebreak
             Linebreak ->
-              pure [linebreak]
+              pure linebreak
             SectionBreak ->
-              pure [sectionBreak]
+              pure sectionBreak
             Aside d -> do
               contents <- toMarkdown_ d
-              pure [aside contents]
+              pure $ aside contents
             Callout icon content -> do
               contents <- toMarkdown_ content
-              pure [callout (text ico) contents]
+              pure $ callout (text ico) contents
               where
                 (ico :: Text) =
                   case icon of
@@ -241,7 +256,7 @@ toMarkdown docNamesByRef doc =
                     Nothing -> ("")
             Table rows -> do
               renderedRows <- traverse (traverse toMarkdown_) rows
-              pure [table renderedRows]
+              pure $ table renderedRows
             -- TODO: test out fold behaviour
             Folded _isFolded _summary details -> do
               -- summary' <- toMarkdown_ summary
@@ -253,31 +268,31 @@ toMarkdown docNamesByRef doc =
                   toMarkdown_ d
                 ds -> do
                   rendered <- foldMapM toMarkdown_ ds
-                  pure [paragraph rendered]
+                  pure $ paragraph rendered
             BulletedList items -> do
               rendered <- traverse toMarkdown_ items
-              pure [bulletList rendered]
+              pure $ bulletList rendered
             NumberedList startNum items -> do
               rendered <- traverse toMarkdown_ items
-              pure [numberedList startNum rendered]
+              pure $ numberedList startNum rendered
             Section title docs -> do
               sectionLevel <- ask
               renderedTitle <- toMarkdown_ title
               body <- local (+ 1) $ foldMapM (sectionContentToMarkdown toMarkdown_) docs
-              pure $ (heading sectionLevel renderedTitle : body)
+              pure $ heading sectionLevel renderedTitle <> body
             NamedLink label url -> do
               renderedLabel <- toMarkdown_ label
               pure $ case normalizeHref docNamesByRef url of
-                Href h -> [link h (nodeToMarkdownText renderedLabel)]
+                Href h -> link h (markdownToText renderedLabel)
                 -- We don't currently support linking to other docs within markdown.
                 DocLinkHref _name -> renderedLabel
                 ReferenceHref _ref -> renderedLabel
                 InvalidHref -> renderedLabel
             Image altText src caption -> do
-              renderedAltText <- nodeToMarkdownText <$> toMarkdown_ altText
+              renderedAltText <- markdownToText <$> toMarkdown_ altText
               renderedCaption <- traverse toMarkdown_ caption
               let srcText = toText "" src
-              pure $ [image srcText renderedAltText] <> (fromMaybe [] renderedCaption)
+              pure $ image srcText renderedAltText <> (fromMaybe mempty renderedCaption)
             Special specialForm ->
               case specialForm of
                 Source sources ->
@@ -286,100 +301,102 @@ toMarkdown docNamesByRef doc =
                   -- We can't fold in markdown
                   pure $ foldMap (foldMap toMarkdownSource . embeddedSource) sources
                 Example syntax ->
-                  pure [inlineCodeText (Syntax.toPlainText syntax)]
+                  pure $ inlineCodeText (Syntax.toPlainText syntax)
                 ExampleBlock syntax ->
-                  pure [codeBlockText "unison" (Syntax.toPlainText syntax)]
+                  pure $ codeBlockText "unison" (Syntax.toPlainText syntax)
                 Link syntax ->
                   -- TODO: Is this correct?
-                  pure [inlineCodeText (Syntax.toPlainText syntax)]
+                  pure $ inlineCodeText (Syntax.toPlainText syntax)
                 Signature signatures ->
-                  pure (codeBlockText "unison " . Syntax.toPlainText <$> signatures)
+                  signatures
+                    & foldMap (codeBlockText "unison " . Syntax.toPlainText)
+                    & pure
                 SignatureInline sig ->
-                  pure [inlineCodeText $ Syntax.toPlainText sig]
+                  pure . inlineCodeText $ Syntax.toPlainText sig
                 Eval source result -> do
-                  pure
-                    [ codeBlockText "unison" $
-                        Text.unlines
+                  pure $
+                    codeBlockText
+                      "unison"
+                      ( Text.unlines
                           [ Syntax.toPlainText source,
                             "⧨",
                             Syntax.toPlainText result
                           ]
-                    ]
+                      )
                 EvalInline source result ->
                   --  I'm not sure of a good way to express this 'inline' in markdown
-                  pure
-                    [ codeBlockText "unison" $
-                        Text.unlines
-                          [ Syntax.toPlainText source,
-                            "⧨",
-                            Syntax.toPlainText result
-                          ]
-                    ]
+                  pure $
+                    codeBlockText "unison" $
+                      Text.unlines
+                        [ Syntax.toPlainText source,
+                          "⧨",
+                          Syntax.toPlainText result
+                        ]
                 Video sources _attrs ->
                   case sources of
-                    [] -> pure []
+                    [] -> pure mempty
                     (MediaSource src _ : _) -> do
-                      pure [video src ""]
+                      pure $ video src ""
                 Doc.FrontMatter fm -> do
                   lift $ Writer.tell (Seq.singleton $ FrontMatterContent fm)
                   pure mempty
-                LaTeXInline latex -> pure [codeBlockText "latex" latex]
+                LaTeXInline latex -> pure $ codeBlockText "latex" latex
                 Svg src ->
-                  pure [svg src]
+                  pure $ svg src
                 Embed syntax ->
-                  pure [codeBlockText "unison" (Syntax.toPlainText syntax)]
+                  pure $ codeBlockText "unison" (Syntax.toPlainText syntax)
                 EmbedInline syntax ->
-                  pure [inlineCodeText (Syntax.toPlainText syntax)]
+                  pure $ inlineCodeText (Syntax.toPlainText syntax)
                 RenderError (InvalidTerm err) ->
-                  pure [text $ Syntax.toPlainText err]
+                  pure . text $ Syntax.toPlainText err
             Join docs -> foldMapM toMarkdown_ docs
             UntitledSection docs -> foldMapM (sectionContentToMarkdown toMarkdown_) docs
             Column docs -> foldMapM toMarkdown_ docs
             Group content -> toMarkdown_ content
 
-nodeToMarkdownText :: [M.Node] -> MarkdownText
-nodeToMarkdownText = \case
+markdownToText :: Markdown -> MarkdownText
+markdownToText (Markdown ns) = case toList ns of
   [] -> mempty
   [n] -> M.nodeToCommonmark markdownOptions Nothing n
   nodes -> M.nodeToCommonmark markdownOptions Nothing (M.Node Nothing M.PARAGRAPH nodes)
 
 type BlockType = Text
 
-codeBlock :: BlockType -> [Markdown] -> Markdown
-codeBlock typ contents = M.Node Nothing (M.CODE_BLOCK typ $ nodeToMarkdownText contents) []
+codeBlock :: BlockType -> Markdown -> Markdown
+codeBlock typ contents = markdownNode (M.Node Nothing (M.CODE_BLOCK typ $ markdownToText contents) [])
 
 codeBlockText :: BlockType -> Text -> Markdown
-codeBlockText typ contents = M.Node Nothing (M.CODE_BLOCK typ contents) []
+codeBlockText typ contents = markdownNode $ M.Node Nothing (M.CODE_BLOCK typ contents) []
 
-inlineCode :: [Markdown] -> Markdown
-inlineCode contents = M.Node Nothing (M.CODE $ nodeToMarkdownText contents) []
+inlineCode :: Markdown -> Markdown
+inlineCode contents = markdownNode $ M.Node Nothing (M.CODE $ markdownToText contents) []
 
 inlineCodeText :: Text -> Markdown
-inlineCodeText contents = M.Node Nothing (M.CODE contents) []
+inlineCodeText contents = markdownNode $ M.Node Nothing (M.CODE contents) []
 
 text :: Text -> Markdown
-text contents = M.Node Nothing (M.TEXT contents) []
+text contents = markdownNode $ M.Node Nothing (M.TEXT contents) []
 
-bold :: [Markdown] -> Markdown
-bold contents = M.Node Nothing M.STRONG contents
+bold :: Markdown -> Markdown
+bold contents = markdownNode $ M.Node Nothing M.STRONG (toNodes contents)
 
-italic :: [Markdown] -> Markdown
-italic contents = M.Node Nothing M.EMPH contents
+italic :: Markdown -> Markdown
+italic contents = markdownNode $ M.Node Nothing M.EMPH (toNodes contents)
 
-strikethrough :: [Markdown] -> Markdown
-strikethrough contents = M.Node Nothing M.STRIKETHROUGH contents
+strikethrough :: Markdown -> Markdown
+strikethrough contents = markdownNode $ M.Node Nothing M.STRIKETHROUGH (toNodes contents)
 
-paragraph :: [Markdown] -> Markdown
-paragraph contents = M.Node Nothing M.PARAGRAPH contents
+paragraph :: Markdown -> Markdown
+paragraph contents = markdownNode $ M.Node Nothing M.PARAGRAPH (toNodes contents)
 
-document :: [Markdown] -> Markdown
-document contents = M.Node Nothing M.DOCUMENT contents
+document :: Markdown -> Markdown
+document contents = markdownNode $ M.Node Nothing M.DOCUMENT (toNodes contents)
 
 link :: Text -> Text -> Markdown
-link url label = M.Node Nothing (M.LINK url label) []
+link url label = markdownNode $ M.Node Nothing (M.LINK url label) []
 
 image :: Text -> Text -> Markdown
-image url label = M.Node Nothing (M.IMAGE url label) []
+image url label = markdownNode $ M.Node Nothing (M.IMAGE url label) []
 
 svg :: Text -> Markdown
 svg _src =
@@ -390,33 +407,35 @@ svg _src =
 video :: Text -> Text -> Markdown
 video url label = image url label
 
-blockquote :: [Markdown] -> Markdown
-blockquote contents = M.Node Nothing M.BLOCK_QUOTE contents
+blockquote :: Markdown -> Markdown
+blockquote contents = markdownNode $ M.Node Nothing M.BLOCK_QUOTE (toNodes contents)
 
 linebreak :: Markdown
-linebreak = M.Node Nothing M.LINEBREAK []
+linebreak = markdownNode $ M.Node Nothing M.LINEBREAK []
 
 sectionBreak :: Markdown
-sectionBreak = M.Node Nothing M.THEMATIC_BREAK []
+sectionBreak = markdownNode $ M.Node Nothing M.THEMATIC_BREAK []
 
-table :: [[[Markdown]]] -> Markdown
+table :: [[Markdown]] -> Markdown
 table rows =
   rows
-    & (fmap . fmap) (M.Node Nothing M.TABLE_CELL)
+    & (fmap . fmap) (M.Node Nothing M.TABLE_CELL . toNodes)
     & fmap (M.Node Nothing M.TABLE_ROW)
     & M.Node Nothing (M.TABLE [M.NoAlignment])
+    & markdownNode
 
-aside :: [Markdown] -> Markdown
+aside :: Markdown -> Markdown
 aside = blockquote
 
-callout :: Markdown -> [Markdown] -> Markdown
-callout ico contents = blockquote $ ico : linebreak : contents
+callout :: Markdown -> Markdown -> Markdown
+callout ico contents = blockquote $ ico <> linebreak <> contents
 
-bulletList :: [[Markdown]] -> Markdown
+bulletList :: [Markdown] -> Markdown
 bulletList items =
   items
-    & fmap (M.Node Nothing M.ITEM)
+    & fmap (M.Node Nothing M.ITEM . toNodes)
     & M.Node Nothing (M.LIST listAttrs)
+    & markdownNode
   where
     listAttrs =
       M.ListAttributes
@@ -428,11 +447,12 @@ bulletList items =
           listDelim = M.PERIOD_DELIM
         }
 
-numberedList :: Word64 -> [[Markdown]] -> Markdown
+numberedList :: Word64 -> [Markdown] -> Markdown
 numberedList startNum items =
   items
-    & fmap (M.Node Nothing M.ITEM)
+    & fmap (M.Node Nothing M.ITEM . toNodes)
     & M.Node Nothing (M.LIST listAttrs)
+    & markdownNode
   where
     listAttrs =
       M.ListAttributes
@@ -444,5 +464,5 @@ numberedList startNum items =
           listDelim = M.PERIOD_DELIM
         }
 
-heading :: Word64 -> [Markdown] -> Markdown
-heading level contents = M.Node Nothing (M.HEADING $ fromIntegral level) contents
+heading :: Word64 -> Markdown -> Markdown
+heading level contents = markdownNode $ M.Node Nothing (M.HEADING $ fromIntegral level) (toNodes contents)
