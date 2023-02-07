61dc3a32d49f77705949db12d28caeb1ae790c23
Jared Forsyth <jared@jaredforsyth.com>
2023-02-06 20:33:57 -0600

    [racket-fixes] nicer

--- scheme-libs/racket/unison/isolated-string-copy.rkt
+++ scheme-libs/racket/unison/isolated-string-copy.rkt
@@ -1,3 +1,4 @@
+#lang racket/base
 ; This library exists solely to
 ; export "string-copy!" for use by "core.ss"
 ;
@@ -5,7 +6,7 @@
 ; as racket/base has name clashes with rnrs
 ; and the #r6rs language can't handle name
 ; clashes (the normal racket language can)
-(module isolated-string-copy racket
-    (provide string-copy!)
 
-    (require racket/base))
+(provide string-copy!)
+
+(require racket/base)
