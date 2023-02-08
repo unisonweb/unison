86a0b7c5865551ff243b5f70e9da038ed5c7e2a8
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-02-08 12:04:00 -0500

    push some uncommitted project.push scraps from the other week

--- unison-cli/src/Unison/Codebase/Editor/HandleInput/ProjectPush.hs
+++ unison-cli/src/Unison/Codebase/Editor/HandleInput/ProjectPush.hs
@@ -4,9 +4,45 @@ module Unison.Codebase.Editor.HandleInput.ProjectPush
   )
 where
 
+import Control.Lens ((^.))
 import Unison.Cli.Monad (Cli)
+import Unison.Cli.ProjectUtils (getCurrentProjectBranch)
+import Unison.Prelude
+import Unison.Project (ProjectAndBranch, ProjectBranchName, ProjectName)
+import Witch (unsafeFrom)
 
 -- | Push a project branch.
-projectPush :: Cli ()
-projectPush = do
+projectPush :: Maybe (ProjectAndBranch ProjectName (Maybe ProjectBranchName)) -> Cli ()
+projectPush maybeProjectAndBranch = do
+  (projectId, currentBranchId) <- getCurrentProjectBranch & onNothingM (error "not on branch")
+
+  -- Resolve where to push:
+  --   if (project/branch names provided)
+  --     if (ids in remote_project / remote_project_branch tables)
+  --       use those
+  --     else
+  --       ask Share
+  --   else if (default push location exists),
+  --     if (its remote branch id is non-null)
+  --       use that
+  --     else
+  --       if (this branch name exists in that project)
+  --         use that
+  --       else
+  --         create a branch with this name
+  --   else
+  --     ask Share for my username
+  --     if (I'm not logged in)
+  --       fail -- don't know where to push
+  --     else
+  --
+
+  projectAndBranch <-
+    case maybeProjectAndBranch of
+      Just projectAndBranch -> pure projectAndBranch
+      Nothing -> error "must provide project and branch"
+
+  let projectName = projectAndBranch ^. #project
+  let branchName = fromMaybe (unsafeFrom @Text "main") (projectAndBranch ^. #branch)
+
   pure ()
