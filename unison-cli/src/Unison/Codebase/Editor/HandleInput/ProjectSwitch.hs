c39ff049101554c26c46627a3cfa6ee3d7d7c79d
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-02-08 12:44:08 -0500

    couple debug statements

--- unison-cli/src/Unison/Codebase/Editor/HandleInput/ProjectSwitch.hs
+++ unison-cli/src/Unison/Codebase/Editor/HandleInput/ProjectSwitch.hs
@@ -15,7 +15,7 @@ import qualified Unison.Codebase.Path as Path
 import Unison.Prelude
 import Unison.Project (ProjectAndBranch (..), ProjectBranchName, ProjectName)
 import qualified Unison.Sqlite as Sqlite
-import Witch (into, unsafeFrom)
+import Witch (unsafeFrom)
 
 -- | Switch to (or create) a project or project branch.
 projectSwitch :: ProjectAndBranch (Maybe ProjectName) (Maybe ProjectBranchName) -> Cli ()
@@ -24,7 +24,6 @@ projectSwitch projectAndBranch =
     (Just projectName, Just branchName) -> switchToProjectAndBranch projectName branchName
     (Just projectName, Nothing) -> switchToProject projectName
     (Nothing, Just branchName) -> switchToBranch branchName
-    -- Kind of an impossible case :shrug:
     (Nothing, Nothing) -> pure ()
 
 -- Switch to a project name (going to branch "main" for now, but ideally we'd store the last branch a user was on).
@@ -39,7 +38,10 @@ data SwitchToBranchOutcome
 -- Switch to a branch in the current project. If it doesn't exist, create it.
 switchToBranch :: ProjectBranchName -> Cli ()
 switchToBranch branchName = do
-  (projectId, currentBranchId) <- getCurrentProjectBranch & onNothingM (error "not on project branch")
+  (projectId, currentBranchId) <-
+    getCurrentProjectBranch & onNothingM do
+      liftIO (putStrLn "[coolbeans] Not currently on a branch")
+      Cli.returnEarlyWithoutOutput
   (outcome, newBranchId) <-
     Cli.runTransaction do
       Queries.loadProjectBranchByName projectId (into @Text branchName) >>= \case
@@ -53,9 +55,8 @@ switchToBranch branchName = do
   case outcome of
     SwitchedToExistingBranch -> pure ()
     SwitchedToNewBranch -> do
-      Cli.stepAt "project.create-branch" (Path.unabsolute path, id) -- id creates empty branch
-      -- TODO print "I created a new branch"
-      pure ()
+      Cli.stepAt "project.switch" (Path.unabsolute path, id) -- id creates empty branch
+      liftIO (putStrLn "[coolbeans] I created a new branch")
   Cli.cd path
 
 -- Switch to a project+branch.
@@ -71,8 +72,8 @@ switchToProjectAndBranch projectName branchName = do
             Queries.loadProjectBranchByName projectId (into @Text branchName) <&> \case
               Nothing -> Nothing
               Just branch -> Just (projectId, branch ^. #branchId)
-    case maybeIds of
-      Just ids -> pure ids
-      Nothing -> error "project not found"
+    maybeIds & onNothing do
+      liftIO (putStrLn "[coolbeans] No such project")
+      Cli.returnEarlyWithoutOutput
 
   Cli.cd (projectBranchPath projectId branchId)
