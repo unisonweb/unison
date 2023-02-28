e3bf626ccde9370dcdf2a0945d2e9f1941478075
Jared Forsyth <jared@jaredforsyth.com>
2023-02-28 06:14:32 -0600

    [tcp] socket accept

--- scheme-libs/common/unison/primops.ss
+++ scheme-libs/common/unison/primops.ss
@@ -162,6 +162,10 @@
     unison-FOp-IO.closeSocket.impl.v3
     unison-FOp-IO.socketReceive.impl.v3
     unison-FOp-IO.socketSend.impl.v3
+    unison-FOp-IO.socketPort.impl.v3
+    unison-FOp-IO.serverSocket.impl.v3
+    unison-FOp-IO.socketAccept.impl.v3
+    unison-FOp-IO.listen.impl.v3
     )
 
   (import (rnrs)
@@ -241,6 +245,8 @@
   (define (unison-POp-TAKT n t) (istring-take n t))
   (define (unison-POp-TRCE s x)
     (display s)
+    (display "\n")
+    (display x)
     (display "\n")
     (display (describe-value x))
     (display "\n"))
--- scheme-libs/racket/unison/tcp.rkt
+++ scheme-libs/racket/unison/tcp.rkt
@@ -1,36 +1,77 @@
 ; TLS primitives! Supplied by openssl (libssl)
 #lang racket/base
 (require racket/exn
+         racket/match
          racket/tcp
          unison/data)
 
-(provide (prefix-out unison-FOp-IO.   (combine-out
-        clientSocket.impl.v3
-        closeSocket.impl.v3
-        socketReceive.impl.v3
-        socketSend.impl.v3)))
+(provide
+ (prefix-out
+  unison-FOp-IO.
+  (combine-out
+   clientSocket.impl.v3
+   closeSocket.impl.v3
+   socketReceive.impl.v3
+   socketPort.impl.v3
+   serverSocket.impl.v3
+   listen.impl.v3
+   socketAccept.impl.v3
+   socketSend.impl.v3)))
 
 (define (input socket) (car socket))
 (define (output socket) (car (cdr socket)))
 
 (define (closeSocket.impl.v3 socket)
-    (close-input-port (input socket))
-    (close-output-port (output socket))
-    (right none))
+  (close-input-port (input socket))
+  (close-output-port (output socket))
+  (right none))
 
 (define (clientSocket.impl.v3 host port)
-    (with-handlers
-        [[exn:fail:network? (lambda (e) (exception "IOFailure" (exn->string e) '()))]
-         [exn:fail:contract? (lambda (e) (exception "InvalidArguments" (exn->string e) '()))]
-         [(lambda _ #t) (lambda (e) (exception "MiscFailure" "Unknown exception" e))] ]
+  (with-handlers
+      [[exn:fail:network? (lambda (e) (exception "IOFailure" (exn->string e) '()))]
+       [exn:fail:contract? (lambda (e) (exception "InvalidArguments" (exn->string e) '()))]
+       [(lambda _ #t) (lambda (e) (exception "MiscFailure" "Unknown exception" e))] ]
 
-        (let-values ([(input output) (tcp-connect host (string->number port))])
-            (right (list input output)))))
+    (let-values ([(input output) (tcp-connect host (string->number port))])
+      (right (list input output)))))
 
 (define (socketSend.impl.v3 socket data)
-    (write-bytes data (output socket))
-    (flush-output (output socket))
-    (right none))
+  (write-bytes data (output socket))
+  (flush-output (output socket))
+  (right none))
 
 (define (socketReceive.impl.v3 socket amt)
-    (right (read-bytes amt (input socket))))
+  (let ([buffer (make-bytes amt)])
+    (read-bytes-avail! buffer (input socket))
+    (right buffer))
+  )
+
+(define (socketPort.impl.v3 socket)
+  (let-values ([(_ local-port __ ___) (tcp-addresses (input socket) #t)])
+    (right local-port)))
+
+(define serverSocket.impl.v3
+  (lambda args
+    (let-values ([(hostname port)
+                  (match args
+                    [(list _ port) (values #f port)]
+                    [(list _ hostname port) (values hostname port)])])
+
+      (with-handlers
+          [[exn:fail:network? (lambda (e) (exception "IOFailure" (exn->string e) '()))]
+           [exn:fail:contract? (lambda (e) (exception "InvalidArguments" (exn->string e) '()))]
+           [(lambda _ #t) (lambda (e) (exception "MiscFailure" "Unknown exception" e))] ]
+        (display "hostname ")
+        (display hostname)
+        (display "\n")
+        (let ([listener (tcp-listen (string->number port) 4 #f (if (equal? 0 hostname) #f hostname))])
+          (right listener))))))
+
+; NOTE: This is a no-op, because racket's public TCP stack doesn't have separate operations for
+; "bind" vs "listen". Because of this, `serverSocket` binds and listens at the same time.
+(define (listen.impl.v3 _listener)
+  (right none))
+
+(define (socketAccept.impl.v3 listener)
+  (let-values ([(input output) (tcp-accept listener)])
+    (right (list input output))))
