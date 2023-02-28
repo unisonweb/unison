d045cb05142f6323305b2e7901673753d8868575
Jared Forsyth <jared@jaredforsyth.com>
2023-02-27 23:53:23 -0600

    [tcp] handle contract

--- scheme-libs/racket/unison/tcp.rkt
+++ scheme-libs/racket/unison/tcp.rkt
@@ -21,6 +21,7 @@
 (define (clientSocket.impl.v3 host port)
     (with-handlers
         [[exn:fail:network? (lambda (e) (exception "IOFailure" (exn->string e) '()))]
+         [exn:fail:contract? (lambda (e) (exception "InvalidArguments" (exn->string e) '()))]
          [(lambda _ #t) (lambda (e) (exception "MiscFailure" "Unknown exception" e))] ]
 
         (let-values ([(input output) (tcp-connect host (string->number port))])
