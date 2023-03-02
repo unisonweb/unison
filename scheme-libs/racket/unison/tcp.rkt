abde5b9f0b74af5da6a8f5f53ed3d2a1d8b6771a
Jared Forsyth <jared@jaredforsyth.com>
2023-03-01 21:59:01 -0600

    [tcp] docs, more defensiveness around arguments

--- scheme-libs/racket/unison/tcp.rkt
+++ scheme-libs/racket/unison/tcp.rkt
@@ -22,8 +22,11 @@
 (define (output socket) (car (cdr socket)))
 
 (define (closeSocket.impl.v3 socket)
-  (close-input-port (input socket))
-  (close-output-port (output socket))
+  (if (pair? socket)
+      (begin
+        (close-input-port (input socket))
+        (close-output-port (output socket)))
+      (tcp-close socket))
   (right none))
 
 (define (clientSocket.impl.v3 host port)
@@ -36,16 +39,23 @@
       (right (list input output)))))
 
 (define (socketSend.impl.v3 socket data)
-  (write-bytes data (output socket))
-  (flush-output (output socket))
-  (right none))
+  (if (not (pair? socket))
+      (exception "InvalidArguments" "Cannot send on a server socket")
+      (begin
+        (write-bytes data (output socket))
+        (flush-output (output socket))
+        (right none))))
 
 (define (socketReceive.impl.v3 socket amt)
-  (let ([buffer (make-bytes amt)])
-    (read-bytes-avail! buffer (input socket))
-    (right buffer))
-  )
+  (if (not (pair? socket))
+      (exception "InvalidArguments" "Cannot receive on a server socket")
+      (begin
+        (let ([buffer (make-bytes amt)])
+          (read-bytes-avail! buffer (input socket))
+          (right buffer)))))
 
+; A "connected" socket is represented as a list of (list input-port output-port),
+; while a "listening" socket is just the tcp-listener itself.
 (define (socketPort.impl.v3 socket)
   (let-values ([(_ local-port __ ___) (tcp-addresses (if (pair? socket) (input socket) socket) #t)])
     (right local-port)))
@@ -64,11 +74,18 @@
         (let ([listener (tcp-listen (string->number port) 4 #f (if (equal? 0 hostname) #f hostname))])
           (right listener))))))
 
-; NOTE: This is a no-op, because racket's public TCP stack doesn't have separate operations for
-; "bind" vs "listen". Because of this, `serverSocket` binds and listens at the same time.
+; NOTE: This is a no-op because racket's public TCP stack doesn't have separate operations for
+; "bind" vs "listen". We've decided to have `serverSocket` do the "bind & listen", and have
+; this do nothing.
+; If we want ~a little better parity with the haskell implementation, we might set a flag or
+; something on the listener, and error if you try to `accept` on a server socket that you haven't
+; called `listen` on yet.
 (define (listen.impl.v3 _listener)
   (right none))
 
 (define (socketAccept.impl.v3 listener)
-  (let-values ([(input output) (tcp-accept listener)])
-    (right (list input output))))
+  (if (pair? listener)
+      (exception "InvalidArguments" "Cannot accept on a non-server socket")
+      (begin
+        (let-values ([(input output) (tcp-accept listener)])
+          (right (list input output))))))
