08d5a108048d5f2e4bbe08acff9dcbb2722d671d
Chris Penner <christopher.penner@gmail.com>
2023-03-03 11:36:56 -0600

    Remove "on conflict do nothing" when building name lookups

--- codebase2/codebase-sqlite/U/Codebase/Sqlite/Queries.hs
+++ codebase2/codebase-sqlite/U/Codebase/Sqlite/Queries.hs
@@ -1657,7 +1657,6 @@ insertScopedTermNames bhId names = do
       [here|
       INSERT INTO scoped_term_name_lookup (root_branch_hash_id, reversed_name, namespace, last_name_segment, referent_builtin, referent_component_hash, referent_component_index, referent_constructor_index, referent_constructor_type)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ON CONFLICT DO NOTHING
         |]
 
 -- | Insert the given set of type names into the name lookup table
@@ -1669,7 +1668,6 @@ insertScopedTypeNames bhId names =
       [here|
       INSERT INTO scoped_type_name_lookup (root_branch_hash_id, reversed_name, namespace, last_name_segment, reference_builtin, reference_component_hash, reference_component_index)
         VALUES (?, ?, ?, ?, ?, ?, ?)
-        ON CONFLICT DO NOTHING
         |]
 
 -- | Remove the given set of term names into the name lookup table
