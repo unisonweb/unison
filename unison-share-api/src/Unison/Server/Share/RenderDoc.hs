d732c0ca3a66fbd8b003f3b01ad163de81882a82
Chris Penner <christopher.penner@gmail.com>
2023-05-08 18:44:23 -0700

    Add helpers for rendering arbitrary top-level docs.

--- unison-share-api/src/Unison/Server/Share/RenderDoc.hs
+++ unison-share-api/src/Unison/Server/Share/RenderDoc.hs
@@ -7,12 +7,12 @@
 {-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
-module Unison.Server.Endpoints.NamespaceDetails where
+-- | Helper for rendering docs within a given namespace
+module Unison.Server.Share.RenderDoc where
 
 import Control.Monad.Except
 import Data.Aeson
 import Data.OpenApi (ToSchema)
-import qualified Data.Set as Set
 import Servant (Capture, QueryParam, (:>))
 import Servant.Docs (DocCapture (..), ToCapture (..), ToSample (..))
 import Servant.OpenApi ()
@@ -36,51 +36,17 @@ import Unison.Server.Types
   )
 import Unison.Symbol (Symbol)
 import Unison.Util.Pretty (Width)
+import Unison.NameSegment (NameSegment)
 
-type NamespaceDetailsAPI =
-  "namespaces"
-    :> Capture "namespace" Path.Path
-    :> QueryParam "rootBranch" ShortCausalHash
-    :> QueryParam "renderWidth" Width
-    :> APIGet NamespaceDetails
-
-instance ToCapture (Capture "namespace" Text) where
-  toCapture _ =
-    DocCapture
-      "namespace"
-      "The fully qualified name of a namespace. The leading `.` is optional."
-
-instance ToSample NamespaceDetails where
-  toSamples _ =
-    [ ( "When no value is provided for `namespace`, the root namespace `.` is "
-          <> "listed by default",
-        NamespaceDetails
-          Path.empty
-          "#gjlk0dna8dongct6lsd19d1o9hi5n642t8jttga5e81e91fviqjdffem0tlddj7ahodjo5"
-          Nothing
-      )
-    ]
-
-data NamespaceDetails = NamespaceDetails
-  { fqn :: Path.Path,
-    hash :: UnisonHash,
-    readme :: Maybe Doc
-  }
-  deriving (Generic, Show)
-
-instance ToJSON NamespaceDetails where
-  toEncoding = genericToEncoding defaultOptions
-
-deriving instance ToSchema NamespaceDetails
-
-namespaceDetails ::
+renderDoc ::
+  Set NameSegment ->
   Rt.Runtime Symbol ->
   Codebase IO Symbol Ann ->
   Path.Path ->
   Maybe (Either ShortCausalHash CausalHash) ->
   Maybe Width ->
-  Backend IO NamespaceDetails
-namespaceDetails runtime codebase namespacePath mayRoot mayWidth =
+  Backend IO (Maybe Doc)
+renderDoc docNames runtime codebase namespacePath mayRoot mayWidth =
   let width = mayDefaultWidth mayWidth
    in do
         (rootCausal, namespaceCausal, shallowBranch) <-
@@ -94,19 +60,13 @@ namespaceDetails runtime codebase namespacePath mayRoot mayWidth =
             namespaceCausal <- lift $ Codebase.getShallowCausalAtPath namespacePath (Just rootCausalHash)
             shallowBranch <- lift $ V2Causal.value namespaceCausal
             pure (rootCausalHash, namespaceCausal, shallowBranch)
-        namespaceDetails <- do
-          (_localNamesOnly, ppe) <- Backend.scopedNamesForBranchHash codebase (Just rootCausal) namespacePath
-          readme <-
-            Backend.findDocInBranchAndRender
-              readmeNames
-              width
-              runtime
-              codebase
-              ppe
-              shallowBranch
-          let causalHash = v2CausalBranchToUnisonHash namespaceCausal
-          pure $ NamespaceDetails namespacePath causalHash readme
-
-        pure $ namespaceDetails
-  where
-    readmeNames = Set.fromList ["README", "Readme", "ReadMe", "readme"]
+        (_localNamesOnly, ppe) <- Backend.scopedNamesForBranchHash codebase (Just rootCausal) namespacePath
+        renderedDoc <-
+          Backend.findDocInBranchAndRender
+            docNames
+            width
+            runtime
+            codebase
+            ppe
+            shallowBranch
+        pure renderedDoc
