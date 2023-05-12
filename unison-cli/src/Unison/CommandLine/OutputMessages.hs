0004980021595631a883aaf82d3d378817d95ab3
Travis Staton <hello@travisstaton.com>
2023-05-12 11:26:17 -0400

    ormolu

--- unison-cli/src/Unison/CommandLine/OutputMessages.hs
+++ unison-cli/src/Unison/CommandLine/OutputMessages.hs
@@ -2788,7 +2788,7 @@ renderEditConflicts ppe Patch {..} = do
                  then "deprecated and also replaced with"
                  else "replaced with"
              )
-          `P.hang` P.lines replacements
+            `P.hang` P.lines replacements
     formatTermEdits ::
       (Reference.TermReference, Set TermEdit.TermEdit) ->
       Numbered Pretty
@@ -2803,7 +2803,7 @@ renderEditConflicts ppe Patch {..} = do
                  then "deprecated and also replaced with"
                  else "replaced with"
              )
-          `P.hang` P.lines replacements
+            `P.hang` P.lines replacements
     formatConflict ::
       Either
         (Reference, Set TypeEdit.TypeEdit)
