63f62b6ad5147f76a5206aa02660fab0e7c98d98
Chris Penner <christopher.penner@gmail.com>
2023-05-23 09:14:08 -0600

    Remove uni-patterns

--- unison-share-api/src/Unison/Server/Doc.hs
+++ unison-share-api/src/Unison/Server/Doc.hs
@@ -5,7 +5,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
 module Unison.Server.Doc where
 
@@ -360,7 +359,9 @@ evalDoc terms typeOf eval types tm =
          in goSignatures rs <&> \s -> ESignature s
       -- SignatureInline Doc2.Term
       DD.Doc2SpecialFormSignatureInline (DD.Doc2Term (Term.Referent' r)) ->
-        goSignatures [r] <&> \[s] -> ESignatureInline s
+        goSignatures [r] <&> \case
+          [s] -> ESignatureInline s
+          _ -> error "impossible error: evalDoc: expected exactly one signature"
       -- Eval Doc2.Term
       DD.Doc2SpecialFormEval (DD.Doc2Term tm) -> do
         result <- eval tm
