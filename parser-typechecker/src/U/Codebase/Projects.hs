05e0a5ff2c8f876960863e72a4ea0075530991e5
Chris Penner <christopher.penner@gmail.com>
2023-06-05 10:19:30 -0600

    Use non-empty children when computing dependency mounts

--- parser-typechecker/src/U/Codebase/Projects.hs
+++ parser-typechecker/src/U/Codebase/Projects.hs
@@ -6,6 +6,7 @@ where
 import Control.Lens (ifoldMap)
 import Data.Map qualified as Map
 import U.Codebase.Branch
+import U.Codebase.Branch qualified as Branch
 import U.Codebase.Causal qualified as Causal
 import U.Codebase.HashTags (BranchHash (..))
 import Unison.Codebase.Path
@@ -23,21 +24,24 @@ import Unison.Util.Monoid (ifoldMapM)
 -- For the top-level name lookup of a user codebase it returns the project roots, and will return something like:
 -- @[(public.nested.myproject.latest, #abc), (public.other.namespace.otherproject.main, #def)]@
 inferDependencyMounts :: Branch Sqlite.Transaction -> Sqlite.Transaction [(Path, BranchHash)]
-inferDependencyMounts Branch {children} =
+inferDependencyMounts branch = do
+  children <- Branch.nonEmptyChildren branch
   do
     children
     & ifoldMapM \segment child -> do
       case segment of
         seg
           | seg == libSegment -> do
-              Branch {children = deps} <- Causal.value child
+              childBranch <- Causal.value child
+              deps <- Branch.nonEmptyChildren childBranch
               deps
                 & ( ifoldMap \depName depBranch ->
                       [(Path.fromList [seg, depName], Causal.valueHash depBranch)]
                   )
                 & pure
           | otherwise -> do
-              childBranch@Branch {children = nestedChildren} <- Causal.value child
+              childBranch <- Causal.value child
+              nestedChildren <- Branch.nonEmptyChildren childBranch
               -- If a given child has a lib child, then it's inferred to be a project root.
               -- This allows us to detect most project roots in loose code.
               -- Note, we only do this on children nested at least one level deep
