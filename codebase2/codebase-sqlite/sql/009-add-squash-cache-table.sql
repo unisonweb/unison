560a6074394ba79e380d25d64c138e2b0d97ba6f
Chris Penner <christopher.penner@gmail.com>
2023-06-19 12:34:00 -0600

    Fix squash_results sql

--- codebase2/codebase-sqlite/sql/009-add-squash-cache-table.sql
+++ codebase2/codebase-sqlite/sql/009-add-squash-cache-table.sql
@@ -6,5 +6,5 @@ CREATE TABLE "squash_results" (
   branch_hash_id INTEGER PRIMARY KEY NOT NULL REFERENCES causal(value_hash_id),
 
   -- The causal hash id which is the result of squashing the 'branch_hash_id's causal.'
-  squashed_causal_hash_id INTEGER NOT NULL REFERENCES causal(self_hash_id),
+  squashed_causal_hash_id INTEGER NOT NULL REFERENCES causal(self_hash_id)
 ) WITHOUT ROWID;
