64c558804f41ccaa5d88b568106761750f2f83e6
Chris Penner <christopher.penner@gmail.com>
2023-06-19 13:22:16 -0600

    Remove duplicate hashCausal

--- parser-typechecker/src/U/Codebase/Causal/Squash.hs
+++ parser-typechecker/src/U/Codebase/Causal/Squash.hs
@@ -1,24 +1,13 @@
 module U.Codebase.Causal.Squash (squashCausal) where
 
-import Data.Set
-import Data.Set qualified as Set
 import U.Codebase.Branch.Hashing qualified as Branch
 import U.Codebase.Branch.Type
 import U.Codebase.Causal (Causal (..))
-import U.Codebase.HashTags (BranchHash (..), CausalHash (..))
+import U.Codebase.Causal.Hashing (hashCausal)
 import U.Codebase.Sqlite.Operations qualified as SqliteOps
 import U.Codebase.Sqlite.V2.HashHandle (v2HashHandle)
-import Unison.Hashing.V2 qualified as Hashing
 import Unison.Prelude
 import Unison.Sqlite qualified as Sqlite
-
-hashCausal :: Set CausalHash -> BranchHash -> CausalHash
-hashCausal ancestors branchHash =
-  CausalHash . Hashing.contentHash $
-    Hashing.Causal
-      { Hashing.branchHash = unBranchHash branchHash,
-        Hashing.parents = Set.map unCausalHash ancestors
-      }
 
 -- Recursively discards history, resulting in a namespace tree with only single a single
 -- Causal node at every level.
