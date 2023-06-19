0775a7f9f6bbdec1cf78f1141e03a2edbdd52f8b
Chris Penner <christopher.penner@gmail.com>
2023-06-19 12:40:47 -0600

    Fix foreign key reference

--- codebase2/codebase-sqlite/sql/009-add-squash-cache-table.sql
+++ codebase2/codebase-sqlite/sql/009-add-squash-cache-table.sql
@@ -3,7 +3,7 @@
 CREATE TABLE "squash_results" (
   -- The branch hash of the namespace to be squashed.
   -- There should only ever be one result for each unsquashed value hash.
-  branch_hash_id INTEGER PRIMARY KEY NOT NULL REFERENCES causal(value_hash_id),
+  branch_hash_id INTEGER PRIMARY KEY NOT NULL REFERENCES hash(id),
 
   -- The causal hash id which is the result of squashing the 'branch_hash_id's causal.'
   squashed_causal_hash_id INTEGER NOT NULL REFERENCES causal(self_hash_id)
