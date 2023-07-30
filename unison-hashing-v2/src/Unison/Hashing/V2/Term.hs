5f4f79f1c9de135d33740f836499e45ffbf7f44a
Rúnar <runarorama@gmail.com>
2023-07-30 15:56:58 -0400

    Picking another tag

--- unison-hashing-v2/src/Unison/Hashing/V2/Term.hs
+++ unison-hashing-v2/src/Unison/Hashing/V2/Term.hs
@@ -162,8 +162,7 @@ instance (Var v) => Hashable1 (TermF v a p) where
                         [tag 1, Hashable.Text (Text.pack s)]
                       B.Recorded (B.Resolve _ s) ->
                         [tag 2, Hashable.Text (Text.pack s)]
-                      B.Recorded (B.MissingResultPlaceholder _) ->
-                        [tag 3, Hashable.Text (Text.pack "_")]
+                      B.Recorded (B.MissingResultPlaceholder _) -> [tag 247]
                   TermRef (ReferenceBuiltin name) -> [tag 2, accumulateToken name]
                   TermApp a a2 -> [tag 3, hashed (hash a), hashed (hash a2)]
                   TermAnn a t -> [tag 4, hashed (hash a), hashed (ABT.hash t)]
