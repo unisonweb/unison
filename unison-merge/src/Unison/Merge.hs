617fd74286165b82090035345a4db1ee75c1e206
Travis Staton <hello@travisstaton.com>
2023-08-08 12:41:07 -0400

    v2 stuff

--- unison-merge/src/Unison/Merge.hs
+++ unison-merge/src/Unison/Merge.hs
@@ -1,13 +1,12 @@
 module Unison.Merge () where
 
-import Data.Graph qualified as Graph
+
 import Data.Map.Strict qualified as Map
-import Data.Set qualified as Set
 import Unison.ABT qualified as ABT
-import Unison.DataDeclaration (DataDeclaration, Decl)
-import Unison.DataDeclaration qualified as Decl
+import U.Codebase.Decl (Decl)
+import U.Codebase.Decl qualified as Decl
 import Unison.Prelude
-import Unison.Type (Type)
+import U.Codebase.Type ()
 import Unison.Util.Relation (Relation)
 import Unison.Util.Relation qualified as Relation
 
@@ -23,45 +22,10 @@ type ConstructorMapping = forall a. [a] -> [a]
 computeTypeECs :: Map reference reference -> Map reference reference -> [Set reference]
 computeTypeECs = undefined
 
-candidateUpdatesToReachabilityGraph ::
-  forall reference.
-  Ord reference =>
-  Relation reference reference ->
-  reference ->
-  reference ->
-  Bool
-candidateUpdatesToReachabilityGraph inputRelation =
-  let adjList :: [(reference, reference, [reference])]
-      adjList = (\(i, sos) -> (i, i, Set.toList sos)) <$> Map.toList (Relation.domain inputRelation)
-      (graph, lookupNode, lookupVertex) = Graph.graphFromEdges adjList
-      transitiveClosure :: Map reference (Set reference)
-      transitiveClosure =
-        let vertices :: [reference]
-            vertices = Map.keys (Relation.domain inputRelation)
-
-            makeSingletonMap :: reference -> Map reference (Set reference)
-            makeSingletonMap ref =
-              let vertex :: Graph.Vertex
-                  vertex = case lookupVertex ref of
-                    Just v -> v
-                    Nothing -> undefined
-
-                  lookupNode' :: Graph.Vertex -> reference
-                  lookupNode' v = case lookupNode v of
-                    (ref, _, _) -> ref
-                    
-                  reachable :: [reference]
-                  reachable = lookupNode' <$> Graph.reachable graph vertex
-              in Map.singleton ref (Set.fromList reachable)
-        in foldMap makeSingletonMap vertices
-   in \a b -> case Map.lookup a transitiveClosure of
-             Nothing -> False
-             Just refs -> Set.member b refs
-
 computeTypeUserUpdates ::
   forall a m reference v.
   (Monad m, Ord reference, Ord v) =>
-  (reference -> m (Decl v a)) ->
+  (reference -> m (Decl v)) ->
   (reference -> reference -> ConstructorMapping) ->
   Relation reference reference ->
   m (Relation reference reference)
@@ -72,40 +36,34 @@ computeTypeUserUpdates loadDecl constructorMapping allUpdates =
     isUserUpdate0 (oldRef, newRef) = do
       oldDecl <- loadDecl oldRef
       newDecl <- loadDecl newRef
-      pure case (oldDecl, newDecl) of
-        (Left oldDecl1, Left newDecl1) ->
-          isUserUpdate2
-            oldRef
-            (Decl.toDataDecl oldDecl1)
-            newRef
-            (Decl.toDataDecl newDecl1)
-        (Right oldDecl1, Right newDecl1) -> isUserUpdate2 oldRef oldDecl1 newRef newDecl1
-        _ -> True
+      pure
+        case Decl.declType oldDecl == Decl.declType newDecl of
+          True -> isUserUpdate2 oldRef oldDecl newRef newDecl
+          False -> True
 
     isUserUpdate2 ::
       reference ->
-      DataDeclaration v a ->
+      Decl v ->
       reference ->
-      DataDeclaration v a ->
+      Decl v ->
       Bool
     isUserUpdate2 oldRef oldDecl newRef newDecl =
       let oldBounds = boundsIndices oldDecl
           newBounds = boundsIndices newDecl
           -- FIXME rename
-          twiddle bounds = map (\(_, _, ty) -> ABT.vmap (bounds Map.!) ty)
        in or
             [ Decl.modifier oldDecl /= Decl.modifier newDecl,
               Map.size oldBounds /= Map.size newBounds,
-              length (Decl.constructors' oldDecl) /= length (Decl.constructors' newDecl),
+              length (Decl.constructorTypes oldDecl) /= length (Decl.constructorTypes newDecl),
               any
                 isUserUpdate3
                 ( zip
-                    (twiddle oldBounds (Decl.constructors' oldDecl))
-                    (twiddle newBounds (constructorMapping oldRef newRef (Decl.constructors' newDecl)))
+                    (Decl.constructorTypes oldDecl)
+                    (constructorMapping oldRef newRef (Decl.constructorTypes newDecl))
                 )
             ]
       where
-        boundsIndices :: DataDeclaration v a -> Map v Int
+        boundsIndices :: Decl v -> Map v Int
         boundsIndices =
           fst . foldl' step (Map.empty, 0) . Decl.bound
           where
@@ -114,5 +72,5 @@ computeTypeUserUpdates loadDecl constructorMapping allUpdates =
                   !i' = i + 1
                in (acc', i')
 
-    isUserUpdate3 :: (Type Int a, Type Int a) -> Bool
-    isUserUpdate3 (oldType, newType) = undefined
+isUserUpdate3 :: (Decl.Type v, Decl.Type v) -> Bool
+isUserUpdate3 = undefined
