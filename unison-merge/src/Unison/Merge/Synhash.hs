14fbc9c0862a2fae70039e8716ddda7d7ca036c7
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-09-27 10:24:43 -0400

    some minor cleanup in Unison.Merge.Synhash

--- unison-merge/src/Unison/Merge/Synhash.hs
+++ unison-merge/src/Unison/Merge/Synhash.hs
@@ -33,6 +33,7 @@ module Unison.Merge.Synhash
 where
 
 import Data.Char (ord)
+import U.Codebase.Reference (TypeReference)
 import Unison.ABT qualified as ABT
 import Unison.ConstructorReference (GConstructorReference (ConstructorReference))
 import Unison.ConstructorType qualified as CT
@@ -44,9 +45,11 @@ import Unison.Hashable qualified as H
 import Unison.Kind qualified as K
 import Unison.Pattern qualified as Pattern
 import Unison.Prelude
+import Unison.PrettyPrintEnv (PrettyPrintEnv)
 import Unison.PrettyPrintEnv qualified as PPE
 import Unison.Reference (Reference' (..), TypeReferenceId)
-import Unison.Referent qualified as Referent
+import Unison.Referent qualified as V1 (Referent)
+import Unison.Referent qualified as V1.Referent
 import Unison.Syntax.Name qualified as Name (toText)
 import Unison.Term (Term)
 import Unison.Term qualified as Term
@@ -70,13 +73,13 @@ isTermTag = H.Tag 1
 -- | Syntactically hash a type, using reference names rather than hashes.
 -- Two types will have the same syntactic hash if they would
 -- print the the same way under the given pretty-print env.
-hashType :: Var v => PPE.PrettyPrintEnv -> Type v a -> Hash
+hashType :: Var v => PrettyPrintEnv -> Type v a -> Hash
 hashType ppe t = H.accumulate $ hashTypeTokens ppe t
 
 -- | Syntactically hash a term, using reference names rather than hashes.
 -- Two terms will have the same syntactic hash if they would
 -- print the the same way under the given pretty-print env.
-hashTerm :: Var v => PPE.PrettyPrintEnv -> Term v a -> Hash
+hashTerm :: Var v => PrettyPrintEnv -> Term v a -> Hash
 hashTerm ppe t = H.accumulate $ isNotBuiltinTag : hashTermTokens ppe t
 
 hashBuiltinTerm :: Text -> Hash
@@ -88,14 +91,14 @@ hashBuiltinTerm name =
 -- the same sort of decl (both are data decls or both are effect decls),
 -- the unique type guid is the same, and the constructors appear in the
 -- same order and their types have the same syntactic hash.
-hashDecl :: Var v => PPE.PrettyPrintEnv -> TypeReferenceId -> Decl v a -> Hash
+hashDecl :: Var v => PrettyPrintEnv -> TypeReferenceId -> Decl v a -> Hash
 hashDecl ppe r t = H.accumulate $ isNotBuiltinTag : hashDeclTokens ppe r t
 
 hashBuiltinDecl :: Text -> Hash
 hashBuiltinDecl name =
   H.accumulate [isBuiltinTag, isDeclTag, H.Text name]
 
-hashDeclTokens :: Var v => PPE.PrettyPrintEnv -> TypeReferenceId -> Decl v a -> [Token]
+hashDeclTokens :: Var v => PrettyPrintEnv -> TypeReferenceId -> Decl v a -> [Token]
 hashDeclTokens ppe r = \case
   Left (DD.EffectDeclaration dd) -> H.Tag 0 : go CT.Effect dd
   Right dd -> H.Tag 1 : go CT.Data dd
@@ -103,13 +106,10 @@ hashDeclTokens ppe r = \case
     goMod = \case
       DD.Structural -> [H.Tag 0]
       DD.Unique txt -> [H.Tag 1, H.Text txt]
-    goVs vs =
-      H.Nat (fromIntegral (length vs)) : (H.Text . Var.name <$> vs)
+    goVs vs = hashLengthToken vs : (H.Text . Var.name <$> vs)
     -- separating constructor types with tag of 99, which isn't used elsewhere
     goCtor ct ((_, _, ty), i) = H.Tag 99 : ctorName ct i : hashTypeTokens ppe ty
-    ctorName ct i =
-      let cr = ConstructorReference (ReferenceDerived r) i
-       in H.Text (HQ.toTextWith Name.toText $ PPE.termNameOrHashOnlyFq ppe (Referent.Con cr ct))
+    ctorName ct i = hashReferentToken ppe (V1.Referent.Con (ConstructorReference (ReferenceDerived r) i) ct)
     go ct (DD.DataDeclaration mod _ vs ctors) =
       goMod mod <> goVs vs <> ((zip ctors [0 ..]) >>= goCtor ct)
 
@@ -118,17 +118,17 @@ hashKindTokens k = case k of
   K.Star -> [H.Tag 0]
   K.Arrow k1 k2 -> H.Tag 1 : (hashKindTokens k1 <> hashKindTokens k2)
 
-hashTypeTokens :: forall v a. Var v => PPE.PrettyPrintEnv -> Type v a -> [Token]
+hashTypeTokens :: forall v a. Var v => PrettyPrintEnv -> Type v a -> [Token]
 hashTypeTokens ppe = go
   where
     ft :: Type.F () -> [Token]
     ft = \case
-      Type.Ref r -> [H.Tag 0, H.Text (HQ.toTextWith Name.toText $ PPE.typeNameOrHashOnlyFq ppe r)]
+      Type.Ref r -> [H.Tag 0, hashTypeReferenceToken ppe r]
       Type.Arrow {} -> [H.Tag 1]
       Type.Ann _ k -> H.Tag 2 : hashKindTokens k
       Type.App {} -> [H.Tag 3]
       Type.Effect {} -> [H.Tag 4]
-      Type.Effects es -> [H.Tag 5, H.Nat (fromIntegral (length es))]
+      Type.Effects es -> [H.Tag 5, hashLengthToken es]
       Type.Forall {} -> [H.Tag 6]
       Type.IntroOuter {} -> [H.Tag 7]
     go :: Type v a -> [Token]
@@ -140,11 +140,11 @@ hashTypeTokens ppe = go
         ABT.Cycle c -> H.Tag 2 : go c
         ABT.Abs v body -> H.Tag 3 : H.Text (Var.name v) : go body
 
-hashCaseTokens :: PPE.PrettyPrintEnv -> Term.MatchCase loc a -> [Token]
+hashCaseTokens :: PrettyPrintEnv -> Term.MatchCase loc a -> [Token]
 hashCaseTokens ppe (Term.MatchCase pat Nothing _) = H.Tag 0 : hashPatternTokens ppe pat
 hashCaseTokens ppe (Term.MatchCase pat (Just _) _) = H.Tag 1 : hashPatternTokens ppe pat
 
-hashPatternTokens :: PPE.PrettyPrintEnv -> Pattern.Pattern loc -> [Token]
+hashPatternTokens :: PrettyPrintEnv -> Pattern.Pattern loc -> [Token]
 hashPatternTokens ppe = \case
   Pattern.Unbound {} -> [H.Tag 0]
   Pattern.Var {} -> [H.Tag 1]
@@ -155,24 +155,26 @@ hashPatternTokens ppe = \case
   Pattern.Text _ t -> [H.Tag 6, H.Text t]
   Pattern.Char _ c -> [H.Tag 7, H.Nat (fromIntegral (ord c))]
   Pattern.Constructor _ cr ps ->
-    H.Tag 8 : tn : len ps : (ps >>= hashPatternTokens ppe)
-    where
-      tn = H.Text (HQ.toTextWith Name.toText $ PPE.termNameOrHashOnlyFq ppe (Referent.Con cr CT.Data))
+    H.Tag 8
+      : hashReferentToken ppe (V1.Referent.Con cr CT.Data)
+      : hashLengthToken ps
+      : (ps >>= hashPatternTokens ppe)
   Pattern.As _ p -> H.Tag 9 : hashPatternTokens ppe p
   Pattern.EffectPure _ p -> H.Tag 10 : hashPatternTokens ppe p
-  Pattern.EffectBind _ cr ps k -> H.Tag 11 : tn : len ps : (hashPatternTokens ppe k <> (ps >>= hashPatternTokens ppe))
-    where
-      tn = H.Text (HQ.toTextWith Name.toText $ PPE.termNameOrHashOnlyFq ppe (Referent.Con cr CT.Effect))
-  Pattern.SequenceLiteral _ ps -> H.Tag 12 : len ps : (ps >>= hashPatternTokens ppe)
+  Pattern.EffectBind _ cr ps k ->
+    H.Tag 11
+      : hashReferentToken ppe (V1.Referent.Con cr CT.Effect)
+      : hashLengthToken ps
+      : (hashPatternTokens ppe k <> (ps >>= hashPatternTokens ppe))
+  Pattern.SequenceLiteral _ ps -> H.Tag 12 : hashLengthToken ps : (ps >>= hashPatternTokens ppe)
   Pattern.SequenceOp _ p op q -> H.Tag 16 : top op : (hashPatternTokens ppe p <> hashPatternTokens ppe q)
     where
-      top Pattern.Concat = H.Tag 0
-      top Pattern.Snoc = H.Tag 1
-      top Pattern.Cons = H.Tag 2
-  where
-    len ps = H.Nat (fromIntegral (length ps))
+      top = \case
+        Pattern.Concat -> H.Tag 0
+        Pattern.Snoc -> H.Tag 1
+        Pattern.Cons -> H.Tag 2
 
-hashTermTokens :: forall v a. Var v => PPE.PrettyPrintEnv -> Term v a -> [Token]
+hashTermTokens :: forall v a. Var v => PrettyPrintEnv -> Term v a -> [Token]
 hashTermTokens ppe = go
   where
     ft :: Term.F v a a () -> [Token]
@@ -185,23 +187,23 @@ hashTermTokens ppe = go
       Term.Char c -> [H.Tag 5, H.Nat (fromIntegral (ord c))]
       Term.Blank {} -> error "tried to hash a term with blanks, something's very wrong"
       -- note: these are all hashed the same, just based on the name
-      Term.Ref r -> [H.Tag 7, H.Text (HQ.toTextWith Name.toText $ PPE.termNameOrHashOnlyFq ppe (Referent.Ref r))]
-      Term.Constructor cr -> [H.Tag 7, H.Text (HQ.toTextWith Name.toText $ PPE.termNameOrHashOnlyFq ppe (Referent.Con cr CT.Data))]
-      Term.Request cr -> [H.Tag 7, H.Text (HQ.toTextWith Name.toText $ PPE.termNameOrHashOnlyFq ppe (Referent.Con cr CT.Effect))]
+      Term.Ref r -> [H.Tag 7, hashReferentToken ppe (V1.Referent.Ref r)]
+      Term.Constructor cr -> [H.Tag 7, hashReferentToken ppe (V1.Referent.Con cr CT.Data)]
+      Term.Request cr -> [H.Tag 7, hashReferentToken ppe (V1.Referent.Con cr CT.Effect)]
       Term.Handle {} -> [H.Tag 8]
       Term.App {} -> [H.Tag 9]
       Term.Ann _ ty -> H.Tag 10 : hashTypeTokens ppe ty
-      Term.List xs -> [H.Tag 11, H.Nat (fromIntegral (length xs))]
+      Term.List xs -> [H.Tag 11, hashLengthToken xs]
       Term.If {} -> [H.Tag 12]
       Term.And {} -> [H.Tag 13]
       Term.Or {} -> [H.Tag 14]
       Term.Lam {} -> [H.Tag 15]
-      Term.LetRec _ bs _ -> [H.Tag 16, H.Nat (fromIntegral (length bs))]
+      Term.LetRec _ bs _ -> [H.Tag 16, hashLengthToken bs]
       Term.Let {} -> [H.Tag 17]
       Term.Match _scrute cases ->
-        H.Tag 18 : H.Nat (fromIntegral (length cases)) : (cases >>= hashCaseTokens ppe)
-      Term.TermLink rf -> [H.Tag 19, H.Text (HQ.toTextWith Name.toText $ PPE.termNameOrHashOnlyFq ppe rf)]
-      Term.TypeLink r -> [H.Tag 20, H.Text (HQ.toTextWith Name.toText $ PPE.typeNameOrHashOnlyFq ppe r)]
+        H.Tag 18 : hashLengthToken cases : (cases >>= hashCaseTokens ppe)
+      Term.TermLink rf -> [H.Tag 19, hashReferentToken ppe rf]
+      Term.TypeLink r -> [H.Tag 20, hashTypeReferenceToken ppe r]
     go :: Term v a -> [Token]
     go t =
       H.Tag 255 : case ABT.out t of
@@ -210,3 +212,15 @@ hashTermTokens ppe = go
         ABT.Tm f -> H.Tag 1 : (ft (void f) <> (toList f >>= go))
         ABT.Cycle c -> H.Tag 2 : go c
         ABT.Abs v body -> H.Tag 3 : H.Text (Var.name v) : go body
+
+hashLengthToken :: Foldable t => t a -> Token
+hashLengthToken =
+  H.Nat . unsafeInto @Word64 . length
+
+hashReferentToken :: PrettyPrintEnv -> V1.Referent -> Token
+hashReferentToken ppe =
+  H.Text . HQ.toTextWith Name.toText . PPE.termNameOrHashOnlyFq ppe
+
+hashTypeReferenceToken :: PrettyPrintEnv -> TypeReference -> Token
+hashTypeReferenceToken ppe =
+  H.Text . HQ.toTextWith Name.toText . PPE.typeNameOrHashOnlyFq ppe
