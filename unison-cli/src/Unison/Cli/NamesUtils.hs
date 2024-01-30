7cc5afe1c0b1038929eabded57ae9cc894424ab0
Chris Penner <christopher.penner@gmail.com>
2024-01-30 10:42:16 -0800

    Undo unnecessary namesearch utils

--- unison-cli/src/Unison/Cli/NamesUtils.hs
+++ unison-cli/src/Unison/Cli/NamesUtils.hs
@@ -1,26 +1,15 @@
--- | Utilities that have to do with names objects.
+-- | Utilities that have to do with constructing names objects.
 module Unison.Cli.NamesUtils
   ( currentNames,
-    currentNameSearch,
   )
 where
 
 import Unison.Cli.Monad (Cli)
-import Unison.Cli.Monad qualified as Cli
 import Unison.Cli.MonadUtils (getCurrentBranch0)
-import Unison.Codebase qualified as Codebase
 import Unison.Codebase.Branch.Names qualified as Branch
 import Unison.Names (Names)
-import Unison.Server.NameSearch
-import Unison.Server.NameSearch.FromNames qualified as NameSearch
 
 -- | Produce a 'Names' object which contains names for the current branch.
 currentNames :: Cli Names
 currentNames = do
   Branch.toNames <$> getCurrentBranch0
-
--- | Produce a name searcher for things in the scope of the current branch.
-currentNameSearch :: Applicative m => Cli (NameSearch m)
-currentNameSearch = do
-  hqLength <- Cli.runTransaction Codebase.hashLength
-  NameSearch.makeNameSearch hqLength <$> currentNames
