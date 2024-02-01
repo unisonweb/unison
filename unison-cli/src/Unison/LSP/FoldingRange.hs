c73b2468e02eaa3240c31d20ad73327842efd510
Chris Penner <christopher.penner@gmail.com>
2024-01-31 16:58:03 -0800

    Add names to folding ranges

--- unison-cli/src/Unison/LSP/FoldingRange.hs
+++ unison-cli/src/Unison/LSP/FoldingRange.hs
@@ -3,10 +3,11 @@
 module Unison.LSP.FoldingRange where
 
 import Control.Lens hiding (List)
+import Data.Map qualified as Map
+import Data.Text qualified as Text
 import Language.LSP.Protocol.Lens hiding (id, to)
 import Language.LSP.Protocol.Message qualified as Msg
 import Language.LSP.Protocol.Types
-import Unison.ABT qualified as ABT
 import Unison.DataDeclaration qualified as DD
 import Unison.Debug qualified as Debug
 import Unison.LSP.Conversions (annToRange)
@@ -14,6 +15,7 @@ import Unison.LSP.FileAnalysis (getFileAnalysis)
 import Unison.LSP.Types
 import Unison.Prelude
 import Unison.UnisonFile (UnisonFile (..))
+import Unison.Var qualified as Var
 
 foldingRangeRequest :: Msg.TRequestMessage 'Msg.Method_TextDocumentFoldingRange -> (Either Msg.ResponseError (Msg.MessageResult 'Msg.Method_TextDocumentFoldingRange) -> Lsp ()) -> Lsp ()
 foldingRangeRequest m respond = do
@@ -27,24 +29,38 @@ foldingRangesForFile fileUri =
   fromMaybe []
     <$> runMaybeT do
       FileAnalysis {parsedFile} <- getFileAnalysis fileUri
-      UnisonFileId {dataDeclarationsId, effectDeclarationsId, terms} <- MaybeT $ pure parsedFile
-      let dataFolds = dataDeclarationsId ^.. folded . _2 . to dataDeclSpan
-      let abilityFolds = effectDeclarationsId ^.. folded . _2 . to DD.toDataDecl . to dataDeclSpan
-      let termFolds = terms ^.. folded . _3 . to ABT.annotation
-      let folds = dataFolds <> abilityFolds <> termFolds
-      let ranges = mapMaybe annToRange folds
+      UnisonFileId {dataDeclarationsId, effectDeclarationsId, terms, watches} <- MaybeT $ pure parsedFile
+      let dataFolds =
+            dataDeclarationsId
+              & Map.toList
+              & map \(sym, (_typ, decl)) -> (Just sym, DD.annotation decl)
+      let abilityFolds =
+            effectDeclarationsId
+              & Map.toList
+              & map \(sym, (_typ, decl)) -> (Just sym, DD.annotation . DD.toDataDecl $ decl)
+      let termFolds = terms & fmap \(sym, ann, _trm) -> (Just sym, ann)
+      let watchFolds =
+            watches
+              & fold
+              & fmap
+                ( \(_sym, ann, _trm) ->
+                    -- We don't use the symbol here because watch symbols are often auto-generated
+                    -- and ugly.
+                    (Nothing, ann)
+                )
+      let folds =
+            dataFolds <> abilityFolds <> termFolds <> watchFolds
+      let ranges =
+            folds
+              & mapMaybe \(sym, range) ->
+                (Text.pack . Var.nameStr <$> sym,) <$> annToRange range
       pure $
-        ranges <&> \r ->
+        ranges <&> \(maySym, r) ->
           FoldingRange
             { _startLine = r ^. start . line,
               _startCharacter = Just (r ^. start . character),
               _endLine = r ^. end . line,
               _endCharacter = Just (r ^. end . character),
               _kind = Just FoldingRangeKind_Region,
-              _collapsedText = Nothing
+              _collapsedText = maySym
             }
-  where
-    dataDeclSpan dd =
-      -- We don't have a proper Annotation for data decls so we take the span of all the
-      -- constructors using their monoid instance.
-      DD.annotation dd <> DD.constructors' dd ^. folded . to (\(a, _v, typ) -> a <> ABT.annotation typ)
