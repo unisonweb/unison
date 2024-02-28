52c0dd58abb9cc8f8f751fb58d7e1110121e54fe
Chris Penner <christopher.penner@gmail.com>
2024-02-28 12:25:02 -0800

    Add alternative rehash method

--- unison-cli/src/Unison/Codebase/Editor/HandleInput/DebugHashValidate.hs
+++ unison-cli/src/Unison/Codebase/Editor/HandleInput/DebugHashValidate.hs
@@ -1,13 +1,30 @@
 module Unison.Codebase.Editor.HandleInput.DebugHashValidate (debugHashValidate) where
 
+import Control.Monad.Reader
+import Data.Map qualified as Map
 import Unison.Cli.Monad (Cli)
 import Unison.Cli.Monad qualified as Cli
-import Unison.Codebase.Editor.Output (Output (HashValidationResult))
+import Unison.Codebase.Type qualified as Codebase
 import Unison.Hash32 (Hash32)
-import Unison.Sync.Common (expectEntity)
-import Unison.Sync.EntityValidation qualified as EV
+import Unison.Hash32 qualified as Hash32
+import Unison.Hashing.V2.Convert (hashTermComponents)
+import Unison.Prelude
+import Unison.Symbol (symbol)
+
+-- debugHashValidate :: Hash32 -> Cli ()
+-- debugHashValidate hash = do
+--   entity <- Cli.runTransaction $ expectEntity hash
+--   Cli.respond . HashValidationResult $ EV.validateEntity hash entity
 
 debugHashValidate :: Hash32 -> Cli ()
 debugHashValidate hash = do
-  entity <- Cli.runTransaction $ expectEntity hash
-  Cli.respond . HashValidationResult $ EV.validateEntity hash entity
+  Cli.Env {codebase} <- ask
+  mayComponent <- Cli.runTransaction $ Codebase.getTermComponentWithTypes codebase (Hash32.toHash hash)
+  case mayComponent of
+    Nothing -> liftIO $ putStrLn "No component found"
+    Just component ->
+      zip (symbol . tShow @Int <$> [1 ..]) component
+        & fmap (\(v, (trm, typ)) -> (v, (trm, typ, ())))
+        & Map.fromList
+        & hashTermComponents
+        & liftIO . print
