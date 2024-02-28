cf604905e3ec3693207906752d3e4cf978ac5608
Chris Penner <christopher.penner@gmail.com>
2024-02-28 14:26:36 -0800

    Share validation

--- unison-cli/src/Unison/Codebase/Editor/HandleInput/DebugHashValidate.hs
+++ unison-cli/src/Unison/Codebase/Editor/HandleInput/DebugHashValidate.hs
@@ -1,30 +1,26 @@
 module Unison.Codebase.Editor.HandleInput.DebugHashValidate (debugHashValidate) where
 
-import Control.Monad.Reader
-import Data.Map qualified as Map
 import Unison.Cli.Monad (Cli)
 import Unison.Cli.Monad qualified as Cli
-import Unison.Codebase.Type qualified as Codebase
+import Unison.Codebase.Editor.Output (Output (..))
 import Unison.Hash32 (Hash32)
-import Unison.Hash32 qualified as Hash32
-import Unison.Hashing.V2.Convert (hashTermComponents)
-import Unison.Prelude
-import Unison.Symbol (symbol)
-
--- debugHashValidate :: Hash32 -> Cli ()
--- debugHashValidate hash = do
---   entity <- Cli.runTransaction $ expectEntity hash
---   Cli.respond . HashValidationResult $ EV.validateEntity hash entity
+import Unison.Sync.Common qualified as Common
+import Unison.Sync.EntityValidation qualified as EV
 
 debugHashValidate :: Hash32 -> Cli ()
 debugHashValidate hash = do
-  Cli.Env {codebase} <- ask
-  mayComponent <- Cli.runTransaction $ Codebase.getTermComponentWithTypes codebase (Hash32.toHash hash)
-  case mayComponent of
-    Nothing -> liftIO $ putStrLn "No component found"
-    Just component ->
-      zip (symbol . tShow @Int <$> [1 ..]) component
-        & fmap (\(v, (trm, typ)) -> (v, (trm, typ, ())))
-        & Map.fromList
-        & hashTermComponents
-        & liftIO . print
+  entity <- Cli.runTransaction $ Common.expectEntity hash
+  Cli.respond . HashValidationResult $ EV.validateEntity hash entity
+
+-- debugHashValidate :: Hash32 -> Cli ()
+-- debugHashValidate hash = do
+--   Cli.Env {codebase} <- ask
+--   mayComponent <- Cli.runTransaction $ Codebase.getTermComponentWithTypes codebase (Hash32.toHash hash)
+--   case mayComponent of
+--     Nothing -> liftIO $ putStrLn "No component found"
+--     Just component ->
+--       zip (symbol . tShow @Int <$> [1 ..]) component
+--         & fmap (\(v, (trm, typ)) -> (v, (trm, typ, ())))
+--         & Map.fromList
+--         & hashTermComponents
+--         & liftIO . pPrint
