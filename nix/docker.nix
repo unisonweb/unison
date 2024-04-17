ad2e790a1e33659e07f99f46d83b786e3598de54
Cody Allen <cody.allen@unison.cloud>
2024-04-17 13:43:39 -0400

    nix ucm docker image: add cacert and fzf, use layered image

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,16 +1,10 @@
 { pkgs, haskell-nix }:
 
 {
-  ucm = pkgs.dockerTools.buildImage {
+  ucm = pkgs.dockerTools.buildLayeredImage {
     name = "ucm";
     tag = "latest";
-    copyToRoot = pkgs.buildEnv {
-      name = "image-root";
-      paths = [ haskell-nix."unison-cli:exe:unison" ];
-      pathsToLink = [ "/bin" ];
-    };
-    config = {
-      Cmd = [ "/bin/unison" ];
-    };
+    contents = with pkgs; [ cacert fzf ];
+    config.Cmd = [ "${haskell-nix."unison-cli:exe:unison"}/bin/unison" ];
   };
 }
