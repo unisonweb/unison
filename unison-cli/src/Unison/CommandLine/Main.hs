ac4420115e3f7ef42d00a3926f74851d8610c973
Chris Penner <christopher.penner@gmail.com>
2024-04-29 12:17:56 -0700

    Add history lines AFTER numbered arg and fzf expansion

--- unison-cli/src/Unison/CommandLine/Main.hs
+++ unison-cli/src/Unison/CommandLine/Main.hs
@@ -13,7 +13,9 @@ import Data.IORef
 import Data.Text qualified as Text
 import Data.Text.IO qualified as Text
 import Ki qualified
+import System.Console.Haskeline (Settings (autoAddHistory))
 import System.Console.Haskeline qualified as Line
+import System.Console.Haskeline.History qualified as Line
 import System.IO (hGetEcho, hPutStrLn, hSetEcho, stderr, stdin)
 import System.IO.Error (isDoesNotExistError)
 import U.Codebase.HashTags (CausalHash)
@@ -102,6 +104,9 @@ getUserInput codebase authHTTPClient currentPath numberedArgs =
           ws -> do
             liftIO (parseInput codebase currentPath numberedArgs IP.patternMap ws) >>= \case
               Left msg -> do
+                -- We still add history that failed to parse so the user can easily reload
+                -- the input and fix it.
+                Line.modifyHistory $ Line.addHistoryUnlessConsecutiveDupe $ l
                 liftIO $ putPrettyLn msg
                 go
               Right Nothing -> do
@@ -109,10 +114,17 @@ getUserInput codebase authHTTPClient currentPath numberedArgs =
                 go
               Right (Just (expandedArgs, i)) -> do
                 when (expandedArgs /= ws) $ do
-                  liftIO . putStrLn $ fullPrompt <> unwords expandedArgs
+                  let expandedArgsStr = unwords expandedArgs
+                  Line.modifyHistory $ Line.addHistoryUnlessConsecutiveDupe $ unwords expandedArgs
+                  liftIO . putStrLn $ fullPrompt <> expandedArgsStr
                 pure i
     settings :: Line.Settings IO
-    settings = Line.Settings tabComplete (Just ".unisonHistory") True
+    settings =
+      Line.Settings
+        { complete = tabComplete,
+          historyFile = Just ".unisonHistory",
+          autoAddHistory = False
+        }
     tabComplete = haskelineTabComplete IP.patternMap codebase authHTTPClient currentPath
 
 main ::
