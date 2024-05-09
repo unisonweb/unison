baf6098365e7f4c52098a5b329112cf4c6ec2729
hojberg <hojberg@users.noreply.github.com>
2024-05-09 15:31:57 +0000

    automatically run ormolu

--- unison-share-api/src/Unison/Server/Doc/AsHtml.hs
+++ unison-share-api/src/Unison/Server/Doc/AsHtml.hs
@@ -9,11 +9,11 @@ import Control.Monad.Writer.Class qualified as Writer
 import Control.Monad.Writer.Lazy (runWriterT)
 import Data.Char qualified as Char
 import Data.Foldable
+import Data.List (intersperse)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe
 import Data.Sequence (Seq)
-import Data.List (intersperse)
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Lucid
@@ -358,8 +358,8 @@ toHtml docNamesByRef document =
                 [d] ->
                   currentSectionLevelToHtml d
                 ds ->
-                  span_ [class_ "span"] <$>
-                  (renderSequence currentSectionLevelToHtml  (intersperse (Word " ")  (mergeWords " " ds)))
+                  span_ [class_ "span"]
+                    <$> (renderSequence currentSectionLevelToHtml (intersperse (Word " ") (mergeWords " " ds)))
             BulletedList items ->
               let itemToHtml i =
                     li_ [] <$> currentSectionLevelToHtml i
