42a40c7c46c5740d2832056559725b9f14d01d23
Greg Pfeil <greg@technomadic.org>
2024-05-31 14:11:05 -0600

    Add a `TypeError` for `IsString NameSegment`
    We don’t want someone to come along and add this instance without thinking about it.

--- codebase2/core/Unison/NameSegment/Internal.hs
+++ codebase2/core/Unison/NameSegment/Internal.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 -- | This module exposes the underlying representation of `NameSegment`, and
 --   thus should only be imported by parsers & printers.
 module Unison.NameSegment.Internal (NameSegment (..)) where
 
+import GHC.TypeLits (ErrorMessage ((:$$:)), TypeError)
+import GHC.TypeLits qualified as TypeError (ErrorMessage (Text))
 import Unison.Prelude
 import Unison.Util.Alphabetical (Alphabetical)
 
--- Represents the parts of a name between the `.`s
+-- | Represents the parts of a name between the @.@s.
 newtype NameSegment = NameSegment
   { -- | Convert a name segment to unescaped text.
     --
@@ -19,3 +25,14 @@ newtype NameSegment = NameSegment
   }
   deriving stock (Eq, Generic, Ord, Show)
   deriving newtype (Alphabetical)
+
+instance
+  TypeError
+    ( 'TypeError.Text "You cannot implicitly convert a ‘String’ to a ‘NameSegment’. If you need a"
+        ':$$: 'TypeError.Text "special-cased segment it should exist as a constant in"
+        ':$$: 'TypeError.Text "“Unison.NameSegment”, otherwise it should be parsed via"
+        ':$$: 'TypeError.Text "“Unison.Syntax.NameSegment”."
+    ) =>
+  IsString NameSegment
+  where
+  fromString = undefined
