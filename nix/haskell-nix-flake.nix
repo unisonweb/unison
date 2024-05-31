a188bb0e5b71b66faf6637017860f151ad0d3bfb
Greg Pfeil <greg@technomadic.org>
2024-05-31 16:04:37 -0600

    Run the Nix formatter for the first time

--- nix/haskell-nix-flake.nix
+++ nix/haskell-nix-flake.nix
@@ -1,27 +1,33 @@
-{ stack, hpack, pkgs, versions }:
-let
-  haskell-nix-flake = pkgs.unison-project.flake { };
+{
+  stack,
+  hpack,
+  pkgs,
+  versions,
+}: let
+  haskell-nix-flake = pkgs.unison-project.flake {};
   commonShellArgs = args:
-    args // {
+    args
+    // {
       # workaround:
       # https://github.com/input-output-hk/haskell.nix/issues/1793
       # https://github.com/input-output-hk/haskell.nix/issues/1885
       allToolDeps = false;
-      additional = hpkgs: with hpkgs; [ Cabal stm exceptions ghc ghc-heap ];
-      buildInputs =
-        let
-          native-packages = pkgs.lib.optionals pkgs.stdenv.isDarwin
-            (with pkgs.darwin.apple_sdk.frameworks; [ Cocoa ]);
-        in
-        (args.buildInputs or [ ]) ++ [ stack hpack pkgs.pkg-config pkgs.zlib pkgs.glibcLocales ] ++ native-packages;
+      additional = hpkgs: with hpkgs; [Cabal stm exceptions ghc ghc-heap];
+      buildInputs = let
+        native-packages =
+          pkgs.lib.optionals pkgs.stdenv.isDarwin
+          (with pkgs.darwin.apple_sdk.frameworks; [Cocoa]);
+      in
+        (args.buildInputs or []) ++ [stack hpack pkgs.pkg-config pkgs.zlib pkgs.glibcLocales] ++ native-packages;
       # workaround for https://gitlab.haskell.org/ghc/ghc/-/issues/11042
       shellHook = ''
         export LD_LIBRARY_PATH=${pkgs.zlib}/lib:$LD_LIBRARY_PATH
       '';
       tools =
-        (args.tools or { }) // {
-          cabal = { };
-          ormolu = { version = versions.ormolu; };
+        (args.tools or {})
+        // {
+          cabal = {};
+          ormolu = {version = versions.ormolu;};
           haskell-language-server = {
             version = versions.hls;
             modules = [
@@ -48,29 +54,30 @@ let
   localPackages = with pkgs.lib;
     filterAttrs (k: v: v.isLocal or false) pkgs.unison-project.hsPkgs;
   localPackageNames = builtins.attrNames localPackages;
-  devShells =
-    let
-      mkDevShell = pkgName:
-        shellFor {
-          packages = hpkgs: [ hpkgs."${pkgName}" ];
-          withHoogle = true;
-        };
-      localPackageDevShells =
-        pkgs.lib.genAttrs localPackageNames mkDevShell;
-    in
+  devShells = let
+    mkDevShell = pkgName:
+      shellFor {
+        packages = hpkgs: [hpkgs."${pkgName}"];
+        withHoogle = true;
+      };
+    localPackageDevShells =
+      pkgs.lib.genAttrs localPackageNames mkDevShell;
+  in
     {
       only-tools = shellFor {
-        packages = _: [ ];
+        packages = _: [];
         withHoogle = false;
       };
       local = shellFor {
         packages = hpkgs: (map (p: hpkgs."${p}") localPackageNames);
         withHoogle = false;
       };
-    } // localPackageDevShells;
+    }
+    // localPackageDevShells;
 in
-haskell-nix-flake // {
-  defaultPackage = haskell-nix-flake.packages."unison-cli-main:exe:unison";
-  inherit (pkgs) unison-project;
-  inherit devShells localPackageNames;
-}
+  haskell-nix-flake
+  // {
+    defaultPackage = haskell-nix-flake.packages."unison-cli-main:exe:unison";
+    inherit (pkgs) unison-project;
+    inherit devShells localPackageNames;
+  }
--- nix/haskell-nix-overlay.nix
+++ nix/haskell-nix-overlay.nix
@@ -1,23 +1,20 @@
 final: prev: {
-  unison-project = with prev.lib.strings;
-    let
-      cleanSource = pth:
-        let
-          src' = prev.lib.cleanSourceWith {
-            filter = filt;
-            src = pth;
-          };
-          filt = path: type:
-            let
-              bn = baseNameOf path;
-              isHiddenFile = hasPrefix "." bn;
-              isFlakeLock = bn == "flake.lock";
-              isNix = hasSuffix ".nix" bn;
-            in
-            !isHiddenFile && !isFlakeLock && !isNix;
-        in
-        src';
+  unison-project = with prev.lib.strings; let
+    cleanSource = pth: let
+      src' = prev.lib.cleanSourceWith {
+        filter = filt;
+        src = pth;
+      };
+      filt = path: type: let
+        bn = baseNameOf path;
+        isHiddenFile = hasPrefix "." bn;
+        isFlakeLock = bn == "flake.lock";
+        isNix = hasSuffix ".nix" bn;
+      in
+        !isHiddenFile && !isFlakeLock && !isNix;
     in
+      src';
+  in
     final.haskell-nix.project' {
       src = cleanSource ./..;
       projectFileName = "stack.yaml";
@@ -28,20 +25,18 @@ final: prev: {
           profilingDetail = "none";
         }
         # remove buggy build tool dependencies
-        ({ lib, ... }: {
+        ({lib, ...}: {
           # this component has the build tool
           # `unison-cli:unison` and somehow haskell.nix
           # decides to add some file sharing package
           # `unison` as a build-tool dependency.
           packages.unison-cli.components.exes.cli-integration-tests.build-tools =
-            lib.mkForce [ ];
+            lib.mkForce [];
         })
       ];
       branchMap = {
-        "https://github.com/unisonweb/configurator.git"."e47e9e9fe1f576f8c835183b9def52d73c01327a" =
-          "unison";
-        "https://github.com/unisonweb/shellmet.git"."2fd348592c8f51bb4c0ca6ba4bc8e38668913746" =
-          "topic/avoid-callCommand";
+        "https://github.com/unisonweb/configurator.git"."e47e9e9fe1f576f8c835183b9def52d73c01327a" = "unison";
+        "https://github.com/unisonweb/shellmet.git"."2fd348592c8f51bb4c0ca6ba4bc8e38668913746" = "topic/avoid-callCommand";
       };
     };
 }
