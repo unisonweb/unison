95fd37ad3fff82e987d80318382ccf3ba7b369c9
Chris Penner <christopher.penner@gmail.com>
2024-06-12 12:39:33 -0700

    Bootstrap scratch project in migration

--- parser-typechecker/src/Unison/Codebase/SqliteCodebase/Migrations/MigrateSchema16To17.hs
+++ parser-typechecker/src/Unison/Codebase/SqliteCodebase/Migrations/MigrateSchema16To17.hs
@@ -3,12 +3,27 @@
 
 module Unison.Codebase.SqliteCodebase.Migrations.MigrateSchema16To17 (migrateSchema16To17) where
 
+import U.Codebase.Sqlite.ProjectBranch (ProjectBranch (..))
 import U.Codebase.Sqlite.Queries qualified as Q
+import Unison.Codebase qualified as Codebase
+import Unison.Codebase.SqliteCodebase.Operations qualified as Ops
+import Unison.Core.Project (ProjectBranchName (UnsafeProjectBranchName), ProjectName (UnsafeProjectName))
 import Unison.Sqlite qualified as Sqlite
 
--- | This migration adds the causal_object_id column to the project_branches table.
+-- | This migration adds a new table to the schema, `currentProjectPath`, and sets it to contain the path to the scratch project.
 migrateSchema16To17 :: Sqlite.Transaction ()
 migrateSchema16To17 = do
   Q.expectSchemaVersion 16
+  scratchMain <-
+    Q.loadProjectBranchByNames scratchProjectName scratchBranchName >>= \case
+      Just pb -> pure pb
+      Nothing -> do
+        (_, emptyCausalHashId) <- Codebase.emptyCausalHash
+        (_proj, pb) <- Ops.insertProjectAndBranch scratchProjectName scratchBranchName emptyCausalHashId
+        pure pb
   Q.addCurrentProjectPathTable
+  Q.setCurrentProjectPath scratchMain.projectId scratchMain.branchId []
   Q.setSchemaVersion 17
+  where
+    scratchProjectName = UnsafeProjectName "scratch"
+    scratchBranchName = UnsafeProjectBranchName "main"
