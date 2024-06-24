b57ada5e929c45fddeac98adb73f0f895f7ba9db
Mitchell Rosen <mitchellwrosen@gmail.com>
2024-06-24 09:15:32 -0400

    include dependents of `todo` builtin in `todo` command output

--- unison-cli/src/Unison/Codebase/Editor/HandleInput/Todo.hs
+++ unison-cli/src/Unison/Codebase/Editor/HandleInput/Todo.hs
@@ -6,6 +6,7 @@ where
 
 import Data.Set qualified as Set
 import U.Codebase.Sqlite.Operations qualified as Operations
+import Unison.Builtin qualified as Builtin
 import Unison.Cli.Monad (Cli)
 import Unison.Cli.Monad qualified as Cli
 import Unison.Cli.MonadUtils qualified as Cli
@@ -15,7 +16,11 @@ import Unison.Codebase.Branch qualified as Branch
 import Unison.Codebase.Branch.Names qualified as Branch
 import Unison.Codebase.Editor.Output
 import Unison.Names qualified as Names
+import Unison.Prelude
+import Unison.Reference (TermReference)
+import Unison.Syntax.Name qualified as Name
 import Unison.Util.Defns (Defns (..))
+import Unison.Util.Set qualified as Set
 
 handleTodo :: Cli ()
 handleTodo = do
@@ -24,16 +29,29 @@ handleTodo = do
   currentNamespace <- Cli.getCurrentBranch0
   let currentNamespaceWithoutLibdeps = Branch.deleteLibdeps currentNamespace
 
-  (hashLen, directDependencies) <-
+  (dependentsOfTodo, directDependencies, hashLen) <-
     Cli.runTransaction do
-      hashLen <- Codebase.hashLength
+      let todoReference :: TermReference
+          todoReference =
+            Set.asSingleton (Names.refTermsNamed Builtin.names (Name.unsafeParseText "todo"))
+              & fromMaybe (error (reportBug "E260496" "No reference for builtin named 'todo'"))
+
+      -- All type-and-term dependents of the `todo` builtin, but we know they're all terms.
+      dependentsOfTodo <-
+        Operations.dependentsWithinScope
+          (Branch.deepTermReferenceIds currentNamespaceWithoutLibdeps)
+          (Set.singleton todoReference)
+
       directDependencies <-
         Operations.directDependenciesOfScope
           Defns
             { terms = Branch.deepTermReferenceIds currentNamespaceWithoutLibdeps,
               types = Branch.deepTypeReferenceIds currentNamespaceWithoutLibdeps
             }
-      pure (hashLen, directDependencies)
+
+      hashLen <- Codebase.hashLength
+
+      pure (dependentsOfTodo.terms, directDependencies, hashLen)
 
   ppe <- Cli.currentPrettyPrintEnvDecl
 
@@ -41,7 +59,7 @@ handleTodo = do
     Output'Todo
       TodoOutput
         { hashLen,
-          dependentsOfTodo = Set.empty,
+          dependentsOfTodo,
           directDependenciesWithoutNames =
             Defns
               { terms = Set.difference directDependencies.terms (Branch.deepTermReferences currentNamespace),
