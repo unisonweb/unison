dad9d6d6338c86d191a5c5cf42deccb8a4bf5a4c
Greg Pfeil <greg@technomadic.org>
2024-06-27 16:14:57 -0600

    Minor simplification of haskell-nix-flake

--- nix/haskell-nix-flake.nix
+++ nix/haskell-nix-flake.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   pkgs,
   unison-project,
   versions,
@@ -14,7 +15,7 @@
       additional = hpkgs: with hpkgs; [Cabal stm exceptions ghc ghc-heap];
       buildInputs = let
         native-packages =
-          pkgs.lib.optionals pkgs.stdenv.isDarwin
+          lib.optionals pkgs.stdenv.isDarwin
           (with pkgs.darwin.apple_sdk.frameworks; [Cocoa]);
       in
         (args.buildInputs or [])
@@ -52,46 +53,44 @@
 
   shellFor = args: unison-project.shellFor (commonShellArgs args);
 
-  localPackages = with pkgs.lib; filterAttrs (k: v: v.isLocal or false) unison-project.hsPkgs;
+  localPackages = lib.filterAttrs (k: v: v.isLocal or false) unison-project.hsPkgs;
   localPackageNames = builtins.attrNames localPackages;
-  devShells = let
-    mkDevShell = pkgName:
-      shellFor {
-        packages = hpkgs: [hpkgs."${pkgName}"];
-        withHoogle = true;
-      };
-    localPackageDevShells =
-      pkgs.lib.genAttrs localPackageNames mkDevShell;
-  in
-    {
-      only-tools = shellFor {
-        packages = _: [];
-        withHoogle = false;
-      };
-      local = shellFor {
-        packages = hpkgs: (map (p: hpkgs."${p}") localPackageNames);
-        withHoogle = false;
-      };
-    }
-    // localPackageDevShells;
-
-  checks =
-    haskell-nix-flake.checks
-    // {
-      ## This check has a test that tries to write to $HOME, so we give it a fake one.
-      "unison-cli:test:cli-tests" = haskell-nix-flake.checks."unison-cli:test:cli-tests".overrideAttrs (old: {
-        ## The builder here doesn’t `runHook preBuild`, so we just prepend onto `buildPhase`.
-        buildPhase =
-          ''
-            export HOME="$TMP/fake-home"
-            mkdir -p "$HOME"
-          ''
-          + old.buildPhase or "";
-      });
-    };
 in
   haskell-nix-flake
   // {
+    checks =
+      haskell-nix-flake.checks
+      // {
+        ## This check has a test that tries to write to $HOME, so we give it a fake one.
+        "unison-cli:test:cli-tests" = haskell-nix-flake.checks."unison-cli:test:cli-tests".overrideAttrs (old: {
+          ## The builder here doesn’t `runHook preBuild`, so we just prepend onto `buildPhase`.
+          buildPhase =
+            ''
+              export HOME="$TMP/fake-home"
+              mkdir -p "$HOME"
+            ''
+            + old.buildPhase or "";
+        });
+      };
+
     defaultPackage = haskell-nix-flake.packages."unison-cli-main:exe:unison";
-    inherit checks devShells;
+
+    devShells = let
+      mkDevShell = pkgName:
+        shellFor {
+          packages = hpkgs: [hpkgs."${pkgName}"];
+          withHoogle = true;
+        };
+    in
+      {
+        only-tools = shellFor {
+          packages = _: [];
+          withHoogle = false;
+        };
+        local = shellFor {
+          packages = hpkgs: (map (p: hpkgs."${p}") localPackageNames);
+          withHoogle = false;
+        };
+      }
+      // lib.genAttrs localPackageNames mkDevShell;
   }
