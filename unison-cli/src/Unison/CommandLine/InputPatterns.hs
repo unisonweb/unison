b14931f7b467ccef59cd0c65b4d4a2fe594ae735
Arya Irani <arya.irani@gmail.com>
2024-07-06 14:37:55 -0400

    avoid numeric vs textual clash in args length error
    `numerals` doesn't seem to be maintained anymore, however

--- unison-cli/src/Unison/CommandLine/InputPatterns.hs
+++ unison-cli/src/Unison/CommandLine/InputPatterns.hs
@@ -152,6 +152,8 @@ import System.Console.Haskeline.Completion (Completion (Completion))
 import System.Console.Haskeline.Completion qualified as Haskeline
 import System.Console.Haskeline.Completion qualified as Line
 import Text.Megaparsec qualified as Megaparsec
+import Text.Numeral (defaultInflection)
+import Text.Numeral.Language.ENG qualified as Numeral
 import U.Codebase.HashTags (CausalHash (..))
 import U.Codebase.Sqlite.DbId (ProjectBranchId)
 import U.Codebase.Sqlite.Project qualified as Sqlite
@@ -342,7 +344,11 @@ wrongStructuredArgument expected actual =
 
 wrongArgsLength :: Text -> [a] -> Either (P.Pretty CT.ColorText) b
 wrongArgsLength expected args =
-  Left . P.text $ "I expected " <> expected <> ", but received " <> Text.pack (show $ length args) <> "."
+  let foundCount =
+        if null args
+          then "none"
+          else fromMaybe (tShow $ length args) $ Numeral.us_cardinal defaultInflection (length args)
+   in Left . P.text $ "I expected " <> expected <> ", but received " <> foundCount <> "."
 
 patternName :: InputPattern -> P.Pretty P.ColorText
 patternName = fromString . I.patternName
