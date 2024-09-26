c662bfca7c763914842529a9f15675eeda207e5b
Chris Penner <christopher.penner@gmail.com>
2024-09-26 15:56:46 -0700

    Serialization WIP

--- unison-runtime/src/Unison/Runtime/Stack/Serialize.hs
+++ unison-runtime/src/Unison/Runtime/Stack/Serialize.hs
@@ -49,16 +49,18 @@ putGClosure :: (MonadPut m) => (comb -> m ()) -> GClosure comb -> m ()
 putGClosure putComb = \case
   GPAp comb uargs bargs ->
     putTag GPApT *> putComb comb *> putByteArray uargs *> putArray (putGClosure putComb) bargs
-  GEnum r i -> _
-  GDataU1 r w i -> _
-  GDataU2 r w i j -> _
-  GDataB1 r w c -> _
-  GDataB2 r w c1 c2 -> _
-  GDataUB r w i c -> _
-  GDataG r w s1 s2 -> _
-  GCaptured k i j s1 s2 -> _
+  GEnum r w -> putTag GEnumT *> putReference r *> putNat w
+  GDataU1 r w i -> putTag GDataU1T *> putReference r *> putNat w *> putI i
+  GDataU2 r w i j -> putTag GDataU2T *> putReference r *> putNat w *> putI i *> putInt j
+  GDataB1 r w clos -> putTag GDataB1T *> putReference r *> putNat w *> putGClosure putComb clos
+  GDataB2 r w c1 c2 -> putTag GDataB2T *> putReference r *> putNat w *> putGClosure putComb c1 *> putGClosure putComb c2
+  GDataUB r w i c -> putTag GDataUBT *> putReference r *> putNat w *> putI i *> putGClosure putComb c
+  GDataG r w usegs bsegs -> putTag GDataGT *> putReference r *> putNat w *> putByteArray usegs *> putArray (putGClosure putComb) bsegs
+  GCaptured k i j s1 s2 -> putTag GCapturedT *> putInt k *> putInt i *> putInt j *> putGCaptured putComb s1 *> putGCaptured putComb s2
   GForeign f -> _
   GBlackHole -> _
+  where
+    putI = putInt . fromIntegral
 
 getGClosure :: (MonadGet m) => m Closure
 getGClosure = error "getClosure not implemented"
