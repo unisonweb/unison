856a616aaa71bb9a75f9c2154cc1656f81af05bb
Greg Pfeil <greg@technomadic.org>
2025-05-12 16:51:14 -0600

    Add a script to check & format with Ormolu
    This is also run by ./scripts/check.sh.

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# eventually: ormolu -c `find . -name '*.hs'`
 true \
   && stack build --fast --test \
   && stack exec transcripts \
   && stack exec unison transcript unison-src/transcripts-round-trip/main.md \
   && stack exec unison transcript unison-src/transcripts-manual/rewrites.md \
   && stack exec unison transcript unison-src/transcripts-manual/docs.to-html.md \
-  && stack exec cli-integration-tests
+  && stack exec cli-integration-tests \
+  && ./scripts/check-formatting
