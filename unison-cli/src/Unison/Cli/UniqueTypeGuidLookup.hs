07fc7d4f8d9519d5729ffddc64f4957450defcf7
Mitchell Dalvi Rosen <mitchellwrosen@gmail.com>
2025-05-20 11:07:51 -0400

    fetch unique type guids from merge parents more efficiently

--- unison-cli/src/Unison/Cli/UniqueTypeGuidLookup.hs
+++ unison-cli/src/Unison/Cli/UniqueTypeGuidLookup.hs
@@ -5,8 +5,8 @@ module Unison.Cli.UniqueTypeGuidLookup
   )
 where
 
-import Data.Map.Strict qualified as Map
 import U.Codebase.Branch qualified as Codebase.Branch
+import U.Codebase.Sqlite.DbId qualified as Sqlite
 import U.Codebase.Sqlite.Queries qualified as Queries
 import Unison.Codebase qualified as Codebase
 import Unison.Codebase.Path qualified as Path
@@ -33,33 +33,50 @@ loadUniqueTypeGuid pp name0 = do
     Nothing ->
       Queries.loadMergeBranchParents pp.project.projectId pp.branch.branchId >>= \case
         Nothing -> pure Nothing
-        Just (bobMaybeBranchId, bobCausalHashId, aliceMaybeBranchId, aliceCausalHashId) -> do
-          aliceNamespaceHashId <- Queries.expectCausalValueHashId aliceCausalHashId
-          bobNamespaceHashId <- Queries.expectCausalValueHashId bobCausalHashId
-
-          aliceUniqueTypeGuids <- Queries.loadNamespaceUniqueTypeGuids aliceNamespaceHashId
-          bobUniqueTypeGuids <- Queries.loadNamespaceUniqueTypeGuids bobNamespaceHashId
-
-          case (Map.lookup name0 aliceUniqueTypeGuids, Map.lookup name0 bobUniqueTypeGuids) of
-            -- A few simple cases – reuse a GUID if it is sensible to do so
-            (Just aliceGuid, Just bobGuid) | aliceGuid == bobGuid -> pure (Just aliceGuid)
-            (Just aliceGuid, Nothing) -> pure (Just aliceGuid)
-            (Nothing, Just bobGuid) -> pure (Just bobGuid)
-            (Nothing, Nothing) -> pure Nothing
-            -- If alice and bob have different guids, and there is a parent-child relationship between them (i.e. alice
-            -- was directly branched off of bob or vice versa), then prefer the parent's GUID. Otherwise, just make up
-            -- a new GUID because it's not clear whether alice's or bob's should be preferred.
-            (Just aliceGuid, Just bobGuid) -> do
-              case (aliceMaybeBranchId, bobMaybeBranchId) of
-                (Just aliceBranchId, Just bobBranchId) -> do
-                  aliceParentBranchId <- Queries.loadProjectBranchParent pp.project.projectId aliceBranchId
-                  if aliceParentBranchId == Just bobBranchId
-                    then pure (Just bobGuid)
-                    else do
-                      bobParentBranchId <- Queries.loadProjectBranchParent pp.project.projectId bobBranchId
-                      pure
-                        if bobParentBranchId == Just aliceBranchId
-                          then Just aliceGuid
-                          else Nothing
-                _ -> pure Nothing
+        Just (bobMaybeBranchId, bobCausalHashId, aliceMaybeBranchId, aliceCausalHashId) ->
+          loadUniqueTypeGuidFromMergeParents
+            pp
+            name0
+            bobMaybeBranchId
+            bobCausalHashId
+            aliceMaybeBranchId
+            aliceCausalHashId
     Just guid -> pure (Just guid)
+
+loadUniqueTypeGuidFromMergeParents ::
+  ProjectPath ->
+  Name ->
+  Maybe Sqlite.ProjectBranchId ->
+  Sqlite.CausalHashId ->
+  Maybe Sqlite.ProjectBranchId ->
+  Sqlite.CausalHashId ->
+  Sqlite.Transaction (Maybe Text)
+loadUniqueTypeGuidFromMergeParents pp name0 bobMaybeBranchId bobCausalHashId aliceMaybeBranchId aliceCausalHashId = do
+  aliceNamespaceHashId <- Queries.expectCausalValueHashId aliceCausalHashId
+  bobNamespaceHashId <- Queries.expectCausalValueHashId bobCausalHashId
+
+  maybeAliceGuid <- Queries.loadNamespaceUniqueTypeGuid aliceNamespaceHashId name0
+  maybeBobGuid <- Queries.loadNamespaceUniqueTypeGuid bobNamespaceHashId name0
+
+  case (maybeAliceGuid, maybeBobGuid) of
+    -- A few simple cases – reuse a GUID if it is sensible to do so
+    (Just aliceGuid, Just bobGuid) | aliceGuid == bobGuid -> pure (Just aliceGuid)
+    (Just aliceGuid, Nothing) -> pure (Just aliceGuid)
+    (Nothing, Just bobGuid) -> pure (Just bobGuid)
+    (Nothing, Nothing) -> pure Nothing
+    -- If alice and bob have different guids, and there is a parent-child relationship between them (i.e. alice was
+    -- directly branched off of bob or vice versa), then prefer the parent's GUID. Otherwise, just make up a new
+    -- GUID because it's not clear whether alice's or bob's should be preferred.
+    (Just aliceGuid, Just bobGuid) -> do
+      case (aliceMaybeBranchId, bobMaybeBranchId) of
+        (Just aliceBranchId, Just bobBranchId) -> do
+          aliceParentBranchId <- Queries.loadProjectBranchParent pp.project.projectId aliceBranchId
+          if aliceParentBranchId == Just bobBranchId
+            then pure (Just bobGuid)
+            else do
+              bobParentBranchId <- Queries.loadProjectBranchParent pp.project.projectId bobBranchId
+              pure
+                if bobParentBranchId == Just aliceBranchId
+                  then Just aliceGuid
+                  else Nothing
+        _ -> pure Nothing
