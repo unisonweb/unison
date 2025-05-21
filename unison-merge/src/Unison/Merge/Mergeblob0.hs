bb5ad53ad9975534803708ca2dd24f7be7175863
Mitchell Dalvi Rosen <mitchellwrosen@gmail.com>
2025-05-21 13:51:05 -0400

    identify simple renames in merge

--- unison-merge/src/Unison/Merge/Mergeblob0.hs
+++ unison-merge/src/Unison/Merge/Mergeblob0.hs
@@ -4,28 +4,20 @@ module Unison.Merge.Mergeblob0
   )
 where
 
-import Data.Map.Merge.Strict qualified as Map
-import Data.Set qualified as Set
-import Data.Set.NonEmpty (NESet)
-import Data.Set.NonEmpty qualified as Set.NonEmpty
 import Unison.Merge.ThreeWay (ThreeWay)
-import Unison.Merge.ThreeWay qualified as ThreeWay
-import Unison.Merge.TwoWay (TwoWay)
 import Unison.Name (Name)
 import Unison.NameSegment (NameSegment)
 import Unison.Prelude
 import Unison.Reference (TypeReference)
 import Unison.Referent (Referent)
 import Unison.Util.BiMultimap (BiMultimap)
-import Unison.Util.BiMultimap qualified as BiMultimap
-import Unison.Util.Defns (Defns, DefnsF, zipDefnsWith)
+import Unison.Util.Defns (Defns, DefnsF)
 import Unison.Util.Nametree (Nametree, flattenNametrees)
 
 data Mergeblob0 libdep = Mergeblob0
   { defns :: ThreeWay (Defns (BiMultimap Referent Name) (BiMultimap TypeReference Name)),
     libdeps :: ThreeWay (Map NameSegment libdep),
-    nametrees :: ThreeWay (Nametree (DefnsF (Map NameSegment) Referent TypeReference)),
-    renames :: TwoWay (Defns (Map Referent Rename) (Map TypeReference Rename))
+    nametrees :: ThreeWay (Nametree (DefnsF (Map NameSegment) Referent TypeReference))
   }
 
 makeMergeblob0 ::
@@ -37,35 +29,5 @@ makeMergeblob0 nametrees libdeps =
    in Mergeblob0
         { defns,
           libdeps,
-          nametrees,
-          renames = makeRenames defns
+          nametrees
         }
-
--- Invariant: it is not the case the both sets are empty
-data Rename = Rename
-  { deletes :: Set Name,
-    adds :: Set Name
-  }
-
-makeRenames ::
-  ThreeWay (Defns (BiMultimap Referent Name) (BiMultimap TypeReference Name)) ->
-  TwoWay (Defns (Map Referent Rename) (Map TypeReference Rename))
-makeRenames defns =
-  zipDefnsWith f f defns.lca <$> ThreeWay.forgetLca defns
-  where
-    f :: (Ord ref) => BiMultimap ref Name -> BiMultimap ref Name -> Map ref Rename
-    f old new =
-      Map.merge
-        Map.dropMissing
-        Map.dropMissing
-        (Map.zipWithMaybeMatched g)
-        (BiMultimap.domain old)
-        (BiMultimap.domain new)
-
-    g :: ref -> NESet Name -> NESet Name -> Maybe Rename
-    g _ old new =
-      let deletes = Set.NonEmpty.difference old new
-          adds = Set.NonEmpty.difference new old
-       in if Set.null deletes && Set.null adds
-            then Nothing
-            else Just Rename {deletes, adds}
