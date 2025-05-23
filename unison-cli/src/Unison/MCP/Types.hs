c086588db7f3e6be51571ba2f43ff1f38d303cca
Chris Penner <christopher.penner@gmail.com>
2025-05-23 15:21:54 -0700

    Working basic tool setup

---
+++ unison-cli/src/Unison/MCP/Types.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE NoFieldSelectors #-}
+
+module Unison.MCP.Types
+  ( MCP (..),
+    Env (..),
+    runMCP,
+    ToolKind (..),
+    ProjectCodeToolArguments (..),
+    toToolName,
+    fromToolName,
+  )
+where
+
+import Control.Monad.Reader (ReaderT (..))
+import Data.Aeson
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Ki qualified
+import Unison.Codebase (Codebase)
+import Unison.Codebase.Runtime (Runtime)
+import Unison.Parser.Ann (Ann)
+import Unison.Symbol (Symbol)
+import UnliftIO (MonadIO, MonadUnliftIO)
+
+data Env = Env
+  { codebase :: Codebase IO Symbol Ann,
+    runtime :: Runtime Symbol,
+    scope :: Ki.Scope
+  }
+
+newtype MCP a = MCP
+  { unMCP :: ReaderT Env IO a
+  }
+  deriving newtype (Functor, Applicative, Monad, MonadIO, MonadUnliftIO)
+
+runMCP :: Env -> MCP a -> IO a
+runMCP env (MCP m) = do
+  runReaderT m env
+
+data ToolKind
+  = ProjectCodeTool
+  deriving (Eq, Show, Bounded, Enum)
+
+data ProjectCodeToolArguments = ProjectCodeToolArguments {projectName :: Text}
+
+instance FromJSON ProjectCodeToolArguments where
+  parseJSON = withObject "ProjectCodeToolArguments" $ \o -> do
+    projectName <- o .: "projectName"
+    pure $ ProjectCodeToolArguments {projectName}
+
+toToolName :: ToolKind -> Text.Text
+toToolName ProjectCodeTool = "project-code"
+
+fromToolName :: Text.Text -> Maybe ToolKind
+fromToolName = \case
+  "project-code" -> Just ProjectCodeTool
+  _ -> Nothing
