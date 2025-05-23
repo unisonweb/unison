67b0cb2edf9961f3f7210b558040d684db6a3cda
Chris Penner <christopher.penner@gmail.com>
2025-05-23 15:57:03 -0700

    Set up a way to run Cli in MCP

--- unison-cli/src/Unison/MCP/Types.hs
+++ unison-cli/src/Unison/MCP/Types.hs
@@ -11,7 +11,7 @@ module Unison.MCP.Types
   )
 where
 
-import Control.Monad.Reader (ReaderT (..))
+import Control.Monad.Reader (MonadReader, ReaderT (..))
 import Data.Aeson
 import Data.Text (Text)
 import Data.Text qualified as Text
@@ -20,18 +20,21 @@ import Unison.Codebase (Codebase)
 import Unison.Codebase.Runtime (Runtime)
 import Unison.Parser.Ann (Ann)
 import Unison.Symbol (Symbol)
+import Unison.Version (Version)
 import UnliftIO (MonadIO, MonadUnliftIO)
 
 data Env = Env
   { codebase :: Codebase IO Symbol Ann,
     runtime :: Runtime Symbol,
-    scope :: Ki.Scope
+    scope :: Ki.Scope,
+    ucmVersion :: Version,
+    workDir :: FilePath
   }
 
 newtype MCP a = MCP
   { unMCP :: ReaderT Env IO a
   }
-  deriving newtype (Functor, Applicative, Monad, MonadIO, MonadUnliftIO)
+  deriving newtype (Functor, Applicative, Monad, MonadIO, MonadUnliftIO, MonadReader Env)
 
 runMCP :: Env -> MCP a -> IO a
 runMCP env (MCP m) = do
