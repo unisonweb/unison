f816a61d253d1762d2494ed824808c7b2e5a7216
Chris Penner <christopher.penner@gmail.com>
2025-06-13 16:20:13 -0700

    Shell of lib.install

--- unison-cli/src/Unison/MCP.hs
+++ unison-cli/src/Unison/MCP.hs
@@ -55,25 +55,46 @@ runOnStdIO codebase runtime sbRuntime nRuntime workDir ucmVersion = do
         pure . ReadResourceResult $ [content]
       _ -> pure $ ReadResourceResult []
 
-  registerTools server [projectCodeTool]
+  registerTools server [projectCodeTool, installLibTool]
 
   -- Register tool call handler
   registerToolCallHandler server $ \(CallToolRequest {callToolName, callToolArguments}) -> do
-    case (fromToolName callToolName, fromJSON callToolArguments) of
-      (Just ProjectCodeTool, Success (ProjectCodeToolArguments {projectName})) ->
-        do
-          runMCP env do
-            pp <- liftIO $ Codebase.runTransaction codebase $ do
-              ppForProjectName $ UnsafeProjectName projectName
-            output <- handleInputMCP pp (Right $ Input.EditNamespaceI [])
-            let outputJSON = Text.decodeUtf8 . BL.toStrict $ Aeson.encode output
-            pure $
-              CallToolResult
-                { callToolIsError = False,
-                  callToolContent = [ToolContent {toolContentType = TextualContent, toolContentText = Just outputJSON}]
-                }
-      (Just _, Error {}) -> pure $ CallToolResult [] True
-      (Nothing, _) -> pure $ CallToolResult [] True
+    case fromToolName callToolName of
+      Nothing -> pure $ CallToolResult [] True
+      Just LibInstallTool ->
+        case fromJSON callToolArguments of
+          Success (LibInstallToolArguments {}) ->
+            error ""
+            -- do
+            --   runMCP env do
+            --     handleInstallLib (ProjectAndBranch (UnsafeProjectName projectName) (ProjectBranchNameOrLatestRelease'Name . UnsafeProjectBranchName <$> branchName)
+            --     pp <- liftIO $ Codebase.runTransaction codebase $ do
+            --       ProjectBranchNameKind projectBranchNameKind <-
+            --         Codebase.classifyProjectBranchName (UnsafeProjectName projectName) branchName
+            --       ppForProjectName $ UnsafeProjectName projectName
+            --     output <- handleInputMCP pp (Right $ Input.InstallLibI projectBranchNameKind)
+            --     let outputJSON = Text.decodeUtf8 . BL.toStrict $ Aeson.encode output
+            --     pure $
+            --       CallToolResult
+            --         { callToolIsError = False,
+            --           callToolContent = [ToolContent {toolContentType = TextualContent, toolContentText = Just outputJSON}]
+            --         }
+          _ -> pure $ CallToolResult [] True
+      Just ProjectCodeTool ->
+        case fromJSON callToolArguments of
+          Success (ProjectCodeToolArguments {projectName}) ->
+            do
+              runMCP env do
+                pp <- liftIO $ Codebase.runTransaction codebase $ do
+                  ppForProjectName $ UnsafeProjectName projectName
+                output <- handleInputMCP pp (Right $ Input.EditNamespaceI [])
+                let outputJSON = Text.decodeUtf8 . BL.toStrict $ Aeson.encode output
+                pure $
+                  CallToolResult
+                    { callToolIsError = False,
+                      callToolContent = [ToolContent {toolContentType = TextualContent, toolContentText = Just outputJSON}]
+                    }
+          Error {} -> pure $ CallToolResult [] True
 
   -- Start the server with StdIO transport
   runServerWithSTDIO server
@@ -110,10 +131,34 @@ projectCodeTool =
             }
     }
 
--- installLibTool :: Tool
--- installLibTool =
---   Tool
---     { toolName = "install-library",
---       toolDescription = Just "Install a library from Unison Share",
---       toolInputSchema = "{...}" -- JSON schema
---     }
+installLibTool :: Tool
+installLibTool =
+  Tool
+    { toolName = toToolName LibInstallTool,
+      toolDescription = Just "Install a library from Unison Share into the current project.",
+      toolInputSchema = [r|
+        {
+          "type": "object",
+          "properties": {
+            "projectName": {
+              "type": "string",
+              "description": "The name of the library project to install"
+            },
+            "branchName": {
+              "type": ["string", "null"],
+              "description": "The optional branch of the library project to install. If null, the latest release will be used."
+            }
+          },
+          "required": ["projectName"]
+        }
+        |]
+    , toolAnnotations =
+        Just $
+          ToolAnnotations
+            { title = Just "Install Library",
+              readOnlyHint = Just False,
+              destructiveHint = Just True,
+              idempotentHint = Just False,
+              openWorldHint = Just True
+            }
+    }
