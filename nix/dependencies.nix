27269169ba884c301bcf1131e05de3d3e49874e7
Greg Pfeil <greg@technomadic.org>
2025-07-03 22:38:23 -0600

    Update link to Stack docs

--- nix/dependencies.nix
+++ nix/dependencies.nix
@@ -3,8 +3,8 @@
 in {
   stack = pinned-pkgs.stack;
 
-  ## See https://docs.haskellstack.org/en/stable/nix_integration/#supporting-both-nix-and-non-nix-developers for an
-  ## explanation of this package.
+  ## See https://docs.haskellstack.org/en/stable/topics/nix_integration/#supporting-both-nix-and-non-nix-developers for
+  ## an explanation of this package.
   stack-wrapped = final.symlinkJoin {
     name = "stack"; # will be available as the usual `stack` in terminal
     paths = [final.stack];
