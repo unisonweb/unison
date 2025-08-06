61db8d57298f61f68da4109ac3661902bf492a5a
Dan Doel <dan.doel@gmail.com>
2025-08-05 20:24:43 -0400

    Improve an error message when an ability variable can't be solved.
    In definitions like:
    
      foo : '{g} a -> ()
      foo th =
        !th
        foo th
    
    we end up in a situation where we can't actually solve to the type
    `'{g} a ->{g} ()`. The reason is that we make up the existential
    variable immediately so that we can type check the recursive function
    with a specified type of `'{g} a ->{e} ()`. The `g` that `e` needs to
    be solved to ends up later in the context, so the solution is not
    allowed.
    
    This change introduces a new error message that detects this situation
    and suggests an ability set for the signature. There was a previous
    'pretty' error message that had been disabled for this situation, but
    it was fairly uninformative, and might have been just as useless as
    the ugly error message we were currently displaying.

--- parser-typechecker/src/Unison/Typechecker/TypeError.hs
+++ parser-typechecker/src/Unison/Typechecker/TypeError.hs
@@ -83,6 +83,12 @@ data TypeError v loc
         mismatchSite :: C.Term v loc,
         note :: C.ErrorNote v loc
       }
+  | AbilityInstantiationFailure
+      { var :: v,
+        inst :: [C.Type v loc],
+        instSite :: C.Term v loc,
+        note :: C.ErrorNote v loc
+      }
   | UnguardedLetRecCycle
       { cycle :: [v],
         cycleLocs :: [loc],
@@ -146,6 +152,7 @@ allErrors =
       generalMismatch,
       abilityCheckFailure,
       abilityEqFailure,
+      badEffectInstantiation,
       unguardedCycle,
       unknownType,
       unknownTerm,
@@ -216,6 +223,36 @@ unknownTerm = do
   (loc, v, suggs, typ) <- Ex.unknownTerm
   n <- Ex.errorNote
   pure $ UnknownTerm v loc suggs (Type.cleanup typ) n
+
+data EffInst v loc
+  = Eff [C.Type v loc] [C.Type v loc] -- want, have
+  | Normal
+
+-- checks if the top of the path was an instantiation, and indicates
+-- whether it was from an ability variable.
+instantiation ::
+  (Var v, Ord loc) =>
+  Ex.ErrorExtractor v loc (v, C.Type v loc, EffInst v loc)
+instantiation = Ex.path >>= \case
+    C.InInstantiateR ty v : path -> pure $ classify v ty path
+    C.InInstantiateL v ty : path -> pure $ classify v ty path
+    _ -> mzero
+  where
+    classify v ty (C.InSubAbilities want have : _) =
+      (v, ty, Eff want have)
+    classify v ty _ = (v, ty, Normal)
+
+badEffectInstantiation ::
+  (Var v, Ord loc) => Ex.ErrorExtractor v loc (TypeError v loc)
+badEffectInstantiation = do
+  ctx <- Ex.typeMismatch
+  let sub t = C.apply ctx t
+  (v, _ty, isEff) <- instantiation
+  Eff want _ <- pure isEff
+  n <- Ex.errorNote
+  site <- Ex.innermostTerm
+  pure $
+    AbilityInstantiationFailure v (Type.cleanups $ sub <$> want) site n
 
 generalMismatch :: (Var v, Ord loc) => Ex.ErrorExtractor v loc (TypeError v loc)
 generalMismatch = do
