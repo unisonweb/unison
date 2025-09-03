dff9658184cd6fbe89ca79b22ea522e08cd003e6
Greg Pfeil <greg@technomadic.org>
2025-09-03 15:33:53 -0600

    Point the runtime tests at the new release

--- unison-src/builtin-tests/interpreter-tests.sh
+++ unison-src/builtin-tests/interpreter-tests.sh
@@ -8,7 +8,7 @@ else
   ucm="$1"
 fi
 
-runtime_tests_version="@unison/runtime-tests/@sellout/unison-5661"
+runtime_tests_version="@unison/runtime-tests/releases/0.0.4"
 
 codebase=${XDG_CACHE_HOME:-"$HOME/.cache"}/unisonlanguage/runtime-tests.unison
 
