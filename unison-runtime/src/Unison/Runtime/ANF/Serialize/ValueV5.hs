8ae8bb3132ee2c6ca8a3326e6ed35e4d36c49c23
Dan Doel <dan.doel@gmail.com>
2025-10-10 15:44:52 -0400

    Use `getArray` rather than IsList in lit serialization

--- unison-runtime/src/Unison/Runtime/ANF/Serialize/ValueV5.hs
+++ unison-runtime/src/Unison/Runtime/ANF/Serialize/ValueV5.hs
@@ -11,7 +11,6 @@ import Data.Bytes.Get hiding (getBytes)
 import Data.Bytes.Put
 import Data.Serialize.Get qualified as SGet
 import Data.Serialize.Put qualified as SPut
-import GHC.IsList qualified (fromList)
 import Unison.Reference (Reference)
 import Unison.Runtime.ANF as ANF hiding (Tag)
 import Unison.Runtime.ANF.Serialize.CodeV4
@@ -194,7 +193,7 @@ getBLit =
     NegT -> Neg <$> getPositive
     CharT -> Char <$> getChar
     FloatT -> Float <$> getFloat
-    ArrT -> Arr . GHC.IsList.fromList <$> getList getValue
+    ArrT -> Arr <$> getArray getValue
     CachedCodeT -> Code . flip CodeRep Cacheable <$> getGroup
     MapT -> Map <$> getMapping getValue getValue
 {-# SPECIALIZE getBLit :: BGet.Get (BLit RefNum) #-}
