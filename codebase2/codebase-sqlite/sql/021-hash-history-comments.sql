6476649d6f760dd0d0d95175e148a0b1d1e9311f
Chris Penner <christopher.penner@gmail.com>
2025-10-15 16:19:02 -0700

    Add table for key thumbprints

--- codebase2/codebase-sqlite/sql/021-hash-history-comments.sql
+++ codebase2/codebase-sqlite/sql/021-hash-history-comments.sql
@@ -1,7 +1,15 @@
+-- Table for storing personal key thumbprints,
+-- which we may later associate with users.
+CREATE TABLE IF NOT EXISTS key_thumbprints (
+  id INTEGER PRIMARY KEY,
+  thumbprint TEXT UNIQUE NOT NULL
+);
+
 ALTER TABLE history_comments
   -- The hash used for comment identity.
   -- It's the hash of (causal_hash <> author <> created_at)
-  ADD COLUMN comment_hash_id INTEGER UNIQUE NOT NULL REFERENCES hash(id);
+  ADD COLUMN comment_hash_id INTEGER UNIQUE NOT NULL REFERENCES hash(id),
+  ADD COLUMN author_thumbprint_id INTEGER NOT NULL REFERENCES key_thumbprints(id);
 
 ALTER TABLE history_comment_revisions
   -- The hash used for this revision's identity.
