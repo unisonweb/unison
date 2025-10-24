fa6e5a83860b2a02f9aa89a70a26b6b3b2c7ea9b
Chris Penner <christopher.penner@gmail.com>
2025-10-24 13:18:08 -0700

    Helpers for preferences module

--- codebase2/codebase-sqlite/U/Codebase/Preferences.hs
+++ codebase2/codebase-sqlite/U/Codebase/Preferences.hs
@@ -1,8 +1,12 @@
 module U.Codebase.Preferences
   ( AuthorName,
     PreferencesKey (..),
+    allKeys,
     mkAuthorName,
     unAuthorName,
+    keyToText,
+    keyFromText,
+    allKeysText,
   )
 where
 
@@ -11,9 +15,28 @@ import Unison.Prelude
 import Unison.Sqlite qualified as Sqlite
 
 data PreferencesKey = AuthorNameKey
+  deriving stock (Eq, Enum, Bounded)
+
+instance Show PreferencesKey where
+  show k = Text.unpack . keyToText $ k
+
+allKeys :: [PreferencesKey]
+allKeys = [minBound .. maxBound]
+
+allKeysText :: [Text]
+allKeysText = keyToText <$> allKeys
+
+keyToText :: PreferencesKey -> Text
+keyToText = \case
+  AuthorNameKey -> "author.name"
+
+keyFromText :: Text -> Maybe PreferencesKey
+keyFromText t = case t of
+  "author.name" -> Just AuthorNameKey
+  _ -> Nothing
 
 instance Sqlite.ToField PreferencesKey where
-  toField AuthorNameKey = Sqlite.toField ("author.name" :: Text)
+  toField AuthorNameKey = Sqlite.toField (keyToText AuthorNameKey)
 
 mkAuthorName :: Text -> Either Text AuthorName
 mkAuthorName name
@@ -23,3 +46,4 @@ mkAuthorName name
 
 newtype AuthorName = AuthorName {unAuthorName :: Text}
   deriving stock (Eq, Show)
+  deriving newtype (Sqlite.ToField, Sqlite.FromField)
