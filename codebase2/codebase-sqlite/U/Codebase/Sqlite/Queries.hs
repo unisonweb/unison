13405c41f5c0993c5a236ab3d261dece065365d8
Chris Penner <christopher.penner@gmail.com>
2025-11-03 13:51:11 -0800

    Rename hashes to HistoryCommentHash

--- codebase2/codebase-sqlite/U/Codebase/Sqlite/Queries.hs
+++ codebase2/codebase-sqlite/U/Codebase/Sqlite/Queries.hs
@@ -341,8 +341,8 @@ import U.Codebase.Decl qualified as C.Decl
 import U.Codebase.HashTags
   ( BranchHash (..),
     CausalHash (..),
-    CommentHash (..),
-    CommentRevisionHash (..),
+    HistoryCommentHash (..),
+    HistoryCommentRevisionHash (..),
     PatchHash (..),
   )
 import U.Codebase.Reference (Reference' (..))
@@ -647,11 +647,11 @@ expectCausalByCausalHash ch = do
   bhId <- expectCausalValueHashId hId
   pure (hId, bhId)
 
-saveCommentHash :: CommentHash -> Transaction CommentHashId
-saveCommentHash = fmap CommentHashId . saveHashHash . unCommentHash
+saveCommentHash :: HistoryCommentHash -> Transaction CommentHashId
+saveCommentHash = fmap CommentHashId . saveHashHash . unHistoryCommentHash
 
-saveCommentRevisionHash :: CommentRevisionHash -> Transaction CommentRevisionHashId
-saveCommentRevisionHash = fmap CommentRevisionHashId . saveHashHash . unCommentRevisionHash
+saveCommentRevisionHash :: HistoryCommentRevisionHash -> Transaction CommentRevisionHashId
+saveCommentRevisionHash = fmap CommentRevisionHashId . saveHashHash . unHistoryCommentRevisionHash
 
 expectHashIdByHash :: Hash -> Transaction HashId
 expectHashIdByHash = expectHashId . Hash32.fromHash
@@ -4149,7 +4149,7 @@ saveSquashResult bhId chId =
 
 getLatestCausalComment ::
   CausalHashId ->
-  Transaction (Maybe (LatestHistoryComment KeyThumbprintId CausalHash HistoryCommentRevisionId CommentHash))
+  Transaction (Maybe (LatestHistoryComment KeyThumbprintId CausalHash HistoryCommentRevisionId HistoryCommentHash))
 getLatestCausalComment causalHashId =
   queryMaybeRow @(Hash32, Hash32, Text, KeyThumbprintId, HistoryCommentRevisionId, Text, Text, Time.UTCTime)
     [sql|
@@ -4174,11 +4174,11 @@ getLatestCausalComment causalHashId =
                 authorThumbprint,
                 causal = CausalHash . Hash32.toHash $ causalHash,
                 createdAt,
-                commentId = CommentHash . Hash32.toHash $ commentHash
+                commentId = HistoryCommentHash . Hash32.toHash $ commentHash
               }
         }
 
-commentOnCausal :: LatestHistoryComment KeyThumbprint CausalHashId CommentRevisionHash CommentHash -> Transaction ()
+commentOnCausal :: LatestHistoryComment KeyThumbprint CausalHashId HistoryCommentRevisionHash HistoryCommentHash -> Transaction ()
 commentOnCausal
   HistoryCommentRevision
     { content,
--- unison-cli/src/Unison/Codebase/HistoryComments/Hashing.hs
+++ unison-cli/src/Unison/Codebase/HistoryComments/Hashing.hs
@@ -1,8 +1,25 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Unison.Codebase.HistoryComments.Hashing
-  ( hashComment,
-    hashCommentRevision,
+  ( hashHistoryComment,
+    hashHistoryCommentRevision,
   )
 where
+
+import Crypto.Hash qualified as CH
+import Data.ByteArray qualified as BA
+import Data.ByteString.Builder qualified as Builder
+import Data.ByteString.Lazy.Char8 qualified as BL
+import Data.Text.Encoding qualified as Text
+import Data.Time (UTCTime)
+import Data.Time.Clock.POSIX qualified as Time
+import U.Codebase.HashTags (CausalHash, HistoryCommentHash (..), HistoryCommentRevisionHash (..))
+import Unison.Hash (Hash)
+import Unison.Hash qualified as Hash
+import Unison.Hashing.V2 (ContentAddressable (..))
+import Unison.HistoryComment (HistoryComment (..), HistoryCommentRevision (..))
+import Unison.KeyThumbprint (KeyThumbprint (..))
+import Unison.Prelude
 
 commentHashingVersion :: Int32
 commentHashingVersion = 1
@@ -29,7 +46,7 @@ instance ContentAddressable (HistoryComment UTCTime KeyThumbprint CausalHash any
       & Hash.fromByteString
 
 -- Hash a comment revision
-instance ContentAddressable (HistoryCommentRevision any UTCTime CommentHash) where
+instance ContentAddressable (HistoryCommentRevision any UTCTime HistoryCommentHash) where
   contentHash HistoryCommentRevision {subject, content, createdAt, comment = commentHash} =
     CH.hashUpdates
       CH.hashInit
@@ -53,12 +70,12 @@ hashHistoryComment ::
   HistoryComment UTCTime KeyThumbprint CausalHash any ->
   HistoryComment UTCTime KeyThumbprint CausalHash HistoryCommentHash
 hashHistoryComment historyComment =
-  let commentHash = CommentHash $ contentHash historyComment
+  let commentHash = HistoryCommentHash $ contentHash historyComment
    in historyComment {commentId = commentHash}
 
 hashHistoryCommentRevision ::
-  HistoryCommentRevision any UTCTime CommentHash
-  -> HistoryCommentRevision CommentRevisionHash UTCTime CommentHash
+  HistoryCommentRevision any UTCTime HistoryCommentHash ->
+  HistoryCommentRevision HistoryCommentRevisionHash UTCTime HistoryCommentHash
 hashHistoryCommentRevision historyCommentRevision =
-  let commentRevisionHash = CommentRevisionHash $ contentHash historyComment
-    in historyCommentRevision {revisionId = commentRevisionHash}
+  let commentRevisionHash = HistoryCommentRevisionHash $ contentHash historyCommentRevision
+   in historyCommentRevision {revisionId = commentRevisionHash}
