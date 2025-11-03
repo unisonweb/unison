d969eecf3a8a79eab06aebff5df626918d34b28a
Chris Penner <christopher.penner@gmail.com>
2025-11-03 14:03:02 -0800

    Reorganize  modules

--- codebase2/codebase-sqlite/U/Codebase/Sqlite/Queries.hs
+++ codebase2/codebase-sqlite/U/Codebase/Sqlite/Queries.hs
@@ -35,6 +35,8 @@ module U.Codebase.Sqlite.Queries
     expectCausalHash,
     expectBranchHashForCausalHash,
     saveBranchHash,
+    saveHistoryCommentHash,
+    saveHistoryCommentRevisionHash,
 
     -- * hash_object table
     saveHashObject,
@@ -647,11 +649,11 @@ expectCausalByCausalHash ch = do
   bhId <- expectCausalValueHashId hId
   pure (hId, bhId)
 
-saveCommentHash :: HistoryCommentHash -> Transaction CommentHashId
-saveCommentHash = fmap CommentHashId . saveHashHash . unHistoryCommentHash
+saveHistoryCommentHash :: HistoryCommentHash -> Transaction CommentHashId
+saveHistoryCommentHash = fmap CommentHashId . saveHashHash . unHistoryCommentHash
 
-saveCommentRevisionHash :: HistoryCommentRevisionHash -> Transaction CommentRevisionHashId
-saveCommentRevisionHash = fmap CommentRevisionHashId . saveHashHash . unHistoryCommentRevisionHash
+saveHistoryCommentRevisionHash :: HistoryCommentRevisionHash -> Transaction CommentRevisionHashId
+saveHistoryCommentRevisionHash = fmap CommentRevisionHashId . saveHashHash . unHistoryCommentRevisionHash
 
 expectHashIdByHash :: Hash -> Transaction HashId
 expectHashIdByHash = expectHashId . Hash32.fromHash
@@ -4186,8 +4188,8 @@ commentOnCausal
       revisionId = commentRevisionHash,
       comment = HistoryComment {author, authorThumbprint, causal = causalHashId, commentId = commentHash}
     } = do
-    commentHashId <- saveCommentHash commentHash
-    commentRevisionHashId <- saveCommentRevisionHash commentRevisionHash
+    commentHashId <- saveHistoryCommentHash commentHash
+    commentRevisionHashId <- saveHistoryCommentRevisionHash commentRevisionHash
     thumbprintId <- expectPersonalKeyThumbprintId authorThumbprint
     mayExistingCommentId <-
       queryMaybeCol @HistoryCommentId
