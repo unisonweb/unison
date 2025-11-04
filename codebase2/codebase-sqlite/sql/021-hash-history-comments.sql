a67223d83f7c6e52a90e5d08a3f36f72b7398dfb
Chris Penner <christopher.penner@gmail.com>
2025-11-03 16:02:57 -0800

    Add new sql for copying data in order to make non-null columns

--- codebase2/codebase-sqlite/sql/021-hash-history-comments.sql
+++ codebase2/codebase-sqlite/sql/021-hash-history-comments.sql
@@ -8,18 +8,18 @@ CREATE TABLE IF NOT EXISTS key_thumbprints (
 ALTER TABLE history_comments
   -- The hash used for comment identity.
   -- It's the hash of (causal_hash <> author <> created_at)
-  ADD COLUMN comment_hash_id INTEGER NOT NULL REFERENCES hash(id);
+  ADD COLUMN comment_hash_id INTEGER NULL REFERENCES hash(id);
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_history_comments_comment_hash_id
   ON history_comments(comment_hash_id);
 
 ALTER TABLE history_comments
-  ADD COLUMN author_thumbprint_id INTEGER NOT NULL REFERENCES key_thumbprints(id);
+  ADD COLUMN author_thumbprint_id INTEGER NULL REFERENCES key_thumbprints(id);
 
 ALTER TABLE history_comment_revisions
   -- The hash used for this revision's identity.
   -- It's the hash of (comment_hash <> subject <> contents <> hidden <> created_at)
-  ADD COLUMN revision_hash_id INTEGER NOT NULL REFERENCES hash(id);
+  ADD COLUMN revision_hash_id INTEGER NULL REFERENCES hash(id);
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_history_comment_revisions_revision_hash_id
   ON history_comment_revisions(revision_hash_id);
