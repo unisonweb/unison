6d7a9dd697fb2b5c7c3994598472b77672fc06e8
Chris Penner <christopher.penner@gmail.com>
2025-11-03 16:02:57 -0800

    Add new sql for copying data in order to make non-null columns

---
+++ codebase2/codebase-sqlite/sql/022-after-hash-history-comments.sql
@@ -0,0 +1,55 @@
+-- Assert hash columns are non-nullable after we've filled in all the values.
+-- SQLite annoying does not support doing this in place, so we need to make a new table.
+
+CREATE TABLE history_comments_new (
+  id INTEGER PRIMARY KEY,
+  causal_hash_id INTEGER REFERENCES hash(id) NOT NULL,
+  author TEXT NOT NULL,
+
+  -- Remember that SQLITE doesn't have any actual 'time' type,
+  -- This column contains float values constructed
+  -- using strftime('%s', 'now', 'subsec')
+  created_at TEXT NOT NULL,
+
+  comment_hash_id UNIQUE INTEGER NOT NULL REFERENCES hash(id),
+  author_thumbprint_id INTEGER NOT NULL REFERENCES key_thumbprints(id)
+);
+
+CREATE INDEX history_comments_by_causal_hash_id ON history_comments_new(causal_hash_id, created_at DESC);
+
+CREATE TABLE history_comment_revisions_new (
+  id INTEGER PRIMARY KEY,
+  comment_id INTEGER REFERENCES history_comments(id),
+  subject TEXT NOT NULL,
+  contents TEXT NOT NULL,
+
+  -- Remember that SQLITE doesn't have any actual 'time' type,
+  -- This column contains float values constructed
+  -- using strftime('%s', 'now', 'subsec')
+  created_at TEXT NOT NULL,
+
+  -- - In a distributed system you really can’t ever truly delete comments,
+  -- but you can ask to hide them.
+  hidden BOOL NOT NULL DEFAULT FALSE,
+
+  revision_hash_id UNIQUE INTEGER NOT NULL REFERENCES hash(id)
+);
+
+CREATE INDEX history_comment_revisions_by_comment_id_and_created_at ON history_comment_revisions_new(comment_id, created_at DESC);
+
+-- Copy data from old tables to new tables
+INSERT INTO history_comments_new (id, causal_hash_id, author, created_at, comment_hash_id, author_thumbprint_id)
+  SELECT id, causal_hash_id, author, created_at, comment_hash_id, author_thumbprint_id
+  FROM history_comments;
+
+INSERT INTO history_comment_revisions_new (id, comment_id, subject, contents, created_at, hidden, revision_hash_id)
+  SELECT id, comment_id, subject, contents, created_at, hidden, revision_hash_id
+  FROM history_comment_revisions;
+
+-- Drop old tables
+DROP TABLE history_comments;
+DROP TABLE history_comment_revisions;
+
+-- Rename new tables to old table names
+ALTER TABLE history_comments_new RENAME TO history_comments;
+ALTER TABLE history_comment_revisions_new RENAME TO history_comment_revisions;
