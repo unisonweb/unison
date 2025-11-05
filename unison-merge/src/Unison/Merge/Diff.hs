29b0dc44aa221fc50764d107c381feb5e91b2f60
Mitchell Dalvi Rosen <mitchellwrosen@gmail.com>
2025-11-05 15:31:38 -0500

    add more efficient way of computing diffblob for fast forwards

--- unison-merge/src/Unison/Merge/Diff.hs
+++ unison-merge/src/Unison/Merge/Diff.hs
@@ -1,12 +1,12 @@
 module Unison.Merge.Diff
   ( diffSynhashedDefns,
+    diffSynhashedDefns1,
   )
 where
 
 import Data.Map.Merge.Strict qualified as Map
 import Data.Map.Strict qualified as Map
 import Data.Zip qualified as Zip
-import U.Codebase.Reference (TypeReference)
 import Unison.Merge.DiffOp (DiffOp (..), DiffOp2 (..))
 import Unison.Merge.Synhashed (Synhashed (..))
 import Unison.Merge.Synhashed qualified as Synhashed
@@ -15,23 +15,24 @@ import Unison.Merge.Updated (GUpdated (..), Updated)
 import Unison.Merge.Updated qualified as Updated
 import Unison.Name (Name)
 import Unison.Prelude hiding (catMaybes)
-import Unison.Referent (Referent)
 import Unison.Util.Defns (DefnsF, DefnsF2, DefnsF3, unzipDefns, zipDefnsWith)
 
 -- | @diffSynhashedDefns defns@, given the output of @synhashDefns@, computes the two two-way diffs (each consisting of
 -- the "core" diffs, i.e. adds/delete/updates, alongside the propagated updates, i.e. updates that have the same synhash
 -- but different Unison hashes).
 diffSynhashedDefns ::
-  TwoWay (Updated (DefnsF2 (Map Name) Synhashed Referent TypeReference)) ->
+  (Eq term, Eq typ) =>
+  TwoWay (Updated (DefnsF2 (Map Name) Synhashed term typ)) ->
   ( -- Core diffs, i.e. adds, deletes, and updates which have different synhashes.
-    TwoWay (DefnsF3 (Map Name) DiffOp Synhashed Referent TypeReference),
+    TwoWay (DefnsF3 (Map Name) DiffOp Synhashed term typ),
     -- Propagated updates, i.e. updates which have the same synhash but different Unison hashes.
-    TwoWay (DefnsF (Map Name) (Updated Referent) (Updated TypeReference))
+    TwoWay (DefnsF (Map Name) (Updated term) (Updated typ))
   )
 diffSynhashedDefns =
-  Zip.unzip . fmap diffSynhashedDefns0
+  Zip.unzip . fmap diffSynhashedDefns1
 
-diffSynhashedDefns0 ::
+-- | Like 'diffSynhashedDefns', but for just one LCA->Head side.
+diffSynhashedDefns1 ::
   (Eq term, Eq typ) =>
   Updated (DefnsF2 (Map Name) Synhashed term typ) ->
   ( -- Core diffs, i.e. adds, deletes, and updates which have different synhashes.
@@ -39,7 +40,7 @@ diffSynhashedDefns0 ::
     -- Propagated updates, i.e. updates which have the same synhash but different Unison hashes.
     DefnsF (Map Name) (Updated term) (Updated typ)
   )
-diffSynhashedDefns0 defns =
+diffSynhashedDefns1 defns =
   unzipDefns (zipDefnsWith f f defns.old defns.new)
   where
     f ::
@@ -48,17 +49,17 @@ diffSynhashedDefns0 defns =
       Map Name (Synhashed ref) ->
       (Map Name (DiffOp (Synhashed ref)), Map Name (Updated ref))
     f old new =
-      partitionPropagated (diffSynhashedDefns1 old new)
+      partitionPropagated (diffSynhashedDefns2 old new)
 
 -- Compute the diff by comparing old-and-new values, resulting in either an add, delete, update (propagated or not),
 -- or dropping the thing entirely (because old and new have the same hash).
-diffSynhashedDefns1 ::
+diffSynhashedDefns2 ::
   forall ref.
   (Eq ref) =>
   Map Name (Synhashed ref) ->
   Map Name (Synhashed ref) ->
   Map Name (DiffOp2 (Synhashed ref))
-diffSynhashedDefns1 =
+diffSynhashedDefns2 =
   Map.merge
     (Map.mapMissing \_ -> DiffOp2'Delete)
     (Map.mapMissing \_ -> DiffOp2'Add)
