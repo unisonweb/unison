b837145bf3b4f7be46c9d24fe59e52e26b9bd3d3
Greg Pfeil <greg@technomadic.org>
2025-12-10 08:15:26 -0700

    Switch from haskell.nix to stacklock2nix
    This is much simpler Haskell tooling that serves our needs and keeps us
    on a better cache (NixOS).

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,11 +1,11 @@
 {
-  pkgs,
-  haskell-nix,
-}: {
-  ucm = pkgs.dockerTools.buildLayeredImage {
-    name = "ucm";
-    tag = "latest";
-    contents = with pkgs; [cacert fzf];
-    config.Cmd = ["${haskell-nix."unison-cli-main:exe:unison"}/bin/unison"];
-  };
+  cacert,
+  dockerTools,
+  fzf,
+  unisonPkgs,
+}: dockerTools.buildLayeredImage {
+  name = "ucm";
+  tag = "latest";
+  contents = [cacert fzf];
+  config.Cmd = ["${unisonPkgs.unison-cli-main}/bin/unison"];
 }
--- nix/unison-project.nix
+++ nix/unison-project.nix
@@ -1,30 +1,64 @@
-{
-  haskell-nix,
-  lib,
-}: let
-  cleanSource = src:
-    lib.cleanSourceWith {
-      inherit src;
-      filter = path: type: let
-        bn = baseNameOf path;
-        isHiddenFile = lib.hasPrefix "." bn;
-        isFlakeLock = bn == "flake.lock";
-        isNix = lib.hasSuffix ".nix" bn;
-      in
-        !isHiddenFile && !isFlakeLock && !isNix;
-    };
+{pkgs}: let
+  ## This needs to match the version from the stack.yaml `resolver`. See cdepillabout/stacklock2nix#4. We avoid thinking
+  ## about it by having the resolver match the LTS that Nixpkgs’ Haskell package set was built from.
+  baseHaskellPkgSet = pkgs.haskellPackages;
 in
-  haskell-nix.project' {
-    src = cleanSource ./..;
-    projectFileName = "stack.yaml";
-    modules = [
-      # enable profiling
-      {
-        enableLibraryProfiling = true;
-        profilingDetail = "none";
-      }
-    ];
-    branchMap = {
-      "https://github.com/unisonweb/shellmet.git"."2fd348592c8f51bb4c0ca6ba4bc8e38668913746" = "topic/avoid-callCommand";
+  pkgs.stacklock2nix {
+    inherit baseHaskellPkgSet;
+    stackYaml = ../stack.yaml;
+    additionalHaskellPkgSetOverrides = hfinal: hprev: {
+      ##  This check has a test that tries to write to $HOME, so we give it a fake one.
+      unison-cli = hprev.unison-cli.overrideAttrs (old: {
+        ## Don’t run unison-cli checks on macOS, because the test-suite requires access to `security` (because of TLS).
+        ## NixOS/nixpkgs#297775 is maybe a good entry to the discussions.
+        doCheck = !pkgs.stdenv.isDarwin;
+
+        ## This derivation doesn’t `runHook preCheck`, so we just prepend onto `checkPhase`.
+        checkPhase =
+          ''
+            export HOME="$TMP/fake-home"
+            mkdir -p "$HOME"
+          ''
+          + old.checkPhase;
+      });
+
+      ## Tests fail (or require network access) in some packages.
+      ##
+      ## NB: If we use nixpkgs-unstable instead of release-YY.MM, then many of these are likely handled upstream,
+      ##     because Haskell development on Nix merges (into unstable) often, but backports (into release branches)
+      ##     rarely.
+      avro = pkgs.haskell.lib.dontCheck hprev.avro;
+      base32 = pkgs.haskell.lib.dontCheck hprev.base32;
+      conduit-extra = pkgs.haskell.lib.dontCheck hprev.conduit-extra;
+      data-clist = pkgs.haskell.lib.dontCheck hprev.data-clist;
+      fsnotify = pkgs.haskell.lib.dontCheck hprev.fsnotify;
+      doctest-discover = pkgs.haskell.lib.dontCheck hprev.doctest-discover;
+      haskeline = pkgs.haskell.lib.dontCheck hprev.haskeline;
+      hs-mcp = pkgs.haskell.lib.dontCheck hprev.hs-mcp;
+      http2 = pkgs.haskell.lib.dontCheck hprev.http2;
+      network = pkgs.haskell.lib.dontCheck hprev.network;
+      pvar = pkgs.haskell.lib.dontCheck hprev.pvar;
+      servant-client = pkgs.haskell.lib.dontCheck hprev.servant-client;
+      streaming-commons = pkgs.haskell.lib.dontCheck hprev.streaming-commons;
+      tasty-discover = pkgs.haskell.lib.dontCheck hprev.tasty-discover;
+
+      ## stacklock2nix doesn’t yet support `allow-newer-deps`, so that’s duplicated here. See
+      ## cdepillabout/stacklock2nix#54.
+      fuzzyfind = pkgs.haskell.lib.doJailbreak hprev.fuzzyfind;
+      lock-file = pkgs.haskell.lib.doJailbreak hprev.lock-file;
+      numerals = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.doJailbreak hprev.numerals);
     };
+    additionalDevShellNativeBuildInputs = stacklockHaskellPkgSet: [
+      ## NB: Packages that need to be compiled with the same GHC as our project should come from `baseHaskellPkgSet`,
+      ##     not `pkgs`.
+      baseHaskellPkgSet.haskell-language-server
+      baseHaskellPkgSet.weeder
+      pkgs.cabal-install
+      pkgs.cachix
+      pkgs.gettext # for envsubst, used by unison-src/builtin-tests/interpreter-tests.sh
+      pkgs.hpack
+      pkgs.jq # helpful when pushing to Cachix
+      pkgs.ormolu
+      pkgs.stack-wrapped
+    ];
   }
