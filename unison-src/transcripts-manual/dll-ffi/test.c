2ccab2c9b3bdc52231a678c07fdf5fcf6424ea84
Dan Doel <dan.doel@gmail.com>
2025-12-16 15:33:55 -0500

    Update dll-ffi tests to include more functions

--- unison-src/transcripts-manual/dll-ffi/test.c
+++ unison-src/transcripts-manual/dll-ffi/test.c
@@ -3,6 +3,55 @@
 #ifdef WINDOWS_BUILD
 __declspec(dllexport)
 #endif
-int64_t test(int64_t m, int64_t n) {
+int64_t testi64(int64_t m, int64_t n) {
+  return 1 + m + n;
+}
+
+#ifdef WINDOWS_BUILD
+__declspec(dllexport)
+#endif
+int32_t testi32(int32_t m, int32_t n) {
+  return 1 + m + n;
+}
+
+#ifdef WINDOWS_BUILD
+__declspec(dllexport)
+#endif
+int16_t testi16(int16_t m, int16_t n) {
+  return 1 + m + n;
+}
+
+#ifdef WINDOWS_BUILD
+__declspec(dllexport)
+#endif
+uint64_t testu64(uint64_t m, uint64_t n) {
+  return 1 + m + n;
+}
+
+#ifdef WINDOWS_BUILD
+__declspec(dllexport)
+#endif
+uint32_t testu32(uint32_t m, uint32_t n) {
+  return 1 + m + n;
+}
+
+#ifdef WINDOWS_BUILD
+__declspec(dllexport)
+#endif
+uint16_t testu16(uint16_t m, uint16_t n) {
+  return 1 + m + n;
+}
+
+#ifdef WINDOWS_BUILD
+__declspec(dllexport)
+#endif
+float testf(float m, float n) {
+  return 1 + m + n;
+}
+
+#ifdef WINDOWS_BUILD
+__declspec(dllexport)
+#endif
+double testd(double m, double n) {
   return 1 + m + n;
 }
