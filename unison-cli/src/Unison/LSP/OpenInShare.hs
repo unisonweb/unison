a8e71d56997d173f69a5e77c2d4fb926c4282bc5
Chris Penner <christopher.penner@gmail.com>
2026-01-08 17:08:14 -0800

    Actually search and open on Share

--- unison-cli/src/Unison/LSP/OpenInShare.hs
+++ unison-cli/src/Unison/LSP/OpenInShare.hs
@@ -9,7 +9,6 @@ import Control.Lens hiding (List)
 import Control.Monad.Except
 import Control.Monad.Reader
 import Data.Aeson qualified as Aeson
-import Data.Generics.Product (field)
 import Data.Text qualified as Text
 import Language.LSP.Protocol.Lens qualified as LSP
 import Language.LSP.Protocol.Message qualified as Msg
@@ -19,6 +18,9 @@ import Network.HTTP.Client qualified as HTTP
 import Network.HTTP.Client.TLS qualified as HTTP
 import Network.HTTP.Types qualified as HTTP
 import Network.URI (escapeURIString, isUnreserved)
+import U.Codebase.Sqlite.Project
+import Unison.Codebase.ProjectPath
+import Unison.Core.Project (ProjectName (..))
 import Unison.LSP.FileAnalysis (ppedForFile)
 import Unison.LSP.Queries qualified as LSPQ
 import Unison.LSP.Types
@@ -28,12 +30,40 @@ import Unison.PrettyPrintEnv qualified as PPE
 import Unison.PrettyPrintEnvDecl qualified as PPED
 import Unison.Syntax.HashQualified qualified as SyntaxHQ
 import UnliftIO qualified
+import Web.Browser qualified as Web
 
 data OpenInShareParams = OpenInShareParams
   { textDocument :: LSP.Uri,
     position :: LSP.Position
   }
   deriving (Show, Eq)
+
+data DefinitionInfo
+  = DefinitionInfo
+  { definitionFQN :: Text,
+    definitionBranchRef :: Text,
+    definitionProjectRef :: Text,
+    definitionKind :: Text
+  }
+  deriving (Show, Eq)
+
+instance Aeson.FromJSON DefinitionInfo where
+  parseJSON = Aeson.withObject "DefinitionInfo" $ \v -> do
+    definitionFQN <- v Aeson..: "fqn"
+    definitionBranchRef <- v Aeson..: "branchRef"
+    definitionProjectRef <- v Aeson..: "projectRef"
+    definitionKind <- v Aeson..: "kind"
+    pure DefinitionInfo {definitionFQN, definitionBranchRef, definitionProjectRef, definitionKind}
+
+data DefinitionSearchResponse = DefinitionSearchResponse
+  { definitions :: [DefinitionInfo]
+  }
+  deriving (Show, Eq)
+
+instance Aeson.FromJSON DefinitionSearchResponse where
+  parseJSON = Aeson.withObject "DefinitionSearchResponse" $ \v -> do
+    definitions <- v Aeson..: "definitions"
+    pure DefinitionSearchResponse {definitions}
 
 instance Aeson.FromJSON OpenInShareParams where
   parseJSON = Aeson.withObject "OpenInShareParams" $ \v -> do
@@ -50,6 +80,8 @@ openInShareHandler ::
   Lsp ()
 openInShareHandler m respond = do
   result <- runExceptT $ do
+    pp <- lift getCurrentProjectPath
+    let (UnsafeProjectName projectNameText) = pp.project.name
     -- Extract parameters from the request
     let params = m ^. LSP.params
     OpenInShareParams {textDocument, position} <- case Aeson.fromJSON params of
@@ -69,51 +101,72 @@ openInShareHandler m respond = do
           LD.TypeReference typeRef -> PPE.typeName unsuffixifiedPPE typeRef
           LD.TermReferent termRef -> PPE.termName unsuffixifiedPPE termRef
     let fqnText = SyntaxHQ.toText fqn
-    let encodedFqn = Text.pack $ escapeURIString isUnreserved (Text.unpack fqnText)
-    let fileUriText = LSP.uriToText textDocument
-    let encodedUri = Text.pack $ escapeURIString isUnreserved (Text.unpack fileUri)
-    let encodedLoc =
-          Text.pack $
-            escapeURIString
-              isUnreserved
-              ( show (position ^. LSP.line)
-                  <> ":"
-                  <> show (position ^. LSP.character)
-              )
+    let encodedProjectName = Text.pack $ escapeURIString isUnreserved (Text.unpack projectNameText)
+
+    let (projectFilter, query) =
+          case Text.splitOn "." fqnText of
+            ("lib" : _ : rest) -> ("&project-filter=" <> encodedProjectName, Text.intercalate "." rest)
+            _ -> ("", fqnText)
 
     -- Build the request URL
     let shareUrl =
-          "http://localhost:5424/open-on-share"
-            <> "?fqn="
-            <> Text.unpack encodedFqn
-            <> "&fileURI="
-            <> Text.unpack encodedUri
-            <> "&loc="
-            <> Text.unpack encodedLoc
+          "https://api.unison-lang.org/search-definitions"
+            <> "?query="
+            <> Text.unpack query
+            <> Text.unpack projectFilter
 
     -- Make the HTTP POST request
-    httpResult <- liftIO $ UnliftIO.tryAny $ do
+    httpResponse <- liftIO $ UnliftIO.tryAny $ do
       manager <- HTTP.getGlobalManager
       request <- HTTP.parseRequest shareUrl
       let postRequest = request {HTTP.method = "POST"}
       response <- HTTP.httpLbs postRequest manager
-      pure $ HTTP.responseStatus response
+      pure response
 
     -- Handle HTTP errors
-    case httpResult of
+    case httpResponse of
       Left err ->
         throwError $
           Msg.ResponseError
             (InR ErrorCodes_InternalError)
             ("Failed to connect to Share service: " <> Text.pack (show err))
             Nothing
-      Right status
-        | HTTP.statusIsSuccessful status -> pure ()
+      Right response
+        | HTTP.statusIsSuccessful (HTTP.responseStatus response) ->
+            case Aeson.decode (HTTP.responseBody response) of
+              Just (DefinitionSearchResponse {definitions}) ->
+                case definitions of
+                  (DefinitionInfo {definitionProjectRef, definitionBranchRef, definitionKind} : _) -> do
+                    -- E.g. https://share.unison-lang.org/@unison/base/code/releases/7.12.0/latest/terms/data/List/map
+                    let shareUrl =
+                          Text.unpack $
+                            "https://share.unison-lang.org/@"
+                              <> definitionProjectRef
+                              <> "/code/branches/"
+                              <> definitionBranchRef
+                              <> "/latest"
+                              <> "/"
+                              <> definitionKind
+                              <> "s/data/"
+                              <> Text.replace "." "/" fqnText
+                    void . liftIO $ Web.openBrowser shareUrl
+                  [] ->
+                    throwError $
+                      Msg.ResponseError
+                        (InR ErrorCodes_InternalError)
+                        "No definitions found in Share service response"
+                        Nothing
+              Nothing ->
+                throwError $
+                  Msg.ResponseError
+                    (InR ErrorCodes_InternalError)
+                    "Failed to parse Share service response"
+                    Nothing
         | otherwise ->
             throwError $
               Msg.ResponseError
                 (InR ErrorCodes_InternalError)
-                ("Share service returned error: " <> Text.pack (show status))
+                ("Share service returned error: " <> Text.pack (show response))
                 Nothing
 
   -- Send the response
