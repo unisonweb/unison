07e6db4e9c82c7998184060f60e5c867ac3fb302
Chris Penner <christopher.penner@gmail.com>
2026-01-27 16:31:25 -0800

    Murmur hashing for record values

--- unison-runtime/src/Unison/Runtime/ANF/MurmurHash/Untyped.hs
+++ unison-runtime/src/Unison/Runtime/ANF/MurmurHash/Untyped.hs
@@ -29,7 +29,7 @@ import Unison.Runtime.Serialize (naturalToWord64s)
 import Unison.Runtime.TypeTags (mapBinTag, mapTipTag)
 import Unison.Util.Bytes qualified as B
 import Unison.Util.EnumContainers qualified as EC
-import Unison.Util.Text as UT hiding (reverse, pattern Text)
+import Unison.Util.Text qualified as UT hiding (reverse, pattern Text)
 import Unison.Var (Var)
 
 hash64ValueUntyped :: Referenced Value -> Hash64
@@ -51,6 +51,9 @@ data HRefs r = HRefs
 
 hash64AddShort :: SBS.ShortByteString -> Hash64 -> Hash64
 hash64AddShort = flip $ SBS.foldl (flip $ hash64AddInt . fromIntegral)
+
+hash64AddText :: DT.Text -> Hash64 -> Hash64
+hash64AddText t h = DT.foldl' (flip hash64Add) h t
 
 hash64AddRef :: Reference -> Hash64 -> Hash64
 hash64AddRef (ReferenceBuiltin tx) h =
@@ -96,6 +99,10 @@ hash64AddValue rs = \case
     hash64AddUMap rs (M.fromDistinctAscList assocs)
   BLit lit ->
     hash64AddInt 4 `combine` hash64AddBLit rs lit
+  Record (RecordSchema recSchema) vs ->
+    hash64AddInt 5
+      `combine` hash64AddFoldable (hash64AddText) recSchema
+      `combine` hash64AddValues rs vs
 
 hash64AddGroupRef :: HRefs r -> GroupRef r -> Hash64 -> Hash64
 hash64AddGroupRef rs (GR i k) =
@@ -307,6 +314,9 @@ hash64AddFunc rf ctx = \case
   FPrim ins ->
     hash64AddInt 6
       `combine` hash64AddEither hash64AddPOp hash64AddForeign ins
+  FRec (RecordSchema rs) ->
+    hash64AddInt 7
+      `combine` hash64AddFoldable (hash64AddText) rs
 
 hash64AddLit :: HRefs r -> Lit r -> Hash64 -> Hash64
 hash64AddLit rs = \case
@@ -447,6 +457,14 @@ hash64AddEMap f = flip $ EC.foldlWithKey (rot f)
 hash64AddMap ::
   (k -> v -> Hash64 -> Hash64) -> M.Map k v -> Hash64 -> Hash64
 hash64AddMap f = flip $ M.foldlWithKey' (rot f)
+
+hash64AddFoldable ::
+  (Foldable f) =>
+  (a -> Hash64 -> Hash64) ->
+  f a ->
+  Hash64 ->
+  Hash64
+hash64AddFoldable f = flip $ foldl' (flip f)
 
 -- Serializes a map as if it were a unison data type
 hash64AddUMap ::
