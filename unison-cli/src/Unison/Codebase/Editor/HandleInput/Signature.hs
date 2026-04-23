2f9b0762acd2300c2c55db9df56dfddfd15a297b
Chris Penner <christopher.penner@gmail.com>
2026-04-23 12:33:46 -0700

    Change formatting to show sigs on the next line

--- unison-cli/src/Unison/Codebase/Editor/HandleInput/Signature.hs
+++ unison-cli/src/Unison/Codebase/Editor/HandleInput/Signature.hs
@@ -9,7 +9,6 @@ import Data.Set qualified as Set
 import Unison.Cli.Monad (Cli)
 import Unison.Cli.Monad qualified as Cli
 import Unison.Cli.NamesUtils qualified as Cli
-import Unison.Cli.Pretty qualified as Pretty
 import Unison.Codebase.Editor.Output
 import Unison.HashQualified qualified as HQ
 import Unison.Name (Name)
@@ -17,10 +16,12 @@ import Unison.NamesWithHistory qualified as Names
 import Unison.Prelude
 import Unison.PrettyPrintEnv.Names qualified as PPE
 import Unison.PrettyPrintEnvDecl qualified as PPED
+import Unison.Reference qualified as Reference
 import Unison.Server.Backend qualified as Backend
 import Unison.Server.NameSearch.FromNames qualified as NameSearch
 import Unison.Server.QueryResult (QueryResult (..))
 import Unison.Server.SearchResultPrime qualified as SR'
+import Unison.Syntax.NamePrinter qualified as NP
 import Unison.Syntax.TypePrinter qualified as TypePrinter
 import Unison.Util.Pretty qualified as P
 
@@ -36,12 +37,22 @@ handleSignature hqNames = do
     QueryResult {misses, hits} <- Backend.hqNameQuery codebase nameSearch Names.IncludeSuffixes query
     results <- Backend.loadSearchResults codebase hits
     pure (misses, results)
-  let sigs =
-        results >>= \case
-          SR'.Tm name (Just typ) r _ ->
-            TypePrinter.prettySignaturesCT suffixifiedPPE [(r, name, typ)]
-          SR'.Tp' tr ->
-            [Pretty.prettyTypeResultHeader' tr]
-          _ -> []
+  let renderEntry = \case
+        SR'.Tm name (Just typ) _ _ ->
+          let namePretty = P.syntaxToColor (NP.prettyHashQualified name)
+              typePretty = TypePrinter.pretty suffixifiedPPE typ
+           in namePretty <> P.newline <> P.indentN 2 (P.text ": " <> typePretty)
+        SR'.Tp' (SR'.TypeResult' name _ r _) ->
+          let namePretty = P.syntaxToColor (NP.prettyHashQualified name)
+              tag = case r of
+                Reference.Builtin {} -> P.lit "(builtin type)"
+                Reference.DerivedId {} -> P.lit "(type)"
+           in namePretty <> P.newline <> P.indentN 2 tag
+        _ -> mempty
+  let sigs = map renderEntry (filter (not . isMissing) results)
   when (not (null misses)) (Cli.respond (SearchTermsNotFound misses))
-  Cli.respond . DisplayDefinitions $ P.lines sigs
+  Cli.respond . DisplayDefinitions $ P.sep P.newline sigs
+  where
+    isMissing = \case
+      SR'.Tm _ Nothing _ _ -> True
+      _ -> False
